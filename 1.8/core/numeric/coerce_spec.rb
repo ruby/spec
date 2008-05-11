911b0ede6b3ec757222904b84d412a977b41396c
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-11 10:46:01 +0200

    Cleanup Numeric#coerce specs.

--- 1.8/core/numeric/coerce_spec.rb
+++ 1.8/core/numeric/coerce_spec.rb
@@ -1,43 +1,33 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-# FIXME: this awful wording
-describe "Numeric#coerce" do   
-  before(:each) do 
-    @integer = 1
-    @float   = 2.5    
-  end  
+describe "Numeric#coerce" do
+  it "returns an array containing other and self as Fixnums when given an Fixnum" do
+    result = 1.coerce(20)
+    result.should == [20, 1]
+    result.first.should be_instance_of(Fixnum)
+    result.last.should be_instance_of(Fixnum)
+  end
   
-  it "coerce integers" do 
-    @integer.coerce(@integer).should == [1,1]
-    @integer.coerce(@float).should ==[2.5,1]
-  end  
+  it "returns an array containing other and self as Floats when given a String" do
+    result = 1.coerce("10")
+    result.should == [10.0, 1.0]
+    result.first.should be_instance_of(Float)
+    result.last.should be_instance_of(Float)
+  end
+
+  it "returns an array containing other and self as Floats when given a Bignum" do
+    result = 1.coerce(4294967296)
+    result.should == [4294967296.0, 1.0]
+    result.first.should be_instance_of(Float)
+    result.last.should be_instance_of(Float)
+  end
   
-  it "coerce floats" do 
-    @float.coerce(@float).should == [2.5,2.5]
-    @float.coerce(@integer).should == [1,2.5]
-  end  
+  it "raises a TypeError when other can't be coerced" do
+    lambda { 10.coerce(nil)   }.should raise_error(TypeError)
+    lambda { 10.coerce(false) }.should raise_error(TypeError)    
+  end
   
-  it "coerce with 0" do 
-    @integer.coerce(0).should == [0,1]
-    @float.coerce(0).should == [0,2.5]
-  end  
-  
-  it "coerce with bignums" do 
-    @integer.coerce(4294967296).should == [4294967296, 1]
-    @float.coerce(4294967296).should == [4294967296.0, 2.5]
-  end  
- 
-  it "coerces strings to numerics" do
-    @integer.coerce("1").should == [1.0, 1.0]
-    @integer.coerce("3.32").should == [3.32, 1.0]
-    @float.coerce("1").should == [1.0, 2.5]
-    @float.coerce("3.32").should == [3.32, 2.5]
-  end  
-  
-  it "return the vaule if number is different to 0" do 
-    lambda { @integer.coerce("test") }.should raise_error(ArgumentError)
-    lambda { @integer.coerce         }.should raise_error(ArgumentError)
-    lambda { @integer.coerce(nil)    }.should raise_error(TypeError)
-    lambda { @integer.coerce(false)  }.should raise_error(TypeError)    
-  end  
+  it "raises an ArgumentError when other can't be converted to Float" do
+    lambda { 10.coerce("test") }.should raise_error(ArgumentError)
+  end
 end
