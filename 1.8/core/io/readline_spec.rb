fae2747cd2be2b4b827a55bc6b492dcc40bc7055
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-11 02:33:51 +0200

    Fix IO#readline specs. (RSpec Compatibility)

--- 1.8/core/io/readline_spec.rb
+++ 1.8/core/io/readline_spec.rb
@@ -2,10 +2,9 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
 describe "IO#readline" do
-  @testfile = File.dirname(__FILE__) + '/fixtures/gets.txt'
-
   it "raises EOFError on end of stream" do
-    File.open(@testfile, 'r') do |f|
+    testfile = File.dirname(__FILE__) + '/fixtures/gets.txt'
+    File.open(testfile, 'r') do |f|
       lambda { loop { f.readline } }.should raise_error(EOFError)
     end
   end
