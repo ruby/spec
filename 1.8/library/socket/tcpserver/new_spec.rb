f5ef62e9503e6ef86922a570047562e2d481c35f
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-11 09:48:53 +0200

    Fix TCPServer.new specs: Add missing 'should's.

--- 1.8/library/socket/tcpserver/new_spec.rb
+++ 1.8/library/socket/tcpserver/new_spec.rb
@@ -13,7 +13,7 @@ describe "TCPServer.new" do
     @server = TCPServer.new('127.0.0.1', SocketSpecs.port)
     addr = @server.addr
     addr[0].should == 'AF_INET'
-    addr[1].be_kind_of Fixnum
+    addr[1].should be_kind_of(Fixnum)
     # on some platforms (Mac), MRI
     # returns comma at the end.
     addr[2].should =~ /^#{@hostname}\b/
@@ -24,11 +24,11 @@ describe "TCPServer.new" do
     @server = TCPServer.new("localhost", SocketSpecs.port)
     addr = @server.addr
     if addr[0] == 'AF_INET'
-      addr[1].be_kind_of Fixnum
+      addr[1].should be_kind_of(Fixnum)
       addr[2].should =~ /^#{@hostname}\b/
       addr[3].should == '127.0.0.1'
     else
-      addr[1].be_kind_of Fixnum
+      addr[1].should be_kind_of(Fixnum)
       addr[2].should =~ /^#{@hostname}\b/
       addr[3].should == '::1'
     end
