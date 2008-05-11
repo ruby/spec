9f55a5f87388119d1fbfe8e9df280afb53801995
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-11 11:03:34 +0200

    Cleanup Numeric#abs specs.

--- 1.8/core/numeric/abs_spec.rb
+++ 1.8/core/numeric/abs_spec.rb
@@ -1,20 +1,19 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-# Returns the absolute value of num.
 describe "Numeric#abs" do  
-  it "return the abs (integers) " do      
+  it "returns the absolute value for Fixnums" do      
     0.abs.should == 0 
-    100.abs.should  == 100
-    -100.abs.should   == 100
+    100.abs.should == 100
+    -100.abs.should == 100
   end
   
-  it "return the abs (floats) " do 
+  it "returns the absolute value for Floats" do      
     0.0.abs.should == 0.0
     34.56.abs.should == 34.56
     -34.56.abs.should == 34.56 
   end
   
-  it "return the abs (two complement)" do
+  it "returns the absolute value for Bignums" do      
     2147483648.abs.should == 2147483648
     -2147483648.abs.should == 2147483648
     9223372036854775808.abs.should == 9223372036854775808
