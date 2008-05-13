7c7c3081fa84291ebaea092bcb4a575d0194c401
Brian Ford <bford@engineyard.com>
2008-05-13 01:10:12 -0700

    Fix whitespace and unnest describe blocks.

--- 1.8/core/io/read_spec.rb
+++ 1.8/core/io/read_spec.rb
@@ -5,13 +5,13 @@ describe "IO.read" do
   before :each do
     @fname = "test.txt"
     @contents = "1234567890"
-    File.open(@fname, "w") { |f| f.write(@contents) }
+    File.open(@fname, "w") { |f| f.write @contents }
   end
-  
+
   after :each do
-    File.delete(@fname) if File.exists?(@fname)
+    File.delete @fname if File.exists? @fname
   end
-  
+
   it "reads the contents of a file" do
     IO.read(@fname).should == @contents
   end
@@ -29,52 +29,50 @@ describe "IO.read" do
   it "reads the contents of a file up to a certain size when specified" do
     IO.read(@fname, 5).should == @contents.slice(0..4)
   end
-    
+
   it "reads the contents of a file from an offset of a specific size when specified" do
     IO.read(@fname, 5, 3).should == @contents.slice(3, 5)
   end
-  
-  it "returns nil at end-of-file with a length" do
+
+  it "returns nil at end-of-file when length is passed" do
     IO.read(@fname, 1, 10).should == nil
   end
-  
-  describe "on empty file" do
-    
-    before :each do
-      @fname = 'empty_test.txt'
-      File.open(@fname, 'w') {|f| 1 }
-    end
-    
-    it "returns nil when length provided" do
-      IO.read(@fname, 1).should == nil
-    end
-    
-    it "returns empty string when no length provided" do
-      IO.read(@fname).should == ""
-    end
-    
-    after :each do
-      File.delete(@fname) if File.exists?(@fname)
-    end
-    
-  end
-  
+
   it "raises an Errno::ENOENT when the requested file does not exist" do
     File.delete(@fname) if File.exists?(@fname)
     lambda { IO.read @fname }.should raise_error(Errno::ENOENT)
   end
-  
+
   it "raises a TypeError when not passed a String type" do
     lambda { IO.read nil }.should raise_error(TypeError)
   end
-  
+
   it "raises an ArgumentError when not passed a valid length" do
     lambda { IO.read @fname, -1 }.should raise_error(ArgumentError)
   end
-  
+
   it "raises an Errno::EINVAL when not passed a valid offset" do
     lambda { IO.read @fname, 0, -1  }.should raise_error(Errno::EINVAL)
     lambda { IO.read @fname, -1, -1 }.should raise_error(Errno::EINVAL)
+  end
+end
+
+describe "IO.read on an empty file" do
+  before :each do
+    @fname = 'empty_test.txt'
+    File.open(@fname, 'w') {|f| 1 }
+  end
+
+  after :each do
+    File.delete @fname  if File.exists? @fname
+  end
+
+  it "returns nil when length is passed" do
+    IO.read(@fname, 1).should == nil
+  end
+
+  it "returns an empty string when no length is passed" do
+    IO.read(@fname).should == ""
   end
 end
 
@@ -214,7 +212,7 @@ describe "IO#read" do
     @io.read
     @io.read(1).should == nil
   end
-  
+
   it "raises IOError on closed stream" do
     lambda { IOSpecs.closed_file.read }.should raise_error(IOError)
   end
