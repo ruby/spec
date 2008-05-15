fef4deb97ba6669fd945b2a2040489f5c03917d1
Eero Saynatkari <projects@kittensoft.org>
2008-05-15 11:07:25 -0400

    Spec for Kernel.methods and .undef'd methods. They should not show up.

--- 1.8/core/kernel/methods_spec.rb
+++ 1.8/core/kernel/methods_spec.rb
@@ -1,6 +1,20 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
+module KernelSpecs
+  class MethodsUndefd
+    def all; end
+  end
+
+  class MethodsUndefdParent
+    def parent_method; end
+  end
+
+  class MethodsUndefdChild < MethodsUndefdParent
+    undef_method :parent_method
+  end
+end
+
 describe "Kernel#methods" do
 
   it "picks up methods added via self.meth" do
@@ -19,7 +33,7 @@ describe "Kernel#methods" do
     KernelSpecs::Methods.methods(false).should include("roku")
   end
 
-  it "doesn't picks up methods added inside 'class << self; private'" do
+  it "doesn't pick up methods added inside 'class << self; private'" do
     KernelSpecs::Methods.methods(false).should_not include("shichi")
   end
 
@@ -44,4 +58,27 @@ describe "Kernel#methods" do
     m.should include('ni')
     m.should include('juu_san')
   end
+
+  it "does not include any .undef'd methods" do
+    o = KernelSpecs::MethodsUndefd.new
+
+    def o.single; end
+
+    o.methods.should include("all")
+    o.methods.should include("single")
+
+    KernelSpecs::MethodsUndefd.send :undef_method, :all
+
+    o.methods.should_not include("all")
+
+    class << o; self; end.send :undef_method, :single
+
+    o.methods.should_not include("single")
+  end
+
+  it "does not include any .undef'd methods even if method is inherited" do
+    o = KernelSpecs::MethodsUndefdChild.new
+    o.methods.should_not include("inh")
+  end
+
 end
