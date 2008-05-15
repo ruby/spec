e8d13b958ececf69360f66423e0186d27d920e9b
Vladimir Sizikov <vsizikov@gmail.com>
2008-05-15 21:58:52 +0200

    New specs for StringScanner's #getbyte, #get_byte, getch.

--- 1.8/library/stringscanner/getbyte_spec.rb
+++ 1.8/library/stringscanner/getbyte_spec.rb
@@ -4,4 +4,22 @@ require 'strscan'
 
 describe "StringScanner#getbyte" do
     it_behaves_like :strscan_get_byte, :getbyte
+
+    it "warns in verbose mode that the method is obsolete" do
+      s = StringScanner.new("abc")
+      begin
+        old = $VERBOSE
+        lambda {
+          $VERBOSE = true
+          s.getbyte
+        }.should complain(/getbyte.*obsolete.*get_byte/)
+
+        lambda {
+          $VERBOSE = false
+          s.getbyte
+        }.should_not complain
+      ensure
+        $VERBOSE = old
+      end
+    end
 end
