d03b0a61fbafba3ee79e88144f29ae36e2478184
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-16 00:47:50 +0200

    Add spec to check existence of Numeric#truncate.

--- 1.8/core/numeric/truncate_spec.rb
+++ 1.8/core/numeric/truncate_spec.rb
@@ -1,18 +1,22 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe "Numeric#truncate" do   
-  it " truncate integers " do 
+describe "Numeric#truncate" do
+  it "should be provided" do
+    Numeric.instance_methods.should include("truncate")
+  end
+  
+  it "truncates integers " do 
     0.truncate.should == 0.0
     100.truncate.should == 100
     -100.truncate.should == -100
   end  
   
-  it " truncate floats " do  
+  it "truncates floats " do  
     34.56.truncate.should == 34
     -34.56.truncate.should == -34
   end  
   
-  it " truncate two complement " do
+  it "truncates two complement " do
     2147483648.truncate.should == 2147483648
     -2147483648.truncate.should == -2147483648
     9223372036854775808.truncate.should == 9223372036854775808 
