7a71d3c5bc567f90998d5b653ab8e439d56d08fc
Brian Ford <bford@engineyard.com>
2008-05-18 00:28:11 -0700

    Added platform guards for Math#atanh specs under Complex.

--- 1.8/library/complex/math/shared/atanh.rb
+++ 1.8/library/complex/math/shared/atanh.rb
@@ -10,14 +10,26 @@ shared :complex_math_atanh do |obj|
       obj.send(:atanh, -0.2).should be_close(-0.202732554054082, TOLERANCE)
     end
 
-    it "returns Infinity for 1.0" do
-      obj.send(:atanh, 1.0).infinite?.should == 1
+    platform_is :darwin, :freebsd do
+      it "returns Infinity for 1.0" do
+        obj.send(:atanh, 1.0).infinite?.should == 1
+      end
+
+      it "returns -Infinity for -1.0" do
+        obj.send(:atanh, -1.0).infinite?.should == -1
+      end
     end
 
-    it "returns -Infinity for -1.0" do
-      obj.send(:atanh, -1.0).infinite?.should == -1
+    platform_is :windows, :linux, :openbsd do
+      it "raises an Errno::EDOM if x = 1.0" do
+        lambda { obj.send(:atanh, 1.0) }.should raise_error(Errno::EDOM)
+      end
+
+      it "raises an Errno::EDOM if x = -1.0" do
+        lambda { obj.send(:atanh, -1.0) }.should raise_error(Errno::EDOM)
+      end
     end
-    
+
     it "returns the inverse hyperbolic tangent as a Complex number for arguments greater than 1.0" do
       obj.send(:atanh, 1.0 + TOLERANCE).should be_close(Complex(5.55373767837521, 1.5707963267949), TOLERANCE)
       obj.send(:atanh, 10).should be_close(Complex(0.100335347731076, 1.5707963267949), TOLERANCE)
@@ -27,7 +39,7 @@ shared :complex_math_atanh do |obj|
       obj.send(:atanh, -1.0 - TOLERANCE).should be_close(Complex(-5.55373767837521, 1.5707963267949), TOLERANCE)
       obj.send(:atanh, 10).should be_close(Complex(0.100335347731076, 1.5707963267949), TOLERANCE)
     end
-    
+
     it "returns the inverse hyperbolic tangent for Complex numbers" do
       obj.send(:atanh, Complex(3, 4)).should be_close(Complex(0.117500907311434, 1.40992104959658), TOLERANCE)
     end
