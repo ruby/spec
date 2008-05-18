2adb23b175a5e4c9b41079c3d15fe2246a42f1a3
Luis Lavena <luislavena@gmail.com>
2008-05-18 14:22:58 -0300

    Fixed trailing whitespace.

--- 1.8/core/file/ftype_spec.rb
+++ 1.8/core/file/ftype_spec.rb
@@ -31,7 +31,7 @@ describe "File.ftype" do
   end
 
   # Both FreeBSD and Windows does not have block devices
-  platform_is_not :freebsd, :windows do  
+  platform_is_not :freebsd, :windows do
     it "returns 'blockSpecial' when the file is a block" do
       FileSpecs.block_device do |block|
         File.ftype(block).should == 'blockSpecial'
