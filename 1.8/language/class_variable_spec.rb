13bfc2e26eeb84071925a4d7f875ef050af36b28
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-19 09:28:37 +0200

    Add some more class variable related specs.

--- 1.8/language/class_variable_spec.rb
+++ 1.8/language/class_variable_spec.rb
@@ -15,18 +15,28 @@ module ClassVariableSpec
     def cvar
       @@cvar
     end
+    
+    def cvar=(val)
+      @@cvar = val
+    end
   end
 
   class C
     extend M
+    
+    def self.cvar?
+      self.class_variable_defined?(:@@cvar)
+    end
+    
+    def self.cvar2=(val)
+      @@cvar = val
+    end
   end
 end
 
-describe "class variables" do
-
-  # HACK can't compare things directly, wrapping in []
+describe "A Class Variable" do
   it "can be accessed from a subclass" do
-    [ClassVariableSpec::B.new.a_cvar].should == [:a_cvar]
+    ClassVariableSpec::B.new.a_cvar.should == :a_cvar
   end
 
   it "sets the value in the superclass from the subclass" do
@@ -40,6 +50,32 @@ describe "class variables" do
   it "retrieves the value from the place it is defined" do
     [ClassVariableSpec::C.cvar.should] == [:value]
   end
-
 end
 
+describe "A Class Variable defined in a Module" do
+  it "can be accessed from Classes that are extended by the Module" do
+    ClassVariableSpec::C.cvar.should == :value
+  end
+  
+  it "is not defined in these Classes" do
+    ClassVariableSpec::C.cvar?.should be_false
+  end
+  
+  it "only updates the Class Variable in the Module when using a Method defined in the Module" do
+    ClassVariableSpec::C.cvar = "new value"
+    ClassVariableSpec::C.cvar.should == "new value"
+    
+    ClassVariableSpec::C.cvar?.should be_false
+  end
+  
+  it "does updates the Class Variable in the Module but in the Class when using a Method defined in the Class" do
+    ClassVariableSpec::C.cvar2 = "new value"    
+    ClassVariableSpec::C.cvar?.should be_true
+  end
+  
+  it "allows to retrieve the Class Variable in the Class with methods from the Module" do
+    ClassVariableSpec::C.cvar2 = "new value"
+    
+    ClassVariableSpec::C.cvar.should == "new value"
+  end
+end
