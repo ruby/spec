55624eb03c5ac308dce7cb733cc5b50857f99f20
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-20 15:22:32 +0200

    First pass at writing specs for Rational.
    Added specs for:
     * Rational#abs
     * Rational#coerce
     * Rational#<=>
     * Rational#denominator
     * Rational#divide
     * Rational#divmod
     * Rational#==
     * Rational#**

--- 1.8/library/rational/denominator_spec.rb
+++ 1.8/library/rational/denominator_spec.rb
@@ -2,6 +2,10 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require 'rational'
 
 describe "Rational#denominator" do
-  it "needs to be reviewed for spec completeness" do
+  it "returns the denominator" do
+    Rational(3, 4).denominator.should equal(4)
+    Rational(3, -4).denominator.should equal(4)
+    
+    Rational(1, bignum_value).denominator.should == bignum_value
   end
 end
