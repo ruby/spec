593b0f93ad3db16086a6eb71eb5b15e830377847
Brian Ford <bford@engineyard.com>
2008-05-21 12:54:45 -0700

    Fixed Iconv#close specs to not use should_not raise_error.

--- 1.8/library/iconv/close_spec.rb
+++ 1.8/library/iconv/close_spec.rb
@@ -2,18 +2,16 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require 'iconv'
 
 describe "Iconv#close" do
-  it "fails silently if called more than once" do
+  it "ignores multiple calls" do
     conv1 = Iconv.new("us-ascii", "us-ascii")
-    lambda {
-      conv1.close
-      conv1.close
-    }.should_not raise_error
+    conv1.close
+    conv1.close
+  end
 
-    lambda {
-      Iconv.open "us-ascii", "us-ascii" do |conv2|
-        conv2.close
-      end
-    }.should_not raise_error
+  it "does not raise an exception if called inside an .open block" do
+    Iconv.open "us-ascii", "us-ascii" do |conv2|
+      conv2.close
+    end
   end
 
   # return values of #close not tested yet
