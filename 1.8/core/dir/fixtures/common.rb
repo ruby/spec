ac899e51ef6c3bcaa77d9807d0bcd329e4a972c4
Eric Hodel <drbrain@segment7.net>
2008-05-20 18:58:33 -0700

    Use File.join to build up paths in common Dir fixtures.

--- 1.8/core/dir/fixtures/common.rb
+++ 1.8/core/dir/fixtures/common.rb
@@ -26,59 +26,61 @@ module DirSpecs
 
   def DirSpecs.create_mock_dirs
     mock_dir = self.mock_dir
-    files = %W[ #{mock_dir}/.dotfile
-                #{mock_dir}/.dotsubdir/.dotfile
-                #{mock_dir}/.dotsubdir/nondotfile
+    files = %w[
+      .dotfile
+      .dotsubdir/.dotfile
+      .dotsubdir/nondotfile
 
-                #{mock_dir}/deeply/.dotfile
-                #{mock_dir}/deeply/nested/.dotfile.ext
-                #{mock_dir}/deeply/nested/directory/structure/.ext
-                #{mock_dir}/deeply/nested/directory/structure/bar
-                #{mock_dir}/deeply/nested/directory/structure/baz
-                #{mock_dir}/deeply/nested/directory/structure/file_one
-                #{mock_dir}/deeply/nested/directory/structure/file_one.ext
-                #{mock_dir}/deeply/nested/directory/structure/foo
-                #{mock_dir}/deeply/nondotfile
+      deeply/.dotfile
+      deeply/nested/.dotfile.ext
+      deeply/nested/directory/structure/.ext
+      deeply/nested/directory/structure/bar
+      deeply/nested/directory/structure/baz
+      deeply/nested/directory/structure/file_one
+      deeply/nested/directory/structure/file_one.ext
+      deeply/nested/directory/structure/foo
+      deeply/nondotfile
 
-                #{mock_dir}/file_one.ext
-                #{mock_dir}/file_two.ext
+      file_one.ext
+      file_two.ext
 
-                #{mock_dir}/dir_filename_ordering
-                #{mock_dir}/dir/filename_ordering
+      dir_filename_ordering
+      dir/filename_ordering
 
-                #{mock_dir}/nondotfile
+      nondotfile
 
-                #{mock_dir}/subdir_one/.dotfile
-                #{mock_dir}/subdir_one/nondotfile
-                #{mock_dir}/subdir_two/nondotfile
-                #{mock_dir}/subdir_two/nondotfile.ext
+      subdir_one/.dotfile
+      subdir_one/nondotfile
+      subdir_two/nondotfile
+      subdir_two/nondotfile.ext
 
-                #{mock_dir}/special/+
+      special/+
 
-                #{mock_dir}/special/^
-                #{mock_dir}/special/$
+      special/^
+      special/$
 
-                #{mock_dir}/special/(
-                #{mock_dir}/special/)
-                #{mock_dir}/special/[
-                #{mock_dir}/special/]
-                #{mock_dir}/special/{
-                #{mock_dir}/special/}
-              ]
+      special/(
+      special/)
+      special/[
+      special/]
+      special/{
+      special/}
+    ]
 
     platform_is_not :windows do
-      files += %W[  #{mock_dir}/special/*
-                    #{mock_dir}/special/?
+      files += %w[
+        special/*
+        special/?
 
-                    #{mock_dir}/special/|
-                  ]
+        special/|
+      ]
     end
     
     FileUtils.rm_rf mock_dir
     files.each do |file|
+      file = File.join mock_dir, file
       FileUtils.mkdir_p File.dirname(file)
-      # eventually will be FileUtils.touch(File.basename(dir))
-      File.open(file, "w") { }
+      FileUtils.touch file
     end
   end
 
