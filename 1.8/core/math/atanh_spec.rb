41537870ec17b9de4ca1fba93d1378e70329ef13
Vladimir Sizikov <vsizikov@gmail.com>
2008-05-21 20:36:06 +0200

    Adjusted platform guards for Math.atanh, to be suitable for JRuby.

--- 1.8/core/math/atanh_spec.rb
+++ 1.8/core/math/atanh_spec.rb
@@ -13,7 +13,7 @@ describe "Math.atanh" do
     Math.atanh(-0.2).should be_close(-0.202732554054082, TOLERANCE)
   end
   
-  platform_is :darwin, :freebsd do
+  platform_is :darwin, :freebsd, :java do
     it "returns Infinity for 1.0" do
       Math.atanh(1.0).infinite?.should == 1
     end
@@ -24,15 +24,18 @@ describe "Math.atanh" do
   end
 
   platform_is :windows, :linux, :openbsd do
-    it "raises an Errno::EDOM if x = 1.0" do
-      lambda { Math.atanh(1.0) }.should raise_error(Errno::EDOM)
-    end
+    # jruby is cross-platform and behaves as :darwin above
+    not_compliant_on :jruby do
+      it "raises an Errno::EDOM if x = 1.0" do
+        lambda { Math.atanh(1.0) }.should raise_error(Errno::EDOM)
+      end
 
-    it "raises an Errno::EDOM if x = -1.0" do
-      lambda { Math.atanh(-1.0) }.should raise_error(Errno::EDOM)
+      it "raises an Errno::EDOM if x = -1.0" do
+        lambda { Math.atanh(-1.0) }.should raise_error(Errno::EDOM)
+      end
     end
   end
-  
+
   conflicts_with :Complex do
     it "raises an Errno::EDOM if the passed argument is greater than 1.0" do
       lambda { Math.atanh(1.0 + TOLERANCE)  }.should raise_error(Errno::EDOM)
