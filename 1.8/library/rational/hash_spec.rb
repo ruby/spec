28568e1b78b688b1422615e70ea6e9cd4c185dac
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-22 17:04:52 +0200

    Fixed a typo.

--- 1.8/library/rational/hash_spec.rb
+++ 1.8/library/rational/hash_spec.rb
@@ -4,7 +4,7 @@ require 'rational'
 describe "Rational#hash" do
   # BUG: Rational(2, 3).hash == Rational(3, 2).hash
   it "is static" do
-    Rational(2, 3).hash.should == Rational(2, 3)
-    Rational(2, 4).hash.should_not == Rational(2, 3)
+    Rational(2, 3).hash.should == Rational(2, 3).hash
+    Rational(2, 4).hash.should_not == Rational(2, 3).hash
   end
 end
