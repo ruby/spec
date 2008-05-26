9c7fc58e9d6a8ac580a0d8621191e15feff46a1b
Vladimir Sizikov <vsizikov@gmail.com>
2008-05-26 15:40:27 +0200

    Fixed internal contradiction between Bignum specs.

--- 1.8/core/bignum/div_spec.rb
+++ 1.8/core/bignum/div_spec.rb
@@ -5,8 +5,12 @@ describe "Bignum#div" do
   it_behaves_like(:bignum_divide, :div)
 
   ruby_bug "#", "1.8.6.114" do
-    it "returns self divided by float" do
+    it "returns a result of integer division of self by a float argument" do
       @bignum.div(0xffff_ffff.to_f).should == 2147483648
+    end
+    it "raises FloatDomainError if the argument is a float zero" do
+      lambda { @bignum.div(0.0) }.should raise_error(FloatDomainError, "Infinity")
+      lambda { @bignum.div(-0.0) }.should raise_error(FloatDomainError, "-Infinity")
     end
   end
 end
