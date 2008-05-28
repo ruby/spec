cc3c7911110ad38bfdfbb0272e7332bec06b4007
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-28 16:26:59 +0200

    Added Specs for Net::FTP#abort.

--- 1.8/library/net/ftp/abort_spec.rb
+++ 1.8/library/net/ftp/abort_spec.rb
@@ -2,6 +2,36 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'net/ftp'
 
 describe "Net::FTP#abort" do
-  it "needs to be reviewed for spec completeness" do
+  before(:each) do
+    @socket = mock("Socket")
+    @socket.stub!(:send)
+    @socket.stub!(:readline).and_return("226 Ignored")
+    
+    @ftp = Net::FTP.new
+    @ftp.instance_variable_set(:@sock, @socket)
+  end
+  
+  it "sends 'ABOR\\r\\n' over the socket" do
+    @socket.should_receive(:send).with("ABOR\r\n", Socket::MSG_OOB)
+    @ftp.abort
+  end
+  
+  it "returns the full response" do
+    @socket.should_receive(:readline).and_return("226 Ignored")
+    @ftp.abort.should == "226 Ignored\n"
+  end
+  
+  it "does not raise a Net::FTPProtoError when the response code is 426, 226 or 225" do
+    @socket.should_receive(:readline).and_return("426 Ignored", "226 Ignored", "225 Ignored")
+    lambda { @ftp.abort }.should_not raise_error(Net::FTPProtoError)
+    lambda { @ftp.abort }.should_not raise_error(Net::FTPProtoError)
+    lambda { @ftp.abort }.should_not raise_error(Net::FTPProtoError)
+  end
+
+  it "raises a Net::FTPProtoError when the response code is not 426, 226 or 225" do
+    @socket.should_receive(:readline).and_return("227 Ignored", "228 Ignored", "224 Ignored")
+    lambda { @ftp.abort }.should raise_error(Net::FTPProtoError)
+    lambda { @ftp.abort }.should raise_error(Net::FTPProtoError)
+    lambda { @ftp.abort }.should raise_error(Net::FTPProtoError)
   end
 end
