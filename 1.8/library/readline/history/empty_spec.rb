3aa3e72b8155e26e02bf86d275c7aba0dbb14847
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-29 23:45:27 +0200

    Added specs for Readline::HISTORY.empty?.

--- 1.8/library/readline/history/empty_spec.rb
+++ 1.8/library/readline/history/empty_spec.rb
@@ -2,6 +2,11 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'readline'
 
 describe "Readline::HISTORY.empty?" do
-  it "needs to be reviewed for spec completeness" do
+  it "returns true when the history is empty" do
+    Readline::HISTORY.should be_empty
+    Readline::HISTORY.push("test")
+    Readline::HISTORY.should_not be_empty
+    Readline::HISTORY.pop
+    Readline::HISTORY.should be_empty
   end
 end
