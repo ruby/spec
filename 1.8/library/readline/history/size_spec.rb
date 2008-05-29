79e88df3c2f17c4820be1da058eedef78224264f
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-30 00:25:02 +0200

    Added specs for Readline::HISTORY.size.

--- 1.8/library/readline/history/size_spec.rb
+++ 1.8/library/readline/history/size_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'readline'
+require File.dirname(__FILE__) + '/shared/size'
 
 describe "Readline::HISTORY.size" do
-  it "needs to be reviewed for spec completeness" do
-  end
+  it_behaves_like :readline_history_size, :size
 end
