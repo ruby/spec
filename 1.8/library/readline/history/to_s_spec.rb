f20a377f543e58cf6489d902b5817794bebed47d
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-29 23:31:17 +0200

    Added specs for Readline::HISTORY.to_s.

--- 1.8/library/readline/history/to_s_spec.rb
+++ 1.8/library/readline/history/to_s_spec.rb
@@ -2,6 +2,7 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'readline'
 
 describe "Readline::HISTORY.to_s" do
-  it "needs to be reviewed for spec completeness" do
+  it "returns 'HISTORY'" do
+    Readline::HISTORY.to_s.should == "HISTORY"
   end
 end
