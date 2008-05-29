5c4fc270b9a9824b795071047887a5efab4fd3cf
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-30 00:26:50 +0200

    Readline::HISTORY.length is an alias of Readline::HISTORY.size.

--- 1.8/library/readline/history/length_spec.rb
+++ 1.8/library/readline/history/length_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'readline'
+require File.dirname(__FILE__) + '/shared/size'
 
 describe "Readline::HISTORY.length" do
-  it "needs to be reviewed for spec completeness" do
-  end
+  it_behaves_like :readline_history_size, :length
 end
