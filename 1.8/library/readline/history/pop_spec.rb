10cb324805b45872e9b3e270ae61b7f032a6c24c
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-29 23:34:25 +0200

    Added specs for Readline::HISTORY.pop.

--- 1.8/library/readline/history/pop_spec.rb
+++ 1.8/library/readline/history/pop_spec.rb
@@ -2,6 +2,20 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'readline'
 
 describe "Readline::HISTORY.pop" do
-  it "needs to be reviewed for spec completeness" do
+  it "returns nil when the history is empty" do
+    Readline::HISTORY.pop.should be_nil
+  end
+  
+  it "returns the last item of the history" do
+    Readline::HISTORY.push("test")
+    Readline::HISTORY.pop.should == "test"
+  end
+  
+  it "removes the item from the history" do
+    Readline::HISTORY.push("test")
+    Readline::HISTORY.size.should == 1
+    
+    Readline::HISTORY.pop
+    Readline::HISTORY.size.should == 0
   end
 end
