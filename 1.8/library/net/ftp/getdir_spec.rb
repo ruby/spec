02918cc598e53a7a22325b2bbdb3792290b154d7
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-29 21:52:29 +0200

    Net::FTP#getdir is an alias of Net::FTP#pwd.

--- 1.8/library/net/ftp/getdir_spec.rb
+++ 1.8/library/net/ftp/getdir_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'net/ftp'
+require File.dirname(__FILE__) + "/shared/pwd"
 
 describe "Net::FTP#getdir" do
-  it "needs to be reviewed for spec completeness" do
-  end
+  it_behaves_like :net_ftp_pwd, :getdir
 end
