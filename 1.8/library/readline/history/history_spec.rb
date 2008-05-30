fcaa37e11406ad9b4417063d7191b70a0bc761a7
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-30 14:41:24 +0200

    Fix the description for the Readline::HISTORY spec.

--- 1.8/library/readline/history/history_spec.rb
+++ 1.8/library/readline/history/history_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'readline'
 
 describe "Readline::HISTORY" do
-  it "includes the Enumerable module" do
+  it "is extended with the Enumerable module" do
     Readline::HISTORY.should be_kind_of(Enumerable)
   end
 end
