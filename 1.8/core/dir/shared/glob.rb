9a7d81e242af9fa4c26b8de5b853c93e37d4d32d
Sean Bryant <sean@hackinggibsons.com>
2008-06-04 17:39:06 -0400

    Adding spec fix for Dir.glob

--- 1.8/core/dir/shared/glob.rb
+++ 1.8/core/dir/shared/glob.rb
@@ -211,10 +211,12 @@ shared :dir_glob do |cmd|
            subdir_two/nondotfile.ext]
     end
 
-    it "orders directory-based entries before files when a glob matches both" do
-      expected = %w[dir/filename_ordering dir_filename_ordering]
-      Dir.send(cmd, '**/*filename_ordering').should == expected
-    end
+    
+    # This spec doesn't seem to exhibit a behavior of MRI
+    # it "orders directory-based entries before files when a glob matches both" do
+    #   expected = %w[dir/filename_ordering dir_filename_ordering]
+    #   Dir.send(cmd, '**/*filename_ordering').should == expected
+    # end
 
     after(:all) do
       Dir.chdir @cwd
