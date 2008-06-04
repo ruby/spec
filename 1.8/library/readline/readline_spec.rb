5ddcce2224feb3331608fd363de7228cefae0373
Vincent Lu <vincentlu@gmail.com>
2008-06-04 12:14:03 -0700

    Spec for Readline::HISTORY.
    * Readline::HISTORY is a Enumerable.

--- 1.8/library/readline/readline_spec.rb
+++ 1.8/library/readline/readline_spec.rb
@@ -1,6 +1,12 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require 'readline'
 
+describe "Readline::HISTORY" do
+  it "Readline::HISTORY is a Enumerable" do
+    Readline::HISTORY.is_a?(Enumerable).should == true
+  end
+end
+
 describe "Readline#readline" do
   it "needs to be reviewed for spec completeness" do
   end
