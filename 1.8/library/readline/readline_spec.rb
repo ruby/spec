7b240225a944138e6e029a99fa5b857a446b6d2b
Vladimir Sizikov <vsizikov@gmail.com>
2008-06-04 21:21:52 +0200

    Revert "Spec for Readline::HISTORY." We already have such spec.
    * Take a look at history/history_spec
    
    * This reverts commit 5ddcce2224feb3331608fd363de7228cefae0373
      and a35e66854d1441883c8a40ee3316e3393805225d.

--- 1.8/library/readline/readline_spec.rb
+++ 1.8/library/readline/readline_spec.rb
@@ -1,12 +1,6 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require 'readline'
 
-describe "Readline::HISTORY" do
-  it "is a Enumerable" do
-    Readline::HISTORY.is_a?(Enumerable).should == true
-  end
-end
-
 describe "Readline#readline" do
   it "needs to be reviewed for spec completeness" do
   end
