1d3e23da6339cac1eaa6efcd36bba5e83635a755
Michael Latta <mlatta@technomage.com>
2008-06-05 14:14:52 -0700

    Add explanitory comment to dir/shared/pwd spec
    Add an explanitory comment to the patch so it is clear why the
    patch was applied.

--- 1.8/core/dir/shared/pwd.rb
+++ 1.8/core/dir/shared/pwd.rb
@@ -3,6 +3,9 @@ shared :dir_pwd do |cmd|
     it "returns the current working directory" do
       # On ubuntu gutsy, for example, /bin/pwd does not
       # understand -P. With just `pwd -P`, /bin/pwd is run.
+      
+      # The following uses inode rather than file names to account for
+      # case insensitive file systems like default OS/X file systems
       File.stat(Dir.send(cmd)).ino.should == File.stat(`/bin/sh -c "pwd -P"`.chomp).ino
     end
   end
