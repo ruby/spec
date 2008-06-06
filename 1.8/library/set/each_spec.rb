fefd02f73be793c2e5e18c3b3009e88795da63bb
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-07 00:54:42 +0200

    Fixed and updated the Set#each specs.

--- 1.8/library/set/each_spec.rb
+++ 1.8/library/set/each_spec.rb
@@ -8,15 +8,27 @@ describe "Set#each" do
   
   it "yields each Object in self" do
     ret = []
-    @set.each do { |x| ret << x }
+    @set.each { |x| ret << x }
     ret.sort.should == [1, 2, 3]
   end
   
   it "returns self" do
-    @set.each do { |x| x }.should equal(@set)
+    @set.each { |x| x }.should equal(@set)
   end
   
-  it "raises a LocalJumpError when not passed a block" do
-    lambda { @set.each }.should raise_error(LocalJumpError)
+  ruby_version_is "1.8.7" do
+    it "returns an Enumerator when not passed a block" do
+      enum = @set.each
+      
+      ret = []
+      enum.each { |x| ret << x }
+      ret.sort.should == [1, 2, 3]
+    end
+  end
+  
+  ruby_version_is "" ... "1.8.7" do
+    it "raises a LocalJumpError when not passed a block" do
+      lambda { @set.each }.should raise_error(LocalJumpError)
+    end
   end
 end
