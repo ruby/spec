f331abcd4ac42d156061ae85d74c276bc7a89691
Michael Latta <mlatta@technomage.com>
2008-06-05 18:29:14 -0700

    Handle IPv6 only in gethostbyname spec
    The gethostbyname spec was altered to test for the address type (IPv6 / IPv4)
    and look for the proper loopback address in each address type (127.0.0.1 or ::1)

--- 1.8/library/socket/tcpsocket/gethostbyname_spec.rb
+++ 1.8/library/socket/tcpsocket/gethostbyname_spec.rb
@@ -4,6 +4,10 @@ require File.dirname(__FILE__) + '/../fixtures/classes'
 describe "TCPSocket#gethostbyname" do
   it "should resolve a hostname to an address" do
     a = TCPSocket.gethostbyname("localhost")
-    a.should include("127.0.0.1")
+    if a[2] == Socket::AF_INET6
+      a.should include("::1")
+    else
+      a.should include("127.0.0.1")
+    end
   end
 end
