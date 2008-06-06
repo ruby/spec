5b6ff6f469dac19dec11f93df813b5d4664da74c
Federico Builes <federico.builes@gmail.com>
2008-06-06 11:02:20 -0500

    Specs for REXML::Attribute#each and REXML::Attribute#each_attribute

--- 1.8/library/rexml/attributes/each_attribute_spec.rb
+++ 1.8/library/rexml/attributes/each_attribute_spec.rb
@@ -2,6 +2,21 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'rexml/document'
 
 describe "REXML::Attributes#each_attribute" do
-  it "needs to be reviewed for spec completeness" do
+  it "iterates over the attributes yielding actual Attribute objects" do
+    e = REXML::Element.new("root")
+    name = REXML::Attribute.new("name", "Joe")
+    ns_uri = REXML::Attribute.new("xmlns:ns", "http://some_uri")
+    attributes = []
+
+    e.attributes.each do |attr|
+      attr.should be_kind_of(Attribute)
+      attributes << attr 
+    end
+    
+    attributes.first.should == @name
+    attributes.last.should == @ns_uri
   end
 end
+
+
+
