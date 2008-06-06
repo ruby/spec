9324ef27bec1040b32788a94f9387962e9ba4238
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-06 21:54:54 +0200

    Added specs for Set#each.

--- 1.8/library/set/each_spec.rb
+++ 1.8/library/set/each_spec.rb
@@ -1,2 +1,22 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require 'set'
+
+describe "Set#each" do
+  before(:each) do
+    @set = Set[1, 2, 3]
+  end
+  
+  it "yields each Object in self" do
+    ret = []
+    @set.each do { |x| ret << x }
+    ret.sort.should == [1, 2, 3]
+  end
+  
+  it "returns self" do
+    @set.each do { |x| x }.should equal(@set)
+  end
+  
+  it "raises a LocalJumpError when not passed a block" do
+    lambda { @set.each }.should raise_error(LocalJumpError)
+  end
+end
