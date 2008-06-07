c263ac73618f0a156d2f2bfd3659f09ff77c2409
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-07 10:42:10 +0200

    Added specs for Set#inspect.

--- 1.8/library/set/inspect_spec.rb
+++ 1.8/library/set/inspect_spec.rb
@@ -2,6 +2,11 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require 'set'
 
 describe "Set#inspect" do
-  it "needs to be reviewed for spec completeness" do
+  it "returns a human readable representation of self" do
+    Set[].inspect.should == "#<Set: {}>"
+    Set[nil, false, true].inspect.should == "#<Set: {false, true, nil}>"
+    Set[1, 2, 3].inspect.should == "#<Set: {1, 2, 3}>"
+    Set["1", "2", "3"].inspect.should == %q{#<Set: {"1", "2", "3"}>}
+    Set[:a, "b", Set[?c]].inspect.should == %q{#<Set: {:a, "b", #<Set: {99}>}>}
   end
 end
