15c5c033eebe2349cc9db26e0a8944fb1a2ac7c0
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-07 10:27:42 +0200

    Added specs for Set#hash.

--- 1.8/library/set/hash_spec.rb
+++ 1.8/library/set/hash_spec.rb
@@ -2,6 +2,12 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require 'set'
 
 describe "Set#hash" do
-  it "needs to be reviewed for spec completeness" do
+  it "should be static" do
+    Set[].hash.should == Set[].hash
+    Set[1, 2, 3].hash.should == Set[1, 2, 3].hash
+    Set[:a, "b", ?c].hash.should == Set[?c, "b", :a].hash
+    
+    Set[].hash.should_not == Set[1, 2, 3].hash
+    Set[1, 2, 3].hash.should_not == Set[:a, "b", ?c].hash
   end
 end
