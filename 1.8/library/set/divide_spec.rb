1b404c42782a4688c0fa6942f5a996ea00f6381b
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-07 20:20:13 +0200

    Updated Set#divide specs.

--- 1.8/library/set/divide_spec.rb
+++ 1.8/library/set/divide_spec.rb
@@ -22,12 +22,13 @@ end
 
 describe "Set#divide when passed a block with an arity of 2" do
   it "divides self into a set of subsets based on the blocks return values" do
-    set = Set["one", "two", "three", "four", "five"].divide { |x| x.length }
-    set.should == Set[Set["one", "two"], Set["four", "five"], Set["three"]]
+    set = Set[1, 3, 4, 6, 9, 10, 11].divide { |x, y| (x - y).abs == 1 }
+    set.should == Set[Set[1], Set[11, 9, 10], Set[3, 4], Set[6]]
   end
   
   it "yields each two Object to the block" do
-    set = Set[1, 3, 4, 6, 9, 10, 11].divide { |x, y| (x - y).abs == 1 }
-    set.should == Set[Set[1], Set[11, 9, 10], Set[3, 4], Set[6]]
+    ret = []
+    Set[1, 2].divide { |x, y| ret << [x, y] }
+    ret.should == [[1, 1], [1, 2], [2, 1], [2, 2]]
   end
 end
