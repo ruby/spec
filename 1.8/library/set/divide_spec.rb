0eef3d0db120b6fc66dab5f1079332522718ad5a
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-07 15:34:54 +0200

    Added specs for Set#divide.

--- 1.8/library/set/divide_spec.rb
+++ 1.8/library/set/divide_spec.rb
@@ -2,6 +2,32 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require 'set'
 
 describe "Set#divide" do
-  it "needs to be reviewed for spec completeness" do
+  it "divides self into a set of subsets based on the blocks return values" do
+    set = Set["one", "two", "three", "four", "five"].divide { |x| x.length }
+    set.should == Set[Set["one", "two"], Set["four", "five"], Set["three"]]
+  end
+  
+  it "yields each Object to the block" do
+    ret = []
+    Set["one", "two", "three", "four", "five"].divide { |x| ret << x }
+    ret.sort.should == ["one", "two", "three", "four", "five"].sort
+  end
+  
+  # BUG: Does not raise a LocalJumpError, but a NoMethodError
+  #
+  # it "raises a LocalJumpError when not passed a block" do
+  #   lambda { Set[1].divide }.should raise_error(LocalJumpError)
+  # end
+end
+
+describe "Set#divide when passed a block with an arity of 2" do
+  it "divides self into a set of subsets based on the blocks return values" do
+    set = Set["one", "two", "three", "four", "five"].divide { |x| x.length }
+    set.should == Set[Set["one", "two"], Set["four", "five"], Set["three"]]
+  end
+  
+  it "yields each two Object to the block" do
+    set = Set[1, 3, 4, 6, 9, 10, 11].divide { |x, y| (x - y).abs == 1 }
+    set.should == Set[Set[1], Set[11, 9, 10], Set[3, 4], Set[6]]
   end
 end
