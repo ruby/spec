9f99afd131cd36457260cb389a06d91b0461900f
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-09 00:40:03 +0200

    Updated StringIO#closed?, StringIO#closed_read? and StringIO#closed_write? specs.

--- 1.8/library/stringio/closed_read_spec.rb
+++ 1.8/library/stringio/closed_read_spec.rb
@@ -2,6 +2,11 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
 describe "StringIO#closed_read?" do
-  it "needs to be reviewed for spec completeness" do
+  it "returns true if self is not readable" do
+    io = StringIO.new("example", "r+")
+    io.close_write
+    io.closed_read?.should be_false
+    io.close_read
+    io.closed_read?.should be_true
   end
 end
