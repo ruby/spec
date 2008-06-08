4aea3074e974f5df2ea7079a3c35e4de40d82828
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-08 23:51:20 +0200

    Updated StringIO#<< specs.

--- 1.8/library/stringio/append_spec.rb
+++ 1.8/library/stringio/append_spec.rb
@@ -2,15 +2,58 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
 describe "StringIO#<<" do
-  it "appends to the string when in append mode" do
-    @io = StringIO.new("example", 'a')
-    (@io << "x").should == @io
-    @io.string.should == "examplex"
+  before(:each) do
+    @io = StringIO.new("example")
+  end
+  
+  it "returns self" do
+    (@io << "just testing").should equal(@io)
   end
 
-  it "writes to the string when in default mode" do
-    @io = StringIO.new("example")
-    (@io << "x").should == @io
-    @io.string.should == "xxample"
+  it "writes the passed argument onto self" do
+    (@io << "just testing").string.should == "just testing"
+    (@io << " and more testing").string.should == "just testing and more testing"
+  end
+  
+  it "taints self's String when the passed argument is tainted" do
+    obj = "test"
+    obj.taint
+    (@io << obj).string.tainted?.should be_true
+  end
+  
+  # NOTE: This IS NOT what Ruby's Core Libs do!
+  it "tries to convert the passed argument to a String using #to_s" do
+    obj = mock("to_s")
+    obj.should_receive(:to_s).and_return("Test")
+    
+    (@io << obj).string.should == "Testple"
+  end
+
+  # NOTE: This does not work
+  #
+  # it "checks whether the passed argument does respond to #to_s" do
+  #   obj = mock("to_s")
+  #   obj.should_receive(:respond_to?).with(:to_s).and_return(true)
+  #   obj.should_receive(:method_missing).with(:to_s).and_return("Test")
+  #   
+  #   (@io << obj).string#.should == "Testple"
+  # end
+end
+
+describe "StringIO#<< when in read-only mode" do
+  it "raises an IOError" do
+    io = StringIO.new("test", "r")
+    lambda { io << "test" }.should raise_error(IOError)
+  end
+end
+
+describe "StringIO#<< when in append mode" do
+  before(:each) do
+    @io = StringIO.new("example", "a")
+  end
+
+  it "appends the passed argument to the end of self" do
+    (@io << ", just testing").string.should == "example, just testing"
+    (@io << " and more testing").string.should == "example, just testing and more testing"
   end
 end
