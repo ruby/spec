d6dbc08a65d187d59deea4ee6a1405a6d9c7523d
Vladimir Sizikov <vsizikov@gmail.com>
2008-06-09 19:20:55 +0200

    New test cases for StringIO#readline.

--- 1.8/library/stringio/readline_spec.rb
+++ 1.8/library/stringio/readline_spec.rb
@@ -4,7 +4,8 @@ require File.dirname(__FILE__) + '/fixtures/classes'
 describe "StringIO#readline" do
   before(:each) do
     @io = StringIO.new("r e a d")
-    @io_para = StringIO.new("para1-1\npara1-2\n\n\npara2-1\npara2-2\n\n\n\n")
+    @io_para = StringIO.new("\n\n\n\npara1-1\npara1-2\n\n\npara2-1\npara2-2\n\n\n\n")
+    @io_empty_lines = StringIO.new("\n\n\n\n\n\n\n\n\n\n\n")
   end
 
   it "returns the next 'line'" do
@@ -14,6 +15,12 @@ describe "StringIO#readline" do
   it "raises an EOFError at the end" do
     @io.readline
     lambda { @io.readline }.should raise_error(EOFError)
+
+    @io_para.readline("")
+    @io_para.readline("")
+    lambda { @io.readline }.should raise_error(EOFError)
+
+    lambda {@io_empty_lines.readline("")}.should raise_error(EOFError)
   end
 
   it "raises an IOError when it is not open for reading" do
