38837bf017899ce11ff012931ab24c62ebd891cc
Federico Builes <federico.builes@gmail.com>
2008-06-10 07:39:47 -0500

    Specs for REXML::Attributes#new and REXML::CData stubs

---
+++ 1.8/library/rexml/cdata/clone_spec.rb
@@ -0,0 +1,7 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+require 'rexml/document'
+
+describe "REXML::CData#clone" do
+  it "needs to be reviewed for spec completeness" do
+  end
+end
---
+++ 1.8/library/rexml/cdata/value_spec.rb
@@ -0,0 +1,7 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+require 'rexml/document'
+
+describe "REXML::CData#value" do
+  it "needs to be reviewed for spec completeness" do
+  end
+end
