aaf2ebaeabd1645309a76fcfc7d429107ddcb134
Federico Builes <federico.builes@gmail.com>
2008-06-10 18:19:11 -0500

    Initial stubs for REXML::Attribute

---
+++ 1.8/library/rexml/attribute/element_spec.rb
@@ -0,0 +1,12 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+require 'rexml/document'
+
+describe "REXML::Attribute#element" do
+  it "needs to be reviewed for spec completeness" do
+  end
+end
+
+describe "REXML::Attribute#element=" do
+  it "needs to be reviewed for spec completeness" do
+  end
+end
