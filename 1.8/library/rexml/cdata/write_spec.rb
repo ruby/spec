ab63299b699982dc0c3424800a776297358300ec
Federico Builes <federico.builes@gmail.com>
2008-06-10 08:19:25 -0500

    REXML::CData specs

--- 1.8/library/rexml/cdata/write_spec.rb
+++ 1.8/library/rexml/cdata/write_spec.rb
@@ -2,6 +2,6 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'rexml/document'
 
 describe "REXML::CData#write" do
-  it "needs to be reviewed for spec completeness" do
+  it "is deprecated" do
   end
 end
