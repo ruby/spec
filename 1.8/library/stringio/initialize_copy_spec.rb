2d4bad6c2466aad3a62ba4bd0bf6e989b440330e
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-11 12:15:31 +0200

    Added specs for StringIO#initialize_copy.

--- 1.8/library/stringio/initialize_copy_spec.rb
+++ 1.8/library/stringio/initialize_copy_spec.rb
@@ -2,6 +2,34 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require 'stringio'
 
 describe "StringIO#initialize_copy" do
-  it "needs to be reviewed for spec completeness" do
+  before(:each) do
+    @io      = StringIO.new("StringIO example")
+    @orig_io = StringIO.new("Original StringIO")
+  end
+  
+  it "is private" do
+    @io.private_methods.should include("initialize_copy")
+  end
+  
+  it "returns self" do
+    @io.send(:initialize_copy, @orig_io).should equal(@io)
+  end
+
+  it "tries to convert the passed argument to a StringIO using #to_strio" do
+    obj = mock('to_strio')
+    obj.should_receive(:to_strio).and_return(StringIO.new("converted"))
+    @io.send(:initialize_copy, obj)
+    @io.string.should == "converted"
+  end
+  
+  it "copies the passed StringIO's content to self" do
+    @io.send(:initialize_copy, @orig_io)
+    @io.string.should == "Original StringIO"
+  end
+  
+  it "copes the passed StringIO's position to self" do
+    @orig_io.pos = 5
+    @io.send(:initialize_copy, @orig_io)
+    @io.pos.should eql(5)
   end
 end
