02df7652a6e181db9da9859227af0725c81abc45
Federico Builes <federico.builes@gmail.com>
2008-06-10 19:10:14 -0500

    Some specs for REXML::Attribute
    * Covers #clone, #element, #element=, #hash, #initialize and
      #node_type

--- 1.8/library/rexml/attribute/equal_value_spec.rb
+++ 1.8/library/rexml/attribute/equal_value_spec.rb
@@ -2,6 +2,17 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'rexml/document'
 
 describe "REXML::Attribute#==" do
-  it "needs to be reviewed for spec completeness" do
+  it "returns true if other has equal name and value" do
+    a1 = REXML::Attribute.new("foo", "bar")
+    a1.should == a1.clone
+
+    a2 = REXML::Attribute.new("foo", "bar")
+    a1.should == a2
+
+    a3 = REXML::Attribute.new("foo", "bla")
+    a1.should_not == a3
+
+    a4 = REXML::Attribute.new("baz", "bar")
+    a1.should_not == a4
   end
 end
--- 1.8/library/rexml/attribute/node_type_spec.rb
+++ 1.8/library/rexml/attribute/node_type_spec.rb
@@ -2,6 +2,9 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'rexml/document'
 
 describe "REXML::Attribute#node_type" do
-  it "needs to be reviewed for spec completeness" do
+  it "always returns :attribute" do
+    attr = REXML::Attribute.new("foo", "bar")
+    attr.node_type.should == :attribute
+    REXML::Attribute.new(attr).node_type.should == :attribute
   end
 end
