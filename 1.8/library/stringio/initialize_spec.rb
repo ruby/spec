d51c21d692e9a6df8149a42dcf2f4b75064c23f5
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-11 13:33:55 +0200

    Added specs for StringIO#initialize.

--- 1.8/library/stringio/initialize_spec.rb
+++ 1.8/library/stringio/initialize_spec.rb
@@ -1,7 +1,169 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require 'stringio'
 
+describe "StringIO#initialize when passed [Object, Integer]" do
+  it "sets the mode based on the passed Integer" do
+    io = StringIO.allocate
+    io.send(:initialize, "example", IO::RDONLY)
+    io.closed_read?.should be_false
+    io.closed_write?.should be_true
+
+    io = StringIO.allocate
+    io.send(:initialize, "example", IO::WRONLY)
+    io.closed_read?.should be_true
+    io.closed_write?.should be_false
+
+    io = StringIO.allocate
+    io.send(:initialize, "example", IO::RDWR)
+    io.closed_read?.should be_false
+    io.closed_write?.should be_false
+
+    io = StringIO.allocate
+    io.send(:initialize, "example", IO::APPEND)
+    io.closed_read?.should be_false
+    io.closed_write?.should be_true
+
+    io = StringIO.allocate
+    io.send(:initialize, "example", IO::CREAT)
+    io.closed_read?.should be_false
+    io.closed_write?.should be_true
+
+    io = StringIO.allocate
+    io.send(:initialize, "example", IO::EXCL)
+    io.closed_read?.should be_false
+    io.closed_write?.should be_true
+  end
+end
+
+describe "StringIO#initialize when passed [Object, Object]" do
+  before(:each) do
+    @io = StringIO.allocate
+  end
+  
+  it "tries to convert the passed mode-Object to a String using #to_str" do
+    obj = mock('to_str')
+    obj.should_receive(:to_str).and_return("r")
+    @io.send(:initialize, "example", obj)
+    
+    @io.closed_read?.should be_false
+    @io.closed_write?.should be_true
+  end
+
+  ruby_version_is "" ... "1.8.7" do
+    it "checks whether the passed mode-Object responds to #to_str" do
+      obj = mock('method_missing to_str')
+      obj.should_receive(:respond_to?).with(:to_str).and_return(true)
+      obj.should_receive(:method_missing).with(:to_str).and_return("r")
+      @io.send(:initialize, "example", obj)
+    end
+  end
+
+  ruby_version_is "1.8.7" do
+    it "checks whether the passed mode-Object responds to #to_str (including private methods)" do
+      obj = mock('method_missing to_str')
+      obj.should_receive(:respond_to?).with(:to_str, true).and_return(true)
+      obj.should_receive(:method_missing).with(:to_str).and_return("r")
+      @io.send(:initialize, "example", obj)
+    end
+  end
+  
+  it "sets the mode based on the passed mode-Object" do
+    io = StringIO.allocate
+    io.send(:initialize, "example", "r")
+    io.closed_read?.should be_false
+    io.closed_write?.should be_true
+
+    io = StringIO.allocate
+    io.send(:initialize, "example", "w")
+    io.closed_read?.should be_true
+    io.closed_write?.should be_false
+
+    io = StringIO.allocate
+    io.send(:initialize, "example", "r+")
+    io.closed_read?.should be_false
+    io.closed_write?.should be_false
+
+    io = StringIO.allocate
+    io.send(:initialize, "example", "w+")
+    io.closed_read?.should be_false
+    io.closed_write?.should be_false
+
+    io = StringIO.allocate
+    io.send(:initialize, "example", "a")
+    io.closed_read?.should be_true
+    io.closed_write?.should be_false
+
+    io = StringIO.allocate
+    io.send(:initialize, "example", "a+")
+    io.closed_read?.should be_false
+    io.closed_write?.should be_false
+  end
+  
+  # platform_is :windows do
+  #   it "can set the mode to binary mode" do
+  #     io = StringIO.allocate
+  #     io.send(:initialize, "example", "b")
+  #     io.
+  #   end
+  # end
+end
+
 describe "StringIO#initialize" do
-  it "needs to be reviewed for spec completeness" do
+  before(:each) do
+    @io = StringIO.allocate
+  end
+  
+  it "is private" do
+    @io.private_methods.should include("initialize")
+  end
+  
+  it "returns self" do
+    @io.send(:initialize, "example").should equal(@io)
+  end
+
+  it "sets the mode to read-write" do
+    @io.send(:initialize, "example")
+    @io.closed_read?.should be_false
+    @io.closed_write?.should be_false
+  end
+  
+  it "tries to convert the passed Object to a String using #to_str" do
+    obj = mock('to_str')
+    obj.should_receive(:to_str).and_return("example")
+    @io.send(:initialize, obj)
+  end
+
+  ruby_version_is "" ... "1.8.7" do
+    it "checks whether the passed argument responds to #to_str" do
+      obj = mock('method_missing to_str')
+      obj.should_receive(:respond_to?).with(:to_str).and_return(true)
+      obj.should_receive(:method_missing).with(:to_str).and_return("example")
+      @io.send(:initialize, obj)
+    end
+  end
+
+  ruby_version_is "1.8.7" do
+    it "checks whether the passed argument responds to #to_str (including private methods)" do
+      obj = mock('method_missing to_str')
+      obj.should_receive(:respond_to?).with(:to_str, true).and_return(true)
+      obj.should_receive(:method_missing).with(:to_str).and_return("example")
+      @io.send(:initialize, obj)
+    end
+  end
+  
+  compliant_on :ruby, :jruby do
+    it "raises an Errno::EACCES error when passed a frozen string with a write-mode" do
+      (str = "example").freeze
+      lambda { @io.send(:initialize, str, "r+") }.should raise_error(Errno::EACCES)
+      lambda { @io.send(:initialize, str, "w") }.should raise_error(Errno::EACCES)
+      lambda { @io.send(:initialize, str, "a") }.should raise_error(Errno::EACCES)
+    end
+    
+    it "automatically sets the mode when passed a frozen string to read-only" do
+      (str = "example").freeze
+      @io.send(:initialize, str)
+      @io.closed_read?.should be_false
+      @io.closed_write?.should be_true
+    end
   end
 end
