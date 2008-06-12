43741d2c9c932390de5c0bf97c9058db2edabd9f
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-12 09:29:38 +0200

    Added specs for StringIO#lines, which is 1.8.7 specific.

--- 1.8/library/stringio/lines_spec.rb
+++ 1.8/library/stringio/lines_spec.rb
@@ -1,7 +1,9 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require 'stringio'
+require File.dirname(__FILE__) + '/shared/each'
 
-describe "StringIO#lines" do
-  it "needs to be reviewed for spec completeness" do
+ruby_version_is "1.8.7" do
+  describe "StringIO#lines" do
+    it_behaves_like :stringio_each, :lines
   end
 end
