b304274b82ebd72b151cc77754fbc79d6ed6a816
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-12 10:14:01 +0200

    Updated StringIO#print specs.

--- 1.8/library/stringio/print_spec.rb
+++ 1.8/library/stringio/print_spec.rb
@@ -1,22 +1,58 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
+describe "StringIO#print when in read-only mode" do
+  it "raises an IOError" do
+    io = StringIO.new("test", "r")
+    lambda { io.print("test") }.should raise_error(IOError)
+
+    io = StringIO.new("test")
+    io.close_write
+    lambda { io.print("test") }.should raise_error(IOError)
+  end
+end
+
 describe "StringIO#print" do
   before(:each) do
     @io = StringIO.new('')
   end
-  after(:each) do
-    $\ = nil
-  end
 
-  it "prints multiple items to the output" do
-    @io.print(5,6,7,8).should == nil
-    @io.string.should == '5678'
+  it "prints the passed arguments to self" do
+    @io.print(5, 6, 7, 8)
+    @io.string.should == "5678"
+  end
+  
+  it "returns nil" do
+    @io.print(1, 2, 3).should be_nil
   end
 
   it "honors the output record separator global" do
-    $\ = 'x'
-    @io.print(5,6,7,8).should == nil
-    @io.string.should == '5678x'
+    old_rs, $\ = $\, 'x'
+    
+    begin
+      @io.print(5, 6, 7, 8)
+      @io.string.should == '5678x'
+    ensure
+      $\ = old_rs
+    end
+  end
+  
+  it "updates the current position" do
+    @io.print(1, 2, 3)
+    @io.pos.should eql(3)
+    
+    @io.print(1, 2, 3)
+    @io.pos.should eql(6)
+  end
+  
+  it "correctly updates the current position when honoring the output record separator global" do
+    old_rs, $\ = $\, 'x'
+    
+    begin
+      @io.print(5, 6, 7, 8)
+      @io.pos.should eql(5)
+    ensure
+      $\ = old_rs
+    end
   end
 end
