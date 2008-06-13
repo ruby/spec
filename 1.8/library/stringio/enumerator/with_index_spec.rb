ee7ac508f15953aa02f9364cd4e76d4bb461c602
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-13 13:08:17 +0200

    Removed bogus StringIO::Enumerator specs accidently generated by bin/mkspec.

--- 1.8/library/stringio/enumerator/with_index_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(__FILE__) + '/../../../spec_helper'
-require 'stringio'
-
-describe "StringIO::Enumerator#with_index" do
-  it "needs to be reviewed for spec completeness" do
-  end
-end
