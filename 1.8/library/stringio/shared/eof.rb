29c7fd985dd62825f0d15b77a7a22fd9ed82df4f
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-13 13:32:13 +0200

    Small updates to StringIO#eof? and StringIO#eof.

--- 1.8/library/stringio/shared/eof.rb
+++ 1.8/library/stringio/shared/eof.rb
@@ -4,20 +4,23 @@ shared :stringio_eof do |cmd|
       @io = StringIO.new("eof")
     end
 
-    it "returns true when self's position is at the end" do
+    it "returns true when self's position is greater than or equal to self's size" do
       @io.pos = 3
-      @io.send(cmd).should == true
+      @io.send(cmd).should be_true
+      
+      @io.pos = 6
+      @io.send(cmd).should be_true
     end
 
-    it "returns false when self's position is not at the end" do
+    it "returns false when self's position is less than self's size" do
       @io.pos = 0
-      @io.send(cmd).should == false
+      @io.send(cmd).should be_false
 
       @io.pos = 1
-      @io.send(cmd).should == false
+      @io.send(cmd).should be_false
 
       @io.pos = 2
-      @io.send(cmd).should == false
+      @io.send(cmd).should be_false
     end
   end
 end
