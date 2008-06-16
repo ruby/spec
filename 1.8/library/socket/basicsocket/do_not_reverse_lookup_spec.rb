c890d8a1fbc122e4ea671f97c0fcfa6262a20fd2
Federico Builes <federico.builes@gmail.com>
2008-06-16 16:46:52 -0500

    Additional specs for Socket::BasicSocket#do_not_reverse_lookup

--- 1.8/library/socket/basicsocket/do_not_reverse_lookup_spec.rb
+++ 1.8/library/socket/basicsocket/do_not_reverse_lookup_spec.rb
@@ -1,10 +1,11 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 require File.dirname(__FILE__) + '/../fixtures/classes'
 
-describe "BasicSocket#do_not_reverse_lookup" do
+describe "BasicSocket.do_not_reverse_lookup" do
   before(:each) do
     BasicSocket.do_not_reverse_lookup = true
     @server = TCPServer.new('127.0.0.1', SocketSpecs.port)
+    @socket = TCPSocket.new('127.0.0.1', SocketSpecs.port)
   end
   
   after(:each) do
@@ -14,7 +15,16 @@ describe "BasicSocket#do_not_reverse_lookup" do
   end
 
   it "causes 'peeraddr' to avoid name lookups" do
-    @socket = TCPSocket.new('127.0.0.1', SocketSpecs.port)
     @socket.peeraddr.should == ["AF_INET", SocketSpecs.port, "127.0.0.1", "127.0.0.1"]
+  end
+
+  it "looks for hostnames when set to false" do
+    BasicSocket.do_not_reverse_lookup = false
+    @socket.peeraddr[2].should == "localhost"
+  end
+
+  it "looks for numeric addresses when set to true" do
+    BasicSocket.do_not_reverse_lookup = true
+    @socket.peeraddr[2].should == "127.0.0.1"
   end
 end
