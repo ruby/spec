4dbdddaad53aa10c0192b1096ee603140e91a24a
Federico Builes <federico.builes@gmail.com>
2008-06-16 18:13:21 -0500

    Specs for BasicSocket#close_read and BasicSocket#close_write
    * Failures with JRuby have been reported in JRUBY-2672

--- 1.8/library/socket/basicsocket/close_read_spec.rb
+++ 1.8/library/socket/basicsocket/close_read_spec.rb
@@ -1,2 +1,41 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 require File.dirname(__FILE__) + '/../fixtures/classes'
+
+describe "Socket::BasicSocket#close_read" do
+  before :each do
+    @server = TCPServer.new(SocketSpecs.port)    
+  end
+
+  compliant_on :ruby do
+   
+    it "closes the reading end of the socket" do
+      @server.close_read
+      lambda { @server.read }.should raise_error(IOError)
+      @server.close if @server
+    end
+
+    it "it works on sockets with closed ends" do
+      @server.close_read
+      lambda { @server.close_read }.should_not raise_error(Exception)
+      lambda { @server.read }.should raise_error(IOError)
+      @server.close if @server
+    end
+  end
+
+  not_compliant_on :ruby do
+
+    it "closes the reading end of the socket" do
+      @server.close_read
+      lambda { @server.read }.should raise_error(IOError)
+      @server.close unless @server.closed?
+    end
+
+    it "does not work on sockets with closed ends" do
+      @server.close_read
+      lambda { @server.close_read }.should raise_error(Exception)
+      @server.close unless @server.closed?
+    end
+
+  end
+end
+
