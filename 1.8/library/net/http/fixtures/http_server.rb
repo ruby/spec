eceb1d0248834d29b4974224f1620385df745cad
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-24 13:33:22 +0200

    Added simple specs for some request methods.

--- 1.8/library/net/http/fixtures/http_server.rb
+++ 1.8/library/net/http/fixtures/http_server.rb
@@ -9,6 +9,19 @@ module NetHTTPSpecs
     def printf(*args) end
   end
   
+  class RequestServlet < WEBrick::HTTPServlet::AbstractServlet
+    def do_GET(req, res)
+      res.content_type = "text/plain"
+      res.body = "Request type: #{req.request_method}"
+    end
+    
+    %w{ do_HEAD do_POST do_PUT do_PROPPATCH do_LOCK do_UNLOCK
+        do_OPTIONS do_PROPFIND do_DELETE do_MOVE do_COPY
+        do_MKCOL do_TRACE }.each do |method|
+      alias_method method.to_sym, :do_GET
+    end
+  end
+  
   class << self
     def start_server
       server_config = {
@@ -20,6 +33,7 @@ module NetHTTPSpecs
         :ServerType => Thread }
       
       @server = WEBrick::HTTPServer.new(server_config)
+
       @server.mount_proc('/') do |req, res|
         res.content_type = "text/plain"
         res.body = "This is the index page."
@@ -28,6 +42,9 @@ module NetHTTPSpecs
         res.content_type = "text/plain"
         res.body = req.body
       end
+      @server.mount('/request', RequestServlet)
+
+      
       @server.start
     end
     
