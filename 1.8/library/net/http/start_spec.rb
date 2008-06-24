a4e92e9fd223c2032721dfe768688965c11dc213
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-24 10:45:37 +0200

    Added specs for Net::HTTP#start.

--- 1.8/library/net/http/start_spec.rb
+++ 1.8/library/net/http/start_spec.rb
@@ -44,7 +44,11 @@ describe "Net::HTTP.start when passed a block" do
     end
     yielded.should be_true
   end
-  
+
+  it "opens the tcp connection before yielding" do
+    Net::HTTP.start("localhost", 3333) { |http| http.started?.should be_true }
+  end
+
   it "closes the tcp connection after yielding" do
     net = nil
     Net::HTTP.start("localhost", 3333) { |x| net = x }
@@ -53,6 +57,70 @@ describe "Net::HTTP.start when passed a block" do
 end
 
 describe "Net::HTTP#start" do
-  it "needs to be reviewed for spec completeness" do
+  before(:each) do
+    NetHTTPSpecs.start_server
+    @http = Net::HTTP.new("localhost", 3333)
+  end
+  
+  after(:each) do
+    NetHTTPSpecs.stop_server
+  end
+  
+  it "returns self" do
+    @http.start.should equal(@http)
+  end
+  
+  it "opens the tcp connection" do
+    @http.start
+    @http.started?.should be_true
+  end
+end
+
+describe "Net:HTTP#start when self has already been started" do
+  before(:each) do
+    NetHTTPSpecs.start_server
+    @http = Net::HTTP.new("localhost", 3333)
+  end
+  
+  after(:each) do
+    NetHTTPSpecs.stop_server
+  end
+
+  it "raises an IOError" do
+    @http.start
+    lambda { @http.start }.should raise_error(IOError)
+  end
+end
+
+describe "Net::HTTP#start when passed a block" do
+  before(:each) do
+    NetHTTPSpecs.start_server
+    @http = Net::HTTP.new("localhost", 3333)
+  end
+  
+  after(:each) do
+    NetHTTPSpecs.stop_server
+  end
+
+  it "returns the blocks return value" do
+    @http.start { :test }.should == :test
+  end
+  
+  it "yields the new Net::HTTP object to the block" do
+    yielded = false
+    @http.start do |http|
+      yielded = true
+      http.should equal(@http)
+    end
+    yielded.should be_true
+  end
+  
+  it "opens the tcp connection before yielding" do
+    @http.start { |http| http.started?.should be_true }
+  end
+  
+  it "closes the tcp connection after yielding" do
+    @http.start { }
+    @http.started?.should be_false
   end
 end
