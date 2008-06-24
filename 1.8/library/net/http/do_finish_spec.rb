0f12c3ba74f45b66e02ef5e85dabe295d8c67d29
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-24 11:53:28 +0200

    Added specs for Net::HTTP#do_finish and Net::HTTP#do_start.

--- 1.8/library/net/http/do_finish_spec.rb
+++ 1.8/library/net/http/do_finish_spec.rb
@@ -1,7 +1,28 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'net/http'
+require File.dirname(__FILE__) + '/fixtures/http_server'
 
 describe "Net::HTTP#do_finish" do
-  it "needs to be reviewed for spec completeness" do
+  before(:each) do
+    NetHTTPSpecs.start_server
+    @http = Net::HTTP.new("localhost", 3333)
+  end
+  
+  after(:each) do
+    NetHTTPSpecs.stop_server
+  end
+
+  it "is private" do
+    Net::HTTP.private_instance_methods.should include("do_finish")
+  end
+
+  it "closes the tcp connection" do
+    @http.start
+    @http.send(:do_finish)
+    @http.started?.should be_false
+  end
+  
+  it "raises no error when self has not been started yet" do
+    @http.send(:do_finish)
   end
 end
--- 1.8/library/net/http/do_start_spec.rb
+++ 1.8/library/net/http/do_start_spec.rb
@@ -1,7 +1,24 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'net/http'
+require File.dirname(__FILE__) + '/fixtures/http_server'
 
 describe "Net::HTTP#do_start" do
-  it "needs to be reviewed for spec completeness" do
+  before(:each) do
+    NetHTTPSpecs.start_server
+    @http = Net::HTTP.new("localhost", 3333)
+  end
+  
+  after(:each) do
+    NetHTTPSpecs.stop_server
+  end
+  
+  it "is private" do
+    Net::HTTP.private_instance_methods.should include("do_start")
+  end
+
+  it "opens the tcp connection to the current host" do
+    @http.should_receive(:connect)
+    @http.send(:do_start)
+    @http.started?.should be_true
   end
 end
