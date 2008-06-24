6a612f7ac7c5fd5493b00c17e4561a15ec26bbc1
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-24 10:26:46 +0200

    Added specs for Net::HTTP#inspect.

--- 1.8/library/net/http/inspect_spec.rb
+++ 1.8/library/net/http/inspect_spec.rb
@@ -1,7 +1,23 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'net/http'
+require File.dirname(__FILE__) + '/fixtures/http_server'
 
 describe "Net::HTTP#inspect" do
-  it "needs to be reviewed for spec completeness" do
+  before(:each) do
+    NetHTTPSpecs.start_server
+    @net = Net::HTTP.new("localhost", 3333)
+  end
+
+  after(:each) do
+    NetHTTPSpecs.stop_server
+  end
+  
+  it "returns a String representation of self" do
+    net = Net::HTTP.new("localhost", 3333)
+    net.inspect.should be_kind_of(String)
+    net.inspect.should == "#<Net::HTTP localhost:3333 open=false>"
+    
+    net.start
+    net.inspect.should == "#<Net::HTTP localhost:3333 open=true>"
   end
 end
