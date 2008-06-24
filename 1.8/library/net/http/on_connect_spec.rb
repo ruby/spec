8ec95fa74f9d60ba04c9c1ab150009ce7c09d85f
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-24 11:13:00 +0200

    Added specs for Net::HTTP#on_connect.

--- 1.8/library/net/http/on_connect_spec.rb
+++ 1.8/library/net/http/on_connect_spec.rb
@@ -2,6 +2,15 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'net/http'
 
 describe "Net::HTTP#on_connect" do
-  it "needs to be reviewed for spec completeness" do
+  before(:each) do
+    @http = Net::HTTP.new("localhost", 3333)
+  end
+  
+  it "is private" do
+    Net::HTTP.private_instance_methods.should include("on_connect")
+  end
+  
+  it "returns nil" do
+    @http.send(:on_connect).should be_nil
   end
 end
