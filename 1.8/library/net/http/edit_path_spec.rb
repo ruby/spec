dc5db92a080f2a91173faef6833d8b3305c24f12
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-24 11:54:35 +0200

    Added specs for Net::HTTP#edit_path.

--- 1.8/library/net/http/edit_path_spec.rb
+++ 1.8/library/net/http/edit_path_spec.rb
@@ -2,6 +2,12 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'net/http'
 
 describe "Net::HTTP#edit_path" do
-  it "needs to be reviewed for spec completeness" do
+  it "is private" do
+    Net::HTTP.private_instance_methods.should include("edit_path")
+  end
+  
+  it "returns the passed path" do
+    net = Net::HTTP.new("localhost", 3333)
+    net.send(:edit_path, "/test.html").should == "/test.html"
   end
 end
