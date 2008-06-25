2b4c42faf006cd56cadd7aa8da287a01e0eb0ef2
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-25 10:06:10 +0200

    Added Specs for CGI#env_table.

--- 1.8/library/cgi/env_table_spec.rb
+++ 1.8/library/cgi/env_table_spec.rb
@@ -2,6 +2,12 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require 'cgi'
 
 describe "CGI#env_table" do
-  it "needs to be reviewed for spec completeness" do
+  it "is private" do
+    CGI.private_instance_methods.should include("env_table")
+  end
+  
+  it "returns ENV" do
+    cgi = CGI.new
+    cgi.send(:env_table).should equal(ENV)
   end
 end
