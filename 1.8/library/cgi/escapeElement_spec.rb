1b163c75e8c9824a5abf4033b2e69f3242974928
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-27 14:42:53 +0200

    Fixed a small typo in CGI.escapeElement specs and cleaned CGI.rfc1123 specs up.

--- 1.8/library/cgi/escapeElement_spec.rb
+++ 1.8/library/cgi/escapeElement_spec.rb
@@ -4,9 +4,9 @@ require 'cgi'
 describe "CGI.escapeElement when passed String, elements, ..." do
   it "escapes only the tags of the passed elements in the passed String" do
     res = CGI.escapeElement('<BR><A HREF="url"></A>', "A", "IMG")
-    res.should == "<BR>&lt;A HREF=&quot;url&quot;&gt;&lt;/A&gt"
+    res.should == "<BR>&lt;A HREF=&quot;url&quot;&gt;&lt;/A&gt;"
     
     res = CGI.escapeElement('<BR><A HREF="url"></A>', ["A", "IMG"])
-    res.should == "<BR>&lt;A HREF=&quot;url&quot;&gt;&lt;/A&gt"
+    res.should == "<BR>&lt;A HREF=&quot;url&quot;&gt;&lt;/A&gt;"
   end
 end
