5d91d81d99e4414de1e83db52334d9ea1ae496f3
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-27 14:41:16 +0200

    Added specs for CGI#stdinput and CGI#stdoutput.

--- 1.8/library/cgi/stdinput_spec.rb
+++ 1.8/library/cgi/stdinput_spec.rb
@@ -2,6 +2,11 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require 'cgi'
 
 describe "CGI#stdinput" do
-  it "needs to be reviewed for spec completeness" do
+  it "is private" do
+    CGI.private_instance_methods.should include("stdinput")
+  end
+  
+  it "returns $stdin" do
+    CGI.new.send(:stdinput).should equal($stdin)
   end
 end
--- 1.8/library/cgi/stdoutput_spec.rb
+++ 1.8/library/cgi/stdoutput_spec.rb
@@ -2,6 +2,11 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require 'cgi'
 
 describe "CGI#stdoutput" do
-  it "needs to be reviewed for spec completeness" do
+  it "is private" do
+    CGI.private_instance_methods.should include("stdoutput")
+  end
+  
+  it "returns $stdout" do
+    CGI.new.send(:stdoutput).should equal($stdout)
   end
 end
