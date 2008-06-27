310d0dc7d2e30879838e9a35a2f9d32338989b47
Federico Builes <federico.builes@gmail.com>
2008-06-27 16:15:27 -0500

    Closing opened data sockets.

--- 1.8/library/socket/unixsocket/recvfrom_spec.rb
+++ 1.8/library/socket/unixsocket/recvfrom_spec.rb
@@ -19,23 +19,26 @@ describe "UNIXSocket#recvfrom" do
     @client.send("foobar", 0)
     sock = @server.accept
     sock.recvfrom(6).first.should == "foobar"
+    sock.close
   end
 
   it "returns an array with data and information on the sender" do
-     @client.send("foobar", 0)
-     sock = @server.accept
-     data = sock.recvfrom(6)
-     data.first.should == "foobar"
-     data.last.should == ["AF_UNIX", ""]
+    @client.send("foobar", 0)
+    sock = @server.accept
+    data = sock.recvfrom(6)
+    data.first.should == "foobar"
+    data.last.should == ["AF_UNIX", ""]
+    sock.close
   end
 
   it "uses different message options" do
-     @client.send("foobar", Socket::MSG_PEEK)
-     sock = @server.accept
-     peek_data = sock.recvfrom(6, Socket::MSG_PEEK) # Does not retrieve the message
-     real_data = sock.recvfrom(6)
+    @client.send("foobar", Socket::MSG_PEEK)
+    sock = @server.accept
+    peek_data = sock.recvfrom(6, Socket::MSG_PEEK) # Does not retrieve the message
+    real_data = sock.recvfrom(6)
 
     real_data.should == peek_data
     peek_data.should == ["foobar", ["AF_UNIX", ""]]
+    sock.close
   end
 end
