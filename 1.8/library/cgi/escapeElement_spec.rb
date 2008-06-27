dd7ca9fcb9101185c444d5f4c8764fcc919c93c3
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-27 14:53:50 +0200

    Updated Specs fpr CGI.escapeElement and added specs for CGI.unescapeElement.

--- 1.8/library/cgi/escapeElement_spec.rb
+++ 1.8/library/cgi/escapeElement_spec.rb
@@ -9,4 +9,12 @@ describe "CGI.escapeElement when passed String, elements, ..." do
     res = CGI.escapeElement('<BR><A HREF="url"></A>', ["A", "IMG"])
     res.should == "<BR>&lt;A HREF=&quot;url&quot;&gt;&lt;/A&gt;"
   end
+  
+  it "is case-insensitive" do
+    res = CGI.escapeElement('<BR><A HREF="url"></A>', "a", "img")
+    res.should == '<BR>&lt;A HREF=&quot;url&quot;&gt;&lt;/A&gt;'
+
+    res = CGI.escapeElement('<br><a href="url"></a>', "A", "IMG")
+    res.should == '<br>&lt;a href=&quot;url&quot;&gt;&lt;/a&gt;'
+  end
 end
