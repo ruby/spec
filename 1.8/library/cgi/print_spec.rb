39f7b0261343b20e78c7ab089de5be941a22abdd
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-27 14:17:37 +0200

    Added specs fpr CGI#print

--- 1.8/library/cgi/print_spec.rb
+++ 1.8/library/cgi/print_spec.rb
@@ -2,6 +2,20 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require 'cgi'
 
 describe "CGI#print" do
-  it "needs to be reviewed for spec completeness" do
+  before(:each) do
+    @cgi = CGI.new
+  end
+  
+  it "passes all arguments to $stdout.print" do
+    $stdout.should_receive(:print).with("test")
+    @cgi.print("test")
+    
+    $stdout.should_receive(:print).with("one", "two", "three", ["four", "five"])
+    @cgi.print("one", "two", "three", ["four", "five"])
+  end
+  
+  it "returns the result of calling $stdout.print" do
+    $stdout.should_receive(:print).with("test").and_return(:expected)
+    @cgi.print("test").should == :expected
   end
 end
