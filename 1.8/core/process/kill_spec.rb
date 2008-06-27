488e17630b2fc67170e316856138577e8b6dd32f
Ryan Davis <ryand-ruby@zenspider.com>
2008-06-27 15:36:02 -0700

    cleaned whitespace so my next diff is readable

--- 1.8/core/process/kill_spec.rb
+++ 1.8/core/process/kill_spec.rb
@@ -49,7 +49,7 @@ describe "Process.kill" do
     before :all do
       @saved_trap = Signal.trap("HUP") {}
     end
-  
+
     before :each do
       @foo = 0
       @read, @write = IO.pipe
@@ -59,29 +59,29 @@ describe "Process.kill" do
         @write.close
       }
     end
-  
+
     after :each do
       @read.gets # the signal handler has run
       @read.close
       @foo.should == 42
     end
-  
+
     after :all do
       Signal.trap("HUP", @saved_trap)
     end
-  
+
     it "sends the given signal to the current process if pid is zero" do
       Process.kill("HUP", 0).should == 1
     end
-  
+
     it "accepts integer signal numbers" do
       Process.kill(1, 0).should == 1
     end
-  
+
     it "accepts POSIX signal names without 'SIG' prefix" do
       Process.kill("HUP", 0).should == 1
     end
-  
+
     it "accepts POSIX signal names with 'SIG' prefix" do
       Process.kill("SIGHUP", 0).should == 1
     end
@@ -108,24 +108,24 @@ describe "Process.kill" do
       @read.gets # the child has changed process groups
       @read.close
     end
-  
+
     after :each do
       Process.waitpid(@pid).should == @pid
       $?.exitstatus.should == 99
     end
-  
+
     it "sends the given signal to the specified process" do
       Process.kill("HUP", @pid).should == 1
     end
-  
+
     it "kills process groups if signal is negative" do
       Process.kill(-1, Process.getpgid(@pid)).should == 1
     end
-  
+
     it "kills process groups if signal starts with a minus sign" do
       Process.kill("-HUP", Process.getpgid(@pid)).should == 1
     end
-  
+
     it "kills process groups if signal starts with a minus sign and 'SIG'" do
       Process.kill("-SIGHUP", Process.getpgid(@pid)).should == 1
     end
