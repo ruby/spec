80951ab1e30a1958f1725e6bd7109e523476990a
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-07-01 14:18:53 +0200

    Added specs for the CGI::TagMaker module.

--- 1.8/library/cgi/tagmaker/nOE_element_def_spec.rb
+++ 1.8/library/cgi/tagmaker/nOE_element_def_spec.rb
@@ -2,6 +2,36 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'cgi'
 
 describe "CGI::TagMaker#nOE_element_def" do
-  it "needs to be reviewed for spec completeness" do
+  before(:each) do
+    @obj = Object.new
+    @obj.extend(CGI::TagMaker)
+  end
+
+  it "returns code for an empty element represented by the passed String" do
+    @obj.nOE_element_def("BR").should == <<-EOS
+          "<BR" + attributes.collect{|name, value|
+            next unless value
+            " " + CGI::escapeHTML(name) +
+            if true == value
+              ""
+            else
+              \'="\' + CGI::escapeHTML(value) + \'"\'
+            end
+          }.to_s + ">"
+EOS
+  end
+
+  it "automatically converts the tag to capital letters" do
+    @obj.nOE_element_def("br").should == <<-EOS
+          "<BR" + attributes.collect{|name, value|
+            next unless value
+            " " + CGI::escapeHTML(name) +
+            if true == value
+              ""
+            else
+              \'="\' + CGI::escapeHTML(value) + \'"\'
+            end
+          }.to_s + ">"
+EOS
   end
 end
--- 1.8/library/cgi/tagmaker/nO_element_def_spec.rb
+++ 1.8/library/cgi/tagmaker/nO_element_def_spec.rb
@@ -2,6 +2,47 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'cgi'
 
 describe "CGI::TagMaker#nO_element_def" do
-  it "needs to be reviewed for spec completeness" do
+  before(:each) do
+    @obj = Object.new
+    @obj.extend(CGI::TagMaker)
   end
+
+  it "returns code for an element represented by the passed String with optional start/end tags" do
+    @obj.nO_element_def("P").should == <<-EOS
+          "<P" + attributes.collect{|name, value|
+            next unless value
+            " " + CGI::escapeHTML(name) +
+            if true == value
+              ""
+            else
+              \'="\' + CGI::escapeHTML(value) + \'"\'
+            end
+          }.to_s + ">" +
+          if block_given?
+            yield.to_s + "</P>"
+          else
+            ""
+          end
+EOS
+  end
+
+  it "automatically converts the tag to capital letters" do
+    @obj.nO_element_def("p").should == <<-EOS
+          "<P" + attributes.collect{|name, value|
+            next unless value
+            " " + CGI::escapeHTML(name) +
+            if true == value
+              ""
+            else
+              \'="\' + CGI::escapeHTML(value) + \'"\'
+            end
+          }.to_s + ">" +
+          if block_given?
+            yield.to_s + "</P>"
+          else
+            ""
+          end
+EOS
+  end
+
 end
