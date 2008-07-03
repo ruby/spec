1132b7011e06b2e7e2ff0e8b1609fd71c6e241f1
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-07-03 13:41:20 +0200

    Updated Array#fill specs to reflect the correct behaviour.

--- 1.8/core/array/fill_spec.rb
+++ 1.8/core/array/fill_spec.rb
@@ -56,14 +56,12 @@ describe "Array#fill" do
     lambda { [1, 2, 3].fill('a', 3, -3)}.should_not raise_error(ArgumentError)
   end
 
-  # TODO: This is the behavior of MRI 1.8.6 pl 114,
-  # that was later changed (no later than pl 257), and
-  # MRI 1.9 changes it again. See:
-  # http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-core/17480
-  it "raises an ArgumentError if given an index and a negative count whose absolute value exceeds the index" do
-    lambda { [1, 2, 3].fill('a', 3, -4)}.should raise_error(ArgumentError)
-    lambda { [1, 2, 3].fill('a', 1, -3)}.should raise_error(ArgumentError)
-    lambda { [1, 2, 3].fill('a', 1, -300000)}.should raise_error(ArgumentError)
+  # See: http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-core/17481
+  it "returns self when given an index and a negative count whose absolute value exceeds the index" do
+    [1, 2, 3].fill('a', 3, -4).should == [1, 2, 3]
+    [1, 2, 3].fill('a', 1, -3).should == [1, 2, 3]
+    [1, 2, 3].fill('a', 1, -1).should == [1, 2, 3]
+    [1, 2, 3].fill('a', 1, -300000).should == [1, 2, 3]
   end
 
   it "replaces elements in range with object" do
