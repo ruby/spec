da42139c6257f97a1b7cac3800a3a8dc0327f219
Federico Builes <federico.builes@gmail.com>
2008-07-03 22:07:41 -0500

    Singleton#_load is still private in 1.8.6, guarding until backported.

--- 1.8/library/singleton/load_spec.rb
+++ 1.8/library/singleton/load_spec.rb
@@ -5,17 +5,19 @@ require File.dirname(__FILE__) + '/fixtures/classes'
 # TODO: write spec for cloning classes and calling private methods
 # TODO: write spec for private_methods not showing up via extended
 describe "Singleton._load" do
-  it "returns the singleton instance for anything passed in" do
-    klass = SingletonSpecs::MyClass
-    klass._load("").should equal(klass.instance)
-    klass._load("42").should equal(klass.instance)
-    klass._load(42).should equal(klass.instance)
-  end
+  ruby_version_is "1.8.7".."1.9" do
+    it "returns the singleton instance for anything passed in" do
+      klass = SingletonSpecs::MyClass
+      klass._load("").should equal(klass.instance)
+      klass._load("42").should equal(klass.instance)
+      klass._load(42).should equal(klass.instance)
+    end
 
-  it "returns the singleton instance for anything passed in to subclass" do
-    subklass = SingletonSpecs::MyClassChild
-    subklass._load("").should equal(subklass.instance)
-    subklass._load("42").should equal(subklass.instance)
-    subklass._load(42).should equal(subklass.instance)
+    it "returns the singleton instance for anything passed in to subclass" do
+      subklass = SingletonSpecs::MyClassChild
+      subklass._load("").should equal(subklass.instance)
+      subklass._load("42").should equal(subklass.instance)
+      subklass._load(42).should equal(subklass.instance)
+    end
   end
 end
