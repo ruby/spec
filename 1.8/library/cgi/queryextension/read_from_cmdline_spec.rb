1e84096020c3a6edec0909eb5b56f89d0c0a83a4
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-07-06 12:58:46 +0200

    Added specs for CGI::QueryExtension#read_from_cmdline.

--- 1.8/library/cgi/queryextension/read_from_cmdline_spec.rb
+++ 1.8/library/cgi/queryextension/read_from_cmdline_spec.rb
@@ -1,7 +1,48 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'cgi'
+require 'stringio'
 
-describe "CGI::QueryExtension#read_from_cmdline" do
-  it "needs to be reviewed for spec completeness" do
+describe "CGI::QueryExtension#read_from_cmdline when the ARGV Array contains values" do
+  before(:each) do
+    ENV['REQUEST_METHOD'], @old_request_method = "GET", ENV['REQUEST_METHOD']
+    @old_argv, ARGV = ARGV, ["one=value", "two=other_value", "three"]
+    @cgi = CGI.new
+  end
+  
+  after(:each) do
+    ENV['REQUEST_METHOD'] = @old_request_method
+    ARGV = @old_argv
+  end
+
+  it "returns the ARGV array converted to a HTTP Query String" do
+    @cgi.send(:read_from_cmdline).should == "one=value&two=other_value&three"
+  end
+  
+  it "returns the ARGV array concatenated with '+' when no values are given" do
+    ARGV = ["one", "two", "three"]
+    @cgi.send(:read_from_cmdline).should == "one+two+three"
+  end
+end
+
+describe "CGI::QueryExtension#read_from_cmdline when the ARGV Array contains no values" do
+  before(:each) do
+    ENV['REQUEST_METHOD'], @old_request_method = "GET", ENV['REQUEST_METHOD']
+    @old_stdin = $stdin
+    @cgi = CGI.new
+  end
+  
+  after(:each) do
+    ENV['REQUEST_METHOD'] = @old_request_method
+    $stdin = @old_stdin
+  end
+
+  it "returns the lines read from $stdin converted to a HTTP Query String" do
+    $stdin = StringIO.new("one=value\ntwo=other_value\nthree")
+    @cgi.send(:read_from_cmdline).should == "one=value&two=other_value&three"
+  end
+  
+  it "returns the lines read from $stdin concatenated with '+' when no values are given" do
+    $stdin = StringIO.new("one\ntwo\nthree")
+    @cgi.send(:read_from_cmdline).should == "one+two+three"
   end
 end
