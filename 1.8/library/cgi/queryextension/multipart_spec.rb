72e2f20f0e2455d072ce7da6f8a880d9fbdaea52
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-07-07 13:20:00 +0200

    Added specs for CGI::QueryExtension#multipart_spec.

--- 1.8/library/cgi/queryextension/multipart_spec.rb
+++ 1.8/library/cgi/queryextension/multipart_spec.rb
@@ -1,7 +1,34 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'cgi'
+require "stringio"
 
 describe "CGI::QueryExtension#multipart?" do
-  it "needs to be reviewed for spec completeness" do
+  before(:each) do
+    @old_env = ENV.dup
+    @old_stdin = $stdin
+    
+    ENV['REQUEST_METHOD'] = "POST"
+    ENV["CONTENT_TYPE"] = "multipart/form-data; boundary=---------------------------1137522503144128232716531729"
+    ENV["CONTENT_LENGTH"] = "222"
+    
+    $stdin = StringIO.new <<-EOS
+-----------------------------1137522503144128232716531729\r
+Content-Disposition: form-data; name="file"; filename=""\r
+Content-Type: application/octet-stream\r
+\r
+\r
+-----------------------------1137522503144128232716531729--\r
+EOS
+
+    @cgi = CGI.new
+  end
+  
+  after(:each) do
+    ENV = @old_env
+    $stdin = @old_stdin
+  end
+  
+  it "returns true if the current Request is a multipart request" do
+    @cgi.multipart?.should be_true
   end
 end
