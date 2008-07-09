f4722a23387f869b0e8a334582f67c2fe7dd329b
Gordon Thiesfeld <gthiesfeld@gmail.com>
2008-07-09 17:58:17 -0500

    Added windows guards to UNIXServer specs

--- 1.8/library/socket/unixsocket/peeraddr_spec.rb
+++ 1.8/library/socket/unixsocket/peeraddr_spec.rb
@@ -3,23 +3,26 @@ require File.dirname(__FILE__) + '/../fixtures/classes'
 
 describe "UNIXSocket#peeraddr" do
 
-  before :all do
-    @path = SocketSpecs.socket_path
-    File.unlink(@path) if File.exists?(@path)
-    @server = UNIXServer.open(@path)
-    @client = UNIXSocket.open(@path)
+  not_supported_on :windows do
+    before :all do
+      @path = SocketSpecs.socket_path
+      File.unlink(@path) if File.exists?(@path)
+      @server = UNIXServer.open(@path)
+      @client = UNIXSocket.open(@path)
+    end
+  
+    after :all do
+      @client.close
+      @server.close
+    end
+  
+    it "returns the address familly and path of the server end of the connection" do
+      @client.peeraddr.should == ["AF_UNIX", @path]
+    end
+  
+    it "raises an error in server sockets" do
+      lambda { @server.peeraddr }.should raise_error
+    end
   end
 
-  after :all do
-    @client.close
-    @server.close
-  end
-
-  it "returns the address familly and path of the server end of the connection" do
-    @client.peeraddr.should == ["AF_UNIX", @path]
-  end
-
-  it "raises an error in server sockets" do
-    lambda { @server.peeraddr }.should raise_error
-  end
 end
