735a0637be7c891bb1e14c8d294793ce648b1625
Federico Builes <federico.builes@gmail.com>
2008-07-10 15:13:15 -0500

    Don't reverse lookup in the general case of Socket.getaddrinfo.

--- 1.8/library/socket/socket/getaddrinfo_spec.rb
+++ 1.8/library/socket/socket/getaddrinfo_spec.rb
@@ -8,44 +8,44 @@ describe "Socket#getaddrinfo" do
     BasicSocket.do_not_reverse_lookup = false
   end
 
-#   it "gets the address information" do
-#     expected = []
-#     # The check for AP_INET6's class is needed because ipaddr.rb adds
-#     # fake AP_INET6 even in case when IPv6 is not really supported.
-#     # Without such check, this test might fail when ipaddr was required
-#     # by some other specs.
-#     if (Socket.constants.include? 'AF_INET6') &&
-#         (Socket::AF_INET6.class != Object) then
-#       expected.concat [
-#         ['AF_INET6', 80, SocketSpecs.hostname, '::1', Socket::AF_INET6,
-#           Socket::SOCK_DGRAM, Socket::IPPROTO_UDP],
-#         ['AF_INET6', 80, SocketSpecs.hostname, '::1', Socket::AF_INET6,
-#           Socket::SOCK_STREAM, Socket::IPPROTO_TCP],
-#         ['AF_INET6', 80, SocketSpecs.hostname, '::1', Socket::AF_INET6,
-#           Socket::SOCK_DGRAM, Socket::IPPROTO_UDP],
-#         ['AF_INET6', 80, SocketSpecs.hostname, '::1', Socket::AF_INET6,
-#           Socket::SOCK_STREAM, Socket::IPPROTO_TCP],
-#         ['AF_INET6', 80, SocketSpecs.hostname, 'fe80::1%lo0', Socket::AF_INET6,
-#           Socket::SOCK_DGRAM, Socket::IPPROTO_UDP],
-#         ['AF_INET6', 80, SocketSpecs.hostname, 'fe80::1%lo0', Socket::AF_INET6,
-#           Socket::SOCK_STREAM, Socket::IPPROTO_TCP],
-#         ['AF_INET6', 80, SocketSpecs.hostname, 'fe80::1%lo0', Socket::AF_INET6,
-#           Socket::SOCK_DGRAM, Socket::IPPROTO_UDP],
-#         ['AF_INET6', 80, SocketSpecs.hostname, 'fe80::1%lo0', Socket::AF_INET6,
-#           Socket::SOCK_STREAM, Socket::IPPROTO_TCP],
-#       ]
-#     end
+  it "gets the address information" do
+    expected = []
+    # The check for AP_INET6's class is needed because ipaddr.rb adds
+    # fake AP_INET6 even in case when IPv6 is not really supported.
+    # Without such check, this test might fail when ipaddr was required
+    # by some other specs.
+    if (Socket.constants.include? 'AF_INET6') &&
+        (Socket::AF_INET6.class != Object) then
+      expected.concat [
+        ['AF_INET6', 80, SocketSpecs.hostname, '::1', Socket::AF_INET6,
+          Socket::SOCK_DGRAM, Socket::IPPROTO_UDP],
+        ['AF_INET6', 80, SocketSpecs.hostname, '::1', Socket::AF_INET6,
+          Socket::SOCK_STREAM, Socket::IPPROTO_TCP],
+        ['AF_INET6', 80, SocketSpecs.hostname, '::1', Socket::AF_INET6,
+          Socket::SOCK_DGRAM, Socket::IPPROTO_UDP],
+        ['AF_INET6', 80, SocketSpecs.hostname, '::1', Socket::AF_INET6,
+          Socket::SOCK_STREAM, Socket::IPPROTO_TCP],
+        ['AF_INET6', 80, SocketSpecs.hostname, 'fe80::1%lo0', Socket::AF_INET6,
+          Socket::SOCK_DGRAM, Socket::IPPROTO_UDP],
+        ['AF_INET6', 80, SocketSpecs.hostname, 'fe80::1%lo0', Socket::AF_INET6,
+          Socket::SOCK_STREAM, Socket::IPPROTO_TCP],
+        ['AF_INET6', 80, SocketSpecs.hostname, 'fe80::1%lo0', Socket::AF_INET6,
+          Socket::SOCK_DGRAM, Socket::IPPROTO_UDP],
+        ['AF_INET6', 80, SocketSpecs.hostname, 'fe80::1%lo0', Socket::AF_INET6,
+          Socket::SOCK_STREAM, Socket::IPPROTO_TCP],
+      ] 
+    end
 
-#     expected.concat [
-#       ['AF_INET', 80, SocketSpecs.hostname, '127.0.0.1', Socket::AF_INET,
-#         Socket::SOCK_DGRAM, Socket::IPPROTO_UDP],
-#       ['AF_INET', 80, SocketSpecs.hostname, '127.0.0.1', Socket::AF_INET,
-#         Socket::SOCK_STREAM, Socket::IPPROTO_TCP],
-#     ]
-
-#     addrinfo = Socket.getaddrinfo SocketSpecs.hostname, 'http'
-#     addrinfo.each { |a| expected.should include(a) }
-#   end
+    expected.concat [
+      ['AF_INET', 80, SocketSpecs.hostname, '127.0.0.1', Socket::AF_INET,
+        Socket::SOCK_DGRAM, Socket::IPPROTO_UDP],
+      ['AF_INET', 80, SocketSpecs.hostname, '127.0.0.1', Socket::AF_INET,
+        Socket::SOCK_STREAM, Socket::IPPROTO_TCP],
+    ]
+    BasicSocket.do_not_reverse_lookup = true
+    addrinfo = Socket.getaddrinfo SocketSpecs.hostname, 'http'
+    addrinfo.each { |a| expected.should include(a) }
+  end
 
    # #getaddrinfo will return a INADDR_ANY address (0.0.0.0
    # or "::") if it's a passive socket. In the case of non-passive 
