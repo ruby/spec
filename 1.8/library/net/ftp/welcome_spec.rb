9619e139002b438404591d87120bb774f539beae
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-07-16 22:35:13 +0200

    Added specs for Net::FTP#welcome.

--- 1.8/library/net/ftp/welcome_spec.rb
+++ 1.8/library/net/ftp/welcome_spec.rb
@@ -2,5 +2,19 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'net/ftp'
 
 describe "Net::FTP#welcome" do
-  it "needs to be reviewed for spec completeness"
+  before(:each) do
+    @socket = mock("Socket")
+    @socket.stub!(:write)
+    
+    @ftp = Net::FTP.new
+    @ftp.instance_variable_set(:@sock, @socket)
+  end
+  
+  it "returns the server's welcome message" do
+    @socket.should_receive(:readline).and_return("230 User logged in, proceed.")
+    
+    @ftp.welcome.should be_nil
+    @ftp.login
+    @ftp.welcome.should == "230 User logged in, proceed.\n"
+  end
 end
