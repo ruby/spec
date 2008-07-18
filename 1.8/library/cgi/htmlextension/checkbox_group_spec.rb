84713d95894413370725c6d6eef3276ffcb85623
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-07-18 10:57:55 +0200

    Use the equal_element matcher in the specs for CGI::HtmlExtension#checkbox_group.

--- 1.8/library/cgi/htmlextension/checkbox_group_spec.rb
+++ 1.8/library/cgi/htmlextension/checkbox_group_spec.rb
@@ -7,34 +7,38 @@ describe "CGI::HtmlExtension#checkbox_group when passed name, values ..." do
     @html = CGISpecs::HtmlExtension.new
   end
 
+  def split_elements(string)
+    string.split("<").reject { |x| x.empty? }.map { |x| "<#{x}" }
+  end
+
   it "returns a sequence of 'checkbox'-elements with the passed name and the passed values" do
-    expected = '<INPUT NAME="test" TYPE="checkbox" VALUE="foo">foo' +
-      '<INPUT NAME="test" TYPE="checkbox" VALUE="bar">bar' +
-      '<INPUT NAME="test" TYPE="checkbox" VALUE="baz">baz'
-    @html.checkbox_group("test", "foo", "bar", "baz").should == expected
+    output = split_elements(@html.checkbox_group("test", "foo", "bar", "baz"))
+    output[0].should equal_element("INPUT", {"NAME" => "test", "TYPE" => "checkbox", "VALUE" => "foo"}, "foo", :not_closed => true)
+    output[1].should equal_element("INPUT", {"NAME" => "test", "TYPE" => "checkbox", "VALUE" => "bar"}, "bar", :not_closed => true)
+    output[2].should equal_element("INPUT", {"NAME" => "test", "TYPE" => "checkbox", "VALUE" => "baz"}, "baz", :not_closed => true)
   end
   
   it "allows passing a value inside an Array" do
-    expected = '<INPUT NAME="test" TYPE="checkbox" VALUE="foo">foo' +
-      '<INPUT NAME="test" TYPE="checkbox" VALUE="bar">bar' +
-      '<INPUT NAME="test" TYPE="checkbox" VALUE="baz">baz'
-    @html.checkbox_group("test", ["foo"], "bar", ["baz"]).should == expected
+    output = split_elements(@html.checkbox_group("test", ["foo"], "bar", ["baz"]))
+    output[0].should equal_element("INPUT", {"NAME" => "test", "TYPE" => "checkbox", "VALUE" => "foo"}, "foo", :not_closed => true)
+    output[1].should equal_element("INPUT", {"NAME" => "test", "TYPE" => "checkbox", "VALUE" => "bar"}, "bar", :not_closed => true)
+    output[2].should equal_element("INPUT", {"NAME" => "test", "TYPE" => "checkbox", "VALUE" => "baz"}, "baz", :not_closed => true)
   end
   
   it "allows passing a value as an Array containing the value and the checked state or a label" do
-    expected = '<INPUT NAME="test" TYPE="checkbox" VALUE="foo">foo' +
-      '<INPUT CHECKED NAME="test" TYPE="checkbox" VALUE="bar">bar' +
-      '<INPUT NAME="test" TYPE="checkbox" VALUE="baz">label for baz'
-    @html.checkbox_group("test", ["foo"], ["bar", true], ["baz", "label for baz"]).should == expected
+    output = split_elements(@html.checkbox_group("test", ["foo"], ["bar", true], ["baz", "label for baz"]))
+    output[0].should equal_element("INPUT", {"NAME" => "test", "TYPE" => "checkbox", "VALUE" => "foo"}, "foo", :not_closed => true)
+    output[1].should equal_element("INPUT", {"CHECKED" => true, "NAME" => "test", "TYPE" => "checkbox", "VALUE" => "bar"}, "bar", :not_closed => true)
+    output[2].should equal_element("INPUT", {"NAME" => "test", "TYPE" => "checkbox", "VALUE" => "baz"}, "label for baz", :not_closed => true)
   end
   
   # TODO: CGI does not like passing false instead of true.
   ruby_bug "", "1.8.7" do
     it "allows passing a value as an Array containing the value, a label and the checked state" do
-      expected = '<INPUT CHECKED NAME="test" TYPE="checkbox" VALUE="foo">label for foo' +
-        '<INPUT NAME="test" TYPE="checkbox" VALUE="bar">label for bar' +
-        '<INPUT CHECKED NAME="test" TYPE="checkbox" VALUE="baz">label for baz'
-      @html.checkbox_group("test", ["foo", "label for foo", true], ["bar", "label for bar", false], ["baz", "label for baz", true]).should == expected
+      output = split_elements(@html.checkbox_group("test", ["foo", "label for foo", true], ["bar", "label for bar", false], ["baz", "label for baz", true]))
+      output[0].should equal_element("INPUT", {"CHECKED" => true, "NAME" => "test", "TYPE" => "checkbox", "VALUE" => "foo"}, "label for foo", :not_closed => true)
+      output[1].should equal_element("INPUT", {"NAME" => "test", "TYPE" => "checkbox", "VALUE" => "bar"}, "label for bar", :not_closed => true)
+      output[2].should equal_element("INPUT", {"CHECKED" => true, "NAME" => "test", "TYPE" => "checkbox", "VALUE" => "baz"}, "label for baz", :not_closed => true)
     end
   end
   
@@ -43,10 +47,10 @@ describe "CGI::HtmlExtension#checkbox_group when passed name, values ..." do
   end
   
   it "ignores a passed block" do
-    expected = '<INPUT NAME="test" TYPE="checkbox" VALUE="foo">foo' +
-      '<INPUT NAME="test" TYPE="checkbox" VALUE="bar">bar' +
-      '<INPUT NAME="test" TYPE="checkbox" VALUE="baz">baz'
-    @html.checkbox_group("test", "foo", "bar", "baz") { "test" }.should == expected
+    output = split_elements(@html.checkbox_group("test", "foo", "bar", "baz") { "test" })
+    output[0].should equal_element("INPUT", {"NAME" => "test", "TYPE" => "checkbox", "VALUE" => "foo"}, "foo", :not_closed => true)
+    output[1].should equal_element("INPUT", {"NAME" => "test", "TYPE" => "checkbox", "VALUE" => "bar"}, "bar", :not_closed => true)
+    output[2].should equal_element("INPUT", {"NAME" => "test", "TYPE" => "checkbox", "VALUE" => "baz"}, "baz", :not_closed => true)
   end
 end
 
@@ -55,27 +59,31 @@ describe "CGI::HtmlExtension#checkbox_group when passed Hash" do
     @html = CGISpecs::HtmlExtension.new
   end
 
+  def split_elements(string)
+    string.split("<").reject { |x| x.empty? }.map { |x| "<#{x}" }
+  end
+
   it "uses the passed Hash to generate the checkbox sequence" do
-    expected = '<INPUT NAME="name" TYPE="checkbox" VALUE="foo">foo' +
-      '<INPUT NAME="name" TYPE="checkbox" VALUE="bar">bar' +
-      '<INPUT NAME="name" TYPE="checkbox" VALUE="baz">baz'
-    @html.checkbox_group("NAME" => "name", "VALUES" => ["foo", "bar", "baz"]).should == expected
-
-    expected = '<INPUT NAME="name" TYPE="checkbox" VALUE="foo">foo' +
-      '<INPUT CHECKED NAME="name" TYPE="checkbox" VALUE="bar">bar' +
-      '<INPUT NAME="name" TYPE="checkbox" VALUE="baz">baz'
-    @html.checkbox_group("NAME" => "name", "VALUES" => [["foo"], ["bar", true], "baz"]).should == expected
-
-    expected = '<INPUT NAME="name" TYPE="checkbox" VALUE="1">Foo' +
-      '<INPUT CHECKED NAME="name" TYPE="checkbox" VALUE="2">Bar' +
-      '<INPUT NAME="name" TYPE="checkbox" VALUE="Baz">Baz'
-    @html.checkbox_group("NAME" => "name", "VALUES" => [["1", "Foo"], ["2", "Bar", true], "Baz"]).should == expected
+    output = split_elements(@html.checkbox_group("NAME" => "name", "VALUES" => ["foo", "bar", "baz"]))
+    output[0].should equal_element("INPUT", {"NAME" => "name", "TYPE" => "checkbox", "VALUE" => "foo"}, "foo", :not_closed => true)
+    output[1].should equal_element("INPUT", {"NAME" => "name", "TYPE" => "checkbox", "VALUE" => "bar"}, "bar", :not_closed => true)
+    output[2].should equal_element("INPUT", {"NAME" => "name", "TYPE" => "checkbox", "VALUE" => "baz"}, "baz", :not_closed => true)
+    
+    output = split_elements(@html.checkbox_group("NAME" => "name", "VALUES" => [["foo"], ["bar", true], "baz"]))
+    output[0].should equal_element("INPUT", {"NAME" => "name", "TYPE" => "checkbox", "VALUE" => "foo"}, "foo", :not_closed => true)
+    output[1].should equal_element("INPUT", {"CHECKED" => true, "NAME" => "name", "TYPE" => "checkbox", "VALUE" => "bar"}, "bar", :not_closed => true)
+    output[2].should equal_element("INPUT", {"NAME" => "name", "TYPE" => "checkbox", "VALUE" => "baz"}, "baz", :not_closed => true)
+    
+    output = split_elements(@html.checkbox_group("NAME" => "name", "VALUES" => [["1", "Foo"], ["2", "Bar", true], "Baz"]))
+    output[0].should equal_element("INPUT", {"NAME" => "name", "TYPE" => "checkbox", "VALUE" => "1"}, "Foo", :not_closed => true)
+    output[1].should equal_element("INPUT", {"CHECKED" => true, "NAME" => "name", "TYPE" => "checkbox", "VALUE" => "2"}, "Bar", :not_closed => true)
+    output[2].should equal_element("INPUT", {"NAME" => "name", "TYPE" => "checkbox", "VALUE" => "Baz"}, "Baz", :not_closed => true)
   end
 
   it "ignores a passed block" do
-    expected = '<INPUT NAME="name" TYPE="checkbox" VALUE="foo">foo' +
-      '<INPUT NAME="name" TYPE="checkbox" VALUE="bar">bar' +
-      '<INPUT NAME="name" TYPE="checkbox" VALUE="baz">baz'
-    @html.checkbox_group("NAME" => "name", "VALUES" => ["foo", "bar", "baz"]) { "test" }.should == expected
+    output = split_elements(@html.checkbox_group("NAME" => "name", "VALUES" => ["foo", "bar", "baz"]) { "test" })
+    output[0].should equal_element("INPUT", {"NAME" => "name", "TYPE" => "checkbox", "VALUE" => "foo"}, "foo", :not_closed => true)
+    output[1].should equal_element("INPUT", {"NAME" => "name", "TYPE" => "checkbox", "VALUE" => "bar"}, "bar", :not_closed => true)
+    output[2].should equal_element("INPUT", {"NAME" => "name", "TYPE" => "checkbox", "VALUE" => "baz"}, "baz", :not_closed => true)
   end
 end
