f8158cf1cbce668b171bcd187d397203fdb721a0
Federico Builes <federico.builes@gmail.com>
2008-07-21 08:59:09 -0500

    Fixing Socket#close_read to remove wrong guards.
    * non_compliant_on :ruby should not exist in these specs.

--- 1.8/library/socket/basicsocket/close_read_spec.rb
+++ 1.8/library/socket/basicsocket/close_read_spec.rb
@@ -10,31 +10,15 @@ describe "Socket::BasicSocket#close_read" do
     @server.close unless @server.closed?
   end
 
-  compliant_on :ruby do
-   
-    it "closes the reading end of the socket" do
-      @server.close_read
-      lambda { @server.read }.should raise_error(IOError)
-    end
-
-    it "it works on sockets with closed ends" do
-      @server.close_read
-      lambda { @server.close_read }.should_not raise_error(Exception)
-      lambda { @server.read }.should raise_error(IOError)
-    end
+  it "closes the reading end of the socket" do
+    @server.close_read
+    lambda { @server.read }.should raise_error(IOError)
   end
 
-  not_compliant_on :ruby do
-
-    it "closes the reading end of the socket" do
-      @server.close_read
-      lambda { @server.read }.should raise_error(IOError)
-    end
-
-    it "does not work on sockets with closed ends" do
-      @server.close_read
-      lambda { @server.close_read }.should raise_error(Exception)
-    end
+  it "it works on sockets with closed ends" do
+    @server.close_read
+    lambda { @server.close_read }.should_not raise_error(Exception)
+    lambda { @server.read }.should raise_error(IOError)
   end
 end
 
