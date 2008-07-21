4eead39a61b61875c7c0a3c16aec763e6a60c88e
Federico Builes <federico.builes@gmail.com>
2008-07-21 09:17:12 -0500

    Refactoring BasicSocket#getsockname specs.

--- 1.8/library/socket/basicsocket/getsockname_spec.rb
+++ 1.8/library/socket/basicsocket/getsockname_spec.rb
@@ -3,28 +3,25 @@ require File.dirname(__FILE__) + '/../fixtures/classes'
 
 describe "Socket::BasicSocket#getsockname" do
   after :each do
-    @socket.close unless @socket.closed?
+    @socket.closed?.should be_false
+    @socket.close
   end
 
   it "returns the sockaddr associacted with the socket" do
-    @socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
-    @socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
-    @socket.bind(Socket.pack_sockaddr_in(SocketSpecs.port, "127.0.0.1"))
+    @socket = TCPServer.new("127.0.0.1", SocketSpecs.port)
     sockaddr = Socket.unpack_sockaddr_in(@socket.getsockname)
     sockaddr.should == [SocketSpecs.port, "127.0.0.1"]
-  end
+ end
 
-  it "works on server sockets" do
+  it "works on sockets listening in 0.0.0.0" do
     @socket = TCPServer.new(SocketSpecs.port)
     sockaddr = Socket.unpack_sockaddr_in(@socket.getsockname)
-    [SocketSpecs.port, "0.0.0.0"]
-    ["0.0.0.0", "::"].include?(sockaddr[1]).should be_true
-    sockaddr[0].should == SocketSpecs.port
+    sockaddr.should == [SocketSpecs.port, "0.0.0.0"]
   end
 
   it "returns empty sockaddr for unbinded sockets" do
     @socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
     sockaddr = Socket.unpack_sockaddr_in(@socket.getsockname)
-    sockaddr.should == [0, "0.0.0.0"]    
+    sockaddr.should == [0, "0.0.0.0"]
   end
 end
