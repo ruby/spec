04ada2f3472b84f3344b89e8a63180237c51e3f0
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-07-23 14:42:30 +0200

    Net::FTP#retrlines specs use the Dummy FTP now.

--- 1.8/library/net/ftp/retrlines_spec.rb
+++ 1.8/library/net/ftp/retrlines_spec.rb
@@ -1,41 +1,36 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'net/ftp'
+require File.dirname(__FILE__) + "/fixtures/server"
 
 describe "Net::FTP#retrlines" do
   before(:each) do
-    @socket = mock("Socket")
-    @socket.stub!(:addr).and_return(["AF_INET", nil, nil, "127.0.0.1"])
-    @socket.stub!(:peeraddr).and_return(["AF_INET", nil, nil, "127.0.0.1"])
-    @socket.stub!(:write)
-    @socket.stub!(:readline).and_return("200 Command okay.", "200 Command okay.", "123 test", "200 Command okay.")
-    
-    @connection = mock("Connection")
-    @connection.stub!(:gets).and_return("Line 1", "Line 2", "Line 3", nil)
-    @connection.stub!(:close)
+    @server = NetFTPSpecs::DummyFTP.new
+    @server.serve_once
 
-    @server_socket = mock("Server Socket")
-    @server_socket.stub!(:accept).and_return(@connection)
-    @server_socket.stub!(:close)
-    @server_socket.stub!(:addr).and_return(["AF_INET", 1234, nil, "127.0.0.1"])
-    TCPServer.stub!(:open).and_return(@server_socket)
-    
     @ftp = Net::FTP.new
-    @ftp.instance_variable_set(:@sock, @socket)
+    @ftp.connect("localhost", 9921)
   end
 
+  after(:each) do
+    @ftp.quit rescue nil
+    @server.stop
+  end
+  
   it "puts the connection into ASCII mode" do
-    @socket.should_receive(:write).with("TYPE A\r\n")
-    @ftp.retrlines("COMMAND") {}
+    @ftp.retrlines("LIST test.dir") {}
   end
 
   it "sends the passed command over the socket" do
-    @socket.should_receive(:write).with("COMMAND\r\n")
-    @ftp.retrlines("COMMAND") {}
+    @ftp.retrlines("LIST test.dir") {}
   end
 
-  it "yields each line to the passed block" do
+  it "yields each received line to the passed block" do
     res = []
-    @ftp.retrlines("COMMAND") { |x| res << x }
-    res.should == ["Line 1", "Line 2", "Line 3"]
+    @ftp.retrlines("LIST test.dir") { |x| res << x }
+    res.should == [
+      "-rw-r--r--  1 spec  staff  507 17 Jul 18:41 last_response_code.rb",
+      "-rw-r--r--  1 spec  staff   50 17 Jul 18:41 list.rb",
+      "-rw-r--r--  1 spec  staff   48 17 Jul 18:41 pwd.rb"
+    ]
   end
 end
