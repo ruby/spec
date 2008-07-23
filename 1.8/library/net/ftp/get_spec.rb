5b05ee58f5361d9c07082ad2a65e26567d387486
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-07-23 15:51:37 +0200

    Added specs for Net::FTP#getbinaryfile and Net::FTP#get when in binary mode.

--- 1.8/library/net/ftp/get_spec.rb
+++ 1.8/library/net/ftp/get_spec.rb
@@ -2,12 +2,17 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'net/ftp'
 require File.dirname(__FILE__) + "/fixtures/server"
 require File.dirname(__FILE__) + "/shared/gettextfile"
+require File.dirname(__FILE__) + "/shared/getbinaryfile"
 
-describe "Net::FTP#get when in binary mode" do
-  it "behaves like #getbinaryfile"
+describe "Net::FTP#get (binary mode)" do
+  before(:each) do
+    @binary_mode = true
+  end
+  
+  it_behaves_like :net_ftp_getbinaryfile, :get
 end
 
-describe "Net::FTP#get when in text mode" do
+describe "Net::FTP#get (text mode)" do
   before(:each) do
     @binary_mode = false
   end
--- 1.8/library/net/ftp/getbinaryfile_spec.rb
+++ 1.8/library/net/ftp/getbinaryfile_spec.rb
@@ -1,12 +1,8 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'net/ftp'
+require File.dirname(__FILE__) + "/fixtures/server"
+require File.dirname(__FILE__) + "/shared/getbinaryfile"
 
 describe "Net::FTP#getbinaryfile" do
-  it "needs to be reviewed for spec completeness"
-
-  describe "when passed remotefile" do
-    it "retrieves the content of the remotefile and saves it a file named after remotefile in the current working directory" do
-      
-    end
-  end
+  it_behaves_like :net_ftp_getbinaryfile, :getbinaryfile
 end
