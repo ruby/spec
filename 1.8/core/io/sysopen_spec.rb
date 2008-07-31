72521a2a391759978a6533dae8438abc11a9be8f
Federico Builes <federico.builes@gmail.com>
2008-07-31 13:27:00 -0500

    Specs for IO.sysopen.

--- 1.8/core/io/sysopen_spec.rb
+++ 1.8/core/io/sysopen_spec.rb
@@ -1,5 +1,30 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "IO.sysopen" do
-  it "needs to be reviewed for spec completeness"
+
+  before :all do
+    @filename = tmp("rubinius-spec-io-sysopen-#{$$}.txt")
+  end
+
+  after :all do
+    File.unlink @filename
+  end
+
+
+  it "returns the file descriptor for a given path" do
+    fd = IO.sysopen(@filename, "w")
+    fd.should be_kind_of(Fixnum)
+    fd.should_not equal(0)
+  end
+
+  it "accepts a mode" do
+    fd = 0
+    lambda { fd = IO.sysopen(@filename, "w") }.should_not raise_error
+    fd.should_not equal(0)
+  end
+
+  it "accepts permissions for the path if it does not exist" do
+    fd = IO.sysopen(tmp("i-dont-exist-yet"), "r", 777)
+    fd.should_not equal(0)
+  end
 end
