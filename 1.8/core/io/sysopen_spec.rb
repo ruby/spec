03cf1530282a08b28984aadbd77fa95be0ad1242
Federico Builes <federico.builes@gmail.com>
2008-07-31 13:31:48 -0500

    More specs for IO.sysopen

--- 1.8/core/io/sysopen_spec.rb
+++ 1.8/core/io/sysopen_spec.rb
@@ -17,14 +17,20 @@ describe "IO.sysopen" do
     fd.should_not equal(0)
   end
 
-  it "accepts a mode" do
+  it "works on directories" do
+    fd = IO.sysopen(tmp(""))    # /tmp
+    fd.should be_kind_of(Fixnum)
+    fd.should_not equal(0)
+  end
+
+  it "accepts a mode as second argument" do
     fd = 0
     lambda { fd = IO.sysopen(@filename, "w") }.should_not raise_error
     fd.should_not equal(0)
   end
 
-  it "accepts permissions for the path if it does not exist" do
-    fd = IO.sysopen(tmp("i-dont-exist-yet"), "r", 777)
+  it "accepts permissions as third argument" do
+    fd = IO.sysopen(@filename, "w", 777)
     fd.should_not equal(0)
   end
 end
