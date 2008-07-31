7c45d99f93fdf78ab6144addaa27039883a013ef
Federico Builes <federico.builes@gmail.com>
2008-07-31 13:53:24 -0500

    Specs for IO#<<

--- 1.8/core/io/output_spec.rb
+++ 1.8/core/io/output_spec.rb
@@ -1,5 +1,24 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "IO#<<" do
-  it "needs to be reviewed for spec completeness"
+  it "writes an object to the IO stream" do
+    lambda { 
+      $stderr << "Oh noes, an error!"
+    }.should output_to_fd("Oh noes, an error!", STDERR)
+  end
+
+  it "calls #to_s on the object to print it" do
+    lambda { 
+      $stderr << 1337
+    }.should output_to_fd("1337", STDERR)
+  end
+
+  it "raises an error if the stream is closed" do
+    path = tmp("io-output-spec")
+    fd = IO.sysopen(path, "w")
+    io = IO.open(fd)
+    io.close
+    lambda { io << "test" }.should raise_error(IOError)
+    File.unlink(path) if File.exists?(path)
+  end
 end
