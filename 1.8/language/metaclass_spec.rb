b8331fd83a0a8d34851f61c0f859752e82925bc7
Wilson Bilkovich <wilson@supremetyrant.com>
2008-08-04 18:15:21 -0400

    Specs for constant behavior in metaclasses

--- 1.8/language/metaclass_spec.rb
+++ 1.8/language/metaclass_spec.rb
@@ -39,5 +39,88 @@ describe "self in a metaclass body (class << obj)" do
       cls.superclass.should == String
     end
   end
-  
 end
+
+describe "A constant on a metaclass" do
+  before(:each) do
+    @object = Object.new
+    class << @object
+      CONST = self
+    end
+  end
+
+  it "can be accessed after the metaclass body is reopened" do
+    class << @object
+      CONST.should == self
+    end
+  end
+
+  it "can be accessed via self::CONST" do
+    class << @object
+      self::CONST.should == self
+    end
+  end
+
+  it "can be accessed via const_get" do
+    class << @object
+      const_get(:CONST).should == self
+    end
+  end
+
+  it "is not defined on the object's class" do
+    @object.class.const_defined?(:CONST).should be_false
+  end
+
+  it "is not defined in the metaclass opener's scope" do
+    class << @object
+      CONST
+    end
+    lambda { CONST }.should raise_error(NameError)
+  end
+
+  it "cannot be accessed via object::CONST" do
+    lambda do
+      @object::CONST
+    end.should raise_error(TypeError)
+  end
+
+  it "raises a NameError for anonymous_module::CONST" do
+    @object = Class.new
+    class << @object
+      CONST = 100
+    end
+    
+    lambda do
+      @object::CONST
+    end.should raise_error(NameError)
+  end
+
+  it "appears in the metaclass constant list" do
+    constants = class << @object; constants; end 
+    constants.should include("CONST")
+  end
+
+  it "does not appear in the object's class constant list" do
+    @object.class.constants.should_not include("CONST")
+  end
+
+  it "is not preserved when the object is duped" do
+    @object = @object.dup
+
+    lambda do
+      class << @object; CONST; end
+    end.should raise_error(NameError)
+  end
+
+  it "is preserved when the object is cloned" do
+    @object = @object.clone
+
+    class << @object
+      CONST.should_not be_nil
+    end
+  end
+end
+
+
+
+
