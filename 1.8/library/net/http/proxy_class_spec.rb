47fb91829b1c072a1fb14d4f13a0ebac62695cad
Arthur Schreiber <arthur@mac.local>
2008-08-06 01:50:29 +0200

    Added a spec for Net::HTTP.proxy_class?.

--- 1.8/library/net/http/proxy_class_spec.rb
+++ 1.8/library/net/http/proxy_class_spec.rb
@@ -2,5 +2,8 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'net/http'
 
 describe "Net::HTTP.proxy_class?" do
-  it "needs to be reviewed for spec completeness"
+  it "returns true if sels is a class created with Net::HTTP.Proxy" do
+    Net::HTTP.proxy_class?.should be_false
+    Net::HTTP.Proxy("localhost").proxy_class?.should be_true
+  end
 end
