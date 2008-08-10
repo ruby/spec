6d182772aa02cd66a95bba7e02350cc4ff625d4c
Jim Kingdon <kingdon@panix.com>
2008-08-10 18:02:49 -0400

    Make describe/it messages read more naturally.

--- 1.8/language/execution_spec.rb
+++ 1.8/language/execution_spec.rb
@@ -1,18 +1,22 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe "Execution literal" do
-  it "`` should return the result of the executed sub-process" do
+describe "``" do
+  it "should return the result of the executed sub-process" do
     ip = 'world'
     `echo disc #{ip}`.should == "disc world\n"
   end
+end
 
+describe "%x()" do
   # NOTE: Interpolation ? It's not consistant with %w for example.
-  it "%x() is the same (with also interpolation)" do
+  it "is the same (and also can interpolate with #)" do
     ip = 'world'
     %x(echo disc #{ip}).should == "disc world\n"
   end
+end
 
-  it "%X doesn't exist" do
+describe "%X" do
+  it "doesn't exist" do
     lambda { eval '%X(echo disc)' }.should raise_error(SyntaxError)
   end
 end
