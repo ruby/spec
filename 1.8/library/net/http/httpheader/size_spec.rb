858a7d9bcf8e0be9380a4e798ec8b4aeab4f4355
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-08-12 12:51:16 +0200

    Added specs for Net::HTTPHeader#size.

---
+++ 1.8/library/net/http/httpheader/size_spec.rb
@@ -0,0 +1,22 @@
+require File.dirname(__FILE__) + '/../../../../spec_helper'
+require 'net/http'
+require File.dirname(__FILE__) + "/fixtures/classes"
+
+describe "Net::HTTPHeader#size" do
+  before(:each) do
+    @headers = NetHTTPHeaderSpecs::Example.new
+  end
+  
+  it "returns the number of header entries in self" do
+    @headers.size.should eql(0)
+    
+    @headers["a"] = "b"
+    @headers.size.should eql(1)
+    
+    @headers["b"] = "b"
+    @headers.size.should eql(2)
+    
+    @headers["c"] = "c"
+    @headers.size.should eql(3)
+  end
+end
