1de79cef53c7a5bfec94d431331b37a02b15891a
Wilson Bilkovich <wilson@supremetyrant.com>
2008-10-09 15:18:07 -0400

    Spec out Time#_dump and Time#_load for GMT support

--- 1.8/core/time/_dump_spec.rb
+++ 1.8/core/time/_dump_spec.rb
@@ -8,21 +8,29 @@ describe "Time#_dump" do
     @t = @t.gmtime
   end
 
-  it "dumps a Time object to a bytestring" do
-    @s.should be_kind_of(String)
-    @s.should == [2149122123, 2235564032].pack("LL")
+  ruby_bug("http://redmine.ruby-lang.org/issues/show/627", "1.8.7") do
+    it "preserves the GMT flag" do
+      @t.gmt?.should == true
+      dump = @t._dump.unpack("LL").first
+      ((dump >> 30) & 0x1).should == 1
+    end
+
+    it "dumps a Time object to a bytestring" do
+      @s.should be_kind_of(String)
+      @s.should == [3222863947, 2235564032].pack("LL")
+    end
+
+    it "dumps an array with a date as first element" do
+      high =                1 << 31 |
+            (@t.gmt? ? 1 : 0) << 30 |
+             (@t.year - 1900) << 14 |
+                (@t.mon  - 1) << 10 |
+                       @t.mday << 5 |
+                            @t.hour
+
+      high.should == @s.unpack("LL").first
+    end
   end
-
-  it "dumps an array with a date as first element" do
-    high =                1 << 31 |
-           (@t.year - 1900) << 14 |
-              (@t.mon  - 1) << 10 |
-                     @t.mday << 5 |
-                     @t.hour
-
-    high.should == @s.unpack("LL").first
-  end
-
 
   it "dumps an array with a time as second element" do
     low =  @t.min  << 26 |
