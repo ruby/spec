9be49eb0bfafe302bfc06e3fa11f144b76cd57a5
Michael Bevilacqua-Linn <NovusTiro@gmail.com>
2008-10-19 22:17:04 -0400

    Specs for CSV::generate.
    Signed-off-by: Federico Builes <federico.builes@gmail.com>

--- 1.8/library/csv/generate_spec.rb
+++ 1.8/library/csv/generate_spec.rb
@@ -1,6 +1,39 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require 'csv'
+require 'tempfile'
 
 describe "CSV.generate" do
-  it "needs to be reviewed for spec completeness"
+  
+  before :each do
+    @outfile_name = File.join(Dir.tmpdir, "generate_test_#{$$}.csv")  
+  end
+ 
+  it "should create a BasicWriter" do
+    writer = CSV::generate(@outfile_name)
+    writer.should be_kind_of(CSV::BasicWriter)
+    writer.close
+  end
+
+  it "should create a BasicWriter with ; as the seperator" do
+    writer = CSV::generate(@outfile_name)
+    writer.should be_kind_of(CSV::BasicWriter)
+    writer.close
+  end
+  
+  it "should create a BasicWriter inside the block" do
+    CSV::generate(@outfile_name) do |writer|
+      writer.should be_kind_of(CSV::BasicWriter)
+    end
+  end
+
+  it "should create a BasicWriter with ; as the seperator inside the block" do
+    CSV::generate(@outfile_name, ?;) do |writer|
+      writer.should be_kind_of(CSV::BasicWriter)
+    end
+  end
+   
+  after :each do
+    FileUtils.rm(@outfile_name) 
+  end
+
 end
