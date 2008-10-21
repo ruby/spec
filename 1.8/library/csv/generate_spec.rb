ea63cb70ad04a07a93fab57ff1e800e7c0f7f3ae
Federico Builes <federico.builes@gmail.com>
2008-10-21 13:23:23 -0500

    Fixing descriptions and using helpers for last commit.

--- 1.8/library/csv/generate_spec.rb
+++ 1.8/library/csv/generate_spec.rb
@@ -5,35 +5,40 @@ require 'tempfile'
 describe "CSV.generate" do
   
   before :each do
-    @outfile_name = File.join(Dir.tmpdir, "generate_test_#{$$}.csv")  
+    @outfile_name = tmp("generate_test_#{$$}.csv")  
   end
  
-  it "should create a BasicWriter" do
+  it "creates a BasicWriter" do
     writer = CSV::generate(@outfile_name)
     writer.should be_kind_of(CSV::BasicWriter)
     writer.close
   end
 
-  it "should create a BasicWriter with ; as the seperator" do
-    writer = CSV::generate(@outfile_name)
+  it "accepts a field separator" do
+    writer = CSV::generate(@outfile_name, ",")
+    writer.should be_kind_of(CSV::BasicWriter)
+    writer.close
+  end
+
+  it "accepts a row separator" do
+    writer = CSV::generate(@outfile_name, ".")
     writer.should be_kind_of(CSV::BasicWriter)
     writer.close
   end
   
-  it "should create a BasicWriter inside the block" do
+  it "creates a BasicWriter to use in a block" do
     CSV::generate(@outfile_name) do |writer|
       writer.should be_kind_of(CSV::BasicWriter)
     end
   end
 
-  it "should create a BasicWriter with ; as the seperator inside the block" do
+  it "creates a BasicWriter with ; as the seperator inside the block" do
     CSV::generate(@outfile_name, ?;) do |writer|
       writer.should be_kind_of(CSV::BasicWriter)
     end
   end
-   
-  after :each do
-    FileUtils.rm(@outfile_name) 
-  end
 
+  after :each do
+    File.unlink(@outfile_name)
+  end
 end
