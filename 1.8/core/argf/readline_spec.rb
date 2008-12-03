e9593203fba9725b32750d03218cdf213d60ca74
Brian Ford <bford@engineyard.com>
2008-12-03 15:06:06 -0800

    Finished redo of ARGF specs.

--- 1.8/core/argf/readline_spec.rb
+++ 1.8/core/argf/readline_spec.rb
@@ -5,17 +5,14 @@ describe "ARGF.readline" do
   it_behaves_like :argf_gets, :readline
 end
 
-describe "ARGF.gets" do
+describe "ARGF.readline" do
   it_behaves_like :argf_gets_inplace_edit, :readline
 end
 
-describe "ARGF.gets" do
+describe "ARGF.readline" do
   before :each do
-    @file1_name = fixture __FILE__, "file1.txt"
-    @file2_name = fixture __FILE__, "file2.txt"
-
-    @file1 = File.readlines @file1_name
-    @file2 = File.readlines @file2_name
+    @file1 = fixture __FILE__, "file1.txt"
+    @file2 = fixture __FILE__, "file2.txt"
   end
 
   after :each do
@@ -23,10 +20,8 @@ describe "ARGF.gets" do
   end
 
   it "raises an EOFError when reaching end of files" do
-    argv [@file1_name, @file2_name] do
-      lambda {
-        while line = ARGF.readline; end
-      }.should raise_error(EOFError)
+    argv [@file1, @file2] do
+      lambda { while line = ARGF.readline; end }.should raise_error(EOFError)
     end
   end
 end
