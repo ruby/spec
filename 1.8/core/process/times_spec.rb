21f175ee9bf1f41adfbe0c183d7d8bdd079db099
Jim Deville <jdeville@microsoft.com>
2008-12-18 15:21:23 -0800

    modify Process.times comment to match reality

--- 1.8/core/process/times_spec.rb
+++ 1.8/core/process/times_spec.rb
@@ -14,14 +14,14 @@ describe "Process.times" do
     start = Time.now
     1 until (Time.now - start) > 1.2
 
-    # Ensure times is at least one second larger. NOTE that
-    # there is no guarantee of an upper bound since anything
-    # may be happening at the OS level, so we ONLY check
-    # that at least an interval has elapsed. Also, we are
-    # assuming there is a correlation between wall clock
-    # time and process time. In practice, there is an
-    # observed discrepancy often 10% or greater. In other
-    # words, this is a very fuzzy test.
+    # Ensure times is larger. NOTE that there is no 
+    # guarantee of an upper bound since anything may be 
+    # happening at the OS level, so we ONLY check that at 
+    # least an interval has elapsed. Also, we are assuming 
+    # there is a correlation between wall clock time and 
+    # process time. In practice, there is an observed 
+    # discrepancy often 10% or greater. In other words, 
+    # this is a very fuzzy test.
     t2 = Process::times
     diff = (t2.utime + t2.stime) - (t.utime + t.stime)
     diff.should > 0
