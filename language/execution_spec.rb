86d742e0ee2466864dc93ac9924b352c4bad99d9
Brian Ford <bford@engineyard.com>
2008-12-31 09:15:33 -0800

    Cleanup execution spec. See below.
    We don't spec things as not existing. There is an infinite
    number of them. Example strings do not use 'should'. Both
    %x and  do interpolation. If there is a difference between
    them, a spec should be written illustrating that.

--- language/execution_spec.rb
+++ language/execution_spec.rb
@@ -1,22 +1,15 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "``" do
-  it "should return the result of the executed sub-process" do
+  it "returns the output of the executed sub-process" do
     ip = 'world'
     `echo disc #{ip}`.should == "disc world\n"
   end
 end
 
-describe "%x()" do
-  # NOTE: Interpolation ? It's not consistant with %w for example.
-  it "is the same (and also can interpolate with #)" do
+describe "%x" do
+  it "is the same as ``" do
     ip = 'world'
     %x(echo disc #{ip}).should == "disc world\n"
-  end
-end
-
-describe "%X" do
-  it "doesn't exist" do
-    lambda { eval '%X(echo disc)' }.should raise_error(SyntaxError)
   end
 end
