73897e03f93554b3ebf60c76d71bfdf2d9feeb73
Federico Builes <federico.builes@gmail.com>
2009-01-04 23:09:36 -0500

    Fixing capitalization.

--- language/class_variable_spec.rb
+++ language/class_variable_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 require File.dirname(__FILE__) + '/../fixtures/class_variables'
 
-describe "A Class Variable" do
+describe "A class variable" do
   it "can be accessed from a subclass" do
     ClassVariablesSpec::ClassB.new.cvar_a.should == :cvar_a
   end
@@ -15,28 +15,28 @@ describe "A Class Variable" do
   end
 end
 
-describe "A Class Variable defined in a Module" do
-  it "can be accessed from Classes that extend the Module" do
+describe "A class variable defined in a module" do
+  it "can be accessed from classes that extend the module" do
     ClassVariablesSpec::ClassC.cvar_m.should == :value
   end
   
-  it "is not defined in these Classes" do
+  it "is not defined in these classes" do
     ClassVariablesSpec::ClassC.cvar_defined?.should be_false
   end
   
-  it "is only updated in the Module a Method defined in the Module is used" do
+  it "is only updated in the module a method defined in the module is used" do
     ClassVariablesSpec::ClassC.cvar_m = "new value"
     ClassVariablesSpec::ClassC.cvar_m.should == "new value"
     
     ClassVariablesSpec::ClassC.cvar_defined?.should be_false
   end
   
-  it "is updated in the Class when a Method defined in the Class is used" do
+  it "is updated in the class when a Method defined in the class is used" do
     ClassVariablesSpec::ClassC.cvar_c = "new value"    
     ClassVariablesSpec::ClassC.cvar_defined?.should be_true
   end
 
-  it "can be accessed inside the Class using the Module methods" do
+  it "can be accessed inside the class using the module methods" do
     ClassVariablesSpec::ClassC.cvar_c = "new value"
 
     ClassVariablesSpec::ClassC.cvar_m.should == "new value"
