4caa55cac115ff7ef94ad8eaa5168664f0c09617
Charles Nutter <headius@cnutter.local>
2009-01-21 12:52:31 -0600

    Update order-of-evaluation spec to expect block pass last. Bug in Ruby 1.8.

--- language/order_spec.rb
+++ language/order_spec.rb
@@ -24,21 +24,21 @@ describe "A method call" do
     a.should == 2
   end
 
-  not_compliant_on :jruby do
-    it "evaluates block pass before arguments" do
+  ruby_bug "redmine #1034", "1.8" do
+    it "evaluates block pass after arguments" do
       a = 0
       p = proc {true}
-      @obj.foo(a += 1, a += 1, &(a += 1; p)).should == [2, 3, true]
+      @obj.foo(a += 1, a += 1, &(a += 1; p)).should == [1, 2, true]
       a.should == 3
     end
  
-    it "evaluates block pass before receiver" do
+    it "evaluates block pass after receiver" do
       p1 = proc {true}
       p2 = proc {false}
       p1.should_not == p2
   
       p = p1
-      (p = p2; @obj).foo(1, 1, &p).should == [1, 1, true]
+      (p = p2; @obj).foo(1, 1, &p).should == [1, 1, false]
     end
   end
 end
