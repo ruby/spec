b333dee883db44395616f8aa013735a3bbc3d187
Charles Nutter <charles.nutter@sun.com>
2009-02-23 03:33:33 -0600

    Add missing version guards for 1.9 and add missing taint spec for gsub!

--- core/string/gsub_spec.rb
+++ core/string/gsub_spec.rb
@@ -302,26 +302,28 @@ describe "String#gsub with pattern and block" do
     
     "hello".gsub(/.+/) { obj }.should == "ok"
   end
-  
-  it "untrusts the result if the original string or replacement is untrusted" do
-    hello = "hello"
-    hello_t = "hello"
-    a = "a"
-    a_t = "a"
-    empty = ""
-    empty_t = ""
-    
-    hello_t.untrust; a_t.untrust; empty_t.untrust
-    
-    hello_t.gsub(/./) { a }.untrusted?.should == true
-    hello_t.gsub(/./) { empty }.untrusted?.should == true
 
-    hello.gsub(/./) { a_t }.untrusted?.should == true
-    hello.gsub(/./) { empty_t }.untrusted?.should == true
-    hello.gsub(//) { empty_t }.untrusted?.should == true
-    
-    hello.gsub(//.untrust) { "foo" }.untrusted?.should == false
-  end  
+  ruby_version_is "1.9" do
+    it "untrusts the result if the original string or replacement is untrusted" do
+      hello = "hello"
+      hello_t = "hello"
+      a = "a"
+      a_t = "a"
+      empty = ""
+      empty_t = ""
+
+      hello_t.untrust; a_t.untrust; empty_t.untrust
+
+      hello_t.gsub(/./) { a }.untrusted?.should == true
+      hello_t.gsub(/./) { empty }.untrusted?.should == true
+
+      hello.gsub(/./) { a_t }.untrusted?.should == true
+      hello.gsub(/./) { empty_t }.untrusted?.should == true
+      hello.gsub(//) { empty_t }.untrusted?.should == true
+
+      hello.gsub(//.untrust) { "foo" }.untrusted?.should == false
+    end
+  end
 end
 
 describe "String#gsub! with pattern and replacement" do
@@ -331,10 +333,18 @@ describe "String#gsub! with pattern and replacement" do
     a.should == "h*ll*"
   end
 
-  it "untrusts self if replacement is untrusted" do
+  it "taints self if replacement is tainted" do
     a = "hello"
-    a.gsub!(/./.untrust, "foo").untrusted?.should == false
-    a.gsub!(/./, "foo".untrust).untrusted?.should == true
+    a.gsub!(/./.taint, "foo").tainted?.should == false
+    a.gsub!(/./, "foo".taint).tainted?.should == true
+  end
+
+  ruby_version_is "1.9" do
+    it "untrusts self if replacement is untrusted" do
+      a = "hello"
+      a.gsub!(/./.untrust, "foo").untrusted?.should == false
+      a.gsub!(/./, "foo".untrust).untrusted?.should == true
+    end
   end
   
   it "returns nil if no modifications were made" do
@@ -361,10 +371,10 @@ describe "String#gsub! with pattern and block" do
     a.should == "h*ll*"
   end
 
-  it "untrusts self if block's result is untrusted" do
+  it "taints self if block's result is tainted" do
     a = "hello"
-    a.gsub!(/./.untrust) { "foo" }.untrusted?.should == false
-    a.gsub!(/./) { "foo".untrust }.untrusted?.should == true
+    a.gsub!(/./.taint) { "foo" }.tainted?.should == false
+    a.gsub!(/./) { "foo".taint }.tainted?.should == true
   end
 
   ruby_version_is "1.9" do
