00b36f6625f91cabd75a944752ce2c0603bc5b7e
ujihisa <ujihisa@gmail.com>
2009-05-02 01:27:01 -0700

    Fixed Array#to_s specs for recursive arrays. * Now [ruby-dev:37019] occurs only between 1.8.6.319...1.9.2 * Fix the spec of #to_s memtioned in [ruby-dev:37019]

--- core/array/to_s_spec.rb
+++ core/array/to_s_spec.rb
@@ -23,11 +23,24 @@ describe "Array#to_s" do
     end
   end
 
-  ruby_bug "[ruby-dev:37019]", "1.8.6.319" do
-    it "returns '[...]' for the embedded array in a recursive array" do
-      ArraySpecs.empty_recursive_array.to_s.should == "[...]"
-      a = [1, 2, 3]; a << a
-      a.to_s.should == "123[...]"
+  ruby_version_is "" ... "1.9.2" do
+    ruby_bug "[ruby-dev:37019]", "1.8.6.319" do
+      it "returns '[...]' for the embedded array in a recursive array" do
+        ArraySpecs.empty_recursive_array.to_s.should == "[...]"
+        a = [1, 2, 3]; a << a
+        a.to_s.should == "123123[...]"
+      end
+    end
+  end
+
+  ruby_version_is "1.9.2" do
+    it "is equivalent to Array#inspect in a recursive array" do
+      a = [1, 2, 3, 4]; a << a
+      a.to_s.should == a.inspect
+
+      a = ArraySpecs.empty_recursive_array
+      a.to_s.should == "[[...]]"
+      a.inspect == "[[...]]"
     end
   end
 end
