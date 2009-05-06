28536a7d9dbdd01ac61b0ccd2bd5abff424b0de0
Marc-Andre Lafortune <github@marc-andre.ca>
2009-05-06 16:13:13 -0400

    1.9: Enumerator.new accepts a block

--- library/enumerator/new_spec.rb
+++ library/enumerator/new_spec.rb
@@ -40,4 +40,25 @@ describe "Enumerator.new" do
     enum.to_a.should == [:bar]
   end
 
+  ruby_version_is ''...'1.9' do
+    it "requires an argument" do
+      lambda {enumerator_class.new}.should raise_error(ArgumentError)
+    end
+  end
+  ruby_version_is '1.9' do
+    it "accepts a block" do
+      enum = enumerator_class.new do |yielder|
+        yielder.yield 3
+        yielder.yield 2
+        yielder.yield 1
+      end
+      enum.should be_kind_of(enumerator_class)
+      enum.to_a.should == [3,2,1]
+    end
+
+    it "ignores block if arg given" do
+      enum = enumerator_class.new([1,2,3]){|y| y.yield 4}
+      enum.to_a.should == [1,2,3]
+    end
+  end    
 end
