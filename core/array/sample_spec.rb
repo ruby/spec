7c9dc661fe416a012128ef51f858dd825ca7fd53
Marc-Andre Lafortune <github@marc-andre.ca>
2009-05-06 23:50:22 -0400

    1.9: Array#sample(n)  +  found new ruby bug

--- core/array/sample_spec.rb
+++ core/array/sample_spec.rb
@@ -20,8 +20,51 @@ describe "Array#sample" do
       [].sample.should be_nil
     end
 
-    it "raises ArgumentError for a negative sample number" do
-      lambda { [1, 2].sample(-1) }.should raise_error(ArgumentError)
+    describe "passed a number n as an argument" do
+      it "raises ArgumentError for a negative n" do
+        lambda { [1, 2].sample(-1) }.should raise_error(ArgumentError)
+      end
+
+      it "returns different random values from the array" do
+        a = [1,2,3,4]
+        sum = []
+        42.times {
+          pair = a.sample(2)
+          sum.concat(pair)
+          (pair - a).should == []
+          pair[0].should_not == pair[1]
+        }
+        a.should == [1,2,3,4]
+        (a - sum).should == []  # Might fail once every 2^40 times ...
+      end
+
+      it "does not return self even when it returns whole elements" do
+        a = [1, 2, 3, 4, 5]
+        a.pop(5).should_not equal(a)
+
+        a = [1, 2, 3, 4, 5]
+        a.pop(6).should_not equal(a)
+      end
+      
+      it "tries to convert n to an Integer using #to_int" do
+        a = [1, 2, 3, 4]
+        a.sample(2.3).size.should == 2
+
+        obj = mock('to_int')
+        obj.should_receive(:to_int).and_return(2)
+        a.sample(obj).size.should == 2
+      end
+
+      it "returns all values with n big enough" do
+        a = [1,2,3,4]
+        a.sample(4).sort.should == a
+        a.sample(5).sort.should == a
+      end
+
+      it "returns [] for empty arrays or if n <= 0" do
+        [].sample(1).should == []
+        [1,2,3].sample(0).should == []
+      end
     end
   end
 end
