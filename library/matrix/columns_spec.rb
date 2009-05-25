1be872a0fd159433c844f789520b7a17703a704b
Run Paint Run Run <runrun@runpaint.org>
2009-05-25 15:29:25 +0100

    Matrix#columns raises on an empty Matrix.

--- library/matrix/columns_spec.rb
+++ library/matrix/columns_spec.rb
@@ -12,4 +12,11 @@ describe "Matrix.columns" do
     @m.column(0).to_a.should == @a
     @m.column(1).to_a.should == @b
   end
+
+  it "raises an exception on an empty Matrix" do
+    # Note that MRI raises NoMethodError because #size is called
+    # on nil. This appears to be more of an "oops" rather than
+    # an aspect of the interface. We don't spec the exception class.
+    lambda{ Matrix.columns([]) }.should raise_error
+  end
 end
