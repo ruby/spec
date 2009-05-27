b3084df3f1371092e1c6f60db2dea31133a81d57
Run Paint Run Run <runrun@runpaint.org>
2009-05-27 21:37:17 +0100

    Matrix#tr sums the diagonal of the Matrix.

--- library/matrix/shared/trace.rb
+++ library/matrix/shared/trace.rb
@@ -1,5 +1,22 @@
 require 'matrix'
 
 describe :trace, :shared => true do
-  it "needs to be reviewed for spec completeness"
+  it "returns the sum of diagonal elements in a square Matrix" do
+    Matrix[[7,6], [3,9]].trace.should == 16
+  end
+
+  # Crashes with NoMethodError: undefined method `[]' for nil:NilClass
+  ruby_bug "???", "1.9.1.129" do
+    it "returns the sum of diagonal elements in a rectangular Matrix" do
+      Matrix[[1,2,3], [4,5,6]].trace.should == 6  
+    end
+  end
+    
+  # Crashes with NoMethodError: undefined method `[]' for nil:NilClass
+  ruby_bug "???", "1.9.1.129" do
+    it "returns the sum of diagonal elements in a single-row Matrix" do
+      Matrix[[1,2]].trace.should == 3
+    end
+  end
+
 end
