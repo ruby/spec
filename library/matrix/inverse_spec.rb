ad564db2230465bdb1d43703d2b0fef556b920c1
Run Paint Run Run <runrun@runpaint.org>
2009-05-27 04:34:53 +0100

    Matrix#inverse raises if the Matrix is not square.

--- library/matrix/inverse_spec.rb
+++ library/matrix/inverse_spec.rb
@@ -1,8 +1,8 @@
+
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/shared/inverse'
 require 'matrix'
 
 describe "Matrix#inverse" do
   it_behaves_like(:inverse, :inverse)
-
 end
