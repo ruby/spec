ee16b2fd108c5e72900a3e2ee53029765933081e
Marc-Andre Lafortune <github@marc-andre.ca>
2009-05-27 22:59:37 -0400

    Moved Matrix constructor specs.
    Extensive changes.
    Also removed redundant test in Matrix#inspect

---
+++ library/matrix/constructor_spec.rb
@@ -0,0 +1,29 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+require 'matrix'
+
+describe "Matrix.[]" do
+  it "accepts only arrays as parameters" do
+    lambda { Matrix[5] }.should raise_error(TypeError)
+    lambda { Matrix[nil] }.should raise_error(TypeError)
+    lambda { Matrix[1..2] }.should raise_error(TypeError)
+    lambda { Matrix[[1, 2], 3] }.should raise_error(TypeError)
+  end
+
+  it "returns an object of type Matrix" do
+    Matrix[ [1, 2], [3, 4] ].should be_an_instance_of(Matrix)
+  end
+
+  it "doesn't require an argument" do
+    Matrix[].should be_an_instance_of(Matrix)
+  end
+
+  it "accepts vector arguments" do
+    a = Matrix[Vector[1, 2], Vector[3, 4]]
+    a.should be_an_instance_of(Matrix)
+    a.should == Matrix[ [1, 2], [3, 4] ]
+  end
+
+  it "accepts arrays of different sizes" do
+    Matrix[ [1, 2], [3, 4, 5] ].should be_an_instance_of(Matrix)
+  end
+end
--- library/matrix/inspect_spec.rb
+++ library/matrix/inspect_spec.rb
@@ -3,12 +3,6 @@ require 'matrix'
 
 describe "Matrix#inspect" do
   
-  it "returns a String beginning with 'Matrix'" do
-    i = Matrix[ [1,2], [0,1] ].inspect
-    i.should be_an_instance_of(String)
-    i.start_with?('Matrix').should be_true
-  end
-
   it "returns a stringified representation of the Matrix" do
     Matrix[ [1,2], [2,1] ].inspect.should == "Matrix[[1, 2], [2, 1]]"
   end
