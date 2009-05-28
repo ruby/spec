2fe1c34b75126831924a070136d9d62d5e631fee
Marc-Andre Lafortune <github@marc-andre.ca>
2009-05-28 14:32:44 -0400

    Matrix#minor, #row, #column: deal with out of bounds

--- library/matrix/column_spec.rb
+++ library/matrix/column_spec.rb
@@ -3,16 +3,20 @@ require 'matrix'
 
 describe "Matrix#column" do
   before :all do
-    @data =  [[1,2],[1,2]]
+    @m =  Matrix[[1,2],[1,2]]
   end
 
   it "returns a Vector when called without a block" do
-    Matrix[ *@data ].column(1).should == Vector[2,2]
+    @m.column(1).should == Vector[2,2]
   end
 
   it "yields each element in the column to the block" do
-    Matrix[ *@data ].column(1) do |n|
+    @m.column(1) do |n|
       n.should == 2
     end
+  end
+  
+  it "returns nil when out of bounds" do
+    @m.column(2).should == nil
   end
 end
--- library/matrix/row_spec.rb
+++ library/matrix/row_spec.rb
@@ -3,15 +3,19 @@ require 'matrix'
 
 describe "Matrix#row" do
   before :all do
-    @data = [ [1, 2], [1, 2] ]
+    @m = Matrix[ [1, 2], [1, 2] ]
   end
 
   it "returns a Vector when called without a block" do
-    Matrix[ *@data ].row(0).should == Vector[1,2]
+    @m.row(0).should == Vector[1,2]
   end
 
   # Seems like a bad idea...
   it "returns an Array when called with a block" do
-    Matrix[ *@data ].row(0) { |x| x }.should == [1, 2]
+    @m.row(0) { |x| x }.should == [1, 2]
+  end
+
+  it "returns nil when out of bounds" do
+    @m.row(2).should == nil
   end
 end
