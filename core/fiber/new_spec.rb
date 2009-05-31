4a52bd1b83194b7afba64659b2c441b60682e8ed
Run Paint Run Run <runrun@runpaint.org>
2009-05-31 21:16:28 +0100

    Fiber.new: Initial spec.

---
+++ core/fiber/new_spec.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+ruby_version_is "1.9" do
+  describe "Fiber.new" do
+    it "creates a fiber from the given block" do
+      fiber = Fiber.new {}
+      fiber.should be_an_instance_of(Fiber)
+    end
+    
+    it "raises an ArgumentError if called without a block" do
+      lambda { Fiber.new }.should raise_error(ArgumentError)
+    end
+
+    it "does not invoke the block" do
+      invoked = false
+      fiber = Fiber.new { invoked = true }
+      invoked.should be_false
+    end
+  end
+end
