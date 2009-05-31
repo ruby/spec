ca3baaaade2ac6d71b443b42bd818878ea46d819
Brice Figureau <brice-puppet@daysofwonder.com>
2009-05-31 19:19:56 +0200

    Implements Socket#getnameinfo specs
    Signed-off-by: Brice Figureau <brice-puppet@daysofwonder.com>

--- library/socket/socket/getnameinfo_spec.rb
+++ library/socket/socket/getnameinfo_spec.rb
@@ -1,2 +1,41 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 require File.dirname(__FILE__) + '/../fixtures/classes'
+
+require 'socket'
+
+describe "Socket#getnameinfo" do
+  before :each do
+  end
+
+  after :each do
+  end
+
+  it "gets the name information and don't resolve it" do
+    BasicSocket.do_not_reverse_lookup = true
+    expected = [ "#{SocketSpecs.port}", '127.0.0.1']
+    sockaddr = Socket.sockaddr_in SocketSpecs.port, '127.0.0.1'
+    Socket.getnameinfo(sockaddr, Socket::NI_NUMERICHOST | Socket::NI_NUMERICSERV).each do |a|
+        expected.should include(a)
+    end
+  end
+
+  it "gets the name information and resolve the host" do
+    BasicSocket.do_not_reverse_lookup = true
+    expected = [ "#{SocketSpecs.port}", 'localhost']
+    sockaddr = Socket.sockaddr_in SocketSpecs.port, '127.0.0.1'
+    Socket.getnameinfo(sockaddr, Socket::NI_NUMERICSERV).each do |a|
+        expected.should include(a)
+    end
+  end
+
+  it "gets the name information and resolve the port" do
+    BasicSocket.do_not_reverse_lookup = true
+    expected = [ "http", 'localhost']
+    sockaddr = Socket.sockaddr_in 80, '127.0.0.1'
+    Socket.getnameinfo(sockaddr).each do |a|
+        expected.should include(a)
+    end
+  end
+
+end
+
