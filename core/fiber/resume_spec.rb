43a46114c5a8bf539ae910f721838e975022ddfb
Run Paint Run Run <runrun@runpaint.org>
2009-05-31 21:52:37 +0100

    Fiber#resume raises FiberError on "double resume".

--- core/fiber/resume_spec.rb
+++ core/fiber/resume_spec.rb
@@ -52,5 +52,12 @@ ruby_version_is "1.9" do
       lambda { fiber.resume }.should raise_error(FiberError)
     end
 
+    it "raises a FiberError if the Fiber has transfered control to another Fiber" do
+      fiber1 = Fiber.new { true }
+      fiber2 = Fiber.new { fiber1.transfer; Fiber.yield }
+      fiber2.resume
+      lambda { fiber2.resume }.should raise_error(FiberError)
+    end
+
   end
 end
