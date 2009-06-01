72580e65a7ee9f7054fac7d7c6ac56c48da112aa
Run Paint Run Run <runrun@runpaint.org>
2009-06-01 10:16:07 +0100

    Fiber.new closes over lexical environments.

--- core/fiber/new_spec.rb
+++ core/fiber/new_spec.rb
@@ -16,6 +16,17 @@ ruby_version_is "1.9" do
       fiber = Fiber.new { invoked = true }
       invoked.should be_false
     end
+    
+    it "closes over lexical environments" do
+      o = Object.new
+      def o.f
+        a = 1
+        f = Fiber.new { a = 2 }
+        f.resume
+        a 
+      end
+      o.f.should == 2
+    end
 
     it "raises a SyntaxError when the block contains a retry statement" do
       lambda { eval 'Fiber.new { retry; }' }.should raise_error(SyntaxError)
