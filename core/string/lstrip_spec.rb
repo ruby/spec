c1ada521bac11588f8ff860c59b73241105f1b67
Run Paint Run Run <runrun@runpaint.org>
2009-06-01 12:57:37 +0100

    String#lstrip! Guard bug 1550 (prem. RuntimeError)

--- core/string/lstrip_spec.rb
+++ core/string/lstrip_spec.rb
@@ -46,11 +46,13 @@ describe "String#lstrip!" do
   end
 
   ruby_version_is "1.9" do 
-    it "raises a RuntimeError if self is frozen" do
-      "hello".freeze.lstrip! # ok, nothing changed
-      "".freeze.lstrip! # ok, nothing changed
+    ruby_bug "#1550", "1.9.2" do
+      it "raises a RuntimeError if self is frozen" do
+        "hello".freeze.lstrip! # ok, nothing changed
+        "".freeze.lstrip! # ok, nothing changed
 
-      lambda { "  hello  ".freeze.lstrip! }.should raise_error(RuntimeError)
+        lambda { "  hello  ".freeze.lstrip! }.should raise_error(RuntimeError)
+      end
     end
   end
 end
