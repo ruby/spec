d45d182959a05844508c185cc8225bf3e4421e06
Run Paint Run Run <runrun@runpaint.org>
2009-06-02 14:25:26 +0100

    String#replace! Guard frozen behav. [core:23666]

--- core/string/replace_spec.rb
+++ core/string/replace_spec.rb
@@ -36,13 +36,16 @@ describe "String#replace" do
       lambda { a.replace("world") }.should raise_error(TypeError)
     end
   end
-  ruby_version_is ""..."1.9" do
-    it "raises a RuntimeError if self is frozen" do
-      a = "hello".freeze
 
-      a.replace(a) # ok, no change
-      lambda { a.replace("")      }.should raise_error(RuntimeError)
-      lambda { a.replace("world") }.should raise_error(RuntimeError)
+  ruby_version_is "1.9" do
+    ruby_bug "[ruby-core:23666]", "1.9.2" do
+      it "raises a RuntimeError if self is frozen" do
+        a = "hello".freeze
+
+        lambda { a.replace(a)       }.should raise_error(RuntimeError)
+        lambda { a.replace("")      }.should raise_error(RuntimeError)
+        lambda { a.replace("world") }.should raise_error(RuntimeError)
+      end
     end
   end
 end
