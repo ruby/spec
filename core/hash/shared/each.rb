2b12202d8cf961600c339b5cea81fde5ed8cc82e
Run Paint Run Run <runrun@runpaint.org>
2009-06-04 21:54:36 +0100

    Hash#each aliases Hash#each_pair on 1.9 -> shared/

---
+++ core/hash/shared/each.rb
@@ -0,0 +1,28 @@
+describe :hash_each, :shared => true do
+  it "yields one argument: [key, value]" do
+    all_args = []
+    new_hash(1 => 2, 3 => 4).each { |*args| all_args << args }
+    all_args.sort.should == [[[1, 2]], [[3, 4]]]
+  end
+
+  it "calls block once for each entry, passing key, value" do
+    r = new_hash
+    h = new_hash(:a => 1, :b => 2, :c => 3, :d => 5)
+    h.each { |k,v| r[k.to_s] = v.to_s }.should equal(h)
+    r.should == new_hash("a" => "1", "b" => "2", "c" => "3", "d" => "5")
+  end
+
+  it "uses the same order as keys() and values()" do
+    h = new_hash(:a => 1, :b => 2, :c => 3, :d => 5)
+    keys = []
+    values = []
+
+    h.each do |k, v|
+      keys << k
+      values << v
+    end
+
+    keys.should == h.keys
+    values.should == h.values
+  end
+end  
