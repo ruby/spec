576c12c22d1a9e193234b4025fab861cfa2ed801
Run Paint Run Run <runrun@runpaint.org>
2009-06-05 16:39:43 +0100

    Hash#replace on 1.9 raises RuntimeError if frozen

--- core/hash/shared/replace.rb
+++ core/hash/shared/replace.rb
@@ -47,9 +47,10 @@ describe :hash_replace, :shared => true do
 
   ruby_version_is "1.9" do
     it "raises a RuntimeError if called on a frozen instance" do
-      HashSpecs.frozen_hash.send(@method, HashSpecs.frozen_hash) # ok, nothing changed
-      block = lambda { HashSpecs.frozen_hash.send(@method, HashSpecs.empty_frozen_hash) }
-      block.should raise_error(RuntimeError)
+      lambda { HashSpecs.frozen_hash.send(@method, HashSpecs.frozen_hash) }.
+        should_raise(RuntimeError)
+      lambda { HashSpecs.frozen_hash.send(@method, HashSpecs.empty_frozen_hash) }.
+        should raise_error(RuntimeError)
     end
   end
 end
