bdd7e1a84b0f56352effcb2ea8f8a5c98c0c8b34
Run Paint Run Run <runrun@runpaint.org>
2009-06-06 00:51:00 +0100

    Integer(): Merge my dup. spec with the original.

--- core/kernel/Integer_spec.rb
+++ core/kernel/Integer_spec.rb
@@ -44,10 +44,9 @@ describe "Kernel.Integer" do
     lambda { Integer(mock('x')) }.should raise_error(TypeError)
   end
 
-  ruby_version_is ""..."1.9" do
-    it "returns 0 when passed nil" do
-      Integer(nil).should == 0
-    end
+  it "raises a TypeError if to_i doesn't return an Integer" do
+    (obj = mock('ha!')).should_receive(:to_i).and_return("ha!")
+    lambda { Integer(obj) }.should raise_error(TypeError)
   end
 
   ruby_version_is "1.9" do
@@ -55,11 +54,46 @@ describe "Kernel.Integer" do
       lambda { Integer(nil) }.should raise_error(TypeError)
     end
   end
-
-  it "raises a TypeError if to_i doesn't return an Integer" do
-    (obj = mock('ha!')).should_receive(:to_i).and_return("ha!")
-    lambda { Integer(obj) }.should raise_error(TypeError)
+  
+  ruby_version_is ""..."1.9" do
+    it "returns 0 when passed nil" do
+      Integer(nil).should == 0
+    end
   end
+
+  it "returns a Fixnum or Bignum object" do
+    Integer(2).should be_an_instance_of(Fixnum)
+    Integer(9**99).should be_an_instance_of(Bignum)
+  end
+  
+  it "truncates Floats" do
+    Integer(3.14).should == 3
+    Integer(90.8).should == 90
+  end
+    
+  it "calls to_i on Rationals" do
+    Integer(Rational(8,3)).should == 2
+    Integer(3.quo(2)).should == 1
+  end
+
+  it "passes through Bignums as-is" do
+    bignum = 99**99
+    bignum.should be_an_instance_of(Bignum)
+    Integer(bignum).should == bignum
+    Integer(bignum).should be_an_instance_of(Bignum)
+  end
+
+  it "passes through Fixnums as-is" do
+    fixnum = 99
+    fixnum.should be_an_instance_of(Fixnum)
+    Integer(fixnum).should == fixnum
+    Integer(fixnum).should be_an_instance_of(Fixnum)
+  end
+
+
+  it "raises a FloatDomainError when passed NaN" do
+    lambda { Integer(0.0/0.0) }.should raise_error(FloatDomainError)
+  end  
 end
 
 describe "Kernel#Integer" do
