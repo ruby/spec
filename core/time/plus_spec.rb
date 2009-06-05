6bceb45a8137a733b4ed54f02f11b0e40b0d0cbe
Run Paint Run Run <runrun@runpaint.org>
2009-06-05 21:52:01 +0100

    Time#+ coerces args to Rationals on 1.9.

--- core/time/plus_spec.rb
+++ core/time/plus_spec.rb
@@ -14,23 +14,34 @@ describe "Time#+" do
     it "increments the time by the specified amount as float numbers" do
       (Time.at(1.1) + 0.9).should == Time.at(2)
     end
+
+    it "accepts arguments that can be coerced into Float" do
+      (obj = mock('10.5')).should_receive(:to_f).and_return(10.5)
+      (Time.at(100) + obj).should == Time.at(110.5)
+    end
+
+    it "raises TypeError on argument that can't be coerced into Float" do
+      lambda { Time.now + Object.new }.should raise_error(TypeError)
+      lambda { Time.now + "stuff" }.should raise_error(TypeError)
+    end
   end
 
   ruby_version_is "1.9" do
     it "increments the time by the specified amount as rational numbers" do
       (Time.at(1.1) + 0.9).should_not == Time.at(2)
     end
+
+    it "accepts arguments that can be coerced into Rational" do
+      (obj = mock('10')).should_receive(:to_r).and_return(Rational(10))
+      (Time.at(100) + obj).should == Time.at(110)
+    end
+
+    it "raises TypeError on argument that can't be coerced into Rational" do
+      lambda { Time.now + Object.new }.should raise_error(TypeError)
+      lambda { Time.now + "stuff" }.should raise_error(TypeError)
+    end
   end
 
-  it "accepts arguments that can be coerced into Float" do
-    (obj = mock('10.5')).should_receive(:to_f).and_return(10.5)
-    (Time.at(100) + obj).should == Time.at(110.5)
-  end
-
-  it "raises TypeError on argument that can't be coerced into Float" do
-    lambda { Time.now + Object.new }.should raise_error(TypeError)
-    lambda { Time.now + "stuff" }.should raise_error(TypeError)
-  end
 
   it "raises TypeError on Time argument" do
     lambda { Time.now + Time.now }.should raise_error(TypeError)
