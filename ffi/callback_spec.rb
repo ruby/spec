91bc9f0de45fafc9618e4d2eeb834906de625f07
Eloy Duran <eloy.de.enige@gmail.com>
2009-06-10 19:27:56 +0200

    Namespaced all classes so the callback specs run on 1.9 as well.

--- ffi/callback_spec.rb
+++ ffi/callback_spec.rb
@@ -8,7 +8,7 @@ module FFISpecs
   #    attach_function :qsort, [ :pointer, :int, :int, :qsort_cmp ], :int
   #  end
   #  it "arguments get passed correctly" do
-  #    p = MemoryPointer.new(:int, 2)
+  #    p = FFI::MemoryPointer.new(:int, 2)
   #    p.put_array_of_int32(0, [ 1 , 2 ])
   #    args = []
   #    cmp = proc do |p1, p2| args.push(p1.get_int(0)); args.push(p2.get_int(0)); 0; end
@@ -18,7 +18,7 @@ module FFISpecs
   #  end
   #
   #  it "Block can be substituted for Callback as last argument" do
-  #    p = MemoryPointer.new(:int, 2)
+  #    p = FFI::MemoryPointer.new(:int, 2)
   #    p.put_array_of_int32(0, [ 1 , 2 ])
   #    args = []
   #    # this is a bit dodgey, as it relies on qsort passing the args in order
@@ -31,211 +31,211 @@ module FFISpecs
   #  end
 
     it "function with Callback plus another arg should raise error if no arg given" do
-      lambda { LibTest.testCallbackCrV { |*a| } }.should raise_error
+      lambda { FFISpecs::LibTest.testCallbackCrV { |*a| } }.should raise_error
     end
 
     it "returning :char (0)" do
-      LibTest.testCallbackVrS8 { 0 }.should == 0
+      FFISpecs::LibTest.testCallbackVrS8 { 0 }.should == 0
     end
 
     it "returning :char (127)" do
-      LibTest.testCallbackVrS8 { 127 }.should == 127
+      FFISpecs::LibTest.testCallbackVrS8 { 127 }.should == 127
     end
 
     it "returning :char (-128)" do
-      LibTest.testCallbackVrS8 { -128 }.should == -128
+      FFISpecs::LibTest.testCallbackVrS8 { -128 }.should == -128
     end
 
     # test wrap around
     it "returning :char (128)" do
-      LibTest.testCallbackVrS8 { 128 }.should == -128
+      FFISpecs::LibTest.testCallbackVrS8 { 128 }.should == -128
     end
 
     it "returning :char (255)" do
-      LibTest.testCallbackVrS8 { 0xff }.should == -1
+      FFISpecs::LibTest.testCallbackVrS8 { 0xff }.should == -1
     end
 
     it "returning :uchar (0)" do
-      LibTest.testCallbackVrU8 { 0 }.should == 0
+      FFISpecs::LibTest.testCallbackVrU8 { 0 }.should == 0
     end
 
     it "returning :uchar (0xff)" do
-      LibTest.testCallbackVrU8 { 0xff }.should == 0xff
+      FFISpecs::LibTest.testCallbackVrU8 { 0xff }.should == 0xff
     end
 
     it "returning :uchar (-1)" do
-      LibTest.testCallbackVrU8 { -1 }.should == 0xff
+      FFISpecs::LibTest.testCallbackVrU8 { -1 }.should == 0xff
     end
 
     it "returning :uchar (128)" do
-      LibTest.testCallbackVrU8 { 128 }.should == 128
+      FFISpecs::LibTest.testCallbackVrU8 { 128 }.should == 128
     end
 
     it "returning :uchar (-128)" do
-      LibTest.testCallbackVrU8 { -128 }.should == 128
+      FFISpecs::LibTest.testCallbackVrU8 { -128 }.should == 128
     end
 
     it "returning :short (0)" do
-      LibTest.testCallbackVrS16 { 0 }.should == 0
+      FFISpecs::LibTest.testCallbackVrS16 { 0 }.should == 0
     end
 
     it "returning :short (0x7fff)" do
-      LibTest.testCallbackVrS16 { 0x7fff }.should == 0x7fff
+      FFISpecs::LibTest.testCallbackVrS16 { 0x7fff }.should == 0x7fff
     end
 
     # test wrap around
     it "returning :short (0x8000)" do
-      LibTest.testCallbackVrS16 { 0x8000 }.should == -0x8000
+      FFISpecs::LibTest.testCallbackVrS16 { 0x8000 }.should == -0x8000
     end
 
     it "returning :short (0xffff)" do
-      LibTest.testCallbackVrS16 { 0xffff }.should == -1
+      FFISpecs::LibTest.testCallbackVrS16 { 0xffff }.should == -1
     end
 
     it "returning :ushort (0)" do
-      LibTest.testCallbackVrU16 { 0 }.should == 0
+      FFISpecs::LibTest.testCallbackVrU16 { 0 }.should == 0
     end
 
     it "returning :ushort (0x7fff)" do
-      LibTest.testCallbackVrU16 { 0x7fff }.should == 0x7fff
+      FFISpecs::LibTest.testCallbackVrU16 { 0x7fff }.should == 0x7fff
     end
 
     it "returning :ushort (0x8000)" do
-      LibTest.testCallbackVrU16 { 0x8000 }.should == 0x8000
+      FFISpecs::LibTest.testCallbackVrU16 { 0x8000 }.should == 0x8000
     end
 
     it "returning :ushort (0xffff)" do
-      LibTest.testCallbackVrU16 { 0xffff }.should == 0xffff
+      FFISpecs::LibTest.testCallbackVrU16 { 0xffff }.should == 0xffff
     end
 
     it "returning :ushort (-1)" do
-      LibTest.testCallbackVrU16 { -1 }.should == 0xffff
+      FFISpecs::LibTest.testCallbackVrU16 { -1 }.should == 0xffff
     end
 
     it "returning :int (0)" do
-      LibTest.testCallbackVrS32 { 0 }.should == 0
+      FFISpecs::LibTest.testCallbackVrS32 { 0 }.should == 0
     end
 
     it "returning :int (0x7fffffff)" do
-      LibTest.testCallbackVrS32 { 0x7fffffff }.should == 0x7fffffff
+      FFISpecs::LibTest.testCallbackVrS32 { 0x7fffffff }.should == 0x7fffffff
     end
 
     # test wrap around
     it "returning :int (-0x80000000)" do
-      LibTest.testCallbackVrS32 { -0x80000000 }.should == -0x80000000
+      FFISpecs::LibTest.testCallbackVrS32 { -0x80000000 }.should == -0x80000000
     end
 
     it "returning :int (-1)" do
-      LibTest.testCallbackVrS32 { -1 }.should == -1
+      FFISpecs::LibTest.testCallbackVrS32 { -1 }.should == -1
     end
 
     it "returning :uint (0)" do
-      LibTest.testCallbackVrU32 { 0 }.should == 0
+      FFISpecs::LibTest.testCallbackVrU32 { 0 }.should == 0
     end
 
     it "returning :uint (0x7fffffff)" do
-      LibTest.testCallbackVrU32 { 0x7fffffff }.should == 0x7fffffff
+      FFISpecs::LibTest.testCallbackVrU32 { 0x7fffffff }.should == 0x7fffffff
     end
 
     # test wrap around
     it "returning :uint (0x80000000)" do
-      LibTest.testCallbackVrU32 { 0x80000000 }.should == 0x80000000
+      FFISpecs::LibTest.testCallbackVrU32 { 0x80000000 }.should == 0x80000000
     end
 
     it "returning :uint (0xffffffff)" do
-      LibTest.testCallbackVrU32 { 0xffffffff }.should == 0xffffffff
+      FFISpecs::LibTest.testCallbackVrU32 { 0xffffffff }.should == 0xffffffff
     end
 
     it "Callback returning :uint (-1)" do
-      LibTest.testCallbackVrU32 { -1 }.should == 0xffffffff
+      FFISpecs::LibTest.testCallbackVrU32 { -1 }.should == 0xffffffff
     end
 
     it "returning :long (0)" do
-      LibTest.testCallbackVrL { 0 }.should == 0
+      FFISpecs::LibTest.testCallbackVrL { 0 }.should == 0
     end
 
     it "returning :long (0x7fffffff)" do
-      LibTest.testCallbackVrL { 0x7fffffff }.should == 0x7fffffff
+      FFISpecs::LibTest.testCallbackVrL { 0x7fffffff }.should == 0x7fffffff
     end
 
     # test wrap around
     it "returning :long (-0x80000000)" do
-      LibTest.testCallbackVrL { -0x80000000 }.should == -0x80000000
+      FFISpecs::LibTest.testCallbackVrL { -0x80000000 }.should == -0x80000000
     end
 
     it "returning :long (-1)" do
-      LibTest.testCallbackVrL { -1 }.should == -1
+      FFISpecs::LibTest.testCallbackVrL { -1 }.should == -1
     end
 
     it "returning :ulong (0)" do
-      LibTest.testCallbackVrUL { 0 }.should == 0
+      FFISpecs::LibTest.testCallbackVrUL { 0 }.should == 0
     end
 
     it "returning :ulong (0x7fffffff)" do
-      LibTest.testCallbackVrUL { 0x7fffffff }.should == 0x7fffffff
+      FFISpecs::LibTest.testCallbackVrUL { 0x7fffffff }.should == 0x7fffffff
     end
 
     # test wrap around
     it "returning :ulong (0x80000000)" do
-      LibTest.testCallbackVrUL { 0x80000000 }.should == 0x80000000
+      FFISpecs::LibTest.testCallbackVrUL { 0x80000000 }.should == 0x80000000
     end
 
     it "returning :ulong (0xffffffff)" do
-      LibTest.testCallbackVrUL { 0xffffffff }.should == 0xffffffff
+      FFISpecs::LibTest.testCallbackVrUL { 0xffffffff }.should == 0xffffffff
     end
 
     it "Callback returning :ulong (-1)" do
-      LibTest.testCallbackVrUL { -1 }.should == 0xffffffff
+      FFISpecs::LibTest.testCallbackVrUL { -1 }.should == 0xffffffff
     end
 
     it "returning :long_long (0)" do
-      LibTest.testCallbackVrS64 { 0 }.should == 0
+      FFISpecs::LibTest.testCallbackVrS64 { 0 }.should == 0
     end
 
     it "returning :long_long (0x7fffffffffffffff)" do
-      LibTest.testCallbackVrS64 { 0x7fffffffffffffff }.should == 0x7fffffffffffffff
+      FFISpecs::LibTest.testCallbackVrS64 { 0x7fffffffffffffff }.should == 0x7fffffffffffffff
     end
 
     # test wrap around
     it "returning :long_long (-0x8000000000000000)" do
-      LibTest.testCallbackVrS64 { -0x8000000000000000 }.should == -0x8000000000000000
+      FFISpecs::LibTest.testCallbackVrS64 { -0x8000000000000000 }.should == -0x8000000000000000
     end
 
     it "returning :long_long (-1)" do
-      LibTest.testCallbackVrS64 { -1 }.should == -1
+      FFISpecs::LibTest.testCallbackVrS64 { -1 }.should == -1
     end
 
     it "returning :pointer (nil)" do
-      LibTest.testCallbackVrP { nil }.null?.should be_true
+      FFISpecs::LibTest.testCallbackVrP { nil }.null?.should be_true
     end
 
-    it "returning :pointer (MemoryPointer)" do
-      p = MemoryPointer.new :long
-      LibTest.testCallbackVrP { p }.should == p
+    it "returning :pointer (FFI::MemoryPointer)" do
+      p = FFI::MemoryPointer.new :long
+      FFISpecs::LibTest.testCallbackVrP { p }.should == p
     end
 
     it "global variable" do
       proc = Proc.new { 0x1e }
-      LibTest.cbVrS8 = proc
-      LibTest.testGVarCallbackVrS8(LibTest.pVrS8).should == 0x1e
+      FFISpecs::LibTest.cbVrS8 = proc
+      FFISpecs::LibTest.testGVarCallbackVrS8(FFISpecs::LibTest.pVrS8).should == 0x1e
     end
 
     describe "When the callback is considered optional by the underlying library" do
       it "should handle receiving 'nil' in place of the closure" do
-        lambda { LibTest.testOptionalCallbackCrV(nil, 13) }.should_not raise_error
+        lambda { FFISpecs::LibTest.testOptionalCallbackCrV(nil, 13) }.should_not raise_error
       end
     end
 
     describe 'when inlined' do
       it 'could be anonymous' do
-        LibTest.testCallbackVrS8 { 0 }.should == 0
+        FFISpecs::LibTest.testCallbackVrS8 { 0 }.should == 0
       end
     end
 
     describe "as return value" do
       it "should not blow up when a callback is defined that returns a callback" do
         lambda {
-          LibTest.module_eval do
+          FFISpecs::LibTest.module_eval do
             callback :cb_return_type_1, [ :short ], :short
             callback :cb_lookup_1, [ :short ], :cb_return_type_1
             attach_function :testReturnsCallback_1, :testReturnsClosure, [ :cb_lookup_1, :short ], :cb_return_type_1
@@ -256,7 +256,7 @@ module FFISpecs
           return_proc
         end
 
-        val = LibTest.testReturnsCallback(lookup_proc, 0x1234)
+        val = FFISpecs::LibTest.testReturnsCallback(lookup_proc, 0x1234)
         val.should == 0x1234 * 2
         lookup_proc_called.should be_true
         return_proc_called.should be_true
@@ -264,7 +264,7 @@ module FFISpecs
 
       it 'should not blow up when a callback takes a callback as argument' do
         lambda {
-          LibTest.module_eval do
+          FFISpecs::LibTest.module_eval do
             callback :cb_argument, [ :int ], :int
             callback :cb_with_cb_argument, [ :cb_argument, :int ], :int
             attach_function :testCallbackAsArgument, :testArgumentClosure, [ :cb_with_cb_argument, :int ], :int
@@ -274,7 +274,7 @@ module FFISpecs
 
       it 'should be able to use the callback argument' do
         # TODO: Moving this to fixtures/classes.rb breaks
-        module LibTest
+        module FFISpecs::LibTest
           callback :cb_argument, [ :int ], :int
           callback :cb_with_cb_argument, [ :cb_argument, :int ], :int
           attach_function :testCallbackAsArgument, :testArgumentClosure, [ :cb_with_cb_argument, :cb_argument, :int ], :int
@@ -292,7 +292,7 @@ module FFISpecs
           cb.call(val)
         end
 
-        val = LibTest.testCallbackAsArgument(callback_with_callback_arg, callback_arg, 0xff1)
+        val = FFISpecs::LibTest.testCallbackAsArgument(callback_with_callback_arg, callback_arg, 0xff1)
 
         val.should == 0xff1 * 2
         callback_arg_called.should be_true
@@ -300,7 +300,7 @@ module FFISpecs
       end
 
       it 'function returns callable object' do
-        f = LibTest.testReturnsFunctionPointer
+        f = FFISpecs::LibTest.testReturnsFunctionPointer
         f.call(3).should == 6
       end
     end
@@ -309,217 +309,217 @@ module FFISpecs
   describe "primitive argument" do
     it ":char (0) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackCrV(0) { |i| v = i }
+      FFISpecs::LibTest.testCallbackCrV(0) { |i| v = i }
       v.should == 0
     end
 
     it ":char (127) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackCrV(127) { |i| v = i }
+      FFISpecs::LibTest.testCallbackCrV(127) { |i| v = i }
       v.should == 127
     end
 
     it ":char (-128) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackCrV(-128) { |i| v = i }
+      FFISpecs::LibTest.testCallbackCrV(-128) { |i| v = i }
       v.should == -128
     end
 
     it ":char (-1) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackCrV(-1) { |i| v = i }
+      FFISpecs::LibTest.testCallbackCrV(-1) { |i| v = i }
       v.should == -1
     end
 
     it ":uchar (0) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackU8rV(0) { |i| v = i }
+      FFISpecs::LibTest.testCallbackU8rV(0) { |i| v = i }
       v.should == 0
     end
 
     it ":uchar (127) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackU8rV(127) { |i| v = i }
+      FFISpecs::LibTest.testCallbackU8rV(127) { |i| v = i }
       v.should == 127
     end
 
     it ":uchar (128) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackU8rV(128) { |i| v = i }
+      FFISpecs::LibTest.testCallbackU8rV(128) { |i| v = i }
       v.should == 128
     end
 
     it ":uchar (255) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackU8rV(255) { |i| v = i }
+      FFISpecs::LibTest.testCallbackU8rV(255) { |i| v = i }
       v.should == 255
     end
 
     it ":short (0) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackSrV(0) { |i| v = i }
+      FFISpecs::LibTest.testCallbackSrV(0) { |i| v = i }
       v.should == 0
     end
 
     it ":short (0x7fff) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackSrV(0x7fff) { |i| v = i }
+      FFISpecs::LibTest.testCallbackSrV(0x7fff) { |i| v = i }
       v.should == 0x7fff
     end
 
     it ":short (-0x8000) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackSrV(-0x8000) { |i| v = i }
+      FFISpecs::LibTest.testCallbackSrV(-0x8000) { |i| v = i }
       v.should == -0x8000
     end
 
     it ":short (-1) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackSrV(-1) { |i| v = i }
+      FFISpecs::LibTest.testCallbackSrV(-1) { |i| v = i }
       v.should == -1
     end
 
     it ":ushort (0) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackU16rV(0) { |i| v = i }
+      FFISpecs::LibTest.testCallbackU16rV(0) { |i| v = i }
       v.should == 0
     end
 
     it ":ushort (0x7fff) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackU16rV(0x7fff) { |i| v = i }
+      FFISpecs::LibTest.testCallbackU16rV(0x7fff) { |i| v = i }
       v.should == 0x7fff
     end
 
     it ":ushort (0x8000) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackU16rV(0x8000) { |i| v = i }
+      FFISpecs::LibTest.testCallbackU16rV(0x8000) { |i| v = i }
       v.should == 0x8000
     end
 
     it ":ushort (0xffff) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackU16rV(0xffff) { |i| v = i }
+      FFISpecs::LibTest.testCallbackU16rV(0xffff) { |i| v = i }
       v.should == 0xffff
     end
 
     it ":int (0) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackIrV(0) { |i| v = i }
+      FFISpecs::LibTest.testCallbackIrV(0) { |i| v = i }
       v.should == 0
     end
 
     it ":int (0x7fffffff) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackIrV(0x7fffffff) { |i| v = i }
+      FFISpecs::LibTest.testCallbackIrV(0x7fffffff) { |i| v = i }
       v.should == 0x7fffffff
     end
 
     it ":int (-0x80000000) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackIrV(-0x80000000) { |i| v = i }
+      FFISpecs::LibTest.testCallbackIrV(-0x80000000) { |i| v = i }
       v.should == -0x80000000
     end
 
     it ":int (-1) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackIrV(-1) { |i| v = i }
+      FFISpecs::LibTest.testCallbackIrV(-1) { |i| v = i }
       v.should == -1
     end
 
     it ":uint (0) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackU32rV(0) { |i| v = i }
+      FFISpecs::LibTest.testCallbackU32rV(0) { |i| v = i }
       v.should == 0
     end
 
     it ":uint (0x7fffffff) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackU32rV(0x7fffffff) { |i| v = i }
+      FFISpecs::LibTest.testCallbackU32rV(0x7fffffff) { |i| v = i }
       v.should == 0x7fffffff
     end
 
     it ":uint (0x80000000) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackU32rV(0x80000000) { |i| v = i }
+      FFISpecs::LibTest.testCallbackU32rV(0x80000000) { |i| v = i }
       v.should == 0x80000000
     end
 
     it ":uint (0xffffffff) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackU32rV(0xffffffff) { |i| v = i }
+      FFISpecs::LibTest.testCallbackU32rV(0xffffffff) { |i| v = i }
       v.should == 0xffffffff
     end
 
     it ":long (0) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackLrV(0) { |i| v = i }
+      FFISpecs::LibTest.testCallbackLrV(0) { |i| v = i }
       v.should == 0
     end
 
     it ":long (0x7fffffff) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackLrV(0x7fffffff) { |i| v = i }
+      FFISpecs::LibTest.testCallbackLrV(0x7fffffff) { |i| v = i }
       v.should == 0x7fffffff
     end
 
     it ":long (-0x80000000) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackLrV(-0x80000000) { |i| v = i }
+      FFISpecs::LibTest.testCallbackLrV(-0x80000000) { |i| v = i }
       v.should == -0x80000000
     end
 
     it ":long (-1) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackLrV(-1) { |i| v = i }
+      FFISpecs::LibTest.testCallbackLrV(-1) { |i| v = i }
       v.should == -1
     end
 
     it ":ulong (0) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackULrV(0) { |i| v = i }
+      FFISpecs::LibTest.testCallbackULrV(0) { |i| v = i }
       v.should == 0
     end
 
     it ":ulong (0x7fffffff) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackULrV(0x7fffffff) { |i| v = i }
+      FFISpecs::LibTest.testCallbackULrV(0x7fffffff) { |i| v = i }
       v.should == 0x7fffffff
     end
 
     it ":ulong (0x80000000) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackULrV(0x80000000) { |i| v = i }
+      FFISpecs::LibTest.testCallbackULrV(0x80000000) { |i| v = i }
       v.should == 0x80000000
     end
 
     it ":ulong (0xffffffff) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackULrV(0xffffffff) { |i| v = i }
+      FFISpecs::LibTest.testCallbackULrV(0xffffffff) { |i| v = i }
       v.should == 0xffffffff
     end
 
     it ":long_long (0) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackLLrV(0) { |i| v = i }
+      FFISpecs::LibTest.testCallbackLLrV(0) { |i| v = i }
       v.should == 0
     end
 
     it ":long_long (0x7fffffffffffffff) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackLLrV(0x7fffffffffffffff) { |i| v = i }
+      FFISpecs::LibTest.testCallbackLLrV(0x7fffffffffffffff) { |i| v = i }
       v.should == 0x7fffffffffffffff
     end
 
     it ":long_long (-0x8000000000000000) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackLLrV(-0x8000000000000000) { |i| v = i }
+      FFISpecs::LibTest.testCallbackLLrV(-0x8000000000000000) { |i| v = i }
       v.should == -0x8000000000000000
     end
 
     it ":long_long (-1) argument" do
       v = 0xdeadbeef
-      LibTest.testCallbackLLrV(-1) { |i| v = i }
+      FFISpecs::LibTest.testCallbackLLrV(-1) { |i| v = i }
       v.should == -1
     end
   end
