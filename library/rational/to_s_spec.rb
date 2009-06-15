7d30bf4606e3aed741580114489907de2d0960f8
Run Paint Run Run <runrun@runpaint.org>
2009-06-15 01:31:23 +0100

    Rational#to_s: Update for 1.9's format change.
    This change was confirmed as intentional by matz in [ruby-core:23843].

--- library/rational/to_s_spec.rb
+++ library/rational/to_s_spec.rb
@@ -2,11 +2,23 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require 'rational'
 
 describe "Rational#to_s" do
-  it "returns a string representation of self" do
-    Rational(1, 1).to_s.should == "1"
-    Rational(2, 1).to_s.should == "2"
-    Rational(1, 2).to_s.should == "1/2"
-    Rational(-1, 3).to_s.should == "-1/3"
-    Rational(1, -3).to_s.should == "-1/3"
+  ruby_version_is ""..."1.9" do
+    it "returns a string representation of self" do
+      Rational(1, 1).to_s.should == "1"
+      Rational(2, 1).to_s.should == "2"
+      Rational(1, 2).to_s.should == "1/2"
+      Rational(-1, 3).to_s.should == "-1/3"
+      Rational(1, -3).to_s.should == "-1/3"
+    end
+  end
+
+  ruby_version_is "1.9" do
+    it "returns a string representation of self" do
+      Rational(1, 1).to_s.should == "1/1"
+      Rational(2, 1).to_s.should == "2/1"
+      Rational(1, 2).to_s.should == "1/2"
+      Rational(-1, 3).to_s.should == "-1/3"
+      Rational(1, -3).to_s.should == "-1/3"
+    end
   end
 end
