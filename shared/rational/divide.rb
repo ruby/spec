0267d7db28488b742d8b09516d4fb128cd4e8e60
Run Paint Run Run <runrun@runpaint.org>
2009-06-17 21:15:51 +0100

    Rational#/: Abstract method name in shared spec.

--- shared/rational/divide.rb
+++ shared/rational/divide.rb
@@ -3,47 +3,47 @@ require 'rational'
 
 describe :rational_divide_rat, :shared => true do
   it "returns self divided by other as a Rational" do
-    (Rational(3, 4) / Rational(3, 4)).should eql(Rational(1, 1))
-    (Rational(2, 4) / Rational(1, 4)).should eql(Rational(2, 1))
+    Rational(3, 4).send(@method, Rational(3, 4)).should eql(Rational(1, 1))
+    Rational(2, 4).send(@method, Rational(1, 4)).should eql(Rational(2, 1))
     
-    (Rational(2, 4) / 2).should == Rational(1, 4)
-    (Rational(6, 7) / -2).should == Rational(-3, 7)
+    Rational(2, 4).send(@method, 2).should == Rational(1, 4)
+    Rational(6, 7).send(@method, -2).should == Rational(-3, 7)
   end
   
   it "raises a ZeroDivisionError when passed a Rational with a numerator of 0" do
-    lambda { Rational(3, 4) / Rational(0, 1) }.should raise_error(ZeroDivisionError)
+    lambda { Rational(3, 4).send(@method, Rational(0, 1)) }.should raise_error(ZeroDivisionError)
   end
 end
 
 describe :rational_divide_int, :shared => true do
   it "returns self divided by other as a Rational" do
-    (Rational(3, 4) / 2).should eql(Rational(3, 8))
-    (Rational(2, 4) / 2).should eql(Rational(1, 4))
-    (Rational(6, 7) / -2).should eql(Rational(-3, 7))
+    Rational(3, 4).send(@method, 2).should eql(Rational(3, 8))
+    Rational(2, 4).send(@method, 2).should eql(Rational(1, 4))
+    Rational(6, 7).send(@method, -2).should eql(Rational(-3, 7))
   end
   
   it "raises a ZeroDivisionError when passed 0" do
-    lambda { Rational(3, 4) / 0 }.should raise_error(ZeroDivisionError)
+    lambda { Rational(3, 4).send(@method, 0) }.should raise_error(ZeroDivisionError)
   end
 end
 
 describe :rational_divide_float, :shared => true do
   it "returns self divided by other as a Float" do
-    (Rational(3, 4) / 0.75).should eql(1.0)
-    (Rational(3, 4) / 0.25).should eql(3.0)
-    (Rational(3, 4) / 0.3).should eql(2.5)
+    Rational(3, 4).send(@method, 0.75).should eql(1.0)
+    Rational(3, 4).send(@method, 0.25).should eql(3.0)
+    Rational(3, 4).send(@method, 0.3).should eql(2.5)
     
-    (Rational(-3, 4) / 0.3).should eql(-2.5)
-    (Rational(3, -4) / 0.3).should eql(-2.5)
-    (Rational(3, 4) / -0.3).should eql(-2.5)
+    Rational(-3, 4).send(@method, 0.3).should eql(-2.5)
+    Rational(3, -4).send(@method, 0.3).should eql(-2.5)
+    Rational(3, 4).send(@method, -0.3).should eql(-2.5)
   end
   
   it "returns infinity when passed 0" do
-    (Rational(3, 4) / 0.0).infinite?.should eql(1)
-    (Rational(-3, -4) / 0.0).infinite?.should eql(1)
+    Rational(3, 4).send(@method, 0.0).infinite?.should eql(1)
+    Rational(-3, -4).send(@method, 0.0).infinite?.should eql(1)
     
-    (Rational(-3, 4) / 0.0).infinite?.should eql(-1)
-    (Rational(3, -4) / 0.0).infinite?.should eql(-1)
+    Rational(-3, 4).send(@method, 0.0).infinite?.should eql(-1)
+    Rational(3, -4).send(@method, 0.0).infinite?.should eql(-1)
   end
 end
 
@@ -53,7 +53,7 @@ describe :rational_divide, :shared => true do
     obj = mock("Object")
     obj.should_receive(:coerce).with(rational).and_return([1, 2])
     
-    rational / obj
+    rational.send(@method, obj)
   end
 
   it "calls #/ on the coerced Rational with the coerced Object" do
@@ -67,6 +67,6 @@ describe :rational_divide, :shared => true do
     obj = mock("Object")
     obj.should_receive(:coerce).and_return([coerced_rational, coerced_obj])
 
-    (rational / obj).should == :result
+    rational.send(@method, obj).should == :result
   end
 end
