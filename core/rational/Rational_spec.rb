56dba75d55371f6a7b0db19e2cdf73646b8b448c
Run Paint Run Run <runrun@runpaint.org>
2009-06-17 17:59:54 +0100

    Rational(n,d): expanded 1.9 specification.

--- core/rational/Rational_spec.rb
+++ core/rational/Rational_spec.rb
@@ -1,8 +1,8 @@
 require File.dirname(__FILE__) + '/../../shared/rational/Rational'
 
 ruby_version_is "1.9" do
-  describe "Rational() passed Integer, Integer" do
-    it_behaves_like(:rational_rational_int_int, :Rational)
+  describe "Rational() passed two arguments" do
+    it_behaves_like(:rational_rational_two, :Rational)
   end
 
   describe "Rational() passed Integer" do
