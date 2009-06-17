3e6d463c8084f787033f58905d266726fbbb5e7e
Run Paint Run Run <runrun@runpaint.org>
2009-06-17 01:31:02 +0100

    Rational#truncate: Share spec; 1.9 moved Rational to core

--- library/rational/truncate_spec.rb
+++ library/rational/truncate_spec.rb
@@ -1,5 +1,7 @@
-require File.dirname(__FILE__) + '/../../spec_helper'
+require File.dirname(__FILE__) + '/../../shared/rational/truncate'
 
-describe "Rational#truncate" do
-  it "needs to be reviewed for spec completeness"
+ruby_version_is ""..."1.9" do
+  describe "Rational#truncate" do
+    it_behaves_like(:rational_truncate, :truncate)
+  end
 end
