eb4e67e41afb6c1dd5dd49bc357ff8d501aeab41
Run Paint Run Run <runrun@runpaint.org>
2009-06-17 21:21:17 +0100

    Rational#ceil: 1.8.6 choked on BigNum; give it simpler tests.

--- shared/rational/ceil.rb
+++ shared/rational/ceil.rb
@@ -23,10 +23,19 @@ ruby_version_is ""..."1.9" do
       Rational(19).ceil.should be_kind_of(Integer)
     end
 
-    it "returns the smallest integer >= self as an integer" do
-      Rational(200, 87).ceil.should == 3
-      Rational(6, 9).ceil.should == 1
-      Rational(686543**99).ceil.should == 686543**99
-    end  
+    ruby_version_is ""..."1.8.6" do
+      it "returns the smallest integer >= self as an integer" do
+        Rational(200, 87).ceil.should == 3
+        Rational(6, 9).ceil.should == 1
+      end  
+    end
+
+    ruby_version_is "1.8.6"..."" do
+      it "returns the smallest integer >= self as an integer" do
+        Rational(200, 87).ceil.should == 3
+        Rational(6, 9).ceil.should == 1
+        Rational(686543**99).ceil.should == 686543**99
+      end  
+    end
   end
 end
