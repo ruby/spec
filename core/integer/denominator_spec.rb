030b93c22bdced63539e5492834e3e6fafc2aef6
Run Paint Run Run <runrun@runpaint.org>
2009-06-17 21:39:57 +0100

    Integer#denominator: Specify 1.9 method.

--- core/integer/denominator_spec.rb
+++ core/integer/denominator_spec.rb
@@ -1,11 +1,22 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe "Integer#denominator" do
-  ruby_version_is "1.9" do
-    it "always returns 1" do
-      20.denominator.should == 1
-      0.denominator.should == 1
-      389877777262.denominator.should == 1
+ruby_version_is "1.9" do
+  describe "Integer#denominator" do
+    # The Numeric child classes override this method, so their behaviour is
+    # specified in the appropriate place
+    before(:each) do
+      @numbers = [
+        20,             # Integer
+        -2709,          # Negative Integer
+        99999999**99,   # Bignum
+        -99999**621,    # Negative BigNum
+        0,
+        1
+      ] 
     end
+
+    it "returns 1" do
+      @numbers.each {|number| number.denominator.should == 1}
+    end  
   end
 end
