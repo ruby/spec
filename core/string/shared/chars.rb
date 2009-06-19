ad61b9409c967059f3c24ba97bb6821ce6ce2b29
Run Paint Run Run <runrun@runpaint.org>
2009-06-19 06:34:53 +0100

    String#chars returns chars in self's encoding.

--- core/string/shared/chars.rb
+++ core/string/shared/chars.rb
@@ -31,6 +31,13 @@ describe :string_chars, :shared => true do
   end
   
   ruby_version_is "1.9" do
+    it "returns characters in the same encoding as self" do
+      "&%".force_encoding('Shift_JIS').chars.to_a
+        .all? {|c| c.encoding.name.should == 'Shift_JIS'}
+      "&%".encode('ASCII-8BIT').chars.to_a
+        .all? {|c| c.encoding.name.should == 'ASCII-8BIT'}
+    end
+
     it "works with multibyte characters" do
       s = "\u{8987}".force_encoding("UTF-8")
       s.bytesize.should == 3
