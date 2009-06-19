bfcde5465e5b46c34b0536f0831e5d87df82e25f
Run Paint Run Run <runrun@runpaint.org>
2009-06-19 06:01:05 +0100

    String#chars: Uses encoding to determine what a character is

--- core/string/each_char_spec.rb
+++ core/string/each_char_spec.rb
@@ -5,3 +5,4 @@ ruby_version_is '1.8.7' do
     it_behaves_like(:string_chars, :each_char)
   end
 end
+
--- core/string/shared/chars.rb
+++ core/string/shared/chars.rb
@@ -36,5 +36,14 @@ describe :string_chars, :shared => true do
       s.bytesize.should == 3
       s.send(@method).to_a.should == [s]
     end
+
+    it "uses the String's encoding to determine what characters it contains" do
+      s = "\u{287}"
+      s.force_encoding('UTF-8').chars.to_a.should == [s.force_encoding('UTF-8')]
+      s.force_encoding('BINARY').chars.to_a.should == [
+        "\xCA".force_encoding('BINARY'), "\x87".force_encoding('BINARY')]
+      s.force_encoding('SJIS').chars.to_a.should == [
+        "\xCA".force_encoding('SJIS'), "\x87".force_encoding('SJIS')]
+    end
   end
 end  
