04510c4779fe9356ef1a1dce247d4fbb3b4c9948
Run Paint Run Run <runrun@runpaint.org>
2009-06-21 12:46:28 +0100

    Encoding::InvalidByteSequenceError#destination...name: Specify

---
+++ core/encoding/invalid_byte_sequence_error/destination_encoding_name_spec.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__) + '/../fixtures/classes'
+
+ruby_version_is "1.9" do
+  describe "Encoding::InvalidByteSequenceError#destination_encoding_name" do
+    before(:each) do
+      @exception = EncodingSpecs::InvalidByteSequenceError.exception
+    end
+
+    it "returns a String" do
+      @exception.destination_encoding_name.should be_an_instance_of(String)
+    end
+
+    it "is equal to the destination encoding name of the object that raised it" do
+      @exception.destination_encoding_name.should == "ISO-8859-1"
+    end
+  end
+end
