fdef423451207b218623a8fc0f98de9ae6284867
Run Paint Run Run <runrun@runpaint.org>
2009-06-28 21:11:44 +0100

    Complex#abs2: Share btw. 1.8 (library) and 1.9 (core)

--- core/complex/abs2_spec.rb
+++ core/complex/abs2_spec.rb
@@ -1,7 +1,6 @@
 require File.dirname(__FILE__) + '/../../shared/complex/abs2'
-require 'complex'
 
-ruby_version_is ""..."1.9" do
+ruby_version_is "1.9" do
   describe "Complex#abs2" do
     it_behaves_like(:complex_abs2, :abs2)
   end
