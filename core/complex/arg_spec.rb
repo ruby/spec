f18a554f2f7634317edf99534e0975c6d9b61c19
Run Paint Run Run <runrun@runpaint.org>
2009-06-28 21:24:08 +0100

    Complex#arg: Share spec btw. 1.8 (lib) and 1.9 (core)

--- core/complex/arg_spec.rb
+++ core/complex/arg_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/../../shared/complex/arg'
 
-ruby_version_is ""..."1.9" do
+ruby_version_is "1.9" do
   describe "Complex#arg" do
     it_behaves_like(:complex_arg, :arg)
   end
