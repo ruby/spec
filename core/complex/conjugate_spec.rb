d27f01394114730382865696e2d2b67bbdd7c0a0
Run Paint Run Run <runrun@runpaint.org>
2009-06-28 22:10:14 +0100

    Complex#conjugate: Share btw. 1.8 (lib) and 1.9 (core)

--- core/complex/conjugate_spec.rb
+++ core/complex/conjugate_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/../../shared/complex/conjugate'
 
-ruby_version_is ""..."1.9" do
+ruby_version_is "1.9" do
   describe "Complex#conjugate" do
     it_behaves_like(:complex_conjugate, :conjugate)
   end
