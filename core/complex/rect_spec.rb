e5a3b04477fa253ed49cf70cb291e937f8284503
Run Paint Run Run <runrun@runpaint.org>
2009-06-30 21:03:15 +0100

    Complex: Ver. guard requires of shared specs for 1.9

--- core/complex/rect_spec.rb
+++ core/complex/rect_spec.rb
@@ -1,7 +1,8 @@
-require File.dirname(__FILE__) + '/../../shared/complex/rect'
-
 ruby_version_is "1.9" do
-  describe "Complex#rect" do
+  
+  require File.dirname(__FILE__) + '/../../shared/complex/rect'
+  
+  describ "Complex#rect" do
     it_behaves_like(:complex_rect, :rect)
   end
 end
