6f1d389d431587de25770fed4cb7911e6535347a
Run Paint Run Run <runrun@runpaint.org>
2009-07-01 22:20:39 +0100

    Complex#numerator: Share btw. 1.8 (lib) and 1.9 (core)

--- core/complex/numerator_spec.rb
+++ core/complex/numerator_spec.rb
@@ -1,11 +1,6 @@
 require File.dirname(__FILE__) + '/../../shared/complex/numerator'
 
-ruby_version_is ""..."1.9" do
-  
-  require 'complex'
-  # Depends on Rational to be loaded.
-  require "rational"
-
+ruby_version_is "1.9" do
   describe "Complex#numerator" do
     it_behaves_like(:complex_numerator, :numerator)
   end
