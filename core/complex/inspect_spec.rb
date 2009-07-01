11bba3be1d0bb55ef1975172c289d27d4fa9d3a5
Run Paint Run Run <runrun@runpaint.org>
2009-07-02 00:27:58 +0100

    Complex#inspect: Share btw. 1.8 (lib) and 1.9 (core)

--- core/complex/inspect_spec.rb
+++ core/complex/inspect_spec.rb
@@ -1,9 +1,6 @@
 require File.dirname(__FILE__) + '/../../shared/complex/inspect'
 
-ruby_version_is ""..."1.9" do
-
-  require 'complex'
-
+ruby_version_is "1.9" do
   describe "Complex#inspect" do
     it_behaves_like(:complex_inspect, :inspect)
   end
