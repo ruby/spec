91c1aa2904d4849d550ebe4bb8effb40fb63325c
Run Paint Run Run <runrun@runpaint.org>
2009-07-01 21:23:55 +0100

    Complex#real: Share btw. 1.8 (lib) and 1.9 (core)

--- core/complex/real_spec.rb
+++ core/complex/real_spec.rb
@@ -1,9 +1,7 @@
 require File.dirname(__FILE__) + '/../../shared/complex/real'
 
-ruby_version_is ""..."1.9" do
+ruby_version_is "1.9" do
   
-  require 'complex'
-
   describe "Complex#real" do
     it_behaves_like(:complex_real, :real)
   end
