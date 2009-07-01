1d07229669c9b745f03ff7007c0962757828d10a
Eloy Duran <eloy.de.enige@gmail.com>
2009-07-01 20:31:44 +0200

    Fixed bug in spec.

--- core/complex/rect_spec.rb
+++ core/complex/rect_spec.rb
@@ -1,8 +1,7 @@
 ruby_version_is "1.9" do
-  
   require File.dirname(__FILE__) + '/../../shared/complex/rect'
   
-  describ "Complex#rect" do
+  describe "Complex#rect" do
     it_behaves_like(:complex_rect, :rect)
   end
 end
