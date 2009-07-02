91f00d35e2b726c1ff73055e8dbd1593ed04fce5
Run Paint Run Run <runrun@runpaint.org>
2009-07-02 11:49:40 +0100

    Complex#imag: Share btw. 1.8 (lib) and 1.9 (core)

--- core/complex/imag_spec.rb
+++ core/complex/imag_spec.rb
@@ -1,9 +1,6 @@
 require File.dirname(__FILE__) + '/../../shared/complex/image'
 
-ruby_version_is ""..."1.9" do
-
-  require 'complex'
-
+ruby_version_is "1.9" do
   describe "Complex#imag" do
     it_behaves_like(:complex_image, :imag)
   end
