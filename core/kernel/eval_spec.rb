70e127a9d68ad264e5708431370b01d750b8614a
Run Paint Run Run <runrun@runpaint.org>
2009-07-08 23:28:23 +0100

    Kernel#eval on 1.9 no longer lets procs/bindings be nested "horribly"

--- core/kernel/eval_spec.rb
+++ core/kernel/eval_spec.rb
@@ -119,45 +119,47 @@ describe "Kernel#eval" do
     end
   end
 
-  it "allows Proc and binding to be nested in horrible ways" do
-    outer_binding = binding
-    proc_binding = eval("proc {l = 5; binding}.call", outer_binding)
-    inner_binding = eval("proc {k = 6; binding}.call", proc_binding)
+  ruby_version_is ""..."1.9" do
+    it "allows Proc and binding to be nested in horrible ways" do
+      outer_binding = binding
+      proc_binding = eval("proc {l = 5; binding}.call", outer_binding)
+      inner_binding = eval("proc {k = 6; binding}.call", proc_binding)
 
-    eval("w = 1")
-    eval("x = 2", outer_binding)
-    eval("yy = 3", proc_binding)
-    eval("z = 4", inner_binding)
+      eval("w = 1")
+      eval("x = 2", outer_binding)
+      eval("yy = 3", proc_binding)
+      eval("z = 4", inner_binding)
 
-    eval("w").should == 1
-    eval("w", outer_binding).should == 1
-    eval("w", proc_binding).should == 1
-    eval("w", inner_binding).should == 1
+      eval("w").should == 1
+      eval("w", outer_binding).should == 1
+      eval("w", proc_binding).should == 1
+      eval("w", inner_binding).should == 1
 
-    eval("x").should == 2
-    eval("x", outer_binding).should == 2
-    eval("x", proc_binding).should == 2
-    eval("x", inner_binding).should == 2
+      eval("x").should == 2
+      eval("x", outer_binding).should == 2
+      eval("x", proc_binding).should == 2
+      eval("x", inner_binding).should == 2
 
-    lambda { eval("yy") }.should raise_error(NameError)
-    lambda { eval("yy", outer_binding) }.should raise_error(NameError)
-    eval("yy", proc_binding).should == 3
-    eval("yy", inner_binding).should == 3
+      lambda { eval("yy") }.should raise_error(NameError)
+      lambda { eval("yy", outer_binding) }.should raise_error(NameError)
+      eval("yy", proc_binding).should == 3
+      eval("yy", inner_binding).should == 3
 
-    lambda { eval("z") }.should raise_error(NameError)
-    lambda { eval("z", outer_binding) }.should raise_error(NameError)
-    lambda { eval("z", proc_binding)  }.should raise_error(NameError)
-    eval("z", inner_binding).should == 4
+      lambda { eval("z") }.should raise_error(NameError)
+      lambda { eval("z", outer_binding) }.should raise_error(NameError)
+      lambda { eval("z", proc_binding)  }.should raise_error(NameError)
+      eval("z", inner_binding).should == 4
 
-    lambda { eval("l") }.should raise_error(NameError)
-    lambda { eval("l", outer_binding) }.should raise_error(NameError)
-    eval("l", proc_binding).should == 5
-    eval("l", inner_binding).should == 5
+      lambda { eval("l") }.should raise_error(NameError)
+      lambda { eval("l", outer_binding) }.should raise_error(NameError)
+      eval("l", proc_binding).should == 5
+      eval("l", inner_binding).should == 5
 
-    lambda { eval("k") }.should raise_error(NameError)
-    lambda { eval("k", outer_binding) }.should raise_error(NameError)
-    lambda { eval("k", proc_binding)  }.should raise_error(NameError)
-    eval("k", inner_binding).should == 6
+      lambda { eval("k") }.should raise_error(NameError)
+      lambda { eval("k", outer_binding) }.should raise_error(NameError)
+      lambda { eval("k", proc_binding)  }.should raise_error(NameError)
+      eval("k", inner_binding).should == 6
+    end
   end
 
   it "allows creating a new class in a binding" do
