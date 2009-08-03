03f227a06498760734b7376e776998c6c7058161
Run Paint Run Run <runrun@runpaint.org>
2009-08-03 13:21:51 +0100

    Enumerator.new: Factor out version-guards from shared spec

--- library/enumerator/new_spec.rb
+++ library/enumerator/new_spec.rb
@@ -5,6 +5,10 @@ ruby_version_is ""..."1.9" do
   require File.dirname(__FILE__) + '/../../shared/enumerator/new'
 
   describe "Enumerator.new" do
+    it "requires an argument" do
+      lambda {enumerator_class.new}.should raise_error(ArgumentError)
+    end
+
     it_behaves_like(:enum_new, :new)
   end
 end
