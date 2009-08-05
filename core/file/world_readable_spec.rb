8f04ba8c164df59615a270a14b714b723f200524
Run Paint Run Run <runrun@runpaint.org>
2009-08-05 02:47:52 +0100

    File::Stat.world_readable?: Share with File equivalent

--- core/file/world_readable_spec.rb
+++ core/file/world_readable_spec.rb
@@ -1,58 +1,14 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+require File.dirname(__FILE__) + '/../../shared/file/world_readable'
 
 ruby_version_is "1.9" do
   describe "File.world_readable?" do
-    
-    before(:each) do
-      @file = tmp('world-readable')
-      File.open(@file,'w') {|f| f.puts }
-    end
-    
-    after(:each) do
-      File.unlink(@file) if File.exists?(@file)
-    end
-
-    # These will surely fail on Windows.
-    it "returns nil if the file is chmod 600" do
-      File.chmod(0600, @file)
-      File.world_readable?(@file).should be_nil
-    end
-
-    it "returns nil if the file is chmod 000" do
-      File.chmod(0000, @file)
-      File.world_readable?(@file).should be_nil
-    end
-
-    it "returns nil if the file is chmod 700" do
-      File.chmod(0700, @file)
-      File.world_readable?(@file).should be_nil
-    end
-
-    # We don't specify what the Fixnum is because it's system dependent
-    it "returns a Fixnum if the file is chmod 644" do
-      File.chmod(0644, @file)
-      File.world_readable?(@file).should be_an_instance_of(Fixnum)
-    end
-
-    it "returns a Fixnum if the file is a directory and chmod 644" do
-      dir = rand().to_s + '-ww'
-      Dir.mkdir(dir)
-      Dir.exists?(dir).should be_true
-      File.chmod(0644, dir)
-      File.world_readable?(dir).should be_an_instance_of(Fixnum)
-      Dir.rmdir(dir)
-    end
+    it_behaves_like(:file_world_readable, :world_readable?, File)
 
     it "returns nil if the file does not exist" do
       file = rand.to_s + $$.to_s
       File.exists?(file).should be_false
       File.world_readable?(file).should be_nil
-    end
-
-    it "coerces the argument with #to_path" do
-      obj = mock('path')
-      obj.should_receive(:to_path).and_return(@file)
-      File.world_readable?(obj)
     end
   end
 end
