dcffcf61a7cfac1fb34faeae2aaa530dade0843f
Run Paint Run Run <runrun@runpaint.org>
2009-08-05 03:09:05 +0100

    File::Stat.world_readable?L Fix typo

--- core/file/stat/world_readable_spec.rb
+++ core/file/stat/world_readable_spec.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../../../shared/file/world_readable'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
 ruby_version_is "1.9" do
-  describe "File.world_readable?" do
+  describe "File::Stat.world_readable?" do
     it_behaves_like(:file_world_readable, :world_readable?, FileStat)
   end
 end
