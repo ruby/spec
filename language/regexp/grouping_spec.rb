b4d68482e18fd7b53576182e25f9000102c810df
Run Paint Run Run <runrun@runpaint.org>
2009-08-07 14:57:34 +0100

    Regexp grouping: Add some more examples

--- language/regexp/grouping_spec.rb
+++ language/regexp/grouping_spec.rb
@@ -2,8 +2,17 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.expand_path(File.dirname(__FILE__) + '/../fixtures/classes')
 
 describe "Regexps with grouping" do
-  it 'supports ()' do
+  it 'support ()' do
     /(a)/.match("a").to_a.should == ["a", "a"]
+  end
+
+  it "allow groups to be nested" do
+    md = /(hay(st)a)ck/.match('haystack')
+    md.to_a.should == ['haystack','haysta', 'st']
+  end
+
+  it "raise a SyntaxError when parentheses aren't balanced" do
+   lambda { eval "/(hay(st)ack/" }.should raise_error(SyntaxError)
   end
 
   it 'supports (?: ) (non-capturing group)' do
