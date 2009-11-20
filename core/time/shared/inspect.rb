282091bfb38f3e7a34a0afeeecf468e73a93d656
David Calavera <david.calavera@gmail.com>
2009-11-20 13:11:38 +0100

    downcasing Time format pattern

--- core/time/shared/inspect.rb
+++ core/time/shared/inspect.rb
@@ -12,13 +12,13 @@ describe :inspect, :shared => true do
   end
 
   ruby_version_is "1.9" do
-    it "formats the time following the pattern 'yyyy-MM-DD dd HH:mm:ss Z'" do
+    it "formats the time following the pattern 'yyyy-MM-dd HH:mm:ss Z'" do
       with_timezone("PST", +1) do
         Time.local("1", "15", "20", "1", "1", "2000", :ignored, :ignored, :ignored, :ignored).send(@method).should == "2000-01-01 20:15:01 +0100"
       end
     end
 
-    it "formats the UTC time following the pattern 'yyyy-MM-DD HH:mm:ss UTC'" do
+    it "formats the UTC time following the pattern 'yyyy-MM-dd HH:mm:ss UTC'" do
       Time.utc("1", "15", "20", "1", "1", "2000", :ignored, :ignored, :ignored, :ignored).send(@method).should == "2000-01-01 20:15:01 UTC"
     end
   end
