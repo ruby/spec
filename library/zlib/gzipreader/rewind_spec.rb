912bb5930493c3ae89649f7e703658cbcb727e3a
Vladimir Sizikov <vsizikov@gmail.com>
2009-12-30 22:20:51 +0100

    Tweaks to GzipReader#rewind specs.

--- library/zlib/gzipreader/rewind_spec.rb
+++ library/zlib/gzipreader/rewind_spec.rb
@@ -8,6 +8,7 @@ describe "GzipReader#rewind" do
     @data = '12345abcde'
     @zip = "\037\213\b\000,\334\321G\000\00334261MLJNI\005\000\235\005\000$\n\000\000\000"
     @io = StringIO.new @zip
+    ScratchPad.clear
   end
 
   it "resets the position of the stream pointer" do
@@ -17,6 +18,7 @@ describe "GzipReader#rewind" do
 
     gz.rewind
     gz.pos.should == 0
+    gz.lineno.should == 0
   end
 
   it "resets the position of the stream pointer to data previously read" do
@@ -30,10 +32,16 @@ describe "GzipReader#rewind" do
     # first, prepare the mock object:
     (obj = mock("io")).should_receive(:get_io).any_number_of_times.and_return(@io)
     def obj.read(args); get_io.read(args); end
-    obj.should_receive(:seek).and_return(0)
+    def obj.seek(pos, whence = 0)
+      ScratchPad.record :seek
+      get_io.seek(pos, whence)
+    end
 
     gz = Zlib::GzipReader.new(obj)
     gz.rewind()
+
+    ScratchPad.recorded.should == :seek
     gz.pos.should == 0
+    gz.read.should == "12345abcde"
   end
 end
