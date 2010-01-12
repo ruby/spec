9319d167f088d7f52eb2a0685542203699aac4a3
ujihisa <ujihisa@gmail.com>
2010-01-12 14:10:44 +0900

    Eliminates useless spaces

--- core/math/lgamma_spec.rb
+++ core/math/lgamma_spec.rb
@@ -14,7 +14,7 @@ ruby_version_is "1.9" do
       lg1 = Math.lgamma(0.5)
       lg1[0].should be_close(Math.log(Math.sqrt(Math::PI)), TOLERANCE)
       lg1[1].should == 1
-      
+
       lg2 = Math.lgamma(6.0)
       lg2[0].should be_close(Math.log(120.0), TOLERANCE)
       lg2[1].should == 1
@@ -24,21 +24,20 @@ ruby_version_is "1.9" do
       lg1 = Math.lgamma(-0.5)
       lg1[0].should be_close(1.2655121, TOLERANCE)
       lg1[1].should == -1
-      
+
       lg2 = Math.lgamma(-1.5)
       lg2[0].should be_close(0.8600470, TOLERANCE)
       lg2[1].should == 1
     end
-    
+
     it "returns correct values given +/- infinity" do
       Math.lgamma( 1.0/0).should == [1.0/0, 1]
       Math.lgamma(-1.0/0).should == [1.0/0, 1]
     end
-    
+
     it "returns correct value given NaN" do
       Math.lgamma(0.0/0)[0].nan?.should be_true
       Math.lgamma(0.0/0)[1].should == 1
     end
-
   end
 end
