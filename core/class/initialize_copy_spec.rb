b37fd1a2b201e9d0bdf36d7b83756cc9640bda6d
Vladimir Sizikov <vsizikov@gmail.com>
2010-01-13 10:47:13 +0100

    Fix Class#initialize_copy spec to avoid SystemStackError: stack level too deep

--- core/class/initialize_copy_spec.rb
+++ core/class/initialize_copy_spec.rb
@@ -1,22 +1,22 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-it "needs to be reviewed for completeness"
-
-it "raises a TypeError when called on already initialized classes" do
-  lambda{
-    String.send :initialize_copy, Fixnum
-  }.should raise_error(TypeError)
-
-  lambda{
-    Object.send :initialize_copy, String
-  }.should raise_error(TypeError)
-end
-
-ruby_version_is "1.9" do
-  # See [redmine:2601]
-  it "raises a TypeError when called on already initialized classes (even BasicObject)" do
+describe "Class#initialize_copy" do
+  it "raises a TypeError when called on already initialized classes" do
     lambda{
-      BasicObject.send :initialize_copy, String
+      String.send :initialize_copy, Fixnum
     }.should raise_error(TypeError)
+
+    lambda{
+      Object.send :initialize_copy, String
+    }.should raise_error(TypeError)
+  end
+
+  ruby_version_is "1.9" do
+    # See [redmine:2601]
+    it "raises a TypeError when called on BasicObject" do
+      lambda{
+        BasicObject.send :initialize_copy, String
+      }.should raise_error(TypeError)
+    end
   end
 end
