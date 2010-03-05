9f8c5cbc61b34515653148fd14cadaa0aec63d9a
Vladimir Sizikov <vsizikov@gmail.com>
2010-03-05 09:28:57 +0100

    Version guards for IO#lines spec.

--- core/io/lines_spec.rb
+++ core/io/lines_spec.rb
@@ -22,10 +22,18 @@ ruby_version_is '1.8.7' do
       enum.first.should == IOSpecs.lines[0]
     end
 
-    it "yields each line to the passed block" do
-      ScratchPad.record []
-      @io.lines { |s| ScratchPad << s }
-      ScratchPad.recorded.should == IOSpecs.lines
+    ruby_version_is ''...'1.9' do
+      it "ignores a given block" do
+        @io.lines{ raise "oups" }.should be_an_instance_of(enumerator_class)
+      end
+    end
+
+    ruby_version_is '1.9' do
+      it "yields each line to the passed block" do
+        ScratchPad.record []
+        @io.lines { |s| ScratchPad << s }
+        ScratchPad.recorded.should == IOSpecs.lines
+      end
     end
   end
 end
