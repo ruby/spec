8ec7489322bb924a475f986a45ecf07201155c0b
Yuki Sonoda (Yugui) <yugui@yugui.jp>
2010-03-10 20:52:07 +0900

    an alias Fixnum#maginitude was added.

--- core/fixnum/magnitude_spec.rb
+++ core/fixnum/magnitude_spec.rb
@@ -1,7 +1,8 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../shared/abs', __FILE__)
 
-describe "Fixnum#abs" do
-  it_behaves_like :fixnum_abs, :abs
+ruby_version_is '1.9' do
+  describe "Fixnum#magnitude" do
+    it_behaves_like :fixnum_abs, :magnitude
+  end
 end
-
