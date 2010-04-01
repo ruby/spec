0a8e93ad7b8e01aa0fa63a260eb6f7d25282e769
Marc-Andre Lafortune <github@marc-andre.ca>
2010-04-01 13:06:33 -0400

    lib/matrix: New method #real

--- library/matrix/real_spec.rb
+++ library/matrix/real_spec.rb
@@ -17,4 +17,16 @@ ruby_version_is "1.9" do
       Matrix[ [Complex(1,0), 2], [3, 4] ].real?.should be_false
     end
   end
+
+  describe "Matrix#real" do
+    it "returns a matrix with the real part of the elements of the receiver" do
+      Matrix[ [1,   2], [3, 4] ].real.should == Matrix[ [1,   2], [3, 4] ]
+      Matrix[ [1.9, Complex(1,1)], [Complex(-0.42, 0), 4] ].real.should == Matrix[ [1.9, 1], [-0.42, 4] ]
+    end
+
+    it "returns empty matrices on the same size if empty" do
+      Matrix.empty(0, 3).real.should == Matrix.empty(0, 3)
+      Matrix.empty(3, 0).real.should == Matrix.empty(3, 0)
+    end
+  end
 end
