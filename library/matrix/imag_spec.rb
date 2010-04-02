dfd69895bd4a46a9437a4ef3a3a29353b0f01c1a
Hiro Asari <asari.ruby@gmail.com>
2010-04-02 00:53:02 -0500

    Flesh out Matrix#rectangular specs and guard 1.9-only methods.

--- library/matrix/imag_spec.rb
+++ library/matrix/imag_spec.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../shared/imaginary', __FILE__)
 
-describe "Matrix#imag" do
-  it_behaves_like(:matrix_imaginary, :imag)
+ruby_version_is "1.9" do
+  describe "Matrix#imag" do
+    it_behaves_like(:matrix_imaginary, :imag)
+  end
 end
