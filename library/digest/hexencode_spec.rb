dce818522bf12edce0b8f492f677bee039ad75e3
Brian Ford <bford@engineyard.com>
2010-04-12 08:01:27 -0700

    Fixes for 77a22a8.
    Notes:
    * Use one example block for each facet of behavior. In this case,
      hexencoding a blank string is a boundary case. It needs its own
      spec.
    * Try to use complete grammar whenever possible. The articles 'a',
      'an', 'the' are words, too.
    * Use separate example blocks for types of values that cause TypeError
      exceptions. The nil value is different than a random Fixnum.
    
    Besides making the specs more explicit by separating the facets of
    behavior, it also makes the specs more useful. An error handling
    the empty string would mask whether a non-empty string was encoded
    correctly. Failure to deal with nil would mask whether Fixnums are
    handled correctly. This sort of conflation in the specs makes them
    more difficult to use.

--- library/digest/hexencode_spec.rb
+++ library/digest/hexencode_spec.rb
@@ -7,19 +7,25 @@ describe "Digest.hexencode" do
     @encoded  = "73616d706c6520737472696e67"
   end
 
-  it "returns a hex-encoded version of a string" do
+  it "returns '' when passed an empty String" do
     Digest.hexencode('').should == ''
+  end
+
+  it "returns the hex-encoded value of a non-empty String" do
     Digest.hexencode(@string).should == @encoded
   end
 
-  it "returns a hex-encoded version of an object's to_str return value" do
+  it "calls #to_str on an object and returns the hex-encoded value of the result" do
     obj = mock("to_str")
     obj.should_receive(:to_str).and_return(@string)
     Digest.hexencode(obj).should == @encoded
   end
 
-  it "raises TypeError for objects not coercible into String" do
+  it "raises a TypeError when passed nil" do
     lambda { Digest.hexencode(nil) }.should raise_error(TypeError)
+  end
+
+  it "raises a TypeError when passed a Fixnum" do
     lambda { Digest.hexencode(9001) }.should raise_error(TypeError)
   end
 end
