aa134dafeb75d6de6aa76b21c80076b70de80d13
Marc-Andre Lafortune <github@marc-andre.ca>
2010-04-12 22:51:09 -0400

    Marshal: Use the right fixtures for 1.9

--- core/marshal/load_spec.rb
+++ core/marshal/load_spec.rb
@@ -327,12 +327,22 @@ describe "Marshal::load" do
     end
   end
 
-  MarshalSpec::DATA.each do |description, (object, marshal, attributes)|
-    it "loads a #{description}" do
-      Marshal.load(marshal).should == object
+  ruby_version_is ""..."1.9" do
+    MarshalSpec::DATA.each do |description, (object, marshal, attributes)|
+      it "loads a #{description}" do
+        Marshal.load(marshal).should == object
+      end
     end
   end
-  
+
+  ruby_version_is "1.9" do
+    MarshalSpec::DATA_19.each do |description, (object, marshal, attributes)|
+      it "loads a #{description}" do
+        Marshal.load(marshal).should == object
+      end
+    end
+  end
+
   it "returns an untainted object if source is untainted" do
     x = Object.new
     y = Marshal.load(Marshal.dump(x))
