972f054261ed611c132e698f8ee34ba282a6dffb
Brian Ford <bford@engineyard.com>
2010-05-25 16:14:58 -0700

    Use dev_null() helper.

--- command_line/error_message_spec.rb
+++ command_line/error_message_spec.rb
@@ -1,19 +1,11 @@
 require File.expand_path('../../spec_helper', __FILE__)
 
 describe "The error message caused by an exception" do
-
-  before :all do
-    @devnull = "/dev/null"
-    platform_is :windows do
-      @devnull = "NUL"
-    end
-  end
-
   it "is not printed to stdout" do
-    out = ruby_exe("this_does_not_exist", :args => "2> #{@devnull}")
+    out = ruby_exe("this_does_not_exist", :args => "2> #{dev_null()}")
     out.chomp.empty?.should == true
 
-    out = ruby_exe("end #syntax error", :args => "2> #{@devnull}")
+    out = ruby_exe("end #syntax error", :args => "2> #{dev_null()}")
     out.chomp.empty?.should == true
   end
 end
