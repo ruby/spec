11507ff437bbdf0bed8ee432e11517784d9f2c3a
Marc-Andre Lafortune <github@marc-andre.ca>
2010-05-30 12:15:53 -0400

    library/delegate/delegator: Spec Delegator instead of SimpleDelegator.
    Fix whitespace too

--- library/delegate/fixtures/classes.rb
+++ library/delegate/fixtures/classes.rb
@@ -1,14 +1,15 @@
+require 'delegate'
 module DelegateSpecs
   class Simple
     def pub
       :foo
     end
-    
+
     def respond_to_missing?(method, priv=false)
       method == :pub_too ||
         (priv && method == :priv_too)
     end
-    
+
     def method_missing(method, *args)
       super unless respond_to_missing?(method, true)
       method
@@ -19,6 +20,20 @@ module DelegateSpecs
       [:priv, arg]
     end
     private :priv
-    
+
+  end
+
+  class Delegator < ::Delegator
+    attr_reader :__getobj__
+    def __setobj__(o)
+      @__getobj__ = o
+    end
+
+    # Needed for 1.8.x compatibility
+    def initialize(obj)
+      super
+      __setobj__(obj)
+    end
+    include Extra
   end
 end
