4b85dcc38d70b2b5b10fd12a751727006208746a
Tim Felgentreff <timfelgentreff@gmail.com>
2010-09-11 11:57:19 +0200

    add capi spec rb_yield_splat

--- optional/capi/ext/rubyspec.h
+++ optional/capi/ext/rubyspec.h
@@ -160,6 +160,7 @@
 #define HAVE_RB_WARN                       1
 #define HAVE_RB_YIELD                      1
 #define HAVE_RB_YIELD_VALUES               1
+#define HAVE_RB_YIELD_SPLAT                1
 /* GC */
 #define HAVE_RB_GC_REGISTER_ADDRESS        1
 
