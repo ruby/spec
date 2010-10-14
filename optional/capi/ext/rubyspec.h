c2ea787cabf0722cd254dfc3d3f303506150c1f3
Evan Phoenix <ephoenix@engineyard.com>
2010-10-14 16:03:12 -0700

    Add spec for rb_exec_recursive

--- optional/capi/ext/rubyspec.h
+++ optional/capi/ext/rubyspec.h
@@ -164,6 +164,7 @@
 #define HAVE_RB_BLOCK_PROC                 1
 #define HAVE_RB_ENSURE                     1
 #define HAVE_RB_EVAL_STRING                1
+#define HAVE_RB_EXEC_RECURSIVE             1
 #define HAVE_RB_RAISE                      1
 #define HAVE_RB_THROW                      1
 #define HAVE_RB_RESCUE                     1
@@ -173,6 +174,7 @@
 #define HAVE_RB_YIELD                      1
 #define HAVE_RB_YIELD_VALUES               1
 #define HAVE_RB_YIELD_SPLAT                1
+
 /* GC */
 #define HAVE_RB_GC_REGISTER_ADDRESS        1
 
