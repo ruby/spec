bc0248d34a07a40ba4c89f608a8605d2943eeadc
Brian Ford <brixen@gmail.com>
2011-01-26 17:00:30 -0800

    More Proc spec cleanups.

--- core/proc/fixtures/source_location.rb
+++ core/proc/fixtures/source_location.rb
@@ -1,15 +1,4 @@
 module ProcSpecs
-  def self.new_proc_in_method
-    Proc.new
-  end
-
-  class ProcSubclass < Proc
-  end
-
-  def self.new_proc_subclass_in_method
-    ProcSubclass.new
-  end
-
   class SourceLocation
     def self.my_proc
       proc { true }
@@ -58,17 +47,5 @@ module ProcSpecs
       body = Proc.new { true }
       Proc.new &body
     end
-  end
-
-  class MyProc < Proc
-  end
-
-  class MyProc2 < Proc
-    def initialize(a, b)
-      @first = a
-      @second = b
-    end
-
-    attr_reader :first, :second
   end
 end
