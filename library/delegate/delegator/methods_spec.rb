9302b66a67052fd438bbb6063c8f291c78d8264c
Julik <me@julik.nl>
2011-06-14 01:07:26 +0200

    Ensure that Delegator#methods accepts the optional argument
    And that this method returns the singleton methods of the instance

--- library/delegate/delegator/methods_spec.rb
+++ library/delegate/delegator/methods_spec.rb
@@ -4,11 +4,22 @@ require File.expand_path('../../fixtures/classes', __FILE__)
 describe "Delegator#methods" do
   before :all do
     @simple = DelegateSpecs::Simple.new
+    class << @simple
+      def singleton_method
+      end
+    end
+    
     @delegate = DelegateSpecs::Delegator.new(@simple)
     @methods = @delegate.methods
+    @singleton_methods = @delegate.methods(false)
   end
 
   ruby_version_is ""..."1.9" do
+    it "have one method in the singleton version return of methods" do
+      @singleton_methods.length.should equal 1
+      @singleton_methods.should include "singleton_method"
+    end
+    
     it "includes all public methods of the delegate object" do
       @methods.should include "pub"
     end
