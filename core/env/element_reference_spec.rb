2af135c5e8002d827745d2b67d22816c498c384f
NARUSE, Yui <naruse@airemix.jp>
2011-06-15 15:17:42 +0900

    Skip when ENV is masked.
    For example chroot or sudo.

--- core/env/element_reference_spec.rb
+++ core/env/element_reference_spec.rb
@@ -9,14 +9,17 @@ describe "ENV.[]" do
   end
 
   it "returns the specified environment variable" do
+    skip "ENV[#{@variable_name}] is masked" unless ENV[@variable_name]
     ENV[@variable_name].should == username
   end
 
   it "returns nil if the variable isn't found" do
+    skip "ENV[#{@variable_name}] is masked" unless ENV[@variable_name]
     ENV["this_var_is_never_set"].should == nil
   end
 
   it "returns only frozen values" do
+    skip "ENV[#{@variable_name}] is masked" unless ENV[@variable_name]
     ENV[@variable_name].frozen?.should == true
     ENV["returns_only_frozen_values"] = "a non-frozen string"
     ENV["returns_only_frozen_values"].frozen?.should == true
@@ -24,6 +27,7 @@ describe "ENV.[]" do
 
   ruby_version_is "1.9" do
     it "uses the locale encoding" do
+      skip "ENV[#{@variable_name}] is masked" unless ENV[@variable_name]
       ENV[@variable_name].encoding.should == Encoding.find('locale')
       ENV["uses_the_locale_encoding"] = "a binary string".force_encoding('binary')
       ENV["uses_the_locale_encoding"].encoding.should == Encoding.find('locale')
