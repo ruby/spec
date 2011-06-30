2d2c3a1d6c72dc80b23ee0548bc7020a4d51ef80
Ian MacLeod <ian@nevir.net>
2011-06-29 19:54:32 -0700

    Test hanging commas in Hashes

--- language/hash_spec.rb
+++ language/hash_spec.rb
@@ -44,4 +44,10 @@ describe "Hash literal" do
     h.keys.size.should == 1
     h.should == {:foo => :foo}
   end
+
+  it "accepts a hanging comma" do
+    h = {:a => 1, :b => 2,}
+    h.size.should == 2
+    h.should == {:a => 1, :b => 2}
+  end
 end
