224843801e9d15fa0dfeb051d4286449727ac42e
Marc-Andre Lafortune <github@marc-andre.ca>
2011-08-30 22:16:21 -0400

    Float specs: let's use `eql` instead of `==` when comparing with integers

--- core/float/divmod_spec.rb
+++ core/float/divmod_spec.rb
@@ -3,17 +3,17 @@ require File.expand_path('../../../spec_helper', __FILE__)
 describe "Float#divmod" do
   it "returns an [quotient, modulus] from dividing self by other" do
     values = 3.14.divmod(2)
-    values[0].should == 1
+    values[0].should eql(1)
     values[1].should be_close(1.14, TOLERANCE)
     values = 2.8284.divmod(3.1415)
-    values[0].should == 0
+    values[0].should eql(0)
     values[1].should be_close(2.8284, TOLERANCE)
     values = -1.0.divmod(bignum_value)
-    values[0].should == -1
+    values[0].should eql(-1)
     values[1].should be_close(9223372036854775808.000, TOLERANCE)
     values = -1.0.divmod(1)
-    values[0].should == -1
-    values[1].should == 0
+    values[0].should eql(-1)
+    values[1].should eql(0.0)
   end
 
   # Behaviour established as correct in r23953
