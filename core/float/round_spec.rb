f2108ba455dd3bb62bd6436db922ad8b12610e2b
Marc-Andre Lafortune <github@marc-andre.ca>
2011-08-31 00:24:52 -0400

    Float#round: Specs for Infinities and NaN

--- core/float/round_spec.rb
+++ core/float/round_spec.rb
@@ -9,6 +9,12 @@ describe "Float#round" do
     0.49999999999999994.round.should eql(0) # see http://jira.codehaus.org/browse/JRUBY-5048
   end
 
+  it "raises FloatDomainError for exceptional values" do
+    lambda { (+1.0/0).round }.should raise_error(FloatDomainError)
+    lambda { (-1.0/0).round }.should raise_error(FloatDomainError)
+    lambda { ( 0.0/0).round }.should raise_error(FloatDomainError)
+  end
+
   ruby_version_is "1.9" do
     it "rounds self to an optionally given precision" do
       5.5.round(0).should eql(6)
@@ -23,6 +29,21 @@ describe "Float#round" do
     it "raises a TypeError when its argument can not be converted to an Integer" do
       lambda { 1.0.round("4") }.should raise_error(TypeError)
       lambda { 1.0.round(nil) }.should raise_error(TypeError)
+    end
+
+    it "raises FloatDomainError for exceptional values when passed a non-positive precision" do
+      lambda { Float::INFINITY.round( 0) }.should raise_error(FloatDomainError)
+      lambda { Float::INFINITY.round(-2) }.should raise_error(FloatDomainError)
+      lambda { (-Float::INFINITY).round( 0) }.should raise_error(FloatDomainError)
+      lambda { (-Float::INFINITY).round(-2) }.should raise_error(FloatDomainError)
+      lambda { Float::NAN.round(0) }.should raise_error(RangeError)
+      lambda { Float::NAN.round(-2) }.should raise_error(RangeError)
+    end
+
+    it "returns self for exceptional values when passed a non-negative precision" do
+      Float::INFINITY.round(2).should == Float::INFINITY
+      (-Float::INFINITY).round(2).should == -Float::INFINITY
+      Float::NAN.round(2) == Float::NAN
     end
 
     ruby_bug "redmine:5227",  "1.9.2" do
