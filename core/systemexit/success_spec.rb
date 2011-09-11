72dd71e3780b246fd7f24a8da2fd31789eb2cb25
Brian Ford <brixen@gmail.com>
2011-09-11 15:22:43 -0700

    Clarify description string for SystemExit#success?.

--- core/systemexit/success_spec.rb
+++ core/systemexit/success_spec.rb
@@ -1,12 +1,12 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
 describe "SystemExit#success?" do
-  it "is successful when the status is 0" do
+  it "returns true when the status is 0" do
     s = SystemExit.new 0
     s.success?.should == true
   end
 
-  it "is not successful when the status is not 0" do
+  it "returns false when the status is not 0" do
     s = SystemExit.new 1
     s.success?.should == false
   end
