704d1e67af2df7d9a53260de5e9e419a2fa7bb99
NARUSE, Yui <naruse@airemix.jp>
2011-09-15 18:10:51 +0900

    Add the missing 2nd parameter.

--- library/socket/tcpsocket/shared/new.rb
+++ library/socket/tcpsocket/shared/new.rb
@@ -33,7 +33,7 @@ describe :tcpsocket_new, :shared => true do
     end
 
     it "silently ignores 'nil' as the third parameter" do
-      @socket = TCPSocket.send(@method, @hostname, nil)
+      @socket = TCPSocket.send(@method, @hostname, SocketSpecs.port, nil)
       @socket.should be_an_instance_of(TCPSocket)
     end
 
