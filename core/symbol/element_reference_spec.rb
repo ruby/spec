ba0881b9570a72661548d75aa9cf6ea76554eabc
Ian Leitch <port001@gmail.com>
2011-10-27 16:59:07 +0100

    Add specs for Symbol#[] under 1.9

--- core/symbol/element_reference_spec.rb
+++ core/symbol/element_reference_spec.rb
@@ -1,7 +1,29 @@
 require File.expand_path('../../../spec_helper', __FILE__)
+require File.expand_path('../fixtures/classes.rb', __FILE__)
+require File.expand_path('../shared/slice.rb', __FILE__)
 
 ruby_version_is "1.9" do
   describe "Symbol#[]" do
-    it "needs to be reviewed for spec completeness"
+    it_behaves_like :symbol_slice, :[]
+  end
+
+  describe "Symbol#[] with index, length" do
+    it_behaves_like :symbol_slice_index_length, :[]
+  end
+
+  describe "Symbol#[] with Range" do
+    it_behaves_like :symbol_slice_range, :[]
+  end
+
+  describe "Symbol#[] with Regexp" do
+    it_behaves_like :symbol_slice_regexp, :[]
+  end
+
+  describe "Symbol#[] with Regexp, index" do
+    it_behaves_like :symbol_slice_regexp_index, :[]
+  end
+
+  describe "Symbol#[] with String" do
+    it_behaves_like :symbol_slice_string, :[]
   end
 end
