7934946a9d616ab1a496ec37a0cf84f6c7057826
Marc-Andre Lafortune <github@marc-andre.ca>
2012-03-28 16:33:18 -0400

    Array#values_at: fix spec for return class

--- core/array/values_at_spec.rb
+++ core/array/values_at_spec.rb
@@ -42,7 +42,7 @@ describe "Array#values_at" do
   end
 
   it "does not return subclass instance on Array subclasses" do
-    ArraySpecs::MyArray[1, 2, 3].values_at(0, 1..2, 1).should be_kind_of(Array)
+    ArraySpecs::MyArray[1, 2, 3].values_at(0, 1..2, 1).should be_an_instance_of(Array)
   end
 
   ruby_bug "#6203", "1.9.3" do
