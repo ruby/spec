558a40edd5cd43a503641238cabe9f481ce9a723
Marc-Andre Lafortune <github@marc-andre.ca>
2012-03-28 16:05:54 -0400

    Fix erroneous specs for Array#values_at [Bug #6203]

--- core/array/values_at_spec.rb
+++ core/array/values_at_spec.rb
@@ -14,7 +14,7 @@ describe "Array#values_at" do
   end
 
   it "returns an array of elements in the ranges when passes ranges" do
-    [1, 2, 3, 4, 5].values_at(0..2, 1...3, 4..6).should == [1, 2, 3, 2, 3, 5, nil]
+    [1, 2, 3, 4, 5].values_at(0..2, 1...3, 2..-2).should == [1, 2, 3, 2, 3, 3, 4]
     [1, 2, 3, 4, 5].values_at(6..4).should == []
   end
 
@@ -43,5 +43,11 @@ describe "Array#values_at" do
 
   it "does not return subclass instance on Array subclasses" do
     ArraySpecs::MyArray[1, 2, 3].values_at(0, 1..2, 1).should be_kind_of(Array)
+  end
+
+  ruby_bug "#6203", "1.9.3" do
+    it "ignores outof range end of ranges" do
+      [1, 2, 3, 4, 5].values_at(3..41, 2..-10, 3...42).should == [4, 5, 4, 5]
+    end
   end
 end
