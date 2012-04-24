b570b377a078eaa6e34fca73ce3325f78d747c22
NARUSE, Yui <naruse@airemix.jp>
2012-04-24 17:24:10 +0900

    Ruby 2.0 removes ext/iconv. r35444 [Feature #6322]

--- library/iconv/new_spec.rb
+++ library/iconv/new_spec.rb
@@ -2,6 +2,8 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../shared/new', __FILE__)
 require 'iconv'
 
-describe "Iconv.new" do
-  it_behaves_like :iconv_new, :new
+ruby_version_is ''...'2.0' do 
+  describe "Iconv.new" do
+    it_behaves_like :iconv_new, :new
+  end
 end
