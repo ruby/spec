3d8bf67d01a6d503e8540c09579e9a02728f0775
Benoit Daloze <eregontp@gmail.com>
2012-07-05 14:59:03 +0200

    File.readlink: fix description of example to be clearer

--- core/file/readlink_spec.rb
+++ core/file/readlink_spec.rb
@@ -31,7 +31,7 @@ describe "File.readlink" do
         lambda { File.readlink("/this/surely/doesnt/exist") }.should raise_error(Errno::ENOENT)
       end
 
-      it "raises an Errno::EINVAL if called with an invalid argument" do
+      it "raises an Errno::EINVAL if called with a normal file" do
         touch @file
         lambda { File.readlink(@file) }.should raise_error(Errno::EINVAL)
       end
