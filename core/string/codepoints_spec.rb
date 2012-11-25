e88d23364a752a0ef2bfb4f2e766f1cfd8472006
NARUSE, Yui <naruse@airemix.jp>
2012-11-25 16:49:20 +0900

    MRI r37838 changes String#chars to return array
    bytes, codepoints, and lines are also changed [Feature #6670]

--- core/string/codepoints_spec.rb
+++ core/string/codepoints_spec.rb
@@ -2,7 +2,9 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../shared/codepoints', __FILE__)
 
 with_feature :encoding do
-  describe "String#codepoints" do
-    it_behaves_like(:string_codepoints, :codepoints)
+  ruby_version_is '1.8.7'...'2.0' do
+    describe "String#codepoints" do
+      it_behaves_like(:string_codepoints, :codepoints)
+    end
   end
 end
