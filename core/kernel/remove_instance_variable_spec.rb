4c779f799551b506331f3bd198e15517f5e31b8c
Shugo Maeda <shugo@ruby-lang.org>
2013-01-10 22:03:11 +0900

    Kernel#remove_instance_variable is public in Ruby 2.0.

--- core/kernel/remove_instance_variable_spec.rb
+++ core/kernel/remove_instance_variable_spec.rb
@@ -2,8 +2,16 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../fixtures/classes', __FILE__)
 
 describe "Kernel#remove_instance_variable" do
-  it "is a private method" do
-    Kernel.should have_private_instance_method(:remove_instance_variable)
+  ruby_version_is "" ... "2.0" do
+    it "is a private method" do
+      Kernel.should have_private_instance_method(:remove_instance_variable)
+    end
+  end
+
+  ruby_version_is "2.0" do
+    it "is a public method" do
+      Kernel.should have_public_instance_method(:remove_instance_variable)
+    end
   end
 
   it "removes an ivar of a given name and return it's value" do
