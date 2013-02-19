9881ff3b1f43dd8d04252b41465f5ab55fdc4fa9
Brian Shirai <brixen@gmail.com>
2013-02-19 10:57:57 -0800

    Revert "Specify correct version guards"
    This reverts commit dc5ef4f63c7360aa339d2e31b6607f2ceebc665f.

--- core/module/class_exec_spec.rb
+++ core/module/class_exec_spec.rb
@@ -2,8 +2,6 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../fixtures/classes', __FILE__)
 require File.expand_path('../shared/class_exec', __FILE__)
 
-ruby_version_is "1.8.7" do
-  describe "Module#class_exec" do
-    it_behaves_like :module_class_exec, :class_exec
-  end
+describe "Module#class_exec" do
+  it_behaves_like :module_class_exec, :class_exec
 end
--- core/module/module_exec_spec.rb
+++ core/module/module_exec_spec.rb
@@ -2,8 +2,6 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../fixtures/classes', __FILE__)
 require File.expand_path('../shared/class_exec', __FILE__)
 
-ruby_version_is "1.8.7" do
-  describe "Module#module_exec" do
-    it_behaves_like :module_class_exec, :module_exec
-  end
+describe "Module#module_exec" do
+  it_behaves_like :module_class_exec, :module_exec
 end
