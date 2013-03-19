c02c78b3899fcf769084a88777c63de0fcebb48d
NARUSE, Yui <naruse@airemix.jp>
2013-03-20 04:15:03 +0900

    Explicitly close file objects on fdopen
    IO.open(fd) creates an IO object from fd, but if the fd is already used
    by another IO object, the original object can't know the fd is used by
    the new object. Therefore if new one close the fd, old one usually happens
    to get EBADF.
    
    On worse case the fd is recycled by yet another independent IO object and
    old one can close the fd without trouble. But the victim IO object hit EBADF
    because the fd is already closed. Following is such case and it is
    closed in lazy sweep.
    http://c5632.rubyci.org/~chkbuild/ruby-trunk/log/20130316T050302Z.diff.html.gz

--- core/file/reopen_spec.rb
+++ core/file/reopen_spec.rb
@@ -3,25 +3,34 @@ require File.expand_path('../../../spec_helper', __FILE__)
 ruby_version_is "1.9" do
   describe "File#reopen" do
     before :each do
-      @file = tmp('test.txt')
-      @fh = nil
+      @file = tmp('core-file-reopen.txt')
       touch(@file) { |f| f << "1234567890" }
     end
 
     after :each do
-      @fh.close if @fh
       rm_r @file
     end
 
     it "resets the stream to a new file path" do
-      @fh = File.new(@file)
-      text = @fh.read
-      @fh = @fh.reopen(@file, "r")
-      @fh.read.should == text
+      begin
+        fh = File.new(@file)
+        text = fh.read
+        fh2 = fh.reopen(@file, "r")
+        fh.read.should == text
+      ensure
+        fh.close rescue nil
+        fh2.close rescue nil
+      end
     end
 
     it "accepts an object that has a #to_path method" do
-      @fh = File.new(@file).reopen(mock_to_path(@file), "r")
+      begin
+        fh = File.new(@file)
+        fh2 = fh.reopen(mock_to_path(@file), "r")
+      ensure
+        fh.close rescue nil
+        fh2.close rescue nil
+      end
     end
   end
 end
