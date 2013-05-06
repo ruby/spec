b0c484b97f85135746708d99b1d3968bb05a6dca
Marjan Krekoten' (Мар'ян Крекотень) <krekoten@gmail.com>
2013-05-06 18:57:03 +0300

    Exception#set_backtrace
    Specs added:
    
    Exception#set_backtrace
     - allows the user to set the backtrace to string
     - allows the user to set the backtrace to nil
     - raises TypeError when argument is not array
     - raises TypeError when argument contains non String element

--- core/exception/set_backtrace_spec.rb
+++ core/exception/set_backtrace_spec.rb
@@ -15,4 +15,26 @@ describe "Exception#set_backtrace" do
     err.set_backtrace bt
     err.backtrace.should == bt
   end
+
+  it "allows the user to set the backtrace to string" do
+    err = RuntimeError.new
+    err.set_backtrace "unhappy"
+    err.backtrace.should == ["unhappy"]
+  end
+
+  it "allows the user to set the backtrace to nil" do
+    err = RuntimeError.new
+    err.set_backtrace nil
+    err.backtrace.should be_nil
+  end
+
+  it "raises TypeError when argument is not array" do
+    err = RuntimeError.new
+    lambda { err.set_backtrace 1 }.should raise_error(TypeError)
+  end
+
+  it "raises TypeError when argument contains non String element" do
+    err = RuntimeError.new
+    lambda { err.set_backtrace ["String", 1] }.should raise_error(TypeError)
+  end
 end
