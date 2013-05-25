edb4f479079dd7caf7a623f3d1affad168c3cf29
NARUSE, Yui <naruse@airemix.jp>
2013-05-25 20:36:00 +0900

    fix a2b9dc1d0e; add before :each

--- library/stringscanner/versions/element_reference_1.9.rb
+++ library/stringscanner/versions/element_reference_1.9.rb
@@ -1,4 +1,8 @@
 describe "StringScanner#[name]" do
+  before :each do
+    @s = StringScanner.new("Fri Jun 13 2008 22:43")
+  end
+
   ruby_version_is "1.9"..."2.1" do
     it "raises a TypeError when a String is as argument" do
       @s.scan(/(\w+) (\w+) (\d+) /)
