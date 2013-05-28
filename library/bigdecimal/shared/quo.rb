4579c7949c0187183f1ea57729e65a51fd377ed9
Sébastien Le Callonnec <sebastien@weblogism.com>
2013-05-28 22:02:26 +0100

    Add specs for BigDecimal `/` with Float and Rational divisors.

--- library/bigdecimal/shared/quo.rb
+++ library/bigdecimal/shared/quo.rb
@@ -1,4 +1,5 @@
 require 'bigdecimal'
+require 'rational'
 
 describe :bigdecimal_quo, :shared => true do
   before(:each) do
@@ -15,6 +16,8 @@ describe :bigdecimal_quo, :shared => true do
     @one_minus = BigDecimal("-1")
     @frac_1 = BigDecimal("1E-99999")
     @frac_2 = BigDecimal("0.9E-99999")
+    @float = 1.48
+    @rational = Rational(17,3)
   end
 
   it "returns a / b" do
@@ -55,5 +58,47 @@ describe :bigdecimal_quo, :shared => true do
     @zero.send(@method, @zero, *@object).nan?.should == true
     @zero_minus.send(@method, @zero_plus, *@object).nan?.should == true
     @zero_plus.send(@method, @zero_minus, *@object).nan?.should == true
+  end
+
+  context "with a Float" do
+    ruby_version_is "".."1.9" do
+      it "returns a Float" do
+        result = @eleven.send(@method, @float, *@object)
+        result.should be_close(7.4324324324324325, TOLERANCE)
+        result.should be_an_instance_of(Float)
+      end
+    end
+
+    ruby_version_is "2.0.0" do
+      it "returns a BigDecimal" do
+        result = @eleven.send(@method, @float, *@object)
+        result.should be_close(7.4324324324324325, TOLERANCE)
+        result.should be_an_instance_of(BigDecimal)
+      end
+    end
+  end
+
+  context "with a Rational" do
+    ruby_version_is ""..."1.9" do
+      it "returns a Float" do
+        result = @eleven.send(@method, @rational, *@object)
+        result.should be_close(1.94117647058824, TOLERANCE)
+        result.should be_an_instance_of(Float)
+      end
+    end
+
+    ruby_version_is "1.9"..."2.0" do
+      it "raises a TypeError" do
+        lambda { @eleven.send(@method, @rational, *@object) }.should raise_error(TypeError)
+      end
+    end
+
+    ruby_version_is "2.0.0" do
+      it "returns a BigDecimal" do
+        result = @eleven.send(@method, @rational, *@object)
+        result.should be_close(1.94117647058824, TOLERANCE)
+        result.should be_an_instance_of(BigDecimal)
+      end
+    end
   end
 end
