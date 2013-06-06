009d05431aab0ff633ec649a4d422f0b9579f604
DawidJanczak <dawid.janczak@gmail.com>
2013-06-06 11:02:01 +0800

    Correct testcase description.

--- core/kernel/__dir___spec.rb
+++ core/kernel/__dir___spec.rb
@@ -7,8 +7,10 @@ ruby_version_is "2.0" do
     end
 
     ruby_bug "#8346", "2.0" do
-      it "works in eval with top level binding" do
-        eval("__dir__", binding).should == File.dirname(__FILE__)
+      context "when used in eval with top level binding" do
+        it "returns the name of the directory containing the currently-executing file" do
+          eval("__dir__", binding).should == File.dirname(__FILE__)
+        end
       end
     end
   end
