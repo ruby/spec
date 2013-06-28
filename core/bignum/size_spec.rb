1fc40a65663262a1f7cebd41ed63f546cfbc7d5a
NARUSE, Yui <naruse@airemix.jp>
2013-06-28 11:07:41 +0900

    Add current standard behavior

--- core/bignum/size_spec.rb
+++ core/bignum/size_spec.rb
@@ -2,7 +2,38 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 describe "Bignum#size" do
 
-  compliant_on :ironruby do
+  compliant_on do
+    platform_is :wordsize => 32 do
+      it "returns the number of bytes in the machine representation in multiples of four on 32 bit platforms" do
+        (256**7).size   .should ==  8
+        (256**8).size   .should == 12
+        (256**9).size   .should == 12
+        (256**10).size  .should == 12
+        (256**10-1).size.should == 12
+        (256**11).size  .should == 12
+        (256**12).size  .should == 16
+        (256**20-1).size.should == 20
+        (256**40-1).size.should == 40
+      end
+    end
+
+    platform_is :wordsize => 64 do
+      it "returns the number of bytes in the machine representation in multiples of eight on 64 bit platforms" do
+        (256**7).size   .should ==  8
+        (256**8).size   .should == 16
+        (256**9).size   .should == 16
+        (256**10).size  .should == 16
+        (256**10-1).size.should == 16
+        (256**11).size  .should == 16
+        (256**12).size  .should == 16
+        (256**13).size  .should == 16
+        (256**20-1).size.should == 24
+        (256**40-1).size.should == 40
+      end
+    end
+  end
+
+  deviates_on :ironruby do
     it "returns the number of bytes in the machine representation in multiples of four" do
       (256**7).size.should == 8
       (256**8).size.should == 12
