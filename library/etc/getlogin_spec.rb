a3db680ce2c19f9e6380d947fc765a3d47e85272
NARUSE, Yui <naruse@airemix.jp>
2013-08-01 05:05:33 +0900

    getlogin(3)' result is the tty's user
    `who -m` is also th tty's user

--- library/etc/getlogin_spec.rb
+++ library/etc/getlogin_spec.rb
@@ -3,11 +3,9 @@ require 'etc'
 
 describe "Etc.getlogin" do
   it "returns the name of the user who runs this process" do
-    if ENV["SUDO_USER"]
-      # through sudo, getlogin equals SUDO_USER
-      Etc.getlogin.should == ENV["SUDO_USER"]
-    elsif Etc.getlogin
-      Etc.getlogin.should == username
+    if Etc.getlogin
+      login = `who -m`[/\A\w+/]
+      Etc.getlogin.should == login
     else
       # Etc.getlogin may return nil if the login name is not set
       # because of chroot or sudo or something.
