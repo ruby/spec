c82d1ece5eab5174c6255672c5c6f20ea8dbee1a
NARUSE, Yui <naruse@airemix.jp>
2013-08-01 07:03:18 +0900

    Additional fix to getlogin

--- library/etc/getlogin_spec.rb
+++ library/etc/getlogin_spec.rb
@@ -2,14 +2,25 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require 'etc'
 
 describe "Etc.getlogin" do
-  it "returns the name of the user who runs this process" do
-    if Etc.getlogin
-      login = `who -m`[/\A\w+/]
-      Etc.getlogin.should == login
-    else
-      # Etc.getlogin may return nil if the login name is not set
-      # because of chroot or sudo or something.
-      Etc.getlogin.should be_nil
+  it "returns the name associated with the current login activity" do
+    begin
+      # make Etc.getlogin to return nil if getlogin(3) returns NULL
+      envuser, ENV['USER'] = ENV['USER'], nil
+      if Etc.getlogin
+        # who -m is the user of tty attached to the stdout
+        login = `who -m`[/\A\w+/]
+        Etc.getlogin.should == login
+      else
+        # Etc.getlogin may return nil if the login name is not set
+        # because of chroot or sudo or something.
+        Etc.getlogin.should be_nil
+      end
+    ensure
+      ENV['USER'] = envuser
+    end
+
+    if login.nil? && ENV['USER']
+      Etc.getlogin.should == ENV['USER']
     end
   end
 end
