74f2708194bac125bdb9024c65ca79b306fd2d49
NARUSE, Yui <naruse@airemix.jp>
2013-08-01 13:52:22 +0900

    logname(1) returns getlogin(2)'s result

--- library/etc/getlogin_spec.rb
+++ library/etc/getlogin_spec.rb
@@ -2,24 +2,24 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require 'etc'
 
 describe "Etc.getlogin" do
-  it "returns the name associated with the current login activity" do
+  it "returns the name associated with the current login activity or nil" do
+    nil_p = false
     begin
       # make Etc.getlogin to return nil if getlogin(3) returns NULL
       envuser, ENV['USER'] = ENV['USER'], nil
       if Etc.getlogin
-        # who -m is the user of tty attached to the stdout
-        login = `who -m`[/\A\w+/]
-        Etc.getlogin.should == login
+        Etc.getlogin.should == `logname`.chomp
       else
         # Etc.getlogin may return nil if the login name is not set
         # because of chroot or sudo or something.
         Etc.getlogin.should be_nil
+        nil_p = true
       end
     ensure
       ENV['USER'] = envuser
     end
 
-    if login.nil? && ENV['USER']
+    if nil_p
       Etc.getlogin.should == ENV['USER']
     end
   end
