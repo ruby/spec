5cef74ea771bd733ff1c8d39b22b991c39fcb1d4
NARUSE, Yui <naruse@airemix.jp>
2013-08-14 16:21:26 +0900

    Follow CRuby r42551 fixes rubyspec/rubyspec#246
    http://bugs.ruby-lang.org/issues/3753

--- core/module/define_method_spec.rb
+++ core/module/define_method_spec.rb
@@ -181,14 +181,25 @@ describe "Module#define_method" do
     Module.should have_private_instance_method(:define_method)
   end
 
-  it "returns a Proc" do
-    class DefineMethodSpecClass
-      method = define_method("return_test") { || true }
-      method.is_a?(Proc).should be_true
-      # check if it is a lambda:
-      lambda {
-        method.call :too_many_arguments
-      }.should raise_error(ArgumentError)
+  ruby_version_is ""..."2.1" do
+    it "returns a Proc" do
+      class DefineMethodSpecClass
+        method = define_method("return_test") { || true }
+        method.is_a?(Proc).should be_true
+        # check if it is a lambda:
+        lambda {
+          method.call :too_many_arguments
+        }.should raise_error(ArgumentError)
+      end
+    end
+  end
+
+  ruby_version_is "2.1" do
+    it "returns its symbol" do
+      class DefineMethodSpecClass
+        method = define_method("return_test") { || true }
+        method.should == :return_test
+      end
     end
   end
 end
