05bad100a31203750180e26965a7f673cb0706af
NARUSE, Yui <naruse@airemix.jp>
2013-10-25 12:03:49 +0900

    Additional fix for commit 9a39d2220b17cb8ead1eeb53b3edffd1ff8cfb81
    follow CRuby r43402
    rb_error_frozen() in error.c raises RuntimeError.

--- library/openstruct/method_missing_spec.rb
+++ library/openstruct/method_missing_spec.rb
@@ -10,10 +10,19 @@ describe "OpenStruct#method_missing when called with a method name ending in '='
     lambda { @os.method_missing(:test=) }.should raise_error(ArgumentError)
   end
 
-  it "raises a TypeError when self is frozen" do
-    @os.freeze
-    lambda { @os.method_missing(:test=, "test") }.should raise_error(TypeError)
+  ruby_version_is ""..."2.1.0" do
+    it "raises a TypeError when self is frozen" do
+      @os.freeze
+      lambda { @os.method_missing(:test=, "test") }.should raise_error(TypeError)
+    end
   end
+
+  ruby_version_is "2.1.0" do
+    it "raises a TypeError when self is frozen" do
+      @os.freeze
+      lambda { @os.method_missing(:test=, "test") }.should raise_error(RuntimeError)
+    end
+end
 
   it "creates accessor methods" do
     @os.method_missing(:test=, "test")
