9778df0c55c639e3f6debca46de4698bbbb751cb
Ryan Biesemeyer <ryan@simplymeasured.com>
2014-01-30 21:53:29 +0000

    Integer#pred: improve spec to cover fixnum_min

--- core/integer/pred_spec.rb
+++ core/integer/pred_spec.rb
@@ -5,6 +5,7 @@ describe "Integer#pred" do
     0.pred.should eql(-1)
     -1.pred.should eql(-2)
     bignum_value.pred.should eql(bignum_value(-1))
+    fixnum_min.pred.should eql(fixnum_min - 1)
     20.pred.should eql(19)
   end
 end
