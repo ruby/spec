d7434d38cdea05668985d1eb3b793d93d0ad2f00
Joe McIlvain <joe.eli.mac@gmail.com>
2014-09-19 08:40:24 -0700

    Specs for Float#next_float and #prev_float use eql? instead of ==

--- core/float/next_float_spec.rb
+++ core/float/next_float_spec.rb
@@ -4,7 +4,7 @@ describe "Float" do
   ruby_version_is "2.2" do
     it "returns a float the smallest possible step greater than the receiver" do
       barely_positive = 0.0.next_float
-      barely_positive.should == 0.0.next_float
+      barely_positive.should eql 0.0.next_float
       
       barely_positive.should > 0.0
       barely_positive.should < barely_positive.next_float
@@ -14,26 +14,26 @@ describe "Float" do
     end
 
     it "steps directly between MAX and INFINITY" do
-      (-Float::INFINITY).next_float.should == -Float::MAX
-      Float::MAX.next_float.should == Float::INFINITY
+      (-Float::INFINITY).next_float.should eql -Float::MAX
+      Float::MAX.next_float.should eql Float::INFINITY
     end
 
     it "steps directly between 1.0 and EPSILON + 1.0" do
-      1.0.next_float.should == Float::EPSILON + 1.0
+      1.0.next_float.should eql Float::EPSILON + 1.0
     end
 
     it "steps directly between -1.0 and EPSILON/2 - 1.0" do
-      (-1.0).next_float.should == Float::EPSILON/2 - 1.0
+      (-1.0).next_float.should eql Float::EPSILON/2 - 1.0
     end
 
     it "reverses the effect of prev_float" do
-      0.0.prev_float.next_float.should == 0.0
+      0.0.prev_float.next_float.should eql 0.0
       num = rand
-      num.prev_float.next_float.should == num
+      num.prev_float.next_float.should eql num
     end
 
     it "returns NAN if NAN was the receiver" do
-      Float::NAN.next_float.nan?.should == true
+      Float::NAN.next_float.nan?.should eql true
     end
   end
 end
--- core/float/prev_float_spec.rb
+++ core/float/prev_float_spec.rb
@@ -4,7 +4,7 @@ describe "Float" do
   ruby_version_is "2.2" do
     it "returns a float the smallest possible step greater than the receiver" do
       barely_negative = 0.0.prev_float
-      barely_negative.should == 0.0.prev_float
+      barely_negative.should eql 0.0.prev_float
       
       barely_negative.should < 0.0
       barely_negative.should > barely_negative.prev_float
@@ -14,26 +14,26 @@ describe "Float" do
     end
 
     it "steps directly between MAX and INFINITY" do
-      Float::INFINITY.prev_float.should == Float::MAX
-      (-Float::MAX).prev_float.should == -Float::INFINITY
+      Float::INFINITY.prev_float.should eql Float::MAX
+      (-Float::MAX).prev_float.should eql -Float::INFINITY
     end
 
     it "steps directly between 1.0 and -EPSILON/2 + 1.0" do
-      1.0.prev_float.should == -Float::EPSILON/2 + 1.0
+      1.0.prev_float.should eql -Float::EPSILON/2 + 1.0
     end
 
     it "steps directly between -1.0 and -EPSILON - 1.0" do
-      (-1.0).prev_float.should == -Float::EPSILON - 1.0
+      (-1.0).prev_float.should eql -Float::EPSILON - 1.0
     end
 
     it "reverses the effect of next_float" do
-      0.0.next_float.prev_float.should == 0.0
+      0.0.next_float.prev_float.should eql 0.0
       num = rand
-      num.next_float.prev_float.should == num
+      num.next_float.prev_float.should eql num
     end
 
     it "returns NAN if NAN was the receiver" do
-      Float::NAN.prev_float.nan?.should == true
+      Float::NAN.prev_float.nan?.should eql true
     end
   end
 end
