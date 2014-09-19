41a949b183b32da26476606b0d8812ab78d02950
Joe McIlvain <joe.eli.mac@gmail.com>
2014-09-18 19:17:28 -0700

    Specs for Float#next_float and #prev_float

---
+++ core/float/next_float_spec.rb
@@ -0,0 +1,35 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+
+describe "Float" do
+  ruby_version_is "2.2" do
+    it "returns a float the smallest possible step greater than the receiver" do
+      barely_positive = 0.0.next_float
+      barely_positive.should == 0.0.next_float
+      
+      barely_positive.should > 0.0
+      barely_positive.should < barely_positive.next_float
+      
+      midpoint = barely_positive / 2
+      [0.0, barely_positive].should include midpoint
+    end
+
+    it "steps directly between MAX and INFINITY" do
+      (-Float::INFINITY).next_float.should == -Float::MAX
+      Float::MAX.next_float.should == Float::INFINITY
+    end
+
+    it "steps directly between 1.0 and EPSILON + 1.0" do
+      1.0.next_float.should == Float::EPSILON + 1.0
+    end
+
+    it "steps directly between -1.0 and EPSILON/2 - 1.0" do
+      (-1.0).next_float.should == Float::EPSILON/2 - 1.0
+    end
+
+    it "reverses the effect of prev_float" do
+      0.0.prev_float.next_float.should == 0.0
+      num = rand
+      num.prev_float.next_float.should == num
+    end
+  end
+end
---
+++ core/float/prev_float_spec.rb
@@ -0,0 +1,35 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+
+describe "Float" do
+  ruby_version_is "2.2" do
+    it "returns a float the smallest possible step greater than the receiver" do
+      barely_negative = 0.0.prev_float
+      barely_negative.should == 0.0.prev_float
+      
+      barely_negative.should < 0.0
+      barely_negative.should > barely_negative.prev_float
+      
+      midpoint = barely_negative / 2
+      [0.0, barely_negative].should include midpoint
+    end
+
+    it "steps directly between MAX and INFINITY" do
+      Float::INFINITY.prev_float.should == Float::MAX
+      (-Float::MAX).prev_float.should == -Float::INFINITY
+    end
+
+    it "steps directly between 1.0 and -EPSILON/2 + 1.0" do
+      1.0.prev_float.should == -Float::EPSILON/2 + 1.0
+    end
+
+    it "steps directly between -1.0 and -EPSILON - 1.0" do
+      (-1.0).prev_float.should == -Float::EPSILON - 1.0
+    end
+
+    it "reverses the effect of next_float" do
+      0.0.next_float.prev_float.should == 0.0
+      num = rand
+      num.next_float.prev_float.should == num
+    end
+  end
+end
