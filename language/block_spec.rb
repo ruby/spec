91d1f4e7c85c3ad7b43930aa21dfff14936bf8bd
Anthony Crumley <anthony.crumley@gmail.com>
2015-01-23 20:32:57 -0600

    Fixed block parameter destructuring spec
    The scenario being tested involves a block receiving more
    arguments than there are parameters defined and there is a parameter
    for undefined keyword arguments, that is a parameter in the form of **x.
    specification expected the ** parameter to be related to the argument
    that matched its position in the list.  This was incorrect.  The **
    parameter is populated by the last argument in the list when there are
    more arguments than parameters defined.

--- language/block_spec.rb
+++ language/block_spec.rb
@@ -75,13 +75,11 @@ describe "A block yielded a single" do
       result.should == [1, [2, 3], obj, {}]
     end
 
-    it "calls #to_hash on the element that maps to the keyword arguments" do
+    it "calls #to_hash on the last element when there are more arguments than parameters" do
       x = mock("destructure matching block keyword argument")
       x.should_receive(:to_hash).and_return({x: 9})
-      y = mock("destructure non-matching block keyword argument")
-      y.should_not_receive(:to_hash)
 
-      result = m([1, 2, 3, x, 4, 5, y]) { |a, b=5, c, **k| [a, b, c, k] }
+      result = m([1, 2, 3, {y: 9}, 4, 5, x]) { |a, b=5, c, **k| [a, b, c, k] }
       result.should == [1, 2, 3, {x: 9}]
     end
 
