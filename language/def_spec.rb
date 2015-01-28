fb0154f55c135bd184cfa394bc9d24f9cdbe7064
Anthony Crumley <anthony.crumley@gmail.com>
2015-01-27 22:07:55 -0600

    Fixed specification of default value shadowing local variable
    In Ruby <= 2.1, a default value set to a name that is a method name and
    a local variable name will call the method.  In Ruby >= 2.2, this same
    scenario will use the local variable value as the default value instead.
    
    This behavior is discussed in the issue
    https://bugs.ruby-lang.org/issues/10314.

--- language/def_spec.rb
+++ language/def_spec.rb
@@ -174,15 +174,30 @@ describe "An instance method with a default argument" do
     foo(2,3,3).should == [2,3,[3]]
   end
 
-  it "calls a method with the same name as the local" do
-    def bar
-      1
+  ruby_version_is "2.0"..."2.2" do
+    it "calls a method with the same name as the local" do
+      def bar
+        1
+      end
+      def foo(bar = bar)
+        bar
+      end
+      foo.should == 1
+      foo(2).should == 2
     end
-    def foo(bar = bar)
-      bar
+  end
+
+  ruby_version_is "2.2" do
+    it "does not call a method with the same name as the local" do
+      def bar
+        1
+      end
+      def foo(bar = bar)
+        bar
+      end
+      foo.should == nil
+      foo(2).should == 2
     end
-    foo.should == 1
-    foo(2).should == 2
   end
 end
 
