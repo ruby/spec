d76eeb4ba0116606d855fbc866bb18c75f17a0e4
Benoit Daloze <eregontp@gmail.com>
2015-02-04 16:13:17 +0100

    Comparable#== should call #<=> exactly one time.

--- core/comparable/equal_value_spec.rb
+++ core/comparable/equal_value_spec.rb
@@ -16,54 +16,54 @@ describe "Comparable#==" do
   end
 
   it "calls #<=> on self with other and returns true if #<=> returns 0" do
-    a.should_receive(:<=>).any_number_of_times.and_return(0)
+    a.should_receive(:<=>).once.and_return(0)
     (a == b).should == true
   end
 
   it "calls #<=> on self with other and returns true if #<=> returns 0.0" do
-    a.should_receive(:<=>).any_number_of_times.and_return(0.0)
+    a.should_receive(:<=>).once.and_return(0.0)
     (a == b).should == true
   end
 
   it "returns false if calling #<=> on self returns a positive Integer" do
-    a.should_receive(:<=>).any_number_of_times.and_return(1)
+    a.should_receive(:<=>).once.and_return(1)
     (a == b).should == false
   end
 
   it "returns false if calling #<=> on self returns a negative Integer" do
-    a.should_receive(:<=>).any_number_of_times.and_return(-1)
+    a.should_receive(:<=>).once.and_return(-1)
     (a == b).should == false
   end
 
   ruby_version_is ""..."1.9" do
     it "returns nil if calling #<=> on self returns nil" do
-      a.should_receive(:<=>).any_number_of_times.and_return(nil)
+      a.should_receive(:<=>).once.and_return(nil)
       (a == b).should == nil
     end
 
     it "returns nil if calling #<=> on self returns a non-Integer" do
-      a.should_receive(:<=>).any_number_of_times.and_return("abc")
+      a.should_receive(:<=>).once.and_return("abc")
       (a == b).should == nil
     end
   end
 
   ruby_version_is "1.9" do
     it "returns false if calling #<=> on self returns nil" do
-      a.should_receive(:<=>).any_number_of_times.and_return(nil)
+      a.should_receive(:<=>).once.and_return(nil)
       (a == b).should be_false
     end
   end
 
   ruby_version_is "1.9"...no_silent_rescue do
     it "returns false if calling #<=> on self returns a non-Integer" do
-      a.should_receive(:<=>).any_number_of_times.and_return("abc")
+      a.should_receive(:<=>).once.and_return("abc")
       (a == b).should be_false
     end
   end
 
   ruby_version_is no_silent_rescue do
     it "raise an ArgumentError if calling #<=> on self returns a non-Integer" do
-      a.should_receive(:<=>).any_number_of_times.and_return("abc")
+      a.should_receive(:<=>).once.and_return("abc")
       lambda { (a == b) }.should raise_error(ArgumentError)
     end
   end
