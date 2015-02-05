fce0db73a6ecfbc8bf30cc61d5e47e589dff62f5
Anthony Crumley <anthony.crumley@gmail.com>
2015-02-05 07:50:28 -0600

    Bignum#size changed on Ruby 2.1
    The way Bignum#size is calculated changed in Ruby 2.1.  The new behavior
    is now consistent with the way JRuby and Rubinius behave.  The details
    of this change are at https://bugs.ruby-lang.org/issues/8553.

--- core/bignum/size_spec.rb
+++ core/bignum/size_spec.rb
@@ -2,45 +2,91 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 describe "Bignum#size" do
 
-  compliant_on :ironruby do
-    it "returns the number of bytes in the machine representation in multiples of four" do
-      (256**7).size.should == 8
-      (256**8).size.should == 12
-      (256**9).size.should == 12
-      (256**10).size.should == 12
-      (256**10-1).size.should == 12
-      (256**11).size.should == 12
-      (256**12).size.should == 16
-      (256**20-1).size.should == 20
-      (256**40-1).size.should == 40
+  ruby_version_is ""..."2.1" do
+    compliant_on :ironruby do
+      it "returns the number of bytes in the machine representation in multiples of four" do
+        (256**7).size.should == 8
+        (256**8).size.should == 12
+        (256**9).size.should == 12
+        (256**10).size.should == 12
+        (256**10-1).size.should == 12
+        (256**11).size.should == 12
+        (256**12).size.should == 16
+        (256**20-1).size.should == 20
+        (256**40-1).size.should == 40
+      end
+    end
+
+    deviates_on :rubinius, :jruby do
+      it "returns the number of bytes in the machine representation" do
+        (256**7).size   .should == 8
+        (256**8).size   .should == 9
+        (256**9).size   .should == 10
+        (256**10).size  .should == 11
+        (256**10-1).size.should == 10
+        (256**11).size   .should == 12
+        (256**12).size   .should == 13
+        (256**20-1).size .should == 20
+        (256**40-1).size .should == 40
+      end
+    end
+
+    deviates_on :maglev do
+      it "returns the number of bytes in the machine representation in multiples of four" do
+        (256**7).size   .should ==  8
+        (256**8).size   .should == 16
+        (256**9).size   .should == 16
+        (256**10).size  .should == 16
+        (256**10-1).size.should == 16
+        (256**11).size  .should == 16
+        (256**12).size  .should == 20
+        (256**20-1).size.should == 24
+        (256**40-1).size.should == 44
+      end
     end
   end
-
-  deviates_on :rubinius, :jruby do
-    it "returns the number of bytes in the machine representation" do
-      (256**7).size   .should == 8
-      (256**8).size   .should == 9
-      (256**9).size   .should == 10
-      (256**10).size  .should == 11
-      (256**10-1).size.should == 10
-      (256**11).size   .should == 12
-      (256**12).size   .should == 13
-      (256**20-1).size .should == 20
-      (256**40-1).size .should == 40
+  
+  ruby_version_is "2.1" do
+    deviates_on :ironruby do
+      it "returns the number of bytes in the machine representation in multiples of four" do
+        (256**7).size.should == 8
+        (256**8).size.should == 12
+        (256**9).size.should == 12
+        (256**10).size.should == 12
+        (256**10-1).size.should == 12
+        (256**11).size.should == 12
+        (256**12).size.should == 16
+        (256**20-1).size.should == 20
+        (256**40-1).size.should == 40
+      end
     end
-  end
 
-  deviates_on :maglev do
-    it "returns the number of bytes in the machine representation in multiples of four" do
-      (256**7).size   .should ==  8
-      (256**8).size   .should == 16
-      (256**9).size   .should == 16
-      (256**10).size  .should == 16
-      (256**10-1).size.should == 16
-      (256**11).size  .should == 16
-      (256**12).size  .should == 20
-      (256**20-1).size.should == 24
-      (256**40-1).size.should == 44
+    compliant_on :rubinius, :jruby do
+      it "returns the number of bytes in the machine representation" do
+        (256**7).size   .should == 8
+        (256**8).size   .should == 9
+        (256**9).size   .should == 10
+        (256**10).size  .should == 11
+        (256**10-1).size.should == 10
+        (256**11).size   .should == 12
+        (256**12).size   .should == 13
+        (256**20-1).size .should == 20
+        (256**40-1).size .should == 40
+      end
+    end
+
+    deviates_on :maglev do
+      it "returns the number of bytes in the machine representation in multiples of four" do
+        (256**7).size   .should ==  8
+        (256**8).size   .should == 16
+        (256**9).size   .should == 16
+        (256**10).size  .should == 16
+        (256**10-1).size.should == 16
+        (256**11).size  .should == 16
+        (256**12).size  .should == 20
+        (256**20-1).size.should == 24
+        (256**40-1).size.should == 44
+      end
     end
   end
 end
