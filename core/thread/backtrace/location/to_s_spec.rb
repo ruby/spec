e6d5f96b89eacc09703524ca11d5a91d35a6b20c
Benoit Daloze <eregontp@gmail.com>
2015-02-22 21:07:55 +0100

    Unshare Thread::Backtrace::Location#{to_s,inspect}, they are different.

--- core/thread/backtrace/location/to_s_spec.rb
+++ core/thread/backtrace/location/to_s_spec.rb
@@ -1,7 +1,13 @@
 require File.expand_path('../../../../../spec_helper', __FILE__)
 require File.expand_path('../fixtures/classes', __FILE__)
-require File.expand_path('../shared/to_string', __FILE__)
 
 describe 'Thread::Backtrace::Location#to_s' do
-  it_behaves_like :thread_backtrace_location_to_string, :to_s
+  before :each do
+    @frame = ThreadBacktraceLocationSpecs.locations[0]
+    @line  = __LINE__ - 1
+  end
+
+  it 'converts the call frame to a String' do
+    @frame.to_s.should include("#{__FILE__}:#{@line}:in ")
+  end
 end
