c32751601557fff954fba4e04061f4aac9c5b2ed
Nobuyoshi Nakada <nobu@ruby-lang.org>
2015-02-25 15:05:44 +0900

    Use Process::Status#success?
    Should use Process::Status#success? instead of raw exitstatus,
    which exists since 1.8.

--- optional/capi/spec_helper.rb
+++ optional/capi/spec_helper.rb
@@ -70,7 +70,7 @@ def compile_extension(name)
 
   output = `#{cc} #{incflags} #{cflags} -c #{source} -o #{obj}`
 
-  if $?.exitstatus != 0 or !File.exist?(obj)
+  if $?.success? or !File.exist?(obj)
     puts "ERROR:\n#{output}"
     raise "Unable to compile \"#{source}\""
   end
@@ -84,7 +84,7 @@ def compile_extension(name)
 
   output = `#{ldshared} #{obj} #{libpath} #{dldflags} #{libs} -o #{lib}`
 
-  if $?.exitstatus != 0
+  if $?.success?
     puts "ERROR:\n#{output}"
     raise "Unable to link \"#{source}\""
   end
