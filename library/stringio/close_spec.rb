890218b487e730e321a750c4a5033bbaf4a6c2af
Nobuyoshi Nakada <nobu@ruby-lang.org>
2015-03-13 00:18:24 +0900

    Update close after close #52

--- library/stringio/close_spec.rb
+++ library/stringio/close_spec.rb
@@ -22,4 +22,11 @@ describe "StringIO#close" do
       lambda { @io.close }.should raise_error(IOError)
     end
   end
+
+  ruby_version_is "2.3" do
+    it "does not raise anything when self was already closed" do
+      @io.close
+      lambda { @io.close }.should_not raise_error(IOError)
+    end
+  end
 end
