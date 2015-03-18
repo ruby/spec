d26ba36b9ef145a29d6e38deeb53359ceac3f860
Benoit Daloze <eregontp@gmail.com>
2015-03-18 12:19:30 +0100

    Remove specs in core/basicobject unrelated to BasicObject methods.

--- core/kernel/class_spec.rb
+++ core/kernel/class_spec.rb
@@ -2,5 +2,8 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../fixtures/classes', __FILE__)
 
 describe "Kernel#class" do
-  it "needs to be reviewed for spec completeness"
+  it "returns Class for a Class" do
+    BasicObject.class.should equal(Class)
+    String.class.should equal(Class)
+  end
 end
