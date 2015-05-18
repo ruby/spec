d8899fff45470909c079584dee1d855c7aff04fd
Nullset14 <ajoyforever@gmail.com>
2015-05-18 17:35:03 +0000

    remove new_spec.rb. remove Prime.instance.each reference

--- library/prime/next_spec.rb
+++ library/prime/next_spec.rb
@@ -2,8 +2,6 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../shared/next', __FILE__)
 require 'prime'
 
-describe "Prime#next with Prime.instance.each" do
+describe "Prime#next" do
   it_behaves_like :prime_next, :next
 end
-
-
--- library/prime/succ_spec.rb
+++ library/prime/succ_spec.rb
@@ -2,6 +2,6 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../shared/next', __FILE__)
 require 'prime'
 
-describe "Prime#succ with Prime.instance.each" do
+describe "Prime#succ" do
   it_behaves_like :prime_next, :succ
 end
