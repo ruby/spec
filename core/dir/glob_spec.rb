ecac528cd0cfd52f75ad9939fdc3d85791cc4fea
Benoit Daloze <eregontp@gmail.com>
2015-05-27 18:41:22 +0200

    Refactor Dir[] and Dir.glob specs.

--- core/dir/glob_spec.rb
+++ core/dir/glob_spec.rb
@@ -3,26 +3,10 @@ require File.expand_path('../fixtures/common', __FILE__)
 require File.expand_path('../shared/glob', __FILE__)
 
 describe "Dir.glob" do
-  before :all do
-    DirSpecs.create_mock_dirs
-  end
-
-  after :all do
-    DirSpecs.delete_mock_dirs
-  end
-
   it_behaves_like :dir_glob, :glob
 end
 
 describe "Dir.glob" do
-  before :all do
-    DirSpecs.create_mock_dirs
-  end
-
-  after :all do
-    DirSpecs.delete_mock_dirs
-  end
-
   it_behaves_like :dir_glob_recursive, :glob
 end
 
