34a79ed2baa024a7dd6895b35c60322cadca7106
Elia Schito <elia@schito.me>
2015-06-04 00:40:54 +0200

    Fix bad merge, restores 2ec6fc2c9e040fb28942cb54b874c413f2ac0f91
    Remove ruby_bug guards for ruby <= 2.0.
    
    * 2.0.x.x guards mean it was fixed in 2.0, so that is fine.
      Most reported 2.0 bugs have been fixed.
      Remaining bugs are re-added in the next commits.
    * The script used is available at https://gist.github.com/eregon/b617886844cf3bf56b17.

--- core/array/hash_spec.rb
+++ core/array/hash_spec.rb
@@ -19,23 +19,21 @@ describe "Array#hash" do
     lambda { empty.hash }.should_not raise_error
   end
 
-  ruby_bug "redmine #1852", "1.9.1" do
-    it "returns the same hash for equal recursive arrays" do
-      rec = []; rec << rec
-      rec.hash.should == [rec].hash
-      rec.hash.should == [[rec]].hash
-      # This is because rec.eql?([[rec]])
-      # Remember that if two objects are eql?
-      # then the need to have the same hash
-      # Check the Array#eql? specs!
-    end
+  it "returns the same hash for equal recursive arrays" do
+    rec = []; rec << rec
+    rec.hash.should == [rec].hash
+    rec.hash.should == [[rec]].hash
+    # This is because rec.eql?([[rec]])
+    # Remember that if two objects are eql?
+    # then the need to have the same hash
+    # Check the Array#eql? specs!
+  end
 
-    it "returns the same hash for equal recursive arrays through hashes" do
-      h = {} ; rec = [h] ; h[:x] = rec
-      rec.hash.should == [h].hash
-      rec.hash.should == [{:x => rec}].hash
-      # Like above, this is because rec.eql?([{:x => rec}])
-    end
+  it "returns the same hash for equal recursive arrays through hashes" do
+    h = {} ; rec = [h] ; h[:x] = rec
+    rec.hash.should == [h].hash
+    rec.hash.should == [{:x => rec}].hash
+    # Like above, this is because rec.eql?([{:x => rec}])
   end
 
   #  Too much of an implementation detail? -rue
