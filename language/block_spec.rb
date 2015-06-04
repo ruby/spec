1c55da4e61fa42c0c0e8491f112ed518dccdda5c
Benoit Daloze <eregontp@gmail.com>
2015-06-04 23:07:26 +0200

    Fix guard for keyword argument spec.

--- language/block_spec.rb
+++ language/block_spec.rb
@@ -62,12 +62,14 @@ describe "A block yielded a single" do
       result.should == [{"a" => 1}, b: 2]
     end
 
-    it "calls #to_hash on the argument but does not use the result when no keywords are present" do
-      obj = mock("coerce block keyword arguments")
-      obj.should_receive(:to_hash).and_return({"a" => 1, "b" => 2})
+    ruby_version_is "2.2" do
+      it "calls #to_hash on the argument but does not use the result when no keywords are present" do
+        obj = mock("coerce block keyword arguments")
+        obj.should_receive(:to_hash).and_return({"a" => 1, "b" => 2})
 
-      result = m([obj]) { |a=nil, **b| [a, b] }
-      result.should == [obj, {}]
+        result = m([obj]) { |a=nil, **b| [a, b] }
+        result.should == [{"a" => 1, "b" => 2}, {}]
+      end
     end
 
     it "assigns non-symbol keys to non-keyword arguments" do
