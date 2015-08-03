3e9505b9bf196106b37b9420a6a5c0370b630aa3
Kenichi Kamiya <kachick1@gmail.com>
2015-08-04 00:08:24 +0900

    Write Module#deprecate_constant specs

---
+++ core/module/deprecate_constant_spec.rb
@@ -0,0 +1,38 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+
+ruby_version_is "2.3" do
+  describe "Module#deprecate_constant" do
+    before :each do
+      @module = Module.new
+      @value = :value
+      @module::PUBLIC1 = @value
+      @module::PUBLIC2 = @value
+      @module::PRIVATE = @value
+      @module.private_constant :PRIVATE
+      @pattern = /deprecated/
+    end
+    
+    describe "when accessing the deprecated module" do
+      it "passes the accessing" do
+        @module.deprecate_constant :PUBLIC1
+
+        @module::PUBLIC1.should equal(@value)
+        lambda { @module::PRIVATE }.should raise_error(NameError)
+      end
+      
+      it "warns with a message" do
+        @module.deprecate_constant :PUBLIC1
+
+        lambda { @module::PUBLIC1 }.should complain(@pattern)
+        lambda { @module.const_get :PUBLIC1 }.should complain(@pattern)
+      end
+    end
+    
+    it "accepts multiple symbols and strings as constant names" do
+      @module.deprecate_constant "PUBLIC1", :PUBLIC2
+    
+      lambda { @module::PUBLIC1 }.should complain(@pattern)
+      lambda { @module::PUBLIC2 }.should complain(@pattern)
+    end
+  end
+end
