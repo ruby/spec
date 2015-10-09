7472ad9fecd83ff3a62137fa93367e87c5a634d0
Elia Schito <elia@schito.me>
2015-10-10 00:37:31 +0200

    Test Time#yday for each day of each month
    * Spec based on @wmnnd test in opal/opal#636

--- core/time/yday_spec.rb
+++ core/time/yday_spec.rb
@@ -6,4 +6,17 @@ describe "Time#yday" do
       Time.at(9999999).yday.should == 116
     end
   end
+
+  it 'returns the correct value for each day of each month' do
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    previous = 0
+    days.each_with_index do |days, month|
+      days.times do |day|
+        yday = Time.new(2014, month+1, day+1).yday
+        yday.should == previous+1
+        previous = yday
+      end
+    end
+  end
 end
