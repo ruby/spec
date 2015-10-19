278546de7a4a3098ea5e89cad619a00373fcb841
Brady Wied <brady@bswtechconsulting.com>
2015-10-19 11:45:12 -0600

    Tighten up ancestors specs
    All of the includes do not prevent unexpected results from being in the ancestor list. Tried to exclude any test items

--- core/module/ancestors_spec.rb
+++ core/module/ancestors_spec.rb
@@ -2,14 +2,21 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../fixtures/classes', __FILE__)
 
 describe "Module#ancestors" do
-  it "returns a list of modules included in self (including self)" do
-    BasicObject.ancestors.should == [BasicObject]
-    ModuleSpecs.ancestors.should         include(ModuleSpecs)
-    ModuleSpecs::Basic.ancestors.should  include(ModuleSpecs::Basic)
-    ModuleSpecs::Super.ancestors.should  include(ModuleSpecs::Super, ModuleSpecs::Basic)
-    ModuleSpecs::Parent.ancestors.should include(ModuleSpecs::Parent, Object, Kernel)
-    ModuleSpecs::Child.ancestors.should  include(ModuleSpecs::Child, ModuleSpecs::Super, ModuleSpecs::Basic, ModuleSpecs::Parent, Object, Kernel)
+  def without_test_classes(klasses)
+    ignore = %w{MSpec PP::ObjectMixin}
+    klasses.reject {|k| ignore.any? {|i| k.name.start_with?(i) } }
   end
+  
+  it "returns a list of modules included in self (including self)" do
+    BasicObject.ancestors.should         == [BasicObject]
+    ModuleSpecs.ancestors.should         == [ModuleSpecs]
+    ModuleSpecs::Basic.ancestors.should  == [ModuleSpecs::Basic]
+    ModuleSpecs::Super.ancestors.should  == [ModuleSpecs::Super, ModuleSpecs::Basic]
+    without_test_classes(ModuleSpecs::Parent.ancestors)
+    .should                              == [ModuleSpecs::Parent, Object, Kernel, BasicObject]
+    without_test_classes(ModuleSpecs::Child.ancestors)
+    .should                              == [ModuleSpecs::Child, ModuleSpecs::Super, ModuleSpecs::Basic, ModuleSpecs::Parent, Object, Kernel, BasicObject]
+  end  
 
   it "returns only modules and classes" do
     class << ModuleSpecs::Child; self; end.ancestors.should include(ModuleSpecs::Internal, Class, Module, Object, Kernel)
