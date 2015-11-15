643a506f16d93fc8f9bbe879f09521da057a915e
kares <self@kares.org>
2015-11-15 13:47:17 +0100

    tag dig specs with ruby_version_is 2.3

--- core/array/dig_spec.rb
+++ core/array/dig_spec.rb
@@ -1,21 +1,23 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-describe "Array#dig" do
+ruby_version_is '2.3' do
+  describe "Array#dig" do
 
-  it "returns at with one arg" do
-    ['a'].dig(0).should == 'a'
-    ['a'].dig(1).should be_nil
+    it "returns at with one arg" do
+      ['a'].dig(0).should == 'a'
+      ['a'].dig(1).should be_nil
+    end
+
+    it "recurses array elements" do
+      a = [ [ 1, [2, '3'] ] ]
+      a.dig(0, 0).should == 1
+      a.dig(0, 1, 1).should == '3'
+      a.dig(0, -1, 0).should == 2
+    end
+
+    it "raises without any args" do
+      lambda { [10].dig() }.should raise_error(ArgumentError)
+    end
+
   end
-
-  it "recurses array elements" do
-    a = [ [ 1, [2, '3'] ] ]
-    a.dig(0, 0).should == 1
-    a.dig(0, 1, 1).should == '3'
-    a.dig(0, -1, 0).should == 2
-  end
-
-  it "raises without any args" do
-    lambda { [10].dig() }.should raise_error(ArgumentError)
-  end
-
 end
