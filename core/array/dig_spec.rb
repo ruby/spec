fd7383af1351cb497fa1e4a8e2506be2855bc6ee
kares <self@kares.org>
2015-11-20 09:24:24 +0100

    clarify Array#at method is meant in spec desc

--- core/array/dig_spec.rb
+++ core/array/dig_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path('../../../spec_helper', __FILE__)
 ruby_version_is '2.3' do
   describe "Array#dig" do
 
-    it "returns at with one arg" do
+    it "returns #at with one arg" do
       ['a'].dig(0).should == 'a'
       ['a'].dig(1).should be_nil
     end
