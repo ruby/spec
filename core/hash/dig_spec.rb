9011723c1bb4346bd9c2a6b8a88195d29b5c6e41
Benoit Daloze <eregontp@gmail.com>
2016-01-13 22:01:36 +0100

    Use 1.9-style for Hash literals

--- core/hash/dig_spec.rb
+++ core/hash/dig_spec.rb
@@ -4,7 +4,7 @@ ruby_version_is '2.3' do
   describe "Hash#dig" do
 
     it "returns #[] with one arg" do
-      h = { 0 => false, :a => 1 }
+      h = { 0 => false, a: 1 }
       h.dig(:a).should == 1
       h.dig(0).should be_false
       h.dig(1).should be_nil
@@ -24,7 +24,7 @@ ruby_version_is '2.3' do
 
     it "handles type-mixed deep digging" do
       h = {}
-      h[:foo] = [ { :bar => [ 1 ] }, [ obj = Object.new, 'str' ] ]
+      h[:foo] = [ { bar: [ 1 ] }, [ obj = Object.new, 'str' ] ]
       def obj.dig(*args); [ 42 ] end
 
       h.dig(:foo, 0, :bar).should == [ 1 ]
@@ -37,7 +37,7 @@ ruby_version_is '2.3' do
 
     it "raises TypeError if an intermediate element does not respond to #dig" do
       h = {}
-      h[:foo] = [ { :bar => [ 1 ] }, [ nil, 'str' ] ]
+      h[:foo] = [ { bar: [ 1 ] }, [ nil, 'str' ] ]
       lambda { h.dig(:foo, 0, :bar, 0, 0) }.should raise_error(TypeError)
       lambda { h.dig(:foo, 1, 1, 0) }.should raise_error(TypeError)
     end
