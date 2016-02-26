5940ab5012ff067f77b7a589712c190c3bab1daf
Nobuyoshi Nakada <nobu@ruby-lang.org>
2016-02-26 15:09:05 +0900

    Fix lower version

--- core/numeric/step_spec.rb
+++ core/numeric/step_spec.rb
@@ -78,7 +78,7 @@ describe "Numeric#step" do
       1.step(5, "foo").should be_an_instance_of(enumerator_class)
     end
 
-    invalid_step = ruby_version_is("2.1".."2.3") {ArgumentError}
+    invalid_step = ruby_version_is("2.0".."2.3") {ArgumentError}
     invalid_step ||= StandardError # TypeError
     it "raises an #{invalid_step} if given a block" do
       lambda { 1.step(5, "1") {} }.should raise_error(invalid_step)
