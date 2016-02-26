33b90ebd014cf03a66144db05d97ca1f65320bef
Nobuyoshi Nakada <nobu@ruby-lang.org>
2016-02-26 13:44:00 +0900

    Numeric#step now raises TypeError
    `Numeric#step` now raises `TypeError` instead of `ArgumentError`
    when step is a non-numeric object.
    
    https://bugs.ruby-lang.org/issues/9810

--- core/numeric/step_spec.rb
+++ core/numeric/step_spec.rb
@@ -79,7 +79,7 @@ describe "Numeric#step" do
     end
 
     invalid_step = ruby_version_is("2.0".."2.3") {ArgumentError}
-    invalid_step ||= StandardError # TypeError
+    invalid_step ||= TypeError
     it "raises an #{invalid_step} if given a block" do
       lambda { 1.step(5, "1") {} }.should raise_error(invalid_step)
       lambda { 1.step(5, "0.1") {} }.should raise_error(invalid_step)
