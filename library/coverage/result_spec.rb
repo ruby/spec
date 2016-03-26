c3e050fea6fab56f000265e3001f249e7de20ebc
Uwe Kubosch <uwe@kubosch.no>
2016-03-26 21:28:12 +0100

    Added a basic spec for counts

--- library/coverage/result_spec.rb
+++ library/coverage/result_spec.rb
@@ -3,14 +3,24 @@ require 'coverage'
 
 describe 'Coverage.result' do
   before :all do
+    @class_file = fixture __FILE__, 'some_class.rb'
     @config_file = fixture __FILE__, 'start_coverage.rb'
   end
 
   after :each do
+    $LOADED_FEATURES.delete(@class_file)
     $LOADED_FEATURES.delete(@config_file)
   end
 
-  it 'gives the covered files as a hash with arrays' do
+  it 'gives the covered files as a hash with arrays of count or nil' do
+    Coverage.start
+    require @class_file.chomp('.rb')
+    result = Coverage.result
+
+    result.should == { @class_file => [nil, nil, 1, nil, nil, 1, nil, nil, 0, nil, nil, nil, nil, nil, nil, nil] }
+  end
+
+  it 'second Coverage.start does nothing' do
     Coverage.start
     require @config_file.chomp('.rb')
     result = Coverage.result
