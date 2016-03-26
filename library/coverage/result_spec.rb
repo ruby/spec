607e7ea50f1b6ab519dfa496d76d30bc4861e9e0
Uwe Kubosch <uwe@kubosch.no>
2016-03-26 22:06:16 +0100

    Filter old results from result hash

--- library/coverage/result_spec.rb
+++ library/coverage/result_spec.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 require 'coverage'
 
+# Clear old results from the result hash
+# https://bugs.ruby-lang.org/issues/12220
+def filtered_result
+  Coverage.result.select { |_k, v| v.any? }
+end
+
 describe 'Coverage.result' do
   before :all do
     @class_file = fixture __FILE__, 'some_class.rb'
@@ -15,14 +21,18 @@ describe 'Coverage.result' do
   it 'gives the covered files as a hash with arrays of count or nil' do
     Coverage.start
     require @class_file.chomp('.rb')
-    result = Coverage.result
+    result = filtered_result
 
-    result.should == { @class_file => [nil, nil, 1, nil, nil, 1, nil, nil, 0, nil, nil, nil, nil, nil, nil, nil] }
+    result.should == {
+      @class_file => [
+        nil, nil, 1, nil, nil, 1, nil, nil, 0, nil, nil, nil, nil, nil, nil, nil
+      ]
+    }
   end
 
   it 'no requires/loads should give empty hash' do
     Coverage.start
-    result = Coverage.result
+    result = filtered_result
 
     result.should == {}
   end
@@ -39,13 +49,25 @@ describe 'Coverage.result' do
     end
   end
 
-  it 'second run should give empty hash' do
+  it 'second run should give same result' do
+    Coverage.start
+    load @class_file
+    result1 = filtered_result
+
+    Coverage.start
+    load @class_file
+    result2 = filtered_result
+
+    result2.should == result1
+  end
+
+  it 'second run without load/require should give empty hash' do
     Coverage.start
     require @class_file.chomp('.rb')
     Coverage.result
 
     Coverage.start
-    result = Coverage.result
+    result = filtered_result
 
     result.should == {}
   end
@@ -53,21 +75,21 @@ describe 'Coverage.result' do
   it 'second Coverage.start does nothing' do
     Coverage.start
     require @config_file.chomp('.rb')
-    result = Coverage.result
+    result = filtered_result
 
     result.should == { @config_file => [1, 1, 1] }
   end
 
   it 'should list coverage for the required file starting coverage' do
     require @config_file.chomp('.rb')
-    result = Coverage.result
+    result = Coverage.result.select{ |k,v| v.any? || k == @config_file }
 
     result.should == { @config_file => [] }
   end
 
   it 'should list coverage for the loaded file starting coverage' do
     load @config_file
-    result = Coverage.result
+    result = Coverage.result.select{ |k,v| v.any? || k == @config_file }
 
     result.should == { @config_file => [] }
   end
