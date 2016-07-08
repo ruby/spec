c9634e99a2abd0e51d6f2850412b7fa68d9a24b8
Brady Wied <brady@bswtechconsulting.com>
2016-07-08 14:34:59 -0600

    Add ensure to the begin/rescue/else permutations for rescue_spec

--- language/fixtures/rescue.rb
+++ language/fixtures/rescue.rb
@@ -12,6 +12,22 @@ module RescueSpecs
     end
   end
 
+  def self.begin_else_ensure(raise_exception)
+    begin
+      ScratchPad << :one
+      raise "an error occurred" if raise_exception
+    rescue
+      ScratchPad << :rescue_ran
+      :rescue_val
+    else
+      ScratchPad << :else_ran
+      :val
+    ensure
+      ScratchPad << :ensure_ran
+      :ensure_val
+    end
+  end
+
   def self.begin_else_return(raise_exception)
     begin
       ScratchPad << :one
@@ -22,6 +38,24 @@ module RescueSpecs
     else
       ScratchPad << :else_ran
       :val
+    end
+    ScratchPad << :outside_begin
+    :return_val
+  end
+
+  def self.begin_else_return_ensure(raise_exception)
+    begin
+      ScratchPad << :one
+      raise "an error occurred" if raise_exception
+    rescue
+      ScratchPad << :rescue_ran
+      :rescue_val
+    else
+      ScratchPad << :else_ran
+      :val
+    ensure
+      ScratchPad << :ensure_ran
+      :ensure_val
     end
     ScratchPad << :outside_begin
     :return_val
