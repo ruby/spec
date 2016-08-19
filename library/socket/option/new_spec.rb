17b6275c66aac31f3aa1e47724db2e205c33cf23
Konstantin Shabanov <mail@etehtsea.me>
2016-08-19 09:47:26 +0700

    Improve Socket::Option specs
    - Cleanup
    - Add bool spec

--- library/socket/option/new_spec.rb
+++ library/socket/option/new_spec.rb
@@ -8,6 +8,7 @@ describe "Socket::Option.new" do
     so.level.should == Socket::SOL_SOCKET
     so.optname.should == Socket::SO_KEEPALIVE
   end
+
   it "should accept symbols" do
     so = Socket::Option.new(:AF_INET, :SOL_SOCKET, :SO_KEEPALIVE, [0].pack('i'))
     so.family.should == Socket::AF_INET
@@ -19,12 +20,15 @@ describe "Socket::Option.new" do
     so.level.should == Socket::SOL_SOCKET
     so.optname.should == Socket::SO_KEEPALIVE
   end
+
   it "should raise error on unknown family" do
     lambda { Socket::Option.new(:INET4, :SOCKET, :KEEPALIVE, [0].pack('i')) }.should raise_error(SocketError)
   end
+
   it "should raise error on unknown level" do
     lambda { Socket::Option.new(:INET, :ROCKET, :KEEPALIVE, [0].pack('i')) }.should raise_error(SocketError)
   end
+
   it "should raise error on unknown option name" do
     lambda { Socket::Option.new(:INET, :SOCKET, :ALIVE, [0].pack('i')) }.should raise_error(SocketError)
   end
