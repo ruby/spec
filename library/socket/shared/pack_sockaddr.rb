19c28c399b433630d02e2412acd73f778f1b7370
Konstantin Shabanov <mail@etehtsea.me>
2016-08-22 22:26:00 +0700

    Fix pack_sockaddr shared spec
    Expected to test method wasn't actually used

--- library/socket/shared/pack_sockaddr.rb
+++ library/socket/shared/pack_sockaddr.rb
@@ -1,20 +1,20 @@
 describe :socket_pack_sockaddr_in, shared: true do
   it "packs and unpacks" do
-    sockaddr_in = Socket.pack_sockaddr_in 0, nil
+    sockaddr_in = Socket.public_send(@method, 0, nil)
     port, addr = Socket.unpack_sockaddr_in(sockaddr_in)
     ["127.0.0.1", "::1"].include?(addr).should == true
     port.should == 0
 
-    sockaddr_in = Socket.pack_sockaddr_in 0, ''
+    sockaddr_in = Socket.public_send(@method, 0, '')
     Socket.unpack_sockaddr_in(sockaddr_in).should == [0, '0.0.0.0']
 
-    sockaddr_in = Socket.pack_sockaddr_in 80, '127.0.0.1'
+    sockaddr_in = Socket.public_send(@method, 80, '127.0.0.1')
     Socket.unpack_sockaddr_in(sockaddr_in).should == [80, '127.0.0.1']
 
-    sockaddr_in = Socket.pack_sockaddr_in '80', '127.0.0.1'
+    sockaddr_in = Socket.public_send(@method, '80', '127.0.0.1')
     Socket.unpack_sockaddr_in(sockaddr_in).should == [80, '127.0.0.1']
 
-    sockaddr_in = Socket.pack_sockaddr_in nil, '127.0.0.1'
+    sockaddr_in = Socket.public_send(@method, nil, '127.0.0.1')
     Socket.unpack_sockaddr_in(sockaddr_in).should == [0, '127.0.0.1']
   end
 end
@@ -22,7 +22,7 @@ end
 describe :socket_pack_sockaddr_un, shared: true do
   platform_is_not :windows do
     it "packs and unpacks" do
-      sockaddr_un = Socket.pack_sockaddr_un '/tmp/s'
+      sockaddr_un = Socket.public_send(@method, '/tmp/s')
       Socket.unpack_sockaddr_un(sockaddr_un).should == '/tmp/s'
     end
   end
