5b6ebddb9786426e2f7b4dae4478ada22ce9e975
mjago <martyn.jago@btinternet.com>
2016-10-19 22:31:55 +0100

    Array pack with format J and j specs

---
+++ core/array/pack/j_spec.rb
@@ -0,0 +1,127 @@
+require File.expand_path('../../../../spec_helper', __FILE__)
+require File.expand_path('../../fixtures/classes', __FILE__)
+require File.expand_path('../shared/basic', __FILE__)
+require File.expand_path('../shared/numeric_basic', __FILE__)
+require File.expand_path('../shared/integer', __FILE__)
+require 'fiddle'
+
+def pointer_size_is size
+  yield if Fiddle::SIZEOF_INTPTR_T == size
+end
+
+ruby_version_is '2.3' do
+
+  pointer_size_is 8 do
+
+    describe "Array#pack with format 'J'" do
+      it_behaves_like :array_pack_basic, 'J'
+      it_behaves_like :array_pack_basic_non_float, 'J'
+      it_behaves_like :array_pack_arguments, 'J'
+      it_behaves_like :array_pack_numeric_basic, 'J'
+      it_behaves_like :array_pack_integer, 'J'
+    end
+
+    describe "Array#pack with format 'j'" do
+      it_behaves_like :array_pack_basic, 'j'
+      it_behaves_like :array_pack_basic_non_float, 'j'
+      it_behaves_like :array_pack_arguments, 'j'
+      it_behaves_like :array_pack_numeric_basic, 'j'
+      it_behaves_like :array_pack_integer, 'j'
+    end
+
+    describe "Array#pack with modifier '_'" do
+      it_behaves_like :array_pack_64bit_le, 'J_'
+      it_behaves_like :array_pack_64bit_le, 'j_'
+    end
+
+    describe "Array#pack with modifier '!'" do
+      it_behaves_like :array_pack_64bit_le, 'J!'
+      it_behaves_like :array_pack_64bit_le, 'j!'
+    end
+
+    describe "Array#pack with modifier '<' and '_'" do
+      it_behaves_like :array_pack_64bit_le, 'J<_'
+      it_behaves_like :array_pack_64bit_le, 'J_<'
+      it_behaves_like :array_pack_64bit_le, 'j<_'
+      it_behaves_like :array_pack_64bit_le, 'j_<'
+    end
+
+    describe "Array#pack with modifier '<' and '!'" do
+      it_behaves_like :array_pack_64bit_le, 'J<!'
+      it_behaves_like :array_pack_64bit_le, 'J!<'
+      it_behaves_like :array_pack_64bit_le, 'j<!'
+      it_behaves_like :array_pack_64bit_le, 'j!<'
+    end
+
+    describe "Array#pack with modifier '>' and '_'" do
+      it_behaves_like :array_pack_64bit_be, 'J>_'
+      it_behaves_like :array_pack_64bit_be, 'J_>'
+      it_behaves_like :array_pack_64bit_be, 'j>_'
+      it_behaves_like :array_pack_64bit_be, 'j_>'
+    end
+
+    describe "Array#pack with modifier '>' and '!'" do
+      it_behaves_like :array_pack_64bit_be, 'J>!'
+      it_behaves_like :array_pack_64bit_be, 'J!>'
+      it_behaves_like :array_pack_64bit_be, 'j>!'
+      it_behaves_like :array_pack_64bit_be, 'j!>'
+    end
+
+    pointer_size_is 4 do
+
+      describe "Array#pack with format 'J'" do
+        it_behaves_like :array_pack_basic, 'J'
+        it_behaves_like :array_pack_basic_non_float, 'J'
+        it_behaves_like :array_pack_arguments, 'J'
+        it_behaves_like :array_pack_numeric_basic, 'J'
+        it_behaves_like :array_pack_integer, 'J'
+      end
+
+      describe "Array#pack with format 'j'" do
+        it_behaves_like :array_pack_basic, 'j'
+        it_behaves_like :array_pack_basic_non_float, 'j'
+        it_behaves_like :array_pack_arguments, 'j'
+        it_behaves_like :array_pack_numeric_basic, 'j'
+        it_behaves_like :array_pack_integer, 'j'
+      end
+
+      describe "Array#pack with modifier '_'" do
+        it_behaves_like :array_pack_32bit_le, 'J_'
+        it_behaves_like :array_pack_32bit_le, 'j_'
+      end
+
+      describe "Array#pack with modifier '!'" do
+        it_behaves_like :array_pack_32bit_le, 'J!'
+        it_behaves_like :array_pack_32bit_le, 'j!'
+      end
+
+      describe "Array#pack with modifier '<' and '_'" do
+        it_behaves_like :array_pack_32bit_le, 'J<_'
+        it_behaves_like :array_pack_32bit_le, 'J_<'
+        it_behaves_like :array_pack_32bit_le, 'j<_'
+        it_behaves_like :array_pack_32bit_le, 'j_<'
+      end
+
+      describe "Array#pack with modifier '<' and '!'" do
+        it_behaves_like :array_pack_32bit_le, 'J<!'
+        it_behaves_like :array_pack_32bit_le, 'J!<'
+        it_behaves_like :array_pack_32bit_le, 'j<!'
+        it_behaves_like :array_pack_32bit_le, 'j!<'
+      end
+
+      describe "Array#pack with modifier '>' and '_'" do
+        it_behaves_like :array_pack_32bit_be, 'J>_'
+        it_behaves_like :array_pack_32bit_be, 'J_>'
+        it_behaves_like :array_pack_32bit_be, 'j>_'
+        it_behaves_like :array_pack_32bit_be, 'j_>'
+      end
+
+      describe "Array#pack with modifier '>' and '!'" do
+        it_behaves_like :array_pack_32bit_be, 'J>!'
+        it_behaves_like :array_pack_32bit_be, 'J!>'
+        it_behaves_like :array_pack_32bit_be, 'j>!'
+        it_behaves_like :array_pack_32bit_be, 'j!>'
+      end
+    end
+  end
+end
