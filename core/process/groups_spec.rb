97f987ff50ae07bbce40b1056925f06cae35d060
Benoit Daloze <eregontp@gmail.com>
2016-11-11 11:45:12 +0100

    Fix Process#groups spec

--- core/process/groups_spec.rb
+++ core/process/groups_spec.rb
@@ -3,11 +3,9 @@ require File.expand_path('../../../spec_helper', __FILE__)
 describe "Process.groups" do
   platform_is_not :windows do
     it "gets an Array of the gids of groups in the supplemental group access list" do
-      groups = `id -G`.scan(/\d+/).map {|i| i.to_i}
+      groups = `id -G`.scan(/\d+/).map { |i| i.to_i }
 
-      Process.groups.each do |g|
-        groups.should include(g)
-      end
+      Process.groups.sort.should == groups.sort
     end
 
     # NOTE: This is kind of sketchy.
