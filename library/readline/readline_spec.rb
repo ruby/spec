f0d5c14d58fabb98498a98ff4867c3fcdcbe9f87
Benoit Daloze <eregontp@gmail.com>
2016-11-27 17:36:55 +0100

    Simplify Readline.readline spec by delegating to an external process

--- library/readline/readline_spec.rb
+++ library/readline/readline_spec.rb
@@ -5,31 +5,23 @@ process_is_foreground do
     describe "Readline.readline" do
       before :each do
         @file = tmp('readline')
-        File.open(@file, 'w') do |file|
-          file.puts "test\n"
-        end
-        @stdin_back = STDIN.dup
-        @stdout_back = STDOUT.dup
-        STDIN.reopen(@file, 'r')
-        null = (IO.const_defined?(:NULL) ? IO::NULL :
-                  PlatformGuard.windows? ? "nul" : "/dev/null")
-        STDOUT.reopen(null)
+        touch(@file) { |f|
+          f.puts "test"
+        }
       end
 
       after :each do
-        STDIN.reopen(@stdin_back)
-        @stdin_back.close
         rm_r @file
-        STDOUT.reopen(@stdout_back)
-        @stdout_back.close
       end
 
       it "returns the input string" do
-        Readline.readline.should == "test"
+        out = ruby_exe('puts Readline.readline', options: "-rreadline", args: "< #{@file}")
+        out.should == "test\ntest\n"
       end
 
       it "taints the returned strings" do
-        Readline.readline.tainted?.should be_true
+        out = ruby_exe('puts Readline.readline.tainted?', options: "-rreadline", args: "< #{@file}")
+        out.should == "test\ntrue\n"
       end
     end
   end
