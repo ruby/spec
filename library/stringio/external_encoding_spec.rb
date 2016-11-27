6064faaee42ea46cf82296c7d8ac5b15a43b6010
Benoit Daloze <eregontp@gmail.com>
2016-11-27 16:39:57 +0100

    Use another example than UTF-8 for StringIO#external_encoding

--- library/stringio/external_encoding_spec.rb
+++ library/stringio/external_encoding_spec.rb
@@ -4,7 +4,7 @@ require File.expand_path('../../../spec_helper', __FILE__)
 describe "StringIO#external_encoding" do
   it "gets the encoding of the underlying String" do
     io = StringIO.new
-    io.set_encoding Encoding::UTF_8
-    io.external_encoding.should == Encoding::UTF_8
+    io.set_encoding Encoding::EUC_JP
+    io.external_encoding.should == Encoding::EUC_JP
   end
 end
