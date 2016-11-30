360a81a2fd4a7a51a30712020b36120c638f5ba9
Nobuyoshi Nakada <nobu@ruby-lang.org>
2016-11-30 15:24:22 +0900

    Fix for libedit
    Readline of libedit seems not to echo back when input comes from a
    file.
    http://rubyci.org/logs/rubyci.s3.amazonaws.com/osx1011/ruby-trunk/log/20161130T034500Z.fail.html.gz
    
    ```
    Expected "test\n"
     to equal "test\n" + "test\n"
    ```
    ```
    Expected "true\n"
     to equal "test\n" + "true\n"
    ```

--- library/readline/readline_spec.rb
+++ library/readline/readline_spec.rb
@@ -18,12 +18,12 @@ process_is_foreground do
       platform_is_not :windows do
         it "returns the input string" do
           out = ruby_exe('puts Readline.readline', options: "-rreadline", args: "< #{@file}")
-          out.should == "test\ntest\n"
+          out.should =~ /^test\Z/
         end
 
         it "taints the returned strings" do
           out = ruby_exe('puts Readline.readline.tainted?', options: "-rreadline", args: "< #{@file}")
-          out.should == "test\ntrue\n"
+          out.should =~ /^true\Z/
         end
       end
     end
