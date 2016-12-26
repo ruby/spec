5611438c968d5a5c2d8d15becd101a69e81d3db7
Kenichi Kamiya <kachick1@gmail.com>
2016-12-26 22:10:37 +0900

    Write Hash#transform_values! specs

--- core/hash/transform_values_spec.rb
+++ core/hash/transform_values_spec.rb
@@ -25,4 +25,29 @@ ruby_version_is "2.4" do
       end
     end
   end
+
+  describe "Hash#transform_values!" do
+    before :each do
+      @hash = { a: 1, b: 2, c: 3 }
+    end
+
+    it "returns self" do
+      @hash.transform_values!(&:succ).should equal(@hash)
+    end
+
+    it "updates self as transformed values with the given block" do
+      @hash.transform_values!(&:succ)
+      @hash.should ==  { a: 2, b: 3, c: 4 }
+    end
+
+    context "when no block is given" do
+      it "returns a sized Enumerator" do
+        enumerator = @hash.transform_values!
+        enumerator.should be_an_instance_of(enumerator_class)
+        enumerator.size.should == @hash.size
+        enumerator.each(&:succ)
+        @hash.should == { a: 2, b: 3, c: 4 }
+      end
+    end
+  end
 end
