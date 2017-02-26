e3770c353bb1856e7d8f6d0b1f4f6c34f7d7196f
Benoit Daloze <eregontp@gmail.com>
2017-02-26 17:09:26 +0100

    Random.raw_seed is available since 2.3

--- core/random/raw_seed_spec.rb
+++ core/random/raw_seed_spec.rb
@@ -1,26 +1,28 @@
 # -*- encoding: binary -*-
 require File.expand_path('../../../spec_helper', __FILE__)
 
-describe "Random.raw_seed" do
-  it "returns a String" do
-    Random.raw_seed(1).should be_an_instance_of(String)
-  end
+ruby_version_is "2.3" do
+  describe "Random.raw_seed" do
+    it "returns a String" do
+      Random.raw_seed(1).should be_an_instance_of(String)
+    end
 
-  it "returns a String of the length given as argument" do
-    Random.raw_seed(15).length.should == 15
-  end
+    it "returns a String of the length given as argument" do
+      Random.raw_seed(15).length.should == 15
+    end
 
-  it "raises an ArgumentError on a negative size" do
-    lambda {
-      Random.raw_seed(-1)
-    }.should raise_error(ArgumentError)
-  end
+    it "raises an ArgumentError on a negative size" do
+      lambda {
+        Random.raw_seed(-1)
+      }.should raise_error(ArgumentError)
+    end
 
-  it "returns an ASCII-8BIT String" do
-    Random.raw_seed(15).encoding.should == Encoding::ASCII_8BIT
-  end
+    it "returns an ASCII-8BIT String" do
+      Random.raw_seed(15).encoding.should == Encoding::ASCII_8BIT
+    end
 
-  it "returns a random binary String" do
-    Random.raw_seed(12).should_not == Random.raw_seed(12)
+    it "returns a random binary String" do
+      Random.raw_seed(12).should_not == Random.raw_seed(12)
+    end
   end
 end
