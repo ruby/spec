566dcf6abc1b5536ec65a7e4de1c6de24aec6350
Nobuyoshi Nakada <nobu@ruby-lang.org>
2017-03-14 17:26:35 +0900

    Split single % cases
    Split cases `%` before a newline and before a NUL.
    And `%` at the end differs from the both.

--- core/string/modulo_spec.rb
+++ core/string/modulo_spec.rb
@@ -14,14 +14,20 @@ describe "String#%" do
     ("%d%% %s" % [10, "of chickens!"]).should == "10% of chickens!"
   end
 
-  it "formats single % characters before a newline or NULL as literal %s" do
+  it "formats single % characters at the end as literal %s" do
     ("%" % []).should == "%"
     ("foo%" % []).should == "foo%"
+  end
+
+  it "formats single % characters before a newline as literal %s" do
     ("%\n" % []).should == "%\n"
     ("foo%\n" % []).should == "foo%\n"
+    ("%\n.3f" % 1.2).should == "%\n.3f"
+  end
+
+  it "formats single % characters before a NUL as literal %s" do
     ("%\0" % []).should == "%\0"
     ("foo%\0" % []).should == "foo%\0"
-    ("%\n.3f" % 1.2).should == "%\n.3f"
     ("%\0.3f" % 1.2).should == "%\0.3f"
   end
 
