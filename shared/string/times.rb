d13405632c9118ff79d07a3a7f035a2569fb90c3
Satoshi Matsumoto <kaorimatz@gmail.com>
2017-03-29 20:50:45 +0900

    Use fixnum_max helper

--- shared/string/times.rb
+++ shared/string/times.rb
@@ -50,15 +50,7 @@ describe :string_times, shared: true do
     end
   end
 
-  platform_is wordsize: 32 do
-    it "raises an ArgumentError if the length of the resulting string doesn't fit into a long" do
-      lambda { @object.call("abc", 1 << 31 - 1) }.should raise_error(ArgumentError)
-    end
-  end
-
-  platform_is wordsize: 64 do
-    it "raises an ArgumentError if the length of the resulting string doesn't fit into a long" do
-      lambda { @object.call("abc", 1 << 63 - 1) }.should raise_error(ArgumentError)
-    end
+  it "raises an ArgumentError if the length of the resulting string doesn't fit into a fixnum" do
+    lambda { @object.call("abc", fixnum_max) }.should raise_error(ArgumentError)
   end
 end
