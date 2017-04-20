c8bb8f02dfeed424621ecb48dbbdf7babd67df8a
Benoit Daloze <eregontp@gmail.com>
2017-04-20 22:58:09 +0800

    Remove useless before/after in Dir.home spec

--- core/dir/home_spec.rb
+++ core/dir/home_spec.rb
@@ -2,14 +2,6 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../fixtures/common', __FILE__)
 
 describe "Dir.home" do
-  before :all do
-    DirSpecs.create_mock_dirs
-  end
-
-  after :all do
-    DirSpecs.delete_mock_dirs
-  end
-
   it "returns the current user's home directory as a string if called without arguments" do
     Dir.home.should == home_directory
   end
