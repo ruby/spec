27080f0a67ec0ca3709c74e2a14ac351f8a3b22d
Benoit Daloze <eregontp@gmail.com>
2017-04-20 23:54:04 +0800

    Move TTY checks inside the example

--- core/io/shared/tty.rb
+++ core/io/shared/tty.rb
@@ -2,16 +2,16 @@ require File.expand_path('../../fixtures/classes', __FILE__)
 
 describe :io_tty, shared: true do
   platform_is_not :windows do
-    begin
-      # check to enabled tty
-      File.open('/dev/tty') {}
-
-      # Yeah, this will probably break.
-      it "returns true if this stream is a terminal device (TTY)" do
+    it "returns true if this stream is a terminal device (TTY)" do
+      begin
+        # check to enabled tty
+        File.open('/dev/tty') {}
+      rescue Errno::ENXIO
+        # workaround for not configured environment like OS X
+        1.should == 1
+      else
         File.open('/dev/tty') { |f| f.send(@method) }.should == true
       end
-    rescue Errno::ENXIO
-      # workaround for not configured environment
     end
   end
 
