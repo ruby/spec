acb27a7c81fe47aacd56b020bb63652c30221aa2
Benoit Daloze <eregontp@gmail.com>
2017-04-25 23:50:46 +0200

    Expect warnings for circular require

--- core/kernel/shared/require.rb
+++ core/kernel/shared/require.rb
@@ -222,7 +222,10 @@ describe :kernel_require, shared: true do
 
     it "loads a file that recursively requires itself" do
       path = File.expand_path "recursive_require_fixture.rb", CODE_LOADING_DIR
-      @object.require(path).should be_true
+      -> {
+        $VERBOSE = true
+        @object.require(path).should be_true
+      }.should complain(/circular require considered harmful/)
       ScratchPad.recorded.should == [:loaded]
     end
   end
