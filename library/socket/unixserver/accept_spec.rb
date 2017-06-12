83485ce19867df8781080640a1c7f51f708ec3aa
Benoit Daloze <eregontp@gmail.com>
2017-06-12 15:01:46 +0200

    Use before/after in UNIXServer#accept spec

--- library/socket/unixserver/accept_spec.rb
+++ library/socket/unixserver/accept_spec.rb
@@ -5,33 +5,34 @@ platform_is_not :windows do
   describe "UNIXServer#accept" do
     before :each do
       @path = SocketSpecs.socket_path
+      @server = UNIXServer.open(@path)
     end
 
     after :each do
+      @server.close if @server
       SocketSpecs.rm_socket @path
     end
 
     it "accepts what is written by the client" do
-      server = UNIXServer.open(@path)
       client = UNIXSocket.open(@path)
 
       client.send('hello', 0)
 
-      sock = server.accept
-      data, info = sock.recvfrom(5)
+      sock = @server.accept
+      begin
+        data, info = sock.recvfrom(5)
 
-      data.should == 'hello'
-      info.should_not be_empty
-
-      server.close
-      client.close
-      sock.close
+        data.should == 'hello'
+        info.should_not be_empty
+      ensure
+        sock.close
+        client.close
+      end
     end
 
     it "can be interrupted by Thread#kill" do
-      server = UNIXServer.new(@path)
       t = Thread.new {
-        server.accept
+        @server.accept
       }
       Thread.pass while t.status and t.status != "sleep"
 
@@ -43,14 +44,11 @@ platform_is_not :windows do
         a += 1
       end
       a.should < 5000
-
-      server.close
     end
 
     it "can be interrupted by Thread#raise" do
-      server = UNIXServer.new(@path)
       t = Thread.new {
-        server.accept
+        @server.accept
       }
       Thread.pass while t.status and t.status != "sleep"
 
@@ -58,7 +56,6 @@ platform_is_not :windows do
       ex = Exception.new
       t.raise ex
       lambda { t.join }.should raise_error(Exception)
-      server.close
     end
   end
 end
