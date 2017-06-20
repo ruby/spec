5bc06e044f8be5804d7a12ce3dd05d160df21876
Kenichi Kamiya <kachick1@gmail.com>
2017-06-21 04:27:28 +0900

    Back a spec
    ref: https://github.com/ruby/spec/pull/447#discussion_r123060439

--- core/enumerable/uniq_spec.rb
+++ core/enumerable/uniq_spec.rb
@@ -6,5 +6,22 @@ ruby_version_is '2.4' do
     it 'returns an array that contains only unique elements' do
       [0, 1, 2, 3].to_enum.uniq { |n| n.even? }.should == [0, 1]
     end
+
+    context 'when yielded with multiple arguments' do
+      before :each do
+        @enum = Object.new.to_enum
+        class << @enum
+          def each
+            yield 0, 'foo'
+            yield 1, 'FOO'
+            yield 2, 'bar'
+          end
+        end
+      end
+
+      it 'returns an array that contains only first argument of the yield' do
+        @enum.uniq { |_, label| label.downcase }.should == [0, 2]
+      end
+    end
   end
 end
