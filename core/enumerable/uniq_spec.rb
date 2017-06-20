2fa99e3b7f8498af228b861afe62f487fb3df080
Kenichi Kamiya <kachick1@gmail.com>
2017-06-21 03:09:59 +0900

    Simplify an example
    * https://github.com/ruby/spec/pull/447#discussion_r123049073
    * https://github.com/ruby/spec/pull/447#discussion_r123050724

--- core/enumerable/uniq_spec.rb
+++ core/enumerable/uniq_spec.rb
@@ -3,19 +3,8 @@ require File.expand_path('../fixtures/classes', __FILE__)
 
 ruby_version_is '2.4' do
   describe 'Enumerable#uniq'do
-    before :each do
-      @enum = Object.new.to_enum
-      class << @enum
-        def each
-          yield 0, 'foo'
-          yield 1, 'FOO'
-          yield 2, 'bar'
-        end
-      end
-    end
-
-    it 'returns an array that contains only unique result of the given block' do
-      @enum.uniq { |_, label| label.downcase }.should == [0, 2]
+    it 'returns an array that contains only unique elements' do
+      [0, 1, 2, 3].to_enum.uniq { |n| n.even? }.should == [0, 1]
     end
   end
 end
