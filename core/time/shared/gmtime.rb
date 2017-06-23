c2285d80cd675dadd1d1aadd8b4015a25d7d168a
Benoit Daloze <eregontp@gmail.com>
2017-06-23 16:50:31 +0200

    Improve Time#{gmtime,utc} spec

--- core/time/shared/gmtime.rb
+++ core/time/shared/gmtime.rb
@@ -1,10 +1,17 @@
 describe :time_gmtime, shared: true do
-  it "returns the utc representation of time" do
+  it "converts self to UTC, modifying the receiver" do
     # Testing with America/Regina here because it doesn't have DST.
     with_timezone("CST", -6) do
       t = Time.local(2007, 1, 9, 6, 0, 0)
       t.send(@method)
       t.should == Time.gm(2007, 1, 9, 12, 0, 0)
+    end
+  end
+
+  it "returns self" do
+    with_timezone("CST", -6) do
+      t = Time.local(2007, 1, 9, 12, 0, 0)
+      t.send(@method).should equal(t)
     end
   end
 
