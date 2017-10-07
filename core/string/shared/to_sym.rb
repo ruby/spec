3d5efd7458647b2783c5388b72f99b0600fd0393
Benoit Daloze <eregontp@gmail.com>
2017-10-07 22:19:17 +0200

    Symbol are unique so also test their identity with #equal?

--- core/string/shared/to_sym.rb
+++ core/string/shared/to_sym.rb
@@ -1,43 +1,42 @@
 describe :string_to_sym, shared: true do
   it "returns the symbol corresponding to self" do
-    "Koala".send(@method).should == :Koala
-    'cat'.send(@method).should == :cat
-    '@cat'.send(@method).should == :@cat
-    'cat and dog'.send(@method).should == :"cat and dog"
-    "abc=".send(@method).should == :abc=
+    "Koala".send(@method).should equal :Koala
+    'cat'.send(@method).should equal :cat
+    '@cat'.send(@method).should equal :@cat
+    'cat and dog'.send(@method).should equal :"cat and dog"
+    "abc=".send(@method).should equal :abc=
   end
 
   it "does not special case +(binary) and -(binary)" do
-    "+(binary)".send(@method).should == :"+(binary)"
-    "-(binary)".send(@method).should == :"-(binary)"
+    "+(binary)".send(@method).should equal :"+(binary)"
+    "-(binary)".send(@method).should equal :"-(binary)"
   end
 
   it "does not special case certain operators" do
-    [ ["!@", :"!@"],
-      ["~@", :"~@"],
-      ["!(unary)", :"!(unary)"],
-      ["~(unary)", :"~(unary)"],
-      ["+(unary)", :"+(unary)"],
-      ["-(unary)", :"-(unary)"]
-    ].should be_computed_by(@method)
+    "!@".send(@method).should equal :"!@"
+    "~@".send(@method).should equal :"~@"
+    "!(unary)".send(@method).should equal :"!(unary)"
+    "~(unary)".send(@method).should equal :"~(unary)"
+    "+(unary)".send(@method).should equal :"+(unary)"
+    "-(unary)".send(@method).should equal :"-(unary)"
   end
 
   it "returns a US-ASCII Symbol for a UTF-8 String containing only US-ASCII characters" do
     sym = "foobar".send(@method)
     sym.encoding.should == Encoding::US_ASCII
-    sym.should == :"foobar"
+    sym.should equal :"foobar"
   end
 
   it "returns a US-ASCII Symbol for a binary String containing only US-ASCII characters" do
     sym = "foobar".b.send(@method)
     sym.encoding.should == Encoding::US_ASCII
-    sym.should == :"foobar"
+    sym.should equal :"foobar"
   end
 
   it "returns a UTF-8 Symbol for a UTF-8 String containing non US-ASCII characters" do
     sym = "il était une fois".send(@method)
     sym.encoding.should == Encoding::UTF_8
-    sym.should == :"il était une #{'fois'}"
+    sym.should equal :"il était une #{'fois'}"
   end
 
   it "returns a UTF-16LE Symbol for a UTF-16LE String containing non US-ASCII characters" do
