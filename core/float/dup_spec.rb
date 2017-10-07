1153e301087befe041fbfb7c58676b8bd8969dc0
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-06 21:46:52 -0400

    Tweak spec so it doesn't assume that number literals are always the same object.
    It's the case for Fixnums for most but not all Floats (and only in recent versions of Ruby).

--- core/float/dup_spec.rb
+++ core/float/dup_spec.rb
@@ -3,7 +3,8 @@ require File.expand_path('../../../spec_helper', __FILE__)
 ruby_version_is '2.4' do
   describe "Float#dup" do
     it "returns self" do
-      2.4.dup.should equal(2.4)
+      float = 2.4
+      float.dup.should equal(float)
     end
   end
 end
--- core/integer/dup_spec.rb
+++ core/integer/dup_spec.rb
@@ -3,7 +3,8 @@ require File.expand_path('../../../spec_helper', __FILE__)
 ruby_version_is '2.4' do
   describe "Integer#dup" do
     it "returns self" do
-      2.dup.should equal(2)
+      int = 2
+      int.dup.should equal(int)
     end
   end
 end
