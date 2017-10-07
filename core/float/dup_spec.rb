6e943e1df44868be5581b685683157c1488153e7
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-06 21:43:27 -0400

    Fix whitespace [ci-skip]

--- core/float/dup_spec.rb
+++ core/float/dup_spec.rb
@@ -1,9 +1,9 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
 ruby_version_is '2.4' do
-    describe "Float#dup" do
-        it "returns self" do
-            2.4.dup.should equal(2.4)
-        end
+  describe "Float#dup" do
+    it "returns self" do
+      2.4.dup.should equal(2.4)
     end
+  end
 end
--- core/integer/dup_spec.rb
+++ core/integer/dup_spec.rb
@@ -1,9 +1,9 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
 ruby_version_is '2.4' do
-    describe "Integer#dup" do
-        it "returns self" do
-            2.dup.should equal(2)
-        end
+  describe "Integer#dup" do
+    it "returns self" do
+      2.dup.should equal(2)
     end
+  end
 end
