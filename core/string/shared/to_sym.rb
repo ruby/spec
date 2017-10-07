1ff0e0aa2fee134fdeb54a7ac398c03e542d0377
Benoit Daloze <eregontp@gmail.com>
2017-10-07 22:10:20 +0200

    Improve String#to_sym spec to specify the encoding of the resulting Symbol

--- core/string/shared/to_sym.rb
+++ core/string/shared/to_sym.rb
@@ -21,4 +21,44 @@ describe :string_to_sym, shared: true do
       ["-(unary)", :"-(unary)"]
     ].should be_computed_by(@method)
   end
+
+  it "returns a US-ASCII Symbol for a UTF-8 String containing only US-ASCII characters" do
+    sym = "foobar".send(@method)
+    sym.encoding.should == Encoding::US_ASCII
+    sym.should == :"foobar"
+  end
+
+  it "returns a US-ASCII Symbol for a binary String containing only US-ASCII characters" do
+    sym = "foobar".b.send(@method)
+    sym.encoding.should == Encoding::US_ASCII
+    sym.should == :"foobar"
+  end
+
+  it "returns a UTF-8 Symbol for a UTF-8 String containing non US-ASCII characters" do
+    sym = "il était une fois".send(@method)
+    sym.encoding.should == Encoding::UTF_8
+    sym.should == :"il était une #{'fois'}"
+  end
+
+  it "returns a UTF-16LE Symbol for a UTF-16LE String containing non US-ASCII characters" do
+    utf16_str = "UtéF16".encode(Encoding::UTF_16LE)
+    sym = utf16_str.send(@method)
+    sym.encoding.should == Encoding::UTF_16LE
+    sym.to_s.should == utf16_str
+  end
+
+  it "returns a binary Symbol for a binary String containing non US-ASCII characters" do
+    binary_string = "binarí".b
+    sym = binary_string.send(@method)
+    sym.encoding.should == Encoding::BINARY
+    sym.to_s.should == binary_string
+  end
+
+  it "raises an EncodingError for UTF-8 String containing invalid bytes" do
+    invalid_utf8 = "\xC3"
+    invalid_utf8.valid_encoding?.should == false
+    -> {
+      invalid_utf8.send(@method)
+    }.should raise_error(EncodingError, /invalid/)
+  end
 end
