3e49c44b7635c79c5c50a63f4ad306a26cb6dd9d
Andrew Nordman <cadwallion@gmail.com>
2017-10-11 12:10:06 +0000

    Updates for clarity on to_time

--- library/datetime/to_time_spec.rb
+++ library/datetime/to_time_spec.rb
@@ -1,25 +1,23 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 require 'date'
 
-ruby_version_is "2.4" do
-  describe "DateTime#to_time" do
-    it "yields a new Time object" do
-      DateTime.now.to_time.should be_kind_of(Time)
-    end
+describe "DateTime#to_time" do
+  it "yields a new Time object" do
+    DateTime.now.to_time.should be_kind_of(Time)
+  end
 
-    it "preserves the same time regardless of local time or zone" do
-      with_timezone("Pactific/Pago_Pago", -11) do
-        date = DateTime.new(2012, 12, 24, 12, 23, 00, '+03:00')
-        time = date.to_time
+  it "preserves the same time regardless of local time or zone" do
+    date = DateTime.new(2012, 12, 24, 12, 23, 00, '+03:00')
+    time = date.to_time
 
-        time.strftime("%z").should == date.strftime("%z")
-        time.year.should == date.year
-        time.mon.should == date.mon
-        time.day.should == date.day
-        time.hour.should == date.hour
-        time.min.should == date.min
-        time.sec.should == date.sec
-      end
+    with_timezone("Pactific/Pago_Pago", -11) do
+      time.utc_offset.should == 3 * 3600
+      time.year.should == date.year
+      time.mon.should == date.mon
+      time.day.should == date.day
+      time.hour.should == date.hour
+      time.min.should == date.min
+      time.sec.should == date.sec
     end
   end
 end
--- library/time/to_time_spec.rb
+++ library/time/to_time_spec.rb
@@ -4,8 +4,9 @@ require 'time'
 ruby_version_is "2.4" do
   describe "Time#to_time" do
     it "returns itself in the same timezone" do
+      time = Time.new(2012, 2, 21, 10, 11, 12)
+
       with_timezone("America/Regina") do
-        time = Time.new(2012, 2, 21, 10, 11, 12)
         time.to_time.should == time
       end
 
