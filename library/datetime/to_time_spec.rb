2956ad1dcc0eaf4566da5c9c7ef90b1084d6ea5e
Andrew Nordman <cadwallion@gmail.com>
2017-10-11 14:54:10 +0000

    Version guard on timezone preservation of DateTime#to_time

--- library/datetime/to_time_spec.rb
+++ library/datetime/to_time_spec.rb
@@ -6,18 +6,20 @@ describe "DateTime#to_time" do
     DateTime.now.to_time.should be_kind_of(Time)
   end
 
-  it "preserves the same time regardless of local time or zone" do
-    date = DateTime.new(2012, 12, 24, 12, 23, 00, '+03:00')
-    time = date.to_time
+  ruby_version_is "2.4" do
+    it "preserves the same time regardless of local time or zone" do
+      date = DateTime.new(2012, 12, 24, 12, 23, 00, '+03:00')
+      time = date.to_time
 
-    with_timezone("Pactific/Pago_Pago", -11) do
-      time.utc_offset.should == 3 * 3600
-      time.year.should == date.year
-      time.mon.should == date.mon
-      time.day.should == date.day
-      time.hour.should == date.hour
-      time.min.should == date.min
-      time.sec.should == date.sec
+      with_timezone("Pactific/Pago_Pago", -11) do
+        time.utc_offset.should == 3 * 3600
+        time.year.should == date.year
+        time.mon.should == date.mon
+        time.day.should == date.day
+        time.hour.should == date.hour
+        time.min.should == date.min
+        time.sec.should == date.sec
+      end
     end
   end
 end
