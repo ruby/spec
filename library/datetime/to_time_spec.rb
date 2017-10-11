c4cdc6b7a3e296ecb65d977be01bf1ea99977ca0
Andrew Nordman <cadwallion@gmail.com>
2017-10-11 16:15:21 +0000

    Moves the to_time call into the with_timezone to verify no timezone shift

--- library/datetime/to_time_spec.rb
+++ library/datetime/to_time_spec.rb
@@ -9,9 +9,10 @@ describe "DateTime#to_time" do
   ruby_version_is "2.4" do
     it "preserves the same time regardless of local time or zone" do
       date = DateTime.new(2012, 12, 24, 12, 23, 00, '+03:00')
-      time = date.to_time
 
       with_timezone("Pactific/Pago_Pago", -11) do
+        time = date.to_time
+
         time.utc_offset.should == 3 * 3600
         time.year.should == date.year
         time.mon.should == date.mon
