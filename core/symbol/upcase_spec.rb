3f4c816a9fd3115d3a017ee192c98323fd2fcea7
Kaid <kaid@KaidsMacbookPro.local>
2017-10-13 11:14:49 -0400

    Character manipulation coverage for symbol ruby 2.4
    Symbol#upcase, Symbol#downcase, Symbol#capitalize, and Symbol#swapcase now
    work for all of Unicode

--- core/symbol/upcase_spec.rb
+++ core/symbol/upcase_spec.rb
@@ -16,6 +16,13 @@ describe "Symbol#upcase" do
     end
   end
 
+  ruby_version_is '2.4' do
+    it "capitalizes all characters regardless of if it is Unicode"
+      "äöü".to_sym.upcase.should == :"ÄÖÜ"
+      "aou".to_sym.upcase.should == :"AOU"
+    end
+  end
+
   it "leaves non-alphabetic ASCII characters as they were" do
     "Glark?!?".to_sym.upcase.should == :"GLARK?!?"
   end
