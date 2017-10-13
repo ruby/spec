77f23fa538671a5b092c9a2fa0b78b5e24d41838
Andrew Konchin <andry.konchin@gmail.com>
2017-10-13 19:49:03 +0300

    Core review. Add case with long buffer and use `equal`

--- core/array/pack/buffer_spec.rb
+++ core/array/pack/buffer_spec.rb
@@ -8,7 +8,20 @@ ruby_version_is '2.4' do
       n = [ 65, 66, 67 ]
       buffer = " "*3
       result = n.pack("ccc", buffer: buffer)      #=> "ABC"
-      result.object_id.should == buffer.object_id
+      result.should equal(buffer)
+    end
+
+    it "adds result at the end of buffer content" do
+      n = [ 65, 66, 67 ] # result without buffer is "ABC"
+
+      buffer = ""
+      n.pack("ccc", buffer: buffer).should == "ABC"
+
+      buffer = "123"
+      n.pack("ccc", buffer: buffer).should == "123ABC"
+
+      buffer = "12345"
+      n.pack("ccc", buffer: buffer).should == "12345ABC"
     end
 
     it "raises TypeError exception if buffer is not String" do
@@ -27,6 +40,12 @@ ruby_version_is '2.4' do
         n = [ 65, 66, 67 ]
         buffer = "123"
         n.pack("@6ccc", buffer: buffer).should == "123\0\0\0ABC"
+      end
+
+      it 'does not keep buffer content if it is longer than offset + result' do
+        n = [ 65, 66, 67 ]
+        buffer = "1234567890"
+        n.pack("@3ccc", buffer: buffer).should == "123ABC"
       end
     end
   end
