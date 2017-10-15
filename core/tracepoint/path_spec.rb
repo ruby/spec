38b985bab8c85fa8a1c28d7eb3b000e2620c4063
atul <atul1bhosale@gmail.com>
2017-10-15 23:59:58 +0530

    Add spec for TracePoint#path

---
+++ core/tracepoint/path_spec.rb
@@ -0,0 +1,11 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+
+ruby_version_is '2.4' do
+  describe 'TracePoint#path' do
+    it 'returns the path of the file being run' do
+      path = nil
+      TracePoint.new(:line) { |tp| path = tp.path }.enable
+      path.should == "#{__FILE__}"
+    end
+  end
+end
