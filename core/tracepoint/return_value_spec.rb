d980bf746ebf789b389f44e974740b13068f8501
atul <atul1bhosale@gmail.com>
2017-10-16 00:01:06 +0530

    Add spec for TracePoint#return_value

---
+++ core/tracepoint/return_value_spec.rb
@@ -0,0 +1,14 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+
+ruby_version_is '2.4' do
+  describe 'TracePoint#return_value' do
+    def test; end
+
+    it 'returns value from :return event' do
+      trace_value = nil
+      TracePoint.new(:return) { |tp| trace_value = tp.return_value}.enable
+      test
+      trace_value.should equal(nil)
+    end
+  end
+end
