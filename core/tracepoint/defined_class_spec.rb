9ab3a7ab965e1fd3f17ca74bc809a0b12b902985
atul <atul1bhosale@gmail.com>
2017-10-15 23:56:23 +0530

    Add spec for TracePoint#defined_class

---
+++ core/tracepoint/defined_class_spec.rb
@@ -0,0 +1,17 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+
+ruby_version_is '2.4' do
+  describe 'TracePoint#defined_class' do
+    class A; def foo; end; end
+
+    it 'returns class or module of the method being called' do
+      class_name = nil
+      TracePoint.new(:call) do |tp|
+        class_name = tp.defined_class
+      end.enable do
+        A.new.foo
+        class_name.should equal(A)
+      end
+    end
+  end
+end
