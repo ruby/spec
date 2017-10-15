2f2437d866714a7b2b5e697d87b5cebd1053e9d6
atul <atul1bhosale@gmail.com>
2017-10-15 23:57:43 +0530

    Add spec for TracePoint#inspect

---
+++ core/tracepoint/inspect_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+
+ruby_version_is '2.4' do
+  describe 'TracePoint#inspect' do
+    it 'return a string containing a human-readable TracePoint status' do
+      TracePoint.new(:call) {}.inspect.should ==
+        '#<TracePoint:disabled>'
+    end
+  end
+end
