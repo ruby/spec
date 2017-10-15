edf50d2ec3d7a8ccc4cec3cb07abd7f4265197d8
atul <atul1bhosale@gmail.com>
2017-10-15 20:56:59 +0530

    Add spec for TracePoint#trace

---
+++ core/tracepoint/trace_spec.rb
@@ -0,0 +1,9 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+
+ruby_version_is '2.4' do
+  describe 'TracePoint.trace' do
+    it 'activates the trace automatically' do
+      TracePoint.trace(:call) {}.enabled?.should be_true
+    end
+  end
+end
