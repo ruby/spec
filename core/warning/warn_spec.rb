da55149fc9c55879ee46820d84a460a2dd24a4b2
Georg Gadinger <nilsding@nilsding.org>
2017-10-16 20:54:38 +0200

    Add spec for overriding Warning.warn

--- core/warning/warn_spec.rb
+++ core/warning/warn_spec.rb
@@ -9,19 +9,37 @@ describe "Warning.warn" do
     end
 
     it "extends itself" do
-      Warning.ancestors.should include(Warning)
+      Warning.singleton_class.ancestors.should include(Warning)
     end
 
-    ruby_version_is "2.5" do
-      it "is called by Kernel.warn" do
-        Warning.should_receive(:warn)
-        Kernel.warn("Chunky bacon!")
-      end
+    it "has Warning as the method owner" do
+      ruby_exe("p Warning.method(:warn).owner").should == "Warning\n"
+    end
 
-      it "is also called by parser warnings" do
-        Warning.should_receive(:warn)
-        eval "{ key: :value, key: :value2 }"
-      end
+    it "can be overridden" do
+      code = <<-RUBY
+        $stdout.sync = true
+        $stderr.sync = true
+        def Warning.warn(msg)
+          return msg.upcase if msg.start_with?("A")
+          super
+        end
+        p Warning.warn("A warning!")
+        p Warning.warn("warning from stderr\n")
+      RUBY
+      ruby_exe(code, args: "2>&1").should == %Q["A WARNING!"\nwarning from stderr\nnil\n]
+    end
+  end
+
+  ruby_version_is "2.5" do
+    it "is called by Kernel.warn" do
+      Warning.should_receive(:warn)
+      Kernel.warn("Chunky bacon!")
+    end
+
+    it "is also called by parser warnings" do
+      Warning.should_receive(:warn)
+      eval "{ key: :value, key: :value2 }"
     end
   end
 end
