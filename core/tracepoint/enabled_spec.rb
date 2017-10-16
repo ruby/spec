c4f97a1b4ecf9f7a5a9fd554f0e239c8d01dfc30
atul <atul1bhosale@gmail.com>
2017-10-16 16:42:40 +0530

    Add TracePoint#enabled? spec when trace is enabled
    Add spec to check if enabled? returns true when trace is enabled

--- core/tracepoint/enabled_spec.rb
+++ core/tracepoint/enabled_spec.rb
@@ -2,7 +2,13 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 ruby_version_is '2.0' do
   describe 'TracePoint#enabled?' do
-    it 'returns the current status of the trace' do
+    it 'returns true when current status of the trace is enable' do
+      trace = TracePoint.new(:call) {}
+      trace.enable
+      trace.enabled?.should be_true
+    end
+
+    it 'returns false when current status of the trace is disabled' do
       TracePoint.new(:call) {}.enabled?.should be_false
     end
   end
