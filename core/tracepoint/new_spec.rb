7c29a4c0122d62baa91085304f9e44c5e2346b33
atul <atul1bhosale@gmail.com>
2017-10-16 16:46:50 +0530

    Add TracePoint#new specs
    Add specs for #new when called with string argument, multiple argument
    and no arguments.

--- core/tracepoint/new_spec.rb
+++ core/tracepoint/new_spec.rb
@@ -2,8 +2,48 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 ruby_version_is '2.0' do
   describe 'TracePoint.new' do
+    def test; 'test' end
+    class A; def foo; end end
     it 'returns a new TracePoint object, not enabled by default' do
       TracePoint.new(:call) {}.enabled?.should be_false
+    end
+
+    it 'includes :line event when event is not specified' do
+      event_name = nil
+      TracePoint.new() { |tp| event_name = tp.event }.enable
+      event_name.should equal(:line)
+
+      event_name = nil
+      test
+      event_name.should equal(:line)
+
+      event_name = nil
+      A.new.foo
+      event_name.should equal(:line)
+    end
+
+    it 'includes the matching event name if argument is string' do
+      event_name = nil
+      TracePoint.new('return') { |tp| event_name = tp.event}.enable
+      event_name.should equal(nil)
+      test
+      event_name.should equal(:return)
+    end
+
+    it 'includes multiple events when multiple event names are passed as params' do
+      event_name = nil
+      TracePoint.new(:end, :call) do |tp|
+        event_name = tp.event
+      end.enable
+
+      test
+      event_name.should equal(:call)
+
+      A.new.foo
+      event_name.should equal(:call)
+
+      class B; end
+      event_name.should equal(:end)
     end
   end
 end
