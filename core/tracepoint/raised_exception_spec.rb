2b6b2b553108bf696367f7afd7914e7559fd1985
atul <atul1bhosale@gmail.com>
2017-10-16 16:52:06 +0530

    Update TracePoint#raised_exception & #return_value
    Update spec to check the raised_exception with the error_result

--- core/tracepoint/raised_exception_spec.rb
+++ core/tracepoint/raised_exception_spec.rb
@@ -3,13 +3,15 @@ require File.expand_path('../../../spec_helper', __FILE__)
 ruby_version_is '2.0' do
   describe 'TracePoint#raised_exception' do
     it 'returns value from exception raised on the :raise event' do
-      value = nil
-      trace = TracePoint.new(:raise) { |tp| value = tp.raised_exception }
+      raised_exception, error_result = nil
+      trace = TracePoint.new(:raise) { |tp| raised_exception = tp.raised_exception }
       trace.enable
       begin
         raise StandardError
-      rescue => _ ; end
-      value.should be_kind_of(StandardError)
+      rescue => e
+        error_result = e
+      end
+      raised_exception.should equal(error_result)
     end
   end
 end
--- core/tracepoint/return_value_spec.rb
+++ core/tracepoint/return_value_spec.rb
@@ -2,13 +2,13 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 ruby_version_is '2.0' do
   describe 'TracePoint#return_value' do
-    def test; end
+    def test; 'test' end
 
     it 'returns value from :return event' do
       trace_value = nil
       TracePoint.new(:return) { |tp| trace_value = tp.return_value}.enable
       test
-      trace_value.should equal(nil)
+      trace_value.should == 'test'
     end
   end
 end
