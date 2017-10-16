f662facde69d26f8cc48f698eb29efc1fb757347
atul <atul1bhosale@gmail.com>
2017-10-16 16:45:03 +0530

    Add TracePoint#event spec for multiple events
    Add spec to call tace block multiple times for more than one type of
    event.

--- core/tracepoint/event_spec.rb
+++ core/tracepoint/event_spec.rb
@@ -2,10 +2,23 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 ruby_version_is '2.0' do
   describe 'TracePoint#event' do
+    def test; 'test' end
+    class A; def foo; end end
+
     it 'returns the type of event' do
       event_name = nil
-      TracePoint.new(:line) { |tp| event_name = tp.event }.enable
-      event_name.should equal(:line)
+      TracePoint.new(:end, :call) do |tp|
+        event_name = tp.event
+      end.enable
+
+      test
+      event_name.should equal(:call)
+
+      A.new.foo
+      event_name.should equal(:call)
+
+      class B; end
+      event_name.should equal(:end)
     end
   end
 end
