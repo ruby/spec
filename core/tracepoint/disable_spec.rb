996ac8aa6c197c0fc54911bf7940c465f28f8456
atul <atul1bhosale@gmail.com>
2017-10-16 16:38:12 +0530

    TracePoint#enable TracePoint#disable - Add specs
    Add specs to check that a trace doesn't call the block until it's enabled.

--- core/tracepoint/disable_spec.rb
+++ core/tracepoint/disable_spec.rb
@@ -2,19 +2,34 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 ruby_version_is '2.0' do
   describe 'TracePoint#disable' do
-    context 'deactivates the trace' do
-      it 'returns true if trace was enabled' do
-        trace = TracePoint.new(:call) {}
-        trace.enable
-        trace.disable.should be_true
+    def test; end
+    it 'returns true if trace was enabled' do
+      event_name, method_name = nil
+      trace = TracePoint.new(:call) do |tp|
+        event_name = tp.event
+        method_name = tp.method_id
       end
 
-      it 'returns false if trace was disabled' do
-        trace = TracePoint.new(:call) {}
-        trace.enable
-        trace.disable
-        trace.disable.should be_false
+      trace.enable
+      trace.disable.should be_true
+      event_name, method_name = nil
+      test
+      method_name.equal?(:test).should be_false
+      event_name.should equal(nil)
+    end
+
+    it 'returns false if trace was disabled' do
+      event_name, method_name = nil
+      trace = TracePoint.new(:call) do |tp|
+        event_name = tp.event
+        method_name = tp.method_id
       end
+
+      trace.disable.should be_false
+      event_name, method_name = nil
+      test
+      method_name.equal?(:test).should be_false
+      event_name.should equal(nil)
     end
   end
 end
--- core/tracepoint/enable_spec.rb
+++ core/tracepoint/enable_spec.rb
@@ -2,19 +2,48 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 ruby_version_is '2.0' do
   describe 'TracePoint#enable' do
-    context 'activates the trace' do
-      it 'returns true if trace was enabled' do
-        trace = TracePoint.new(:call) {}
-        trace.enable
-        trace.enable.should be_true
+    def test; end
+
+    it 'returns true if trace was enabled' do
+      event_name, method_name = nil
+      trace = TracePoint.new(:call) do |tp|
+        event_name = tp.event
+        method_name = tp.method_id
       end
 
-      it 'returns false if trace was disabled' do
-        trace = TracePoint.new(:call) {}
-        trace.enable
-        trace.disable
-        trace.enable.should be_false
+      event_name.should equal(nil)
+      test
+      method_name.equal?(:test).should be_false
+
+      trace.enable
+      trace.enable.should be_true
+      event_name.should equal(:call)
+      test
+      method_name.equal?(:test).should be_true
+    end
+
+    it 'returns false if trace was disabled' do
+      event_name, method_name = nil
+      trace = TracePoint.new(:call) do |tp|
+        event_name = tp.event
+        method_name = tp.method_id
       end
+
+      trace.enable.should be_false
+      event_name.should equal(:call)
+      test
+      method_name.equal?(:test).should be_true
+
+      trace.disable
+      event_name, method_name = nil
+      test
+      method_name.equal?(:test).should be_false
+      event_name.should equal(nil)
+
+      trace.enable.should be_false
+      event_name.should equal(:call)
+      test
+      method_name.equal?(:test).should be_true
     end
   end
 end
