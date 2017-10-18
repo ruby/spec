7f891a5f9a2d36819dd9b841474c17728954ab85
m-nakamura145 <masato.nakamura145@gmail.com>
2017-10-18 09:36:44 +0900

    Refactoring spec for Date#wday

--- library/date/wday_spec.rb
+++ library/date/wday_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require 'date'
 
 describe "Date#wday" do
-  it "returns saturday" do
+  it "returns the week day as a number starting with Sunday as 0" do
     w = Date.new(2000, 1, 1).wday
     w.should == 6
   end
