cb0fd00a2a8458cc6549da6622e7ab2f5bbc4264
m-nakamura145 <masato.nakamura145@gmail.com>
2017-10-18 09:35:00 +0900

    Add spec for Date#friday?

--- library/date/friday_spec.rb
+++ library/date/friday_spec.rb
@@ -5,4 +5,8 @@ describe "Date#friday?" do
   it "should be friday" do
     Date.new(2000, 1, 7).friday?.should be_true
   end
+
+  it "should not be friday" do
+    Date.new(2000, 1, 8).friday?.should be_false
+  end
 end
