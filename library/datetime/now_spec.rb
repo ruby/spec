d691fbd57edcc8b259adfc8b54c2010ac731e2e7
Andrew Nordman <cadwallion@gmail.com>
2017-10-19 15:07:59 -0500

    Replace data shell out with Process#clock_gettime

--- library/datetime/now_spec.rb
+++ library/datetime/now_spec.rb
@@ -8,22 +8,20 @@ describe "DateTime.now" do
 
   it "sets the current date" do
     dt = DateTime.now
-    check = `date "+%Y-%m-%d"`.strip
-    date = check.match(/(\d{4})-(\d{2})-(\d{2})/)
+    date = Date.today
 
-    dt.year.should == date[1].to_i
-    dt.mon.should == date[2].to_i
-    dt.day.should == date[3].to_i
+    dt.year.should == date.year
+    dt.mon.should == date.mon
+    dt.day.should == date.day
   end
 
   it "sets the current time" do
     dt = DateTime.now
-    check = `date "+%H:%M:%S"`.strip
-    time = check.match(/(\d{2})\:(\d{2})\:(\d{2})/)
+    time = Time.now
 
-    dt.hour.should == time[1].to_i
-    dt.min.should == time[2].to_i
-    dt.sec.should == time[3].to_i
+    dt.hour.should == time.hour
+    dt.min.should == time.min
+    dt.sec.should == time.sec
   end
 
   it "grabs the local timezone" do
