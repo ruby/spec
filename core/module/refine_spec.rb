214743d2a115c535ad9353180940646f727e46df
Andrew Konchin <andry.konchin@gmail.com>
2017-10-19 13:52:57 +0300

    Update specs for Module#refine

--- core/module/refine_spec.rb
+++ core/module/refine_spec.rb
@@ -1,25 +1,23 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-ruby_version_is "2.0.0" do
+ruby_version_is "2.2" do
   describe "Module#refine" do
-    it "runs its block in an anonymous Module" do
-      selves = nil
+    it "runs its block in an anonymous module" do
+      inner_self = nil
       mod = Module.new do
-        outer_self = self
         refine String do
-          selves = [outer_self, self]
+          inner_self = self
         end
       end
 
-      selves[0].should == mod
-      selves[0].should_not == selves[1]
-      selves[1].should be_kind_of(Module)
+      mod.should_not == inner_self
+      inner_self.should be_kind_of(Module)
+      inner_self.name.should == nil
     end
 
     it "uses the same anonymous module for future refines of the same class" do
       selves = []
       mod = Module.new do
-        selves << self
         refine String do
           selves << self
         end
@@ -31,10 +29,10 @@ ruby_version_is "2.0.0" do
         end
       end
 
-      selves[1].should == selves[2]
+      selves[0].should == selves[1]
     end
 
-    it "adds methods defined in its block to the anon module's public instance methods" do
+    it "adds methods defined in its block to the anonymous module's public instance methods" do
       inner_self = nil
       mod = Module.new do
         refine String do
@@ -48,6 +46,18 @@ ruby_version_is "2.0.0" do
       inner_self.public_instance_methods.should include(:blah)
     end
 
+    it "returns created anonymous module" do
+      inner_self = nil
+      result = nil
+      mod = Module.new do
+        result = refine String do
+          inner_self = self
+        end
+      end
+
+      result.should == inner_self
+    end
+
     it "raises ArgumentError if not passed an argument" do
       lambda do
         Module.new do
@@ -56,10 +66,18 @@ ruby_version_is "2.0.0" do
       end.should raise_error(ArgumentError)
     end
 
-    it "raises TypeError if not passed a class or module" do
+    it "raises TypeError if not passed a class" do
       lambda do
         Module.new do
-          refine('foo') {}
+          refine("foo") {}
+        end
+      end.should raise_error(TypeError)
+    end
+
+    it "raises TypeError if passed a module" do
+      lambda do
+        Module.new do
+          refine(Enumerable) {}
         end
       end.should raise_error(TypeError)
     end
@@ -76,20 +94,19 @@ ruby_version_is "2.0.0" do
       result = nil
       Module.new do
         refine(String) do
-          def foo; 'foo'; end
-          result = 'hello'.foo
+          def foo; "foo"; end
+          result = "hello".foo
         end
       end
-      result.should == 'foo'
+      result.should == "foo"
     end
 
     it "doesn't apply refinements outside the refine block" do
       Module.new do
-        refine(String) {def foo; 'foo'; end}
-        lambda do
-          'hello'.foo
-        end
-        should raise_error(NoMethodError)
+        refine(String) {def foo; "foo"; end}
+        -> () {
+          "hello".foo
+        }.should raise_error(NoMethodError)
       end
     end
 
@@ -98,7 +115,7 @@ ruby_version_is "2.0.0" do
         refine(String) {def foo; 'foo'; end}
       end
 
-      lambda {'hello'.foo}.should raise_error(NoMethodError)
+      lambda {"hello".foo}.should raise_error(NoMethodError)
     end
   end
 end
