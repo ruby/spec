d07cc9a6681e62bf0efabeeffb5d23e541ba225f
atul <atul1bhosale@gmail.com>
2017-10-26 00:48:35 +0530

    TracePoint#path spec to check for path inside eval
    Add specs to check for path inside eval for :call, :end, :line & :return
    event.

--- core/tracepoint/path_spec.rb
+++ core/tracepoint/path_spec.rb
@@ -1,5 +1,7 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
+def test; 'test' end
+
 ruby_version_is '2.0' do
   describe 'TracePoint#path' do
     it 'returns the path of the file being run' do
@@ -9,11 +11,35 @@ ruby_version_is '2.0' do
       end
     end
 
-    it 'equals (eval) inside an eval' do
+    it 'equals (eval) inside an eval for :end event' do
       path = nil
       TracePoint.new(:end) { |tp| path = tp.path }.enable do
         eval("class A; end")
         path.should == '(eval)'
+      end
+    end
+
+    it 'equals the path of the file being run for :line event inside an eval' do
+      path = nil
+      TracePoint.new(:line) { |tp| path = tp.path }.enable do
+        eval("2 + 2")
+        path.should == "#{__FILE__}"
+      end
+    end
+
+    it 'equals the path of the file being run for :call event inside an eval' do
+      path = nil
+      TracePoint.new(:call) { |tp| path = tp.path }.enable do
+        eval("test")
+        path.should == "#{__FILE__}"
+      end
+    end
+
+    it 'equals the path of the file being run for :return event inside an eval' do
+      path = nil
+      TracePoint.new(:return) { |tp| path = tp.path }.enable do
+        eval("test")
+        path.should == "#{__FILE__}"
       end
     end
   end
