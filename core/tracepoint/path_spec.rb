e274e66c39b3689efcf4c678e222d7516048477f
atul <atul1bhosale@gmail.com>
2017-10-28 00:29:54 +0530

    TracePoint#path Remove global test definition & other events specs

--- core/tracepoint/path_spec.rb
+++ core/tracepoint/path_spec.rb
@@ -1,7 +1,5 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-def test; 'test' end
-
 ruby_version_is '2.0' do
   describe 'TracePoint#path' do
     it 'returns the path of the file being run' do
@@ -16,30 +14,6 @@ ruby_version_is '2.0' do
       TracePoint.new(:end) { |tp| path = tp.path }.enable do
         eval("class A; end")
         path.should == '(eval)'
-      end
-    end
-
-    it 'equals the path of the file being run for :line event inside an eval' do
-      path = nil
-      TracePoint.new(:line) { |tp| path = tp.path }.enable do
-        eval("2 + 2")
-        path.should == "#{__FILE__}"
-      end
-    end
-
-    it 'equals the path of the file being run for :call event inside an eval' do
-      path = nil
-      TracePoint.new(:call) { |tp| path = tp.path }.enable do
-        eval("test")
-        path.should == "#{__FILE__}"
-      end
-    end
-
-    it 'equals the path of the file being run for :return event inside an eval' do
-      path = nil
-      TracePoint.new(:return) { |tp| path = tp.path }.enable do
-        eval("test")
-        path.should == "#{__FILE__}"
       end
     end
   end
