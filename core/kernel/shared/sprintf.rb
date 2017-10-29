1800767a0888695298079ff855aea9cadf2e3b6e
Andrew Konchin <andry.konchin@gmail.com>
2017-10-29 16:21:32 +0200

    Shared example: Add flags

--- core/kernel/shared/sprintf.rb
+++ core/kernel/shared/sprintf.rb
@@ -459,4 +459,347 @@ describe :kernel_sprintf, shared: true do
       end
     end
   end
+
+  describe "flags" do
+    describe "space" do
+      context "applies to numeric formats bBdiouxXeEfgGaA" do
+        it "leaves a space at the start of non-negative numbers" do
+          format("% b", 10).should == " 1010"
+          format("% B", 10).should == " 1010"
+          format("% d", 112).should == " 112"
+          format("% i", 112).should == " 112"
+          format("% o", 87).should == " 127"
+          format("% u", 112).should == " 112"
+          format("% x", 196).should == " c4"
+          format("% X", 196).should == " C4"
+
+          format("% e", 109.52).should == " 1.095200e+02"
+          format("% E", 109.52).should == " 1.095200E+02"
+          format("% f", 10.952).should == " 10.952000"
+          format("% g", 12.1234).should == " 12.1234"
+          format("% G", 12.1234).should == " 12.1234"
+          format("% a", 196).should == " 0x1.88p+7"
+          format("% A", 196).should == " 0X1.88P+7"
+        end
+
+        it "does not leave a space at the start of negative numbers" do
+          format("% b", -10).should == "-1010"
+          format("% B", -10).should == "-1010"
+          format("% d", -112).should == "-112"
+          format("% i", -112).should == "-112"
+          format("% o", -87).should == "-127"
+          format("% u", -112).should == "-112"
+          format("% x", -196).should == "-c4"
+          format("% X", -196).should == "-C4"
+
+          format("% e", -109.52).should == "-1.095200e+02"
+          format("% E", -109.52).should == "-1.095200E+02"
+          format("% f", -10.952).should == "-10.952000"
+          format("% g", -12.1234).should == "-12.1234"
+          format("% G", -12.1234).should == "-12.1234"
+          format("% a", -196).should == "-0x1.88p+7"
+          format("% A", -196).should == "-0X1.88P+7"
+        end
+
+        # strange behavior
+        it "prevents converting negative argument to two's complement form" do
+          format("% b", -10).should == "-1010"
+          format("% B", -10).should == "-1010"
+          format("% o", -87).should == "-127"
+          format("% x", -196).should == "-c4"
+          format("% X", -196).should == "-C4"
+        end
+
+        it "treats several white spaces as one" do
+          format("%     b", 10).should == " 1010"
+          format("%     B", 10).should == " 1010"
+          format("%     d", 112).should == " 112"
+          format("%     i", 112).should == " 112"
+          format("%     o", 87).should == " 127"
+          format("%     u", 112).should == " 112"
+          format("%     x", 196).should == " c4"
+          format("%     X", 196).should == " C4"
+
+          format("%     e", 109.52).should == " 1.095200e+02"
+          format("%     E", 109.52).should == " 1.095200E+02"
+          format("%     f", 10.952).should == " 10.952000"
+          format("%     g", 12.1234).should == " 12.1234"
+          format("%     G", 12.1234).should == " 12.1234"
+          format("%     a", 196).should == " 0x1.88p+7"
+          format("%     A", 196).should == " 0X1.88P+7"
+        end
+      end
+    end
+
+    describe "(digit)$" do
+      it "specifies the absolute argument number for this field" do
+        format("%2$b", 0, 10).should == "1010"
+        format("%2$B", 0, 10).should == "1010"
+        format("%2$d", 0, 112).should == "112"
+        format("%2$i", 0, 112).should == "112"
+        format("%2$o", 0, 87).should == "127"
+        format("%2$u", 0, 112).should == "112"
+        format("%2$x", 0, 196).should == "c4"
+        format("%2$X", 0, 196).should == "C4"
+
+        format("%2$e", 0, 109.52).should == "1.095200e+02"
+        format("%2$E", 0, 109.52).should == "1.095200E+02"
+        format("%2$f", 0, 10.952).should == "10.952000"
+        format("%2$g", 0, 12.1234).should == "12.1234"
+        format("%2$G", 0, 12.1234).should == "12.1234"
+        format("%2$a", 0, 196).should == "0x1.88p+7"
+        format("%2$A", 0, 196).should == "0X1.88P+7"
+
+        format("%2$c", 1, 97).should == "a"
+        format("%2$p", "a", []).should == "[]"
+        format("%2$s", "-", "abc").should == "abc"
+      end
+
+      it "raises exception if argument number is bigger than actual arguments list" do
+        -> () {
+          format("%4$d", 1, 2, 3)
+        }.should raise_error(ArgumentError)
+      end
+
+      it "ignores '-' sign" do
+        format("%2$d", 1, 2, 3).should == "2"
+        format("%-2$d", 1, 2, 3).should == "2"
+      end
+
+      it "raises ArgumentError exception when absolute and relative argument numbers are mixed" do
+        -> () {
+          format("%1$d %d", 1, 2)
+        }.should raise_error(ArgumentError)
+      end
+    end
+
+    describe "#" do
+      context "applies to format o" do
+        it "increases the precision until the first digit will be `0' if it is not formatted as complements" do
+          format("%#o", 87).should == "0127"
+        end
+
+        it "does nothing for negtive argument" do
+          format("%#o", -87).should == "..7651"
+        end
+      end
+
+      context "applies to formats bBxX" do
+        it "prefixes the result with 0x, 0X, 0b and 0B respectively for non-zero argument" do
+          format("%#b", 10).should == "0b1010"
+          format("%#b", -10).should == "0b..10110"
+          format("%#B", 10).should == "0B1010"
+          format("%#B", -10).should == "0B..10110"
+
+          format("%#x", 196).should == "0xc4"
+          format("%#x", -196).should == "0x..f3c"
+          format("%#X", 196).should == "0XC4"
+          format("%#X", -196).should == "0X..F3C"
+        end
+
+        it "does nothing for zero argument" do
+          format("%#b", 0).should == "0"
+          format("%#B", 0).should == "0"
+
+          format("%#o", 0).should == "0"
+
+          format("%#x", 0).should == "0"
+          format("%#X", 0).should == "0"
+        end
+      end
+
+      context "applies to formats aAeEfgG" do
+        it "forces a decimal point to be added, even if no digits follow" do
+          format("%#.0a", 16.25).should == "0x1.p+4"
+          format("%#.0A", 16.25).should == "0X1.P+4"
+
+          format("%#.0e", 100).should == "1.e+02"
+          format("%#.0E", 100).should == "1.E+02"
+
+          format("%#.0f", 123.4).should == "123."
+
+          format("%#g", 123456).should == "123456."
+          format("%#G", 123456).should == "123456."
+        end
+
+        # strange behavior
+        it "changes format from dd.dddd to exponential form for gG" do
+          format("%#.0g", 123.4).should_not == "123."
+          format("%#.0g", 123.4).should == "1.e+02"
+        end
+      end
+
+      context "applies to gG" do
+        it "does not remove trailing zeros" do
+          format("%#g", 123.4).should == "123.400"
+          format("%#g", 123.4).should == "123.400"
+        end
+      end
+    end
+
+    describe "+" do
+      context "applies to numeric formats bBdiouxXaAeEfgG" do
+        it "adds a leading plus sign to non-negative numbers" do
+          format("%+b", 10).should == "+1010"
+          format("%+B", 10).should == "+1010"
+          format("%+d", 112).should == "+112"
+          format("%+i", 112).should == "+112"
+          format("%+o", 87).should == "+127"
+          format("%+u", 112).should == "+112"
+          format("%+x", 196).should == "+c4"
+          format("%+X", 196).should == "+C4"
+
+          format("%+e", 109.52).should == "+1.095200e+02"
+          format("%+E", 109.52).should == "+1.095200E+02"
+          format("%+f", 10.952).should == "+10.952000"
+          format("%+g", 12.1234).should == "+12.1234"
+          format("%+G", 12.1234).should == "+12.1234"
+          format("%+a", 196).should == "+0x1.88p+7"
+          format("%+A", 196).should == "+0X1.88P+7"
+        end
+
+        it "does not use two's complement form for negative numbers for formats bBoxX" do
+          format("%+b", -10).should == "-1010"
+          format("%+B", -10).should == "-1010"
+          format("%+o", -87).should == "-127"
+          format("%+x", -196).should == "-c4"
+          format("%+X", -196).should == "-C4"
+        end
+      end
+    end
+
+    describe "-" do
+      it "left-justifies the result of conversion if width is specified" do
+        format("%-10b", 10).should == "1010      "
+        format("%-10B", 10).should == "1010      "
+        format("%-10d", 112).should == "112       "
+        format("%-10i", 112).should == "112       "
+        format("%-10o", 87).should == "127       "
+        format("%-10u", 112).should == "112       "
+        format("%-10x", 196).should == "c4        "
+        format("%-10X", 196).should == "C4        "
+
+        format("%-20e", 109.52).should == "1.095200e+02        "
+        format("%-20E", 109.52).should == "1.095200E+02        "
+        format("%-20f", 10.952).should == "10.952000           "
+        format("%-20g", 12.1234).should == "12.1234             "
+        format("%-20G", 12.1234).should == "12.1234             "
+        format("%-20a", 196).should == "0x1.88p+7           "
+        format("%-20A", 196).should == "0X1.88P+7           "
+
+        format("%-10c", 97).should == "a         "
+        format("%-10p", []).should == "[]        "
+        format("%-10s", "abc").should == "abc       "
+      end
+    end
+
+    describe "0 (zero)" do
+      context "applies to numeric formats bBdiouxXaAeEfgG and width is specified" do
+        it "pads with zeros, not spaces" do
+          format("%010b", 10).should == "0000001010"
+          format("%010B", 10).should == "0000001010"
+          format("%010d", 112).should == "0000000112"
+          format("%010i", 112).should == "0000000112"
+          format("%010o", 87).should == "0000000127"
+          format("%010u", 112).should == "0000000112"
+          format("%010x", 196).should == "00000000c4"
+          format("%010X", 196).should == "00000000C4"
+
+          format("%020e", 109.52).should == "000000001.095200e+02"
+          format("%020E", 109.52).should == "000000001.095200E+02"
+          format("%020f", 10.952).should == "0000000000010.952000"
+          format("%020g", 12.1234).should == "000000000000012.1234"
+          format("%020G", 12.1234).should == "000000000000012.1234"
+          format("%020a", 196).should == "0x000000000001.88p+7"
+          format("%020A", 196).should == "0X000000000001.88P+7"
+        end
+
+        it "uses radix-1 when displays negative argument as a two's complemen" do
+          format("%010b", -10).should == "..11110110"
+          format("%010B", -10).should == "..11110110"
+          format("%010o", -87).should == "..77777651"
+          format("%010x", -196).should == "..ffffff3c"
+          format("%010X", -196).should == "..FFFFFF3C"
+        end
+      end
+    end
+
+    describe "*" do
+      it "uses the previous argument as the field width" do
+        format("%*b", 10, 10).should == "      1010"
+        format("%*B", 10, 10).should == "      1010"
+        format("%*d", 10, 112).should == "       112"
+        format("%*i", 10, 112).should == "       112"
+        format("%*o", 10, 87).should == "       127"
+        format("%*u", 10, 112).should == "       112"
+        format("%*x", 10, 196).should == "        c4"
+        format("%*X", 10, 196).should == "        C4"
+
+        format("%*e", 20, 109.52).should == "        1.095200e+02"
+        format("%*E", 20, 109.52).should == "        1.095200E+02"
+        format("%*f", 20, 10.952).should == "           10.952000"
+        format("%*g", 20, 12.1234).should == "             12.1234"
+        format("%*G", 20, 12.1234).should == "             12.1234"
+        format("%*a", 20, 196).should == "           0x1.88p+7"
+        format("%*A", 20, 196).should == "           0X1.88P+7"
+
+        format("%*c", 10, 97).should == "         a"
+        format("%*p", 10, []).should == "        []"
+        format("%*s", 10, "abc").should == "       abc"
+      end
+
+      it "left-justifies the result if width is negative" do
+        format("%*b", -10, 10).should == "1010      "
+        format("%*B", -10, 10).should == "1010      "
+        format("%*d", -10, 112).should == "112       "
+        format("%*i", -10, 112).should == "112       "
+        format("%*o", -10, 87).should == "127       "
+        format("%*u", -10, 112).should == "112       "
+        format("%*x", -10, 196).should == "c4        "
+        format("%*X", -10, 196).should == "C4        "
+
+        format("%*e", -20, 109.52).should == "1.095200e+02        "
+        format("%*E", -20, 109.52).should == "1.095200E+02        "
+        format("%*f", -20, 10.952).should == "10.952000           "
+        format("%*g", -20, 12.1234).should == "12.1234             "
+        format("%*G", -20, 12.1234).should == "12.1234             "
+        format("%*a", -20, 196).should == "0x1.88p+7           "
+        format("%*A", -20, 196).should == "0X1.88P+7           "
+
+        format("%*c", -10, 97).should == "a         "
+        format("%*p", -10, []).should == "[]        "
+        format("%*s", -10, "abc").should == "abc       "
+      end
+
+      it "uses the specified argument as the width if * is followed by a number and $" do
+        format("%1$*2$b", 10, 10).should == "      1010"
+        format("%1$*2$B", 10, 10).should == "      1010"
+        format("%1$*2$d", 112, 10).should == "       112"
+        format("%1$*2$i", 112, 10).should == "       112"
+        format("%1$*2$o", 87, 10).should == "       127"
+        format("%1$*2$u", 112, 10).should == "       112"
+        format("%1$*2$x", 196, 10).should == "        c4"
+        format("%1$*2$X", 196, 10).should == "        C4"
+
+        format("%1$*2$e", 109.52, 20).should == "        1.095200e+02"
+        format("%1$*2$E", 109.52, 20).should == "        1.095200E+02"
+        format("%1$*2$f", 10.952, 20).should == "           10.952000"
+        format("%1$*2$g", 12.1234, 20).should == "             12.1234"
+        format("%1$*2$G", 12.1234, 20).should == "             12.1234"
+        format("%1$*2$a", 196, 20).should == "           0x1.88p+7"
+        format("%1$*2$A", 196, 20).should == "           0X1.88P+7"
+
+        format("%1$*2$c", 97, 10).should == "         a"
+        format("%1$*2$p", [], 10).should == "        []"
+        format("%1$*2$s", "abc", 10).should == "       abc"
+      end
+
+      it "raises ArgumentError when is mixed with width" do
+        -> () {
+          format("%*10d", 10, 112)
+        }.should raise_error(ArgumentError)
+      end
+    end
+  end
+
 end
