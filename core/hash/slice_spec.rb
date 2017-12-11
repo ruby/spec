65c42e686fde44df4eb0a34cb1b4978e166e9c84
Marc-Andre Lafortune <github@marc-andre.ca>
2017-12-11 17:19:09 -0500

    + Hash#slice

---
+++ core/hash/slice_spec.rb
@@ -0,0 +1,32 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+
+ruby_version_is "2.5" do
+  describe "Hash#slice" do
+    before :each do
+      @hash = { a: 1, b: 2, c: 3 }
+    end
+
+    it "returns new hash" do
+      ret = @hash.slice
+      ret.should_not equal(@hash)
+      ret.should be_an_instance_of(Hash)
+    end
+
+    it "returns the requested subset, in that order" do
+      @hash.slice(:c, :a).should == { c: 3, a: 1 }
+    end
+
+    it "returns a hash ordered in the order of the requested keys" do
+      @hash.slice(:c, :a).keys.should == [:c, :a]
+    end
+
+    it "returns a Hash instance, even on subclasses" do
+      klass = Class.new(Hash)
+      h = klass.new
+      h[:foo] = 42
+      r = h.slice(:foo)
+      r.should == {foo: 42}
+      r.class.should == Hash
+    end
+  end
+end
