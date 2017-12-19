43da9f06ba545e457a406614332979c4ee6ce3f4
atul <atul1bhosale@gmail.com>
2017-12-19 09:59:32 +0530

    Update specs to use frozen_error_class for FrozenError

--- core/thread/element_set_spec.rb
+++ core/thread/element_set_spec.rb
@@ -6,13 +6,13 @@ describe "Thread#[]=" do
     Thread.current[:value] = nil
   end
 
-  it "raises a RuntimeError if the thread is frozen" do
+  it "raises a #{frozen_error_class} if the thread is frozen" do
     running = false
     t = Thread.new do
       t.freeze
       -> {
         t[:foo] = "bar"
-      }.should raise_error(RuntimeError, /frozen/)
+      }.should raise_error(frozen_error_class, /frozen/)
     end
     t.join
   end
