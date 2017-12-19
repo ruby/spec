0b0d9cadec08d7596de9832e978ab1e23b6e3bc0
Andriy Konchin <andry.konchin@gmail.com>
2017-12-19 12:16:02 +0200

    [Code review] Change descriptions and add test case with raising not StandardError exceptions

--- core/integer/shared/arithmetic_exception_in_coerce.rb
+++ core/integer/shared/arithmetic_exception_in_coerce.rb
@@ -1,13 +1,25 @@
 require File.expand_path('../../fixtures/classes', __FILE__)
 
 describe :integer_arithmetic_exception_in_coerce, shared: true do
-  ruby_version_is "" ... "2.5" do
-    it "rescues exception raised in other#coerce and raises TypeError" do
+  ruby_version_is ""..."2.5" do
+    it "rescues exception (StandardError and subclasses) raised in other#coerce and raises TypeError" do
       b = mock("numeric with failed #coerce")
-      b.should_receive(:coerce).and_raise(RuntimeError)
+      b.should_receive(:coerce).and_raise(IntegerSpecs::CoerceError)
 
       # e.g. 1 + b
       -> { 1.send(@method, b) }.should raise_error(TypeError, /MockObject can't be coerced into #{1.class}/)
+    end
+
+    it "does not rescue Exception and StandardError siblings raised in other#coerce" do
+      [ Exception.new, NoMemoryError.new, ScriptError.new, SecurityError.new,
+        SignalException.new('INT'), SystemExit.new, SystemStackError.new
+      ].each do |exception|
+        b = mock("numeric with failed #coerce")
+        b.should_receive(:coerce).and_raise(exception)
+
+        # e.g. 1 + b
+        -> { 1.send(@method, b) }.should raise_error(exception)
+      end
     end
   end
 
