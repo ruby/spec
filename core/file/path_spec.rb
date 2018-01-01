40d8c9ab8e84e831209bbf7ff8067e4ba14dc72a
Andrew Konchin <andry.konchin@gmail.com>
2018-01-01 02:28:10 +0200

    Add specs for File.path

--- core/file/path_spec.rb
+++ core/file/path_spec.rb
@@ -40,7 +40,27 @@ describe "File.path" do
     rm_r @name
   end
 
-  it "returns the full path for the given file" do
-    File.path(@name).should == @name
+  it "returns the string argument without any change" do
+    File.path("abc").should == "abc"
+    File.path("./abc").should == "./abc"
+    File.path("../abc").should == "../abc"
+    File.path("/./a/../bc").should == "/./a/../bc"
+  end
+
+  it "returns path for File argument" do
+    File.open(@name, "w") do |f|
+      File.path(f).should == @name
+    end
+  end
+
+  it "returns path for Pathname argument" do
+    require "pathname"
+    File.path(Pathname.new(@name)).should == @name
+  end
+
+  it "calls #to_path for non-string argument and returns result" do
+    path = mock("path")
+    path.should_receive(:to_path).and_return("abc")
+    File.path(path).should == "abc"
   end
 end
