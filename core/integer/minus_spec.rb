6b2b01c7b3badb8cd62ebd466a748cc9ede5cc58
Andrew Konchin <andry.konchin@gmail.com>
2018-01-03 21:15:52 +0200

    Remove ruby version guards from coerce shared examples

--- core/integer/minus_spec.rb
+++ core/integer/minus_spec.rb
@@ -1,7 +1,15 @@
 require File.expand_path('../../../spec_helper', __FILE__)
-require File.expand_path('../shared/arithmetic_exception_in_coerce', __FILE__)
+require File.expand_path('../shared/arithmetic_coerce', __FILE__)
 
-describe "Integer#-" do
-  it_behaves_like :integer_arithmetic_exception_in_coerce, :-
+ruby_version_is "2.4" do
+  describe "Integer#-" do
+    ruby_version_is "2.4"..."2.5" do
+      it_behaves_like :integer_arithmetic_coerce_rescue, :-
+    end
+
+    ruby_version_is "2.5" do
+      it_behaves_like :integer_arithmetic_coerce_not_rescue, :-
+    end
+  end
 end
 
