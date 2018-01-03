394fb12323905a22f30b38261c9ccaf6b80dbd7a
Andrew Konchin <andry.konchin@gmail.com>
2018-01-03 21:15:52 +0200

    Remove ruby version guards from coerce shared examples

--- core/integer/exponent_spec.rb
+++ core/integer/exponent_spec.rb
@@ -2,6 +2,8 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../fixtures/classes', __FILE__)
 require File.expand_path('../shared/exponent', __FILE__)
 
-describe "Integer#**" do
-  it_behaves_like :integer_exponent, :**
+ruby_version_is '2.4' do
+  describe "Integer#**" do
+    it_behaves_like :integer_exponent, :**
+  end
 end
--- core/integer/gt_spec.rb
+++ core/integer/gt_spec.rb
@@ -1,7 +1,15 @@
 require File.expand_path('../../../spec_helper', __FILE__)
-require File.expand_path('../shared/comparison_exception_in_coerce', __FILE__)
+require File.expand_path('../shared/comparison_coerce', __FILE__)
 
-describe "Integer#>" do
-  it_behaves_like :integer_comparison_exception_in_coerce, :>
+ruby_version_is '2.4' do
+  describe "Integer#>" do
+    ruby_version_is "2.4"..."2.5" do
+      it_behaves_like :integer_comparison_coerce_rescue, :>
+    end
+
+    ruby_version_is "2.5" do
+      it_behaves_like :integer_comparison_coerce_not_rescue, :>
+    end
+  end
 end
 
