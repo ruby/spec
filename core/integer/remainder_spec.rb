dc4098ceb4fbf924f71a19f066e13c6ca9a7fd7d
Andrew Konchin <andry.konchin@gmail.com>
2018-01-06 20:39:07 +0200

    Add missing specs and refactor some of them

--- core/integer/remainder_spec.rb
+++ core/integer/remainder_spec.rb
@@ -1,6 +1,34 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
 describe "Integer#remainder" do
+  context "fixnum" do
+    it "returns the remainder of dividing self by other" do
+      5.remainder(3).should == 2
+      5.remainder(3.0).should == 2.0
+      5.remainder(Rational(3, 1)).should == Rational(2, 1)
+    end
+
+    it "means x-y*(x/y).truncate" do
+      5.remainder(3).should == 2
+      5.remainder(3.3).should be_close(1.7, TOLERANCE)
+      5.remainder(3.7).should be_close(1.3, TOLERANCE)
+    end
+
+    it "keeps sign of self" do
+       5.remainder( 3).should ==  2
+       5.remainder(-3).should ==  2
+      -5.remainder( 3).should == -2
+      -5.remainder(-3).should == -2
+    end
+
+    it "raises TypeError if passed non-numeric argument" do
+      -> { 5.remainder("3") }.should raise_error(TypeError)
+      -> { 5.remainder(:"3") }.should raise_error(TypeError)
+      -> { 5.remainder([]) }.should raise_error(TypeError)
+      -> { 5.remainder(nil) }.should raise_error(TypeError)
+    end
+  end
+
   context "bignum" do
     it "returns the remainder of dividing self by other" do
       a = bignum_value(79)
