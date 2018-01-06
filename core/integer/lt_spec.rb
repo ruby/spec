ddea593a788c8a4dc3f5c4ee0c0d5ea540edde14
Andrew Konchin <andry.konchin@gmail.com>
2018-01-06 16:37:07 +0200

    Adopt Bignum specs

--- core/integer/lt_spec.rb
+++ core/integer/lt_spec.rb
@@ -10,20 +10,43 @@ describe "Integer#<" do
     it_behaves_like :integer_comparison_coerce_not_rescue, :<
   end
 
-  it "returns true if self is less than the given argument" do
-    (2 < 13).should == true
-    (-600 < -500).should == true
+  context "fixnum" do
+    it "returns true if self is less than the given argument" do
+      (2 < 13).should == true
+      (-600 < -500).should == true
 
-    (5 < 1).should == false
-    (5 < 5).should == false
+      (5 < 1).should == false
+      (5 < 5).should == false
 
-    (900 < bignum_value).should == true
-    (5 < 4.999).should == false
+      (900 < bignum_value).should == true
+      (5 < 4.999).should == false
+    end
+
+    it "raises an ArgumentError when given a non-Integer" do
+      lambda { 5 < "4"       }.should raise_error(ArgumentError)
+      lambda { 5 < mock('x') }.should raise_error(ArgumentError)
+    end
   end
 
-  it "raises an ArgumentError when given a non-Integer" do
-    lambda { 5 < "4"       }.should raise_error(ArgumentError)
-    lambda { 5 < mock('x') }.should raise_error(ArgumentError)
+  context "bignum" do
+    before :each do
+      @bignum = bignum_value(32)
+    end
+
+    it "returns true if self is less than the given argument" do
+      (@bignum < @bignum + 1).should == true
+      (-@bignum < -(@bignum - 1)).should == true
+
+      (@bignum < 1).should == false
+      (@bignum < 5).should == false
+
+      (@bignum < 4.999).should == false
+    end
+
+    it "raises an ArgumentError when given a non-Integer" do
+      lambda { @bignum < "4" }.should raise_error(ArgumentError)
+      lambda { @bignum < mock('str') }.should raise_error(ArgumentError)
+    end
   end
 end
 
