0c415490273115cebe25689e660a41dfba17be87
Benoit Daloze <eregontp@gmail.com>
2018-01-13 20:15:19 +0100

    Unify usages of it_behaves_like to not include parenthesis

--- core/integer/modulo_spec.rb
+++ core/integer/modulo_spec.rb
@@ -2,9 +2,9 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../shared/modulo', __FILE__)
 
 describe "Integer#%" do
-  it_behaves_like(:integer_modulo, :%)
+  it_behaves_like :integer_modulo, :%
 end
 
 describe "Integer#modulo" do
-  it_behaves_like(:integer_modulo, :modulo)
+  it_behaves_like :integer_modulo, :modulo
 end
