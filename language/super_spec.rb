851d94c9e8f276a4e1c9b0312e1df55b3f8a1734
Andrew Konchin <andry.konchin@gmail.com>
2018-04-09 17:47:13 +0300

    Improve specs for super - reassigning rest arguments

--- language/super_spec.rb
+++ language/super_spec.rb
@@ -191,6 +191,50 @@ describe "The super keyword" do
     Super::RestArgsWithSuper::B.new.a.should == ["foo"]
   end
 
+  ruby_version_is "2.3" do
+    # https://bugs.ruby-lang.org/issues/14279
+    it "passes along reassigned rest args" do
+      # rubocop:disable Lint/ShadowedArgument
+      parent = Class.new do
+        def a(*args)
+          args
+        end
+      end
+
+      child = Class.new(parent) do
+        def a(*args)
+          args = ["foo"]
+
+          super
+        end
+      end
+
+      child.new.a("bar").should == ["foo"]
+      # rubocop:enable Lint/ShadowedArgument
+    end
+
+    # https://bugs.ruby-lang.org/issues/14279
+    it "wraps into array and passes along reassigned rest args with non-array value" do
+      # rubocop:disable Lint/ShadowedArgument
+      parent = Class.new do
+        def a(*args)
+          args
+        end
+      end
+
+      child = Class.new(parent) do
+        def a(*args)
+          args = "foo"
+
+          super
+        end
+      end
+
+      child.new.a("bar").should == ["foo"]
+      # rubocop:enable Lint/ShadowedArgument
+    end
+  end
+
   it "invokes methods from a chain of anonymous modules" do
     Super::AnonymousModuleIncludedTwice.new.a([]).should == ["anon", "anon", "non-anon"]
   end
