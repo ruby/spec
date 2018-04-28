9f61b5b251043253c9302751cc9d2a21751dd4fb
Benoit Daloze <eregontp@gmail.com>
2018-04-28 19:49:53 +0200

    Add version guards

--- security/cve_2018_8780_spec.rb
+++ security/cve_2018_8780_spec.rb
@@ -3,7 +3,6 @@ require_relative '../spec_helper'
 require 'tmpdir'
 
 describe "CVE-2018-8780 is resisted by" do
-
   before :all do
     @root = File.realpath(Dir.mktmpdir('__test_dir__'))
   end
@@ -26,22 +25,25 @@ describe "CVE-2018-8780 is resisted by" do
     }.should raise_error(ArgumentError, /string contains null byte/)
   end
 
-  it "Dir.children by raising an exception when there is a NUL byte" do
-    lambda {
-      Dir.children(@root+"\0")
-    }.should raise_error(ArgumentError, /string contains null byte/)
+  ruby_version_is "2.5" do
+    it "Dir.children by raising an exception when there is a NUL byte" do
+      lambda {
+        Dir.children(@root+"\0")
+      }.should raise_error(ArgumentError, /string contains null byte/)
+    end
+
+    it "Dir.each_child by raising an exception when there is a NUL byte" do
+      lambda {
+        Dir.each_child(@root+"\0").to_a
+      }.should raise_error(ArgumentError, /string contains null byte/)
+    end
   end
 
-  it "Dir.each_child by raising an exception when there is a NUL byte" do
-    lambda {
-      Dir.each_child(@root+"\0").to_a
-    }.should raise_error(ArgumentError, /string contains null byte/)
+  ruby_version_is "2.4" do
+    it "Dir.empty? by raising an exception when there is a NUL byte" do
+      lambda {
+        Dir.empty?(@root+"\0")
+      }.should raise_error(ArgumentError, /string contains null byte/)
+    end
   end
-
-  it "Dir.empty? by raising an exception when there is a NUL byte" do
-    lambda {
-      Dir.empty?(@root+"\0")
-    }.should raise_error(ArgumentError, /string contains null byte/)
-  end
-
 end
