a736a4b9112f2c5dc966a8b6ef46892016238ab4
Maxime Lapointe <hunter_spawn@hotmail.com>
2018-05-15 10:57:43 -0400

    Spec for Kernel#autoload and Kernel.autoload from included module

---
+++ core/kernel/fixtures/autoload_from_included_module.rb
@@ -0,0 +1,7 @@
+module KSAutoloadMethod
+  module KSAutoloadFromIncludedModule
+    def self.loaded
+      :autoload_from_included_module
+    end
+  end
+end
---
+++ core/kernel/fixtures/autoload_from_included_module2.rb
@@ -0,0 +1,7 @@
+module KSAutoloadMethod2
+  module KSAutoloadFromIncludedModule2
+    def self.loaded
+      :autoload_from_included_module2
+    end
+  end
+end
