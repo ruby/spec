a1aa80d0a9301a73c990c9a2b46a29ee495175e9
Benoit Daloze <eregontp@gmail.com>
2018-07-15 21:48:52 +0200

    Fix UDPSocket#inspect spec for Ruby 2.5+

--- library/socket/udpsocket/inspect_spec.rb
+++ library/socket/udpsocket/inspect_spec.rb
@@ -3,7 +3,6 @@ require_relative '../spec_helper'
 describe 'UDPSocket#inspect' do
   before do
     @socket = UDPSocket.new
-
     @socket.bind('127.0.0.1', 0)
   end
 
@@ -11,7 +10,16 @@ describe 'UDPSocket#inspect' do
     @socket.close
   end
 
-  it 'returns a String' do
-    @socket.inspect.should == "#<UDPSocket:fd #{@socket.fileno}>"
+  ruby_version_is ""..."2.5" do
+    it 'returns a String with the fd' do
+      @socket.inspect.should == "#<UDPSocket:fd #{@socket.fileno}>"
+    end
+  end
+
+  ruby_version_is "2.5" do
+    it 'returns a String with the fd, family, address and port' do
+      port = @socket.addr[1]
+      @socket.inspect.should == "#<UDPSocket:fd #{@socket.fileno}, AF_INET, 127.0.0.1, #{port}>"
+    end
   end
 end
