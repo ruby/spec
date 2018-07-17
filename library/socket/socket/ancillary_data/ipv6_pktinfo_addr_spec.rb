e1a0c123d850cf87e1e84e20193123d6316bc44e
Benoit Daloze <eregontp@gmail.com>
2018-07-17 14:21:49 +0200

    Guard Socket::AncillaryData specs
    * The constant doesn't exist on Windows.

--- library/socket/socket/ancillary_data/ipv6_pktinfo_addr_spec.rb
+++ library/socket/socket/ancillary_data/ipv6_pktinfo_addr_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../../spec_helper'
 
-with_feature :ipv6_pktinfo do
+with_feature :ancillary_data, :ipv6_pktinfo do
   describe 'Socket::AncillaryData#ipv6_pktinfo_addr' do
     it 'returns an Addrinfo' do
       data = Socket::AncillaryData.ipv6_pktinfo(Addrinfo.ip('::1'), 4)
--- library/socket/socket/ancillary_data/level_spec.rb
+++ library/socket/socket/ancillary_data/level_spec.rb
@@ -1,7 +1,9 @@
 require_relative '../../spec_helper'
 
-describe 'Socket::AncillaryData#level' do
-  it 'returns the level as a Fixnum' do
-    Socket::AncillaryData.new(:INET, :SOCKET, :RIGHTS, '').level.should == Socket::SOL_SOCKET
+with_feature :ancillary_data do
+  describe 'Socket::AncillaryData#level' do
+    it 'returns the level as a Fixnum' do
+      Socket::AncillaryData.new(:INET, :SOCKET, :RIGHTS, '').level.should == Socket::SOL_SOCKET
+    end
   end
 end
