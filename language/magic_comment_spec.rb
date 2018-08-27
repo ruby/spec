a997f377feabcbd6a0fe478def06e00928d6b149
Benoit Daloze <eregontp@gmail.com>
2018-08-27 14:45:42 +0200

    Remove trailing whitespace

--- language/magic_comment_spec.rb
+++ language/magic_comment_spec.rb
@@ -46,14 +46,14 @@ describe "Magic comments" do
       ruby_exe(nil, args: "< #{fixture(__FILE__, file)}", options: "-r#{print_at_exit}")
     }
   end
-  
+
   describe "in an -e argument" do
     it_behaves_like :magic_comments, nil, -> file {
       print_at_exit = fixture(__FILE__, "print_magic_comment_result_at_exit.rb")
       ruby_exe(nil, args: "-r#{print_at_exit} -e \"$(< #{fixture(__FILE__, file)})\"")
     }
   end
-  
+
   describe "in the main file" do
     it_behaves_like :magic_comments, nil, -> file {
       print_at_exit = fixture(__FILE__, "print_magic_comment_result_at_exit.rb")
--- language/safe_spec.rb
+++ language/safe_spec.rb
@@ -1,34 +1,34 @@
 require_relative '../spec_helper'
 
 describe "The $SAFE variable" do
-  
+
   ruby_version_is "2.6" do
     after :each do
       $SAFE = 0
     end
   end
-  
+
   it "is 0 by default" do
     $SAFE.should == 0
     proc {
       $SAFE.should == 0
     }.call
   end
-  
+
   it "can be set to 0" do
     proc {
       $SAFE = 0
       $SAFE.should == 0
     }.call
   end
-  
+
   it "can be set to 1" do
     proc {
       $SAFE = 1
       $SAFE.should == 1
     }.call
   end
-  
+
   [2, 3, 4].each do |n|
     it "cannot be set to #{n}" do
       lambda {
@@ -38,7 +38,7 @@ describe "The $SAFE variable" do
       }.should raise_error(ArgumentError, /\$SAFE=2 to 4 are obsolete/)
     end
   end
-  
+
   ruby_version_is ""..."2.6" do
     it "cannot be set to values below 0" do
         lambda {
@@ -56,7 +56,7 @@ describe "The $SAFE variable" do
         }.call
       }.should raise_error(ArgumentError, /\$SAFE=2 to 4 are obsolete/)
   end
-  
+
   ruby_version_is ""..."2.6" do
     it "cannot be manually lowered" do
       proc {
@@ -66,7 +66,7 @@ describe "The $SAFE variable" do
         }.should raise_error(SecurityError, /tried to downgrade safe level from 1 to 0/)
       }.call
     end
-    
+
     it "is automatically lowered when leaving a proc" do
       $SAFE.should == 0
       proc {
@@ -74,7 +74,7 @@ describe "The $SAFE variable" do
       }.call
       $SAFE.should == 0
     end
-    
+
     it "is automatically lowered when leaving a lambda" do
       $SAFE.should == 0
       lambda {
@@ -83,11 +83,11 @@ describe "The $SAFE variable" do
       $SAFE.should == 0
     end
   end
-  
+
   it "can be read when default from Thread#safe_level" do
     Thread.current.safe_level.should == 0
   end
-  
+
   it "can be read when modified from Thread#safe_level" do
     proc {
       $SAFE = 1
