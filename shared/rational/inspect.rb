e246194a9cf98a58d3582aed09b4564a8509c129
Benoit Daloze <eregontp@gmail.com>
2018-09-25 11:51:57 +0200

    Fix trailing whitespace

--- shared/rational/inspect.rb
+++ shared/rational/inspect.rb
@@ -5,7 +5,7 @@ describe :rational_inspect, shared: true do
     Rational(3, 4).inspect.should == "(3/4)"
     Rational(-5, 8).inspect.should == "(-5/8)"
     Rational(-1, -2).inspect.should == "(1/2)"
-    
+
     # Guard against the Mathn library
     guard -> { !defined?(Math.rsqrt) } do
       Rational(bignum_value, 1).inspect.should == "(#{bignum_value}/1)"
