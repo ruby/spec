395259b148d836289b554d4b746e92de6847581a
Ivo Anjo <ivo.anjo@ist.utl.pt>
2018-10-14 12:45:45 +0100

    Add specs for Set#intersect? and Set#disjoint?

--- library/set/intersection_spec.rb
+++ library/set/intersection_spec.rb
@@ -9,3 +9,23 @@ end
 describe "Set#&" do
   it_behaves_like :set_intersection, :&
 end
+
+describe "Set#intersect?" do
+  it "returns true when two Sets have at least one element in common" do
+    Set[1, 2].intersect?(Set[2, 3]).should == true
+  end
+
+  it "returns false when two Sets have no element in common" do
+    Set[1, 2].intersect?(Set[3, 4]).should == false
+  end
+end
+
+describe "Set#disjoint?" do
+  it "returns false when two Sets have at least one element in common" do
+    Set[1, 2].disjoint?(Set[2, 3]).should == false
+  end
+
+  it "returns true when two Sets have no element in common" do
+    Set[1, 2].disjoint?(Set[3, 4]).should == true
+  end
+end
