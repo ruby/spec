616aef98eef9349d4014544309dd19973327f59d
NARUSE, Yui <naruse@airemix.jp>
2018-12-29 03:52:36 +0900

    BigDecimal.ver was removed on Ruby 2.6

--- library/bigdecimal/ver_spec.rb
+++ library/bigdecimal/ver_spec.rb
@@ -2,10 +2,10 @@ require_relative '../../spec_helper'
 require 'bigdecimal'
 
 describe "BigDecimal.ver" do
-
-  it "returns the Version number" do
-    lambda {BigDecimal.ver }.should_not raise_error()
-    BigDecimal.ver.should_not == nil
+  ruby_version_is ""..."2.6" do
+    it "returns the Version number" do
+      lambda {BigDecimal.ver }.should_not raise_error()
+      BigDecimal.ver.should_not == nil
+    end
   end
-
 end
