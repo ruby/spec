b685734697ca198b5465810764af20da81aa331e
Ivo Anjo <ivo.anjo@ist.utl.pt>
2019-01-01 21:20:54 +0000

    Move #intersect? and #disjoint? to separate files

--- library/set/intersection_spec.rb
+++ library/set/intersection_spec.rb
@@ -1,6 +1,5 @@
 require_relative '../../spec_helper'
 require_relative 'shared/intersection'
-require_relative 'shared/set_like'
 require 'set'
 
 describe "Set#intersection" do
@@ -9,44 +8,4 @@ end
 
 describe "Set#&" do
   it_behaves_like :set_intersection, :&
-end
-
-describe "Set#intersect?" do
-  it "returns true when two Sets have at least one element in common" do
-    Set[1, 2].intersect?(Set[2, 3]).should == true
-  end
-
-  it "returns false when two Sets have no element in common" do
-    Set[1, 2].intersect?(Set[3, 4]).should == false
-  end
-
-  context "when comparing to a Set-like object" do
-    it "returns true when a Set has at least one element in common with a Set-like object" do
-      Set[1, 2].intersect?(SetLike.new([2, 3])).should be_true
-    end
-
-    it "returns false when a Set has no element in common with a Set-like object" do
-      Set[1, 2].intersect?(SetLike.new([3, 4])).should be_false
-    end
-  end
-end
-
-describe "Set#disjoint?" do
-  it "returns false when two Sets have at least one element in common" do
-    Set[1, 2].disjoint?(Set[2, 3]).should == false
-  end
-
-  it "returns true when two Sets have no element in common" do
-    Set[1, 2].disjoint?(Set[3, 4]).should == true
-  end
-
-  context "when comparing to a Set-like object" do
-    it "returns false when a Set has at least one element in common with a Set-like object" do
-      Set[1, 2].disjoint?(SetLike.new([2, 3])).should be_false
-    end
-
-    it "returns true when a Set has no element in common with a Set-like object" do
-      Set[1, 2].disjoint?(SetLike.new([3, 4])).should be_true
-    end
-  end
 end
