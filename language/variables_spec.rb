9a83e6a8653cfd24920d7ae17edf3a5787f537f0
Andrew Konchin <andry.konchin@gmail.com>
2019-01-02 15:02:10 +0200

    Ruby 2.6: The "shadowing outer local variable" warning is removed

--- language/variables_spec.rb
+++ language/variables_spec.rb
@@ -758,3 +758,38 @@ describe "A local variable assigned only within a conditional block" do
     end
   end
 end
+
+describe 'Local variable shadowing' do
+  ruby_version_is ""..."2.6" do
+    it "leads to warning in verbose mode" do
+      $VERBOSE, @verbose = true, $VERBOSE
+
+      -> do
+        eval <<-CODE
+          a = [1, 2, 3]
+          a.each { |a| a = 3 }
+        CODE
+      end.should complain(/shadowing outer local variable/)
+
+      $VERBOSE = @verbose
+    end
+  end
+
+  ruby_version_is "2.6" do
+    it "does not warn in verbose mode" do
+      $VERBOSE, @verbose = true, $VERBOSE
+      result = nil
+
+      -> do
+        eval <<-CODE
+          a = [1, 2, 3]
+          result = a.map { |a| a = 3 }
+        CODE
+      end.should_not complain
+
+      result.should == [3, 3, 3]
+
+      $VERBOSE = @verbose
+    end
+  end
+end
