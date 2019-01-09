0b6b415e251d4a476d4588d4d2d3c0e73bd33f61
Ilya Bylich <ibylich@gmail.com>
2019-01-09 20:22:16 +0300

    Ensure that Enumerator#+ calls #each on given arguments.

--- core/enumerator/plus_spec.rb
+++ core/enumerator/plus_spec.rb
@@ -17,5 +17,19 @@ ruby_version_is "2.6" do
       chain.each { |item| ScratchPad << item }
       ScratchPad.recorded.should == [1, 2, 3]
     end
+
+    it "calls #each on each argument" do
+      enum = Enumerator.new { |y| y << "one" }
+
+      obj1 = mock("obj1")
+      obj1.should_receive(:each).once.and_yield("two")
+
+      obj2 = mock("obj2")
+      obj2.should_receive(:each).once.and_yield("three")
+
+      chain = enum + obj1 + obj2
+      chain.each { |item| ScratchPad << item }
+      ScratchPad.recorded.should == ["one", "two", "three"]
+    end
   end
 end
