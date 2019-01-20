e4d8201bdb54221fa556735176a1b474c7ca1954
Benoit Daloze <eregontp@gmail.com>
2019-01-20 14:14:32 +0100

    Fix typo

--- optional/capi/numeric_spec.rb
+++ optional/capi/numeric_spec.rb
@@ -40,7 +40,7 @@ describe "CApiNumericSpecs" do
       it "calls #to_int to coerce the value" do
         obj = mock("number")
         obj.should_receive(:to_int).and_return(2)
-        @s.rb_num2long(obj).should == 2
+        @s.rb_num2int(obj).should == 2
       end
     end
   end
