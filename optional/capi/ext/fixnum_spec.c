18f74ff5480a053ea7c33ebeb7f4ef34d6d9f358
Benoit Daloze <eregontp@gmail.com>
2019-01-20 13:30:22 +0100

    Remove HAVE_ constants for C-API specs
    * They create a lot of boilerplate.
    * Any Ruby implementation supporting C-exts should use headers from MRI
      to declare all public C-API functions.
    * Conditions in Ruby code directly test the Ruby version anyway.

--- optional/capi/ext/fixnum_spec.c
+++ optional/capi/ext/fixnum_spec.c
@@ -15,14 +15,12 @@ static VALUE fixnum_spec_FIX2UINT(VALUE self, VALUE value) {
   return UINT2NUM(i);
 }
 
-#ifdef HAVE_RB_FIX2UINT
+#if SIZEOF_INT < SIZEOF_LONG
 static VALUE fixnum_spec_rb_fix2uint(VALUE self, VALUE value) {
   unsigned long i = rb_fix2uint(value);
   return ULONG2NUM(i);
 }
-#endif
 
-#ifdef HAVE_RB_FIX2INT
 static VALUE fixnum_spec_rb_fix2int(VALUE self, VALUE value) {
   long i = rb_fix2int(value);
   return LONG2NUM(i);
@@ -30,17 +28,11 @@ static VALUE fixnum_spec_rb_fix2int(VALUE self, VALUE value) {
 #endif
 
 void Init_fixnum_spec(void) {
-  VALUE cls;
-  cls = rb_define_class("CApiFixnumSpecs", rb_cObject);
-
+  VALUE cls = rb_define_class("CApiFixnumSpecs", rb_cObject);
   rb_define_method(cls, "FIX2INT", fixnum_spec_FIX2INT, 1);
   rb_define_method(cls, "FIX2UINT", fixnum_spec_FIX2UINT, 1);
-
-#ifdef HAVE_RB_FIX2UINT
+#if SIZEOF_INT < SIZEOF_LONG
   rb_define_method(cls, "rb_fix2uint", fixnum_spec_rb_fix2uint, 1);
-#endif
-
-#ifdef HAVE_RB_FIX2INT
   rb_define_method(cls, "rb_fix2int", fixnum_spec_rb_fix2int, 1);
 #endif
 
