01a38a1c876ae2ed666c699ee4b6d5ce6e841edd
Benoit Daloze <eregontp@gmail.com>
2019-04-11 22:40:26 +0200

    Simplify code in Coverage specs

--- library/coverage/fixtures/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-module CoverageSpecs
-  # Clear old results from the result hash
-  # https://bugs.ruby-lang.org/issues/12220
-  def self.filtered_result
-    result = Coverage.result
-    result
-  end
-end
