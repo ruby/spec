e69a14c069e66d91d0f723ccd3e865a3a699acda
Benoit Daloze <eregontp@gmail.com>
2019-09-29 17:48:07 +0200

    Use non-deprecated APIs

--- library/rubygems/gem/bin_path_spec.rb
+++ library/rubygems/gem/bin_path_spec.rb
@@ -13,7 +13,14 @@ describe "Gem.bin_path" do
 
   it "finds executables of default gems, which are the only files shipped for default gems" do
     # For instance, Gem.bin_path("bundler", "bundle") is used by rails new
-    Gem::Specification.each_spec([Gem::Specification.default_specifications_dir]) do |spec|
+
+    if Gem.respond_to? :default_specifications_dir
+      default_specifications_dir = Gem.default_specifications_dir
+    else
+      default_specifications_dir = Gem::Specification.default_specifications_dir
+    end
+
+    Gem::Specification.each_spec([default_specifications_dir]) do |spec|
       spec.executables.each do |exe|
         path = Gem.bin_path(spec.name, exe)
         File.exist?(path).should == true
