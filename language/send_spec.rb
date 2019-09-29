969b80b6f27b9e046dc7e0f8a6e3ae534fb37451
Benoit Daloze <eregontp@gmail.com>
2019-09-29 13:24:58 +0200

    Fix spacing

--- language/send_spec.rb
+++ language/send_spec.rb
@@ -265,6 +265,7 @@ describe "Invoking a private getter method" do
       -> { receiver.call_self_foo_or_equals(6) }.should raise_error(NoMethodError)
     end
   end
+
   ruby_version_is "2.7" do
     it "permits self as a receiver" do
       receiver = LangSendSpecs::PrivateGetter.new
