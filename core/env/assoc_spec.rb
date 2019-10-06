604858c56b2a9f912ff8d935fac5ff06787ea9d8
BurdetteLamar <burdettelamar@yahoo.com>
2019-10-06 11:43:28 -0500

    Accommodate reviews

--- core/env/assoc_spec.rb
+++ core/env/assoc_spec.rb
@@ -1,26 +1,27 @@
-require_relative 'env_spec_helper'
+require_relative '../../spec_helper'
 
 describe "ENV.assoc" do
   before :each do
-    reserve_variables("foo" => "bar")
+    @foo = ENV["foo"]
   end
-
   after :each do
-    release_variables
+    ENV["foo"] = @foo
   end
 
   it "returns an array of the key and value of the environment variable with the given key" do
+    ENV["foo"] = "bar"
     ENV.assoc("foo").should == ["foo", "bar"]
   end
 
   it "returns nil if no environment variable with the given key exists" do
-    ENV.delete("foo")
     ENV.assoc("foo").should == nil
   end
 
-  it "coerces a non-String name by calling #to_str" do
-    mock_object = mock_to_str(:foo)
-    ENV.assoc(mock_object).should == ["foo", "bar"]
+  it "returns the key element coerced with #to_str" do
+    ENV["foo"] = "bar"
+    k = mock('key')
+    k.should_receive(:to_str).and_return("foo")
+    ENV.assoc(k).should == ["foo", "bar"]
   end
 
   it "raises TypeError if the argument is not a String and does not respond to #to_str" do
