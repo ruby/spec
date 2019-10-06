4d1971afe2c0e07de24af848bfeb4ac7b54f6255
BurdetteLamar <burdettelamar@yahoo.com>
2019-10-06 12:31:48 -0500

    Test return value for ENV.clear

--- core/env/clear_spec.rb
+++ core/env/clear_spec.rb
@@ -3,8 +3,9 @@ require_relative '../../spec_helper'
 describe "ENV.clear" do
   it "deletes all environment variables" do
     orig = ENV.to_hash
+    env_object_id = ENV.object_id
     begin
-      ENV.clear
+      ENV.clear.object_id.should == env_object_id
 
       # This used 'env' the helper before. That shells out to 'env' which
       # itself sets up certain environment variables before it runs, because
