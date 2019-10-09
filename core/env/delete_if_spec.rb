fbe105b2057d112625f246029bfc6c03e877b66b
Burdette Lamar <BurdetteLamar@Yahoo.com>
2019-10-09 16:51:56 -0500

    Improve ENV#delete_if spec (#691)

--- core/env/delete_if_spec.rb
+++ core/env/delete_if_spec.rb
@@ -2,14 +2,31 @@ require_relative '../../spec_helper'
 require_relative '../enumerable/shared/enumeratorized'
 
 describe "ENV.delete_if" do
+  before :each do
+    @saved_envvars = {
+        "foo" => ENV["foo"],
+        "bar" => ENV["bar"],
+    }
+  end
+
+  after :each do
+    ENV.update(@saved_envvars)
+  end
+
   it "deletes pairs if the block returns true" do
-    ENV["foo"] = "bar"
-    ENV.delete_if { |k, v| k == "foo" }
+    ENV.update("foo" => "0", "bar" => "1")
+    ENV.delete_if { |k, v| ["foo", "bar"].include?(k) }
     ENV["foo"].should == nil
+    ENV["bar"].should == nil
+  end
+
+  it "returns ENV when block given" do
+    ENV.update("foo" => "0", "bar" => "1")
+    ENV.delete_if { |k, v| ["foo", "bar"].include?(k) }.should equal(ENV)
   end
 
   it "returns ENV even if nothing deleted" do
-    ENV.delete_if { false }.should_not == nil
+    ENV.delete_if { false }.should equal(ENV)
   end
 
   it "returns an Enumerator if no block given" do
@@ -17,10 +34,22 @@ describe "ENV.delete_if" do
   end
 
   it "deletes pairs through enumerator" do
-    ENV["foo"] = "bar"
+    ENV.update("foo" => "0", "bar" => "1")
     enum = ENV.delete_if
-    enum.each { |k, v| k == "foo" }
+    enum.each { |k, v| ["foo", "bar"].include?(k) }
     ENV["foo"].should == nil
+    ENV["bar"].should == nil
+  end
+
+  it "returns ENV from enumerator" do
+    ENV.update("foo" => "0", "bar" => "1")
+    enum = ENV.delete_if
+    enum.each { |k, v| ["foo", "bar"].include?(k) }.should equal(ENV)
+  end
+
+  it "returns ENV from enumerator even if nothing deleted" do
+    enum = ENV.delete_if
+    enum.each { false }.should equal(ENV)
   end
 
   it_behaves_like :enumeratorized_with_origin_size, :delete_if, ENV
