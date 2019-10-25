fdb660c49ed6a27de601954391898d5bc76c8164
BurdetteLamar <burdettelamar@yahoo.com>
2019-10-25 14:11:06 -0500

    Enhancements for ENV.replace

--- core/env/replace_spec.rb
+++ core/env/replace_spec.rb
@@ -1,15 +1,17 @@
 require_relative '../../spec_helper'
 
 describe "ENV.replace" do
-
   it "replaces ENV with a Hash" do
-    ENV["foo"] = "bar"
-    e = ENV.reject { |k, v| k == "foo" }
-    e["baz"] = "bam"
-    ENV.replace e
-    ENV["foo"].should == nil
-    ENV["baz"].should == "bam"
-    ENV.delete "baz"
+    orig = ENV.to_hash
+    begin
+      ENV.replace("foo" => "0", "bar" => "1").should equal(ENV)
+      ENV.size.should == 2
+      ENV["foo"].should == "0"
+      ENV["bar"].should == "1"
+    ensure
+      ENV.replace(orig)
+      ENV.size.should == orig.size
+    end
   end
 
 end
