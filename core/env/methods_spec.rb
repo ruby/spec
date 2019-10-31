e89e44e88f14f1fdd51c1b99592ce7cfca010d68
BurdetteLamar <burdettelamar@yahoo.com>
2019-10-31 14:07:42 -0500

    Update per review and fix error in methods_spec

--- core/env/methods_spec.rb
+++ core/env/methods_spec.rb
@@ -2,15 +2,20 @@ require_relative '../../spec_helper'
 
 ruby_version_is "2.7" do
   describe "ENV.methods" do
-    it "includes method :merge"
-    ENV.methods.should include(:merge)
+    it "includes new methods" do
+      [:merge].each do |new_method|
+        ENV.methods.should include(new_method)
+      end
+    end
   end
 end
 
 ruby_version_is "2.6" do
   describe "ENV.methods" do
-    it "includes method :slice" do
-      ENV.methods.should include(:slice)
+    it "includes new methods" do
+      [:filter, :filter!, :slice].each do |new_method|
+       ENV.methods.should include(new_method)
+      end
     end
   end
 end
@@ -23,12 +28,12 @@ ruby_version_is "2.5" do
   describe "ENV.methods" do
     it "returns an array of methods" do
       methods = ENV.methods false
-      expected_methods = [:[], :[]=, :assoc, :clear, :delete, :delete_if, :each, :each_key, :each_pair, :each_value, :empty?, :fetch, :filter, :filter!, :has_key?, :has_value?, :include?, :index, :inspect, :invert, :keep_if, :key, :key?, :keys, :length, :member?, :rassoc, :rehash, :reject, :reject!, :replace, :select, :select!, :shift, :size, :store, :to_a, :to_h, :to_hash, :to_s, :update, :value?, :values, :values_at]
+      expected_methods = [:[], :[]=, :assoc, :clear, :delete, :delete_if, :each, :each_key, :each_pair, :each_value, :empty?, :fetch, :has_key?, :has_value?, :include?, :index, :inspect, :invert, :keep_if, :key, :key?, :keys, :length, :member?, :rassoc, :rehash, :reject, :reject!, :replace, :select, :select!, :shift, :size, :store, :to_a, :to_h, :to_hash, :to_s, :update, :value?, :values, :values_at]
       expected_methods.each do |method|
         methods.should include(method)
       end
       # :pretty_print included by spec itself, apparently.
-      (methods - expected_methods - [:slice, :merge]).should == [:pretty_print]
+      (methods - expected_methods - [:filter, :filter!, :slice, :merge]).should == [:pretty_print]
     end
   end
 end
