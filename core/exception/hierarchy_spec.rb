79aa66fc35037a1271e762a5aa46d7234abe769c
BurdetteLamar <burdettelamar@yahoo.com>
2019-11-02 11:04:57 -0500

    Check exception class hierarchy

---
+++ core/exception/hierarchy_spec.rb
@@ -0,0 +1,68 @@
+require_relative '../../spec_helper'
+
+describe "Exception" do
+  it "has the right class hierarchy" do
+    def save_pairs(parent, hash, pairs)
+      hash.each do |key, value|
+        pairs.push [parent, key]
+        if value.is_a?(Hash)
+          save_pairs(key, value, pairs)
+        end
+      end
+    end
+    hierarchy = {
+        Exception => {
+            NoMemoryError => nil,
+            ScriptError => {
+                LoadError => nil,
+                NotImplementedError => nil,
+                SyntaxError => nil,
+            },
+            SecurityError => nil,
+            SignalException => {
+                Interrupt => nil,
+            },
+            StandardError => {
+                ArgumentError => {
+                    UncaughtThrowError => nil,
+                },
+                EncodingError => nil,
+                FiberError => nil,
+                IOError => {
+                    EOFError => nil,
+                },
+                IndexError => {
+                    KeyError => nil,
+                    StopIteration => {
+                        ClosedQueueError => nil,
+                    },
+                },
+                LocalJumpError => nil,
+                NameError => {
+                    NoMethodError => nil,
+                },
+                RangeError => {
+                    FloatDomainError => nil,
+                },
+                RegexpError => nil,
+                RuntimeError => {
+                    FrozenError => nil,
+                },
+                SystemCallError => nil,
+            ThreadError => nil,
+            TypeError => nil,
+            ZeroDivisionError => nil,
+            },
+        SystemExit => nil,
+        SystemStackError => nil,
+        },
+    }
+    pairs = []
+    save_pairs(Object, hierarchy, pairs)
+    pairs.each do |pair|
+      this_class, sub_class = *pair
+      sub_class.class.should == Class
+      sub_class.superclass.should == this_class
+    end
+  end
+end
