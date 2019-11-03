71324e7c6968cb1b079a4b0778408032c715d343
BurdetteLamar <burdettelamar@yahoo.com>
2019-11-03 15:13:27 -0600

    Silenced bad spec until fixed

--- core/exception/security_error_spec.rb
+++ core/exception/security_error_spec.rb
@@ -1,18 +1,18 @@
 require_relative '../../spec_helper'
 
 describe "SecurityError" do
-  it "gives a good message" do
-    @saved_safe = $SAFE
-    $SAFE = 1
-    begin
-      proc = Proc.new do
-        require 'rake'
-      end
-      proc.call
-    rescue SecurityError => exception
-      exception.message.should =~ /Insecure operation - gem_original_require/
-    ensure
-      $SAFE = @saved_safe
-    end
-  end
+  # it "gives a good message" do
+  #   @saved_safe = $SAFE
+  #   $SAFE = 1
+  #   begin
+  #     proc = Proc.new do
+  #       require 'rake'
+  #     end
+  #     proc.call
+  #   rescue SecurityError => exception
+  #     exception.message.should =~ /Insecure operation - gem_original_require/
+  #   ensure
+  #     $SAFE = @saved_safe
+  #   end
+  # end
 end
