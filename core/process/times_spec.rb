0d68f0ce8031eb9865d37e7a0dd5508da5507dff
Benoit Daloze <eregontp@gmail.com>
2019-11-03 14:58:07 +0100

    Speed up and simplify Process.times spec

--- core/process/times_spec.rb
+++ core/process/times_spec.rb
@@ -7,21 +7,9 @@ describe "Process.times" do
 
   it "returns current cpu times" do
     t = Process.times
+    user = t.utime
 
-    # Do busy work for a wall-clock interval.
-    start = Time.now
-    1 until (Time.now - start) > 0.5
-
-    # Ensure times is larger. NOTE that there is no
-    # guarantee of an upper bound since anything may be
-    # happening at the OS level, so we ONLY check that at
-    # least an interval has elapsed. Also, we are assuming
-    # there is a correlation between wall clock time and
-    # process time. In practice, there is an observed
-    # discrepancy often 10% or greater. In other words,
-    # this is a very fuzzy test.
-    t2 = Process.times
-    diff = (t2.utime + t2.stime) - (t.utime + t.stime)
-    diff.should > 0
+    1 until Process.times.utime > user
+    Process.times.utime.should > user
   end
 end
