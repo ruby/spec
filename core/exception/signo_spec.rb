7b948a86548feba86030159916885290b7987ad8
BurdetteLamar <burdettelamar@yahoo.com>
2019-12-04 15:36:06 -0600

    Specs for SignalError #signm and #signo

--- core/exception/signo_spec.rb
+++ core/exception/signo_spec.rb
@@ -1,5 +1,11 @@
 require_relative '../../spec_helper'
 
 describe "SignalException#signo" do
-  it "needs to be reviewed for spec completeness"
+  it "returns the signal number" do
+    name = Signal.list.keys.last
+    number = Signal.list[name]
+    -> { Process.kill(name, Process.pid) }.should raise_error(SignalException) { |e|
+      e.signo.should == number
+    }
+  end
 end
