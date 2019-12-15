6266f4f474587749a9c576c5ccfd06a7b4a43ad9
BurdetteLamar <burdettelamar@yahoo.com>
2019-12-15 11:53:03 -0600

    Respond to review

--- core/exception/signm_spec.rb
+++ core/exception/signm_spec.rb
@@ -2,9 +2,8 @@ require_relative '../../spec_helper'
 
 describe "SignalException#signm" do
   it "returns the signal name" do
-    name = Signal.list.keys.last
-    -> { Process.kill(name, Process.pid) }.should raise_error(SignalException) { |e|
-      e.signm.should == 'SIG' + name
+    -> { Process.kill(:TERM, Process.pid) }.should raise_error(SignalException) { |e|
+      e.signm.should == 'SIGTERM'
     }
   end
 end
--- core/exception/status_spec.rb
+++ core/exception/status_spec.rb
@@ -2,8 +2,8 @@ require_relative '../../spec_helper'
 
 describe "SystemExit#status" do
   it "returns the exit status" do
-    -> { exit }.should raise_error(SystemExit) { |e|
-      e.status.should == 0
+    -> { exit 42 }.should raise_error(SystemExit) { |e|
+      e.status.should == 42
     }
   end
 end
