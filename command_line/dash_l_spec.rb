37a64a0c214071c142c5cdbc437ff6830f9ecc89
Benoit Daloze <eregontp@gmail.com>
2020-02-28 17:48:28 +0100

    Add version guard
    * 2.4.9 seems to misbehave here, but 2.5+ is fine.

--- command_line/dash_l_spec.rb
+++ command_line/dash_l_spec.rb
@@ -11,10 +11,12 @@ describe "The -l command line option" do
         "false\nfalse\nfalse\n"
   end
 
-  it "chomps last line based on $/" do
-    ruby_exe('BEGIN { $/ = "ones\n" }; puts $_', options: "-n -l", escape: true,
-             args: " < #{@names}").should ==
-        "alice j\nbob field\njames grey\n"
+  ruby_version_is "2.5" do
+    it "chomps last line based on $/" do
+      ruby_exe('BEGIN { $/ = "ones\n" }; puts $_', options: "-n -l", escape: true,
+               args: " < #{@names}").should ==
+          "alice j\nbob field\njames grey\n"
+    end
   end
 
   it "sets $\\ to the value of $/" do
