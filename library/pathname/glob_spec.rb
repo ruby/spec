5d82c37b8ea5b07b90a014a2192577744db185a9
HeroProtagonist <jstepper91@gmail.com>
2020-10-25 12:14:24 -0400

    Use regex for unknown keyword error

--- library/pathname/glob_spec.rb
+++ library/pathname/glob_spec.rb
@@ -34,17 +34,17 @@ describe 'Pathname#glob' do
     Pathname.glob('*i*.rb', base: @dir + 'lib').sort.should == [Pathname.new('ipaddr.rb'), Pathname.new('irb.rb')].sort
   end
 
+  it "raises an ArgumentError when supplied a keyword argument other than :base" do
+    -> {
+      Pathname.glob('*i*.rb', foo: @dir + 'lib')
+    }.should raise_error(ArgumentError, /unknown keyword: :?foo/)
+  end
+
   ruby_version_is ''...'2.7' do
     it 'raises an ArgumentError when supplied a flag and :base keyword argument' do
       -> {
         Pathname.glob(@dir + 'lib/*i*.rb', File::FNM_DOTMATCH, base: 'lib')
       }.should raise_error(ArgumentError, 'wrong number of arguments (given 3, expected 1..2)')
-    end
-
-    it "raises an ArgumentError when supplied a keyword argument other than :base" do
-      -> {
-        Pathname.glob('*i*.rb', foo: @dir + 'lib')
-      }.should raise_error(ArgumentError, 'unknown keyword: foo')
     end
   end
 
@@ -52,12 +52,6 @@ describe 'Pathname#glob' do
     it "does not raise an ArgumentError when supplied a flag and :base keyword argument" do
       expected = [Pathname.new('ipaddr.rb'), Pathname.new('irb.rb'), Pathname.new('.hidden.rb')].sort
       Pathname.glob('*i*.rb', File::FNM_DOTMATCH, base: @dir + 'lib').sort.should == expected
-    end
-
-    it "raises an ArgumentError when supplied a keyword argument other than :base" do
-      -> {
-        Pathname.glob('*i*.rb', foo: @dir + 'lib')
-      }.should raise_error(ArgumentError, 'unknown keyword: :foo')
     end
   end
 end
