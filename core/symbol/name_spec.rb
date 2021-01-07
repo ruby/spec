df94ad87b5827fdf48c789710025bbf41dab3aff
lxxxvi <lxxxvi@users.noreply.github.com>
2021-01-07 15:41:14 +0100

    spec for `Symbol#name` to return a string even if dynamically generated

--- core/symbol/name_spec.rb
+++ core/symbol/name_spec.rb
@@ -2,6 +2,12 @@ require_relative '../../spec_helper'
 
 ruby_version_is "3.0" do
   describe "Symbol#name" do
+    it "returns a string" do
+      :ruby.name.should == "ruby"
+      :ルビー.name.should == "ルビー"
+      :"ruby_#{1+2}".name.should == "ruby_3"
+    end
+
     it "returns a frozen string" do
       :symbol.name.should.frozen?
     end
