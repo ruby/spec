dffb85a6a2ab3fb546fefd9a35cf32922908b0b9
sampatbadhe <sampat.badhe@kiprosh.com>
2021-08-22 17:57:03 +0530

    add spec for nil?

--- core/kernel/nil_spec.rb
+++ core/kernel/nil_spec.rb
@@ -1,6 +1,16 @@
 require_relative '../../spec_helper'
 require_relative 'fixtures/classes'
 
-describe "Kernel#nil?" do
-  it "needs to be reviewed for spec completeness"
+describe 'Kernel#nil?' do
+  it 'returns false for other than nil' do
+    Object.should_not.nil?
+    Object.new.should_not.nil?
+    ''.should_not.nil?
+    [].should_not.nil?
+    {}.should_not.nil?
+  end
+
+  it 'returns true for nil' do
+    nil.should.nil?
+  end
 end
