b3f9da93a94ce500a0806e08221426caa5243149
sampatbadhe <sampat.badhe@kiprosh.com>
2021-08-22 18:25:48 +0530

    update spec description to verify nil? on non nil object and remove duplicate spec to verify nil? for nil

--- core/kernel/nil_spec.rb
+++ core/kernel/nil_spec.rb
@@ -2,15 +2,11 @@ require_relative '../../spec_helper'
 require_relative 'fixtures/classes'
 
 describe 'Kernel#nil?' do
-  it 'returns false for other than nil' do
+  it 'returns false' do
     Object.should_not.nil?
     Object.new.should_not.nil?
     ''.should_not.nil?
     [].should_not.nil?
     {}.should_not.nil?
-  end
-
-  it 'returns true for nil' do
-    nil.should.nil?
   end
 end
