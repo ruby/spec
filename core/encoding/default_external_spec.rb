61d8642e9327c4d5215bd300bbe677868db98d65
Dmitriy Ivliev <rootkit89@gmail.com>
2021-10-05 19:11:06 +0300

    changed default for Encoding.default_external to UTF-8 on Windows

--- core/encoding/default_external_spec.rb
+++ core/encoding/default_external_spec.rb
@@ -17,6 +17,14 @@ describe "Encoding.default_external" do
     Encoding.default_external = Encoding::SHIFT_JIS
     Encoding.default_external.should == Encoding::SHIFT_JIS
   end
+
+  ruby_version_is "3.0" do
+    platform_is :windows do
+      it 'returns default external encoding for Windows' do
+        Encoding.default_external.should == Encoding::UTF_8
+      end
+    end
+  end
 end
 
 describe "Encoding.default_external=" do
