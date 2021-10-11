a2776f4fa384b4003247fcdd0838f91cacc6ed6b
Benoit Daloze <eregontp@gmail.com>
2021-10-11 15:20:39 +0200

    Rename and clarify SortedSet spec a bit

--- library/set/sortedset/sortedset_spec.rb
+++ library/set/sortedset/sortedset_spec.rb
@@ -2,10 +2,10 @@ require_relative '../../../spec_helper'
 require 'set'
 
 ruby_version_is "3.0" do
-  describe "SortedSet.new" do
-    it "throws error including message that it has been extracted" do
+  describe "SortedSet" do
+    it "raises error including message that it has been extracted from the set stdlib" do
       -> {
-        SortedSet.new
+        SortedSet
       }.should raise_error(RuntimeError) { |e|
         e.message.should.include?("The `SortedSet` class has been extracted from the `set` library")
       }
@@ -14,9 +14,9 @@ ruby_version_is "3.0" do
 end
 
 ruby_version_is ""..."3.0" do
-  describe "SortedSet.new" do
-    it "returns an instance of SortedSet" do
-      SortedSet.new.should be_an_instance_of(SortedSet)
+  describe "SortedSet" do
+    it "is part of the set stdlib" do
+      SortedSet.superclass.should == Set
     end
   end
 end
