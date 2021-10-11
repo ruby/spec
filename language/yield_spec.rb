bfd72afcb791c12d55219caf30df7398d48e41c5
Benoit Daloze <eregontp@gmail.com>
2021-10-11 15:28:28 +0200

    Simplify spec

--- language/yield_spec.rb
+++ language/yield_spec.rb
@@ -189,12 +189,12 @@ describe "Using yield in a singleton class literal" do
   ruby_version_is "2.7"..."3.0" do
     it 'emits a deprecation warning' do
       code = <<~RUBY
-          def m
-            class << Object.new
-              yield
-            end
+        def m
+          class << Object.new
+            yield
           end
-          m { :ok }
+        end
+        m { :ok }
       RUBY
 
       -> { eval(code) }.should complain(/warning: `yield' in class syntax will not be supported from Ruby 3.0/)
@@ -204,12 +204,9 @@ describe "Using yield in a singleton class literal" do
   ruby_version_is "3.0" do
     it 'raises a SyntaxError' do
       code = <<~RUBY
-          def m
-            class << Object.new
-              yield
-            end
-          end
-          m { :ok }
+        class << Object.new
+          yield
+        end
       RUBY
 
       -> { eval(code) }.should raise_error(SyntaxError, /Invalid yield/)
