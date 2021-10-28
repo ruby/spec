db11da1a7b3ad09e9b5acfbdfdd2717bde7d0216
Benoit Daloze <eregontp@gmail.com>
2021-10-28 16:25:39 +0200

    Add missing version guards

--- command_line/dash_w_spec.rb
+++ command_line/dash_w_spec.rb
@@ -4,7 +4,9 @@ require_relative 'shared/verbose'
 describe "The -w command line option" do
   it_behaves_like :command_line_verbose, "-w"
 
-  it "enables both deprecated and experimental warnings" do
-    ruby_exe('p Warning[:deprecated]; p Warning[:experimental]', options: '-w').should == "true\ntrue\n"
+  ruby_version_is "2.7" do
+    it "enables both deprecated and experimental warnings" do
+      ruby_exe('p Warning[:deprecated]; p Warning[:experimental]', options: '-w').should == "true\ntrue\n"
+    end
   end
 end
