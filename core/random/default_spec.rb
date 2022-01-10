df0a96232bd3735fccd6734f92a609ef1d0549b3
Benoit Daloze <eregontp@gmail.com>
2022-01-10 15:45:41 +0100

    Revert "Remove unnecessary Random::DEFAULT expectations"
    * This reverts commit e7cef3489a4e06104c73b2d89492259ea2754a97.
    * These specs are valuable for Ruby implementations, do not remove, cc @nobu.

--- core/random/default_spec.rb
+++ core/random/default_spec.rb
@@ -13,6 +13,28 @@ describe "Random::DEFAULT" do
       seed2 = ruby_exe('p Random::DEFAULT.seed', options: '--disable-gems')
       seed1.should != seed2
     end
+
+    ruby_version_is ''...'3.0' do
+      it "returns a Random instance" do
+        suppress_warning do
+          Random::DEFAULT.should be_an_instance_of(Random)
+        end
+      end
+    end
+
+    ruby_version_is '3.0' do
+      it "refers to the Random class" do
+        suppress_warning do
+          Random::DEFAULT.should.equal?(Random)
+        end
+      end
+
+      it "is deprecated" do
+        -> {
+          Random::DEFAULT.should.equal?(Random)
+        }.should complain(/constant Random::DEFAULT is deprecated/)
+      end
+    end
   end
 
   ruby_version_is '3.2' do
