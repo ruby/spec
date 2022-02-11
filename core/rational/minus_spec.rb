d4a7ab33b9dd4470cb8272628cef56770552ed1a
timcraft <mail@timcraft.com>
2022-02-11 11:55:16 +0000

    Fix core/rational/minus_spec.rb

--- core/rational/minus_spec.rb
+++ core/rational/minus_spec.rb
@@ -5,3 +5,15 @@ describe "Rational#-" do
   it_behaves_like :rational_minus, :-
   it_behaves_like :rational_arithmetic_exception_in_coerce, :-
 end
+
+describe "Rational#- passed a Rational" do
+  it_behaves_like :rational_minus_rat, :-
+end
+
+describe "Rational#- passed a Float" do
+  it_behaves_like :rational_minus_float, :-
+end
+
+describe "Rational#- passed an Integer" do
+  it_behaves_like :rational_minus_int, :-
+end
