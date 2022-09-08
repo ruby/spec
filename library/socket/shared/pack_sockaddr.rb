0e55320e51c74ed6bb8c8bbd5e3ac5dbc06bf1d7
Tim Morgan <tim@timmorgan.org>
2022-09-08 12:57:35 -0500

    Socket: add spec for converting service name to port

--- library/socket/shared/pack_sockaddr.rb
+++ library/socket/shared/pack_sockaddr.rb
@@ -17,6 +17,9 @@ describe :socket_pack_sockaddr_in, shared: true do
 
     sockaddr_in = Socket.public_send(@method, nil, '127.0.0.1')
     Socket.unpack_sockaddr_in(sockaddr_in).should == [0, '127.0.0.1']
+
+    sockaddr_in = Socket.public_send(@method, 'http', '127.0.0.1')
+    Socket.unpack_sockaddr_in(sockaddr_in).should == [80, '127.0.0.1']
   end
 
   describe 'using an IPv4 address' do
