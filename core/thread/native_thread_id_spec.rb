3ad9cfdc7fbeba2855cd892f308a090fa3cd64b6
HeroProtagonist <jstepper91@gmail.com>
2022-10-22 01:08:13 -0400

    Add linux specific check

--- core/thread/native_thread_id_spec.rb
+++ core/thread/native_thread_id_spec.rb
@@ -6,6 +6,7 @@ ruby_version_is ''...'3.1' do
       -> {
         th = Thread.new {}
         th.native_thread_id
+        th.join
       }.should raise_error(NoMethodError)
     end
   end
@@ -13,9 +14,20 @@ end
 
 ruby_version_is "3.1" do
   describe "Thread#native_thread_id" do
-    it "returns an integer" do
-      th = Thread.new {}
-      th.native_thread_id.should be_kind_of(Integer)
+    platform_is_not :linux do
+      it "returns an integer" do
+        th = Thread.new {}
+        th.native_thread_id.should be_kind_of(Integer)
+        th.join
+      end
+    end
+
+    platform_is :linux do
+      it "returns nil" do
+        th = Thread.new {}
+        th.native_thread_id.should be_nil
+        th.join
+      end
     end
   end
 end
