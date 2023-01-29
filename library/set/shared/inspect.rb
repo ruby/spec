f4bb156cbedf7d1c1b68f994b0d1feacf27a8181
Herwin Weststrate <herwin@snt.utwente.nl>
2023-01-29 16:22:18 +0100

    Add check for output of multiple items in Set stringification
    To remove the dependency on ordering, the check only checks for the
    bounds and the separator of the Set, and does not check the entries
    themselves.

--- library/set/shared/inspect.rb
+++ library/set/shared/inspect.rb
@@ -11,6 +11,10 @@ describe "set_inspect", shared: true do
     Set["1"].send(@method).should == '#<Set: {"1"}>'
   end
 
+  it "puts spaces between the elements" do
+    Set["1", "2"].send(@method).should include('", "')
+  end
+
   it "correctly handles self-references" do
     (set = Set[]) << set
     set.send(@method).should be_kind_of(String)
