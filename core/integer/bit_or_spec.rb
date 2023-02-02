46c3cd497fbaebcbae6d43ad210631f1683de4e6
Herwin Weststrate <herwin@snt.utwente.nl>
2023-02-02 08:21:13 +0100

    Fix naming of mocks in bitwise Integer specs
    The test was copy-pasted from bit_and_spec, and was still named 'and'

--- core/integer/bit_or_spec.rb
+++ core/integer/bit_or_spec.rb
@@ -30,7 +30,7 @@ describe "Integer#|" do
     end
 
     it "coerces the rhs and calls #coerce" do
-      obj = mock("fixnum bit and")
+      obj = mock("fixnum bit or")
       obj.should_receive(:coerce).with(6).and_return([6, 3])
       (6 & obj).should == 2
     end
