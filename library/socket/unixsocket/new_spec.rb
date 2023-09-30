3e71d894259f157ebbc32af56d8222c0d35b0ff0
Herwin <herwinw@users.noreply.github.com>
2023-09-30 14:58:46 +0200

    Wrap tests for unixsocket in an outer guard block
    Normalize to `with_feature :unix_socket`, which is more descriptive and
    generic than checking for windows.

--- library/socket/unixsocket/new_spec.rb
+++ library/socket/unixsocket/new_spec.rb
@@ -1,6 +1,8 @@
 require_relative '../spec_helper'
 require_relative 'shared/new'
 
-describe "UNIXSocket.new" do
-  it_behaves_like :unixsocket_new, :new
+with_feature :unix_socket do
+  describe "UNIXSocket.new" do
+    it_behaves_like :unixsocket_new, :new
+  end
 end
