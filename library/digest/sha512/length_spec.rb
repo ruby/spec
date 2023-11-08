cfed5b1434839e0a3507d579a33b1d15d1ddf559
Herwin <herwinw@users.noreply.github.com>
2023-11-08 18:05:01 +0100

    Draft: Convert specs for OpenSSL::Digest#digest_length to shared specs

--- library/digest/sha512/length_spec.rb
+++ library/digest/sha512/length_spec.rb
@@ -1,7 +1,6 @@
 require_relative '../../../spec_helper'
-require_relative 'shared/constants'
 require_relative 'shared/length'
 
 describe "Digest::SHA512#length" do
-  it_behaves_like :sha512_length, :length
+  it_behaves_like :sha512_length, :length, Digest::SHA512.new
 end
--- library/digest/sha512/size_spec.rb
+++ library/digest/sha512/size_spec.rb
@@ -1,7 +1,6 @@
 require_relative '../../../spec_helper'
-require_relative 'shared/constants'
 require_relative 'shared/length'
 
 describe "Digest::SHA512#size" do
-  it_behaves_like :sha512_length, :size
+  it_behaves_like :sha512_length, :size, Digest::SHA512.new
 end
