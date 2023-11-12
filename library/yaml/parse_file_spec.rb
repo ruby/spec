92d220425d8c2190c220933a8cfbb28027c17684
Herwin <herwinw@users.noreply.github.com>
2023-11-12 07:56:45 +0100

    Fix confusion between instance and class methods in YAML specs

--- library/yaml/parse_file_spec.rb
+++ library/yaml/parse_file_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../spec_helper'
 require_relative 'fixtures/common'
 
-describe "YAML#parse_file" do
+describe "YAML.parse_file" do
   it "returns a YAML::Syck::Map object after parsing a YAML file" do
     YAML.parse_file($test_parse_file).should be_kind_of(Psych::Nodes::Document)
   end
