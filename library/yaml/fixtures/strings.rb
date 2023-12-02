3006e4a036c6e2126d0e322db300d9eef231b376
Herwin <herwinw@users.noreply.github.com>
2023-12-02 17:50:54 +0100

    Convert globals of YAML specs into scoped constants

--- library/yaml/fixtures/strings.rb
+++ library/yaml/fixtures/strings.rb
@@ -1,24 +1,26 @@
-$complex_key_1 = <<EOY
-    ? # PLAY SCHEDULE
-      - Detroit Tigers
-      - Chicago Cubs
-    :
-      - 2001-07-23
+module YAMLSpecs
+  COMPLEX_KEY_1 = <<~EOY
+      ? # PLAY SCHEDULE
+        - Detroit Tigers
+        - Chicago Cubs
+      :
+        - 2001-07-23
 
-    ? [ New York Yankees,
-        Atlanta Braves ]
-    : [ 2001-07-02, 2001-08-12,
-       2001-08-14 ]
-EOY
+      ? [ New York Yankees,
+          Atlanta Braves ]
+      : [ 2001-07-02, 2001-08-12,
+         2001-08-14 ]
+  EOY
 
-$multidocument = <<EOY
----
-- Mark McGwire
-- Sammy Sosa
-- Ken Griffey
+  MULTIDOCUMENT = <<~EOY
+  ---
+  - Mark McGwire
+  - Sammy Sosa
+  - Ken Griffey
 
-# Team ranking
----
-- Chicago Cubs
-- St Louis Cardinals
-EOY
+  # Team ranking
+  ---
+  - Chicago Cubs
+  - St Louis Cardinals
+  EOY
+end
