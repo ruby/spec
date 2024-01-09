03212055f2a4ad94ace510e125e6f934db79742a
Herwin <herwinw@users.noreply.github.com>
2024-01-09 17:34:08 +0100

    Expand specs for Thread#thread_variable_set
    * Test with frozen Thread
    * Test with nil value which removes the entry
    * Test different key types

--- core/thread/thread_variable_set_spec.rb
+++ core/thread/thread_variable_set_spec.rb
@@ -23,4 +23,37 @@ describe "Thread#thread_variable_set" do
     @t.thread_variable_get(:thread_variable_get_spec).should == 82
     Thread.current.thread_variable_get(:thread_variable_get_spec).should be_nil
   end
+
+  it "converts a String key into a Symbol" do
+    @t.thread_variable_set('a', 49)
+    @t.thread_variable_get(:a).should == 49
+  end
+
+  it "converts a key that is neither String nor Symbol with #to_str" do
+    key = mock('key')
+    key.should_receive(:to_str).and_return('a')
+    @t.thread_variable_set(key, 49)
+    @t.thread_variable_get(:a).should == 49
+  end
+
+  it "removes a key if the value is nil" do
+    @t.thread_variable_set(:a, 52)
+    @t.thread_variable_set(:a, nil)
+    @t.thread_variable?(:a).should be_false
+  end
+
+  it "raises a FrozenError if the thread is frozen" do
+    @t.freeze
+    -> { @t.thread_variable_set(:a, 1) }.should raise_error(FrozenError, "can't modify frozen thread locals")
+  end
+
+  it "raises a TypeError if the key is neither Symbol nor String, nor responds to #to_str" do
+    -> { @t.thread_variable_set(123, 1) }.should raise_error(TypeError, '123 is not a symbol')
+  end
+
+  it "does not try to convert the key with #to_sym" do
+    key = mock('key')
+    key.should_not_receive(:to_sym)
+    -> { @t.thread_variable_set(key, 42) }.should raise_error(TypeError, "#{key.inspect} is not a symbol")
+  end
 end
