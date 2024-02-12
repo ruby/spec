7eee2cf883f168ed24d4ad043985cf7fb84bf90d
Benoit Daloze <eregontp@gmail.com>
2024-02-12 10:13:19 +0100

    Revert "spec/mspec/tool/wrap_with_guard.rb 'ruby_version_is ""..."3.4"' spec/ruby/core/rational/coerce_spec.rb"
    This reverts commit f19402ac24907a67f3f5e6055911206b180a5ba7.

--- core/rational/coerce_spec.rb
+++ core/rational/coerce_spec.rb
@@ -1,9 +1,6 @@
 require_relative "../../spec_helper"
+require_relative '../../shared/rational/coerce'
 
-ruby_version_is ""..."3.4" do
-  require_relative '../../shared/rational/coerce'
-
-  describe "Rational#coerce" do
-    it_behaves_like :rational_coerce, :coerce
-  end
+describe "Rational#coerce" do
+  it_behaves_like :rational_coerce, :coerce
 end
