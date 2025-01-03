263d09990a568845d3b1a1d295703c865fcb6e5c
Peter Zhu <peter@peterzhu.ca>
2025-01-03 10:26:48 -0500

    Add C extension spec for rb_undefine_finalizer

--- optional/capi/finalizer_spec.rb
+++ optional/capi/finalizer_spec.rb
@@ -20,4 +20,21 @@ describe "CApiFinalizerSpecs" do
       ruby_exe(code).should == "finalizer run\n"
     end
   end
+
+  describe "rb_undefine_finalizer" do
+    ruby_bug "#20981", "3.4.0"..."3.4.2" do
+      it "removes finalizers from the object" do
+        code = <<~RUBY
+          require #{extension_path.dump}
+
+          obj = Object.new
+          finalizer = Proc.new { puts "finalizer run" }
+          ObjectSpace.define_finalizer(obj, finalizer)
+          CApiFinalizerSpecs.new.rb_undefine_finalizer(obj)
+        RUBY
+
+        ruby_exe(code).should.empty?
+      end
+    end
+  end
 end
