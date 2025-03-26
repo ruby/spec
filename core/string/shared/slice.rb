7465fd1819777c7685d6c3a9f7de765e30d420aa
Andrew Konchin <andry.konchin@gmail.com>
2025-03-26 19:34:51 +0200

    @wip

--- core/string/shared/slice.rb
+++ core/string/shared/slice.rb
@@ -122,9 +122,11 @@ describe :string_slice_index_length, shared: true do
   platform_is pointer_size: 64 do
     it "returns nil if the length is negative big value" do
       "hello there".send(@method, 4, -(1 << 31)).should == nil
+      "hello there".send(@method, 4, -(1 << 40)).should == nil
+      "hello there".send(@method, 4, -(1 << 50)).should == nil
+      "hello there".send(@method, 4, -(1 << 55)).should == nil
       "hello there".send(@method, 4, -(1 << 61)).should == nil
-      "hello there".send(@method, 4, -(1 << 62)).should == nil
-      "hello there".send(@method, 4, -(1 << 63)).should == nil
+      #"hello there".send(@method, 4, -(1 << 63)).should == nil
     end
   end
 
