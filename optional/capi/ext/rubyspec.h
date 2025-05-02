95cf2bae6f7cb911d9db62ce822057f1e49ea7da
Andrew Konchin <andry.konchin@gmail.com>
2025-05-02 18:58:24 +0300

    Remove version guards for 3.1

--- optional/capi/ext/rubyspec.h
+++ optional/capi/ext/rubyspec.h
@@ -47,12 +47,4 @@
 #define RUBY_VERSION_IS_3_2
 #endif
 
-#if RUBY_VERSION_SINCE(3, 1)
-#define RUBY_VERSION_IS_3_1
-#endif
-
-#if RUBY_VERSION_SINCE(3, 0)
-#define RUBY_VERSION_IS_3_0
-#endif
-
 #endif
