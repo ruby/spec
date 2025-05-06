ruby/ruby

Expose `ruby_thread_has_gvl_p`.

spec/ruby/optional/capi/ext/thread_spec.c
@@ -118,7 +118,6 @@ static VALUE thread_spec_rb_thread_wait_for(VALUE self, VALUE s, VALUE ms) {
   return Qnil;
 }
 
-
 VALUE thread_spec_call_proc(void *arg_ptr) {
   VALUE arg_array = (VALUE)arg_ptr;
   VALUE arg = rb_ary_pop(arg_array);
@@ -167,6 +166,12 @@ static VALUE thread_spec_ruby_native_thread_p_new_thread(VALUE self) {
 #endif
 }
 
+#ifdef RUBY_VERSION_IS_3_4
+static VALUE thread_spec_ruby_thread_has_gvl_p(VALUE self) {
+  return ruby_thread_has_gvl_p() ? Qtrue : Qfalse;
+}
+#endif
+
 void Init_thread_spec(void) {
   VALUE cls = rb_define_class("CApiThreadSpecs", rb_cObject);
   rb_define_method(cls, "rb_thread_alone", thread_spec_rb_thread_alone, 0);
@@ -180,6 +185,9 @@ void Init_thread_spec(void) {
   rb_define_method(cls,  "rb_thread_create", thread_spec_rb_thread_create, 2);
   rb_define_method(cls,  "ruby_native_thread_p", thread_spec_ruby_native_thread_p, 0);
   rb_define_method(cls,  "ruby_native_thread_p_new_thread", thread_spec_ruby_native_thread_p_new_thread, 0);
+#ifdef RUBY_VERSION_IS_3_4
+  rb_define_method(cls,  "ruby_thread_has_gvl_p", thread_spec_ruby_thread_has_gvl_p, 0);
+#endif
 }
 
 #ifdef __cplusplus

ruby/spec

optional/capi/ext/thread_spec.c
@@ -118,7 +118,6 @@ static VALUE thread_spec_rb_thread_wait_for(VALUE self, VALUE s, VALUE ms) {
   return Qnil;
 }
 
-
 VALUE thread_spec_call_proc(void *arg_ptr) {
   VALUE arg_array = (VALUE)arg_ptr;
   VALUE arg = rb_ary_pop(arg_array);
@@ -167,6 +166,12 @@ static VALUE thread_spec_ruby_native_thread_p_new_thread(VALUE self) {
 #endif
 }
 
+#ifdef RUBY_VERSION_IS_3_5
+static VALUE thread_spec_ruby_thread_has_gvl_p(VALUE self) {
+  return ruby_thread_has_gvl_p() ? Qtrue : Qfalse;
+}
+#endif
+
 void Init_thread_spec(void) {
   VALUE cls = rb_define_class("CApiThreadSpecs", rb_cObject);
   rb_define_method(cls, "rb_thread_alone", thread_spec_rb_thread_alone, 0);
@@ -180,6 +185,9 @@ void Init_thread_spec(void) {
   rb_define_method(cls,  "rb_thread_create", thread_spec_rb_thread_create, 2);
   rb_define_method(cls,  "ruby_native_thread_p", thread_spec_ruby_native_thread_p, 0);
   rb_define_method(cls,  "ruby_native_thread_p_new_thread", thread_spec_ruby_native_thread_p_new_thread, 0);
+#ifdef RUBY_VERSION_IS_3_5
+  rb_define_method(cls,  "ruby_thread_has_gvl_p", thread_spec_ruby_thread_has_gvl_p, 0);
+#endif
 }
 
 #ifdef __cplusplus