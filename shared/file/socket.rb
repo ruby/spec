be4fad62ce205c1a8c2416d1da8798182b242b41
Alexander Bulancov <6594487+trinistr@users.noreply.github.com>
2025-11-12 23:17:00 +0300

    Enable File.socket? test on Windows, improve FileTest.socket?

--- shared/file/socket.rb
+++ shared/file/socket.rb
@@ -1,3 +1,37 @@
 describe :file_socket, shared: true do
-  it "accepts an object that has a #to_path method"
+  it "returns false if file does not exist" do
+    File.socket?("I_am_a_bogus_file").should == false
+  end
+
+  it "returns false if the file is not a socket" do
+    filename = tmp("i_exist")
+    touch(filename)
+
+    File.socket?(filename).should == false
+
+    rm_r filename
+  end
+
+  it "returns true if the file is a socket" do
+    require 'socket'
+
+    # We need a really short name here.
+    # On Linux the path length is limited to 107, see unix(7).
+    name = tmp("s")
+    server = UNIXServer.new(name)
+
+    File.socket?(name).should == true
+
+    server.close
+    rm_r name
+  end
+
+  it "accepts an object that has a #to_path method" do
+    obj = Object.new
+    def obj.to_path
+      __FILE__
+    end
+
+    File.socket?(obj).should == false
+  end
 end
