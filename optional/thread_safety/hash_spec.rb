c14ac43e3a30ebacf6f5987c6a8b430c8526ee16
Benoit Daloze <eregontp@gmail.com>
2025-11-24 17:07:34 +0100

    Extend Hash thread safety specs

--- optional/thread_safety/hash_spec.rb
+++ optional/thread_safety/hash_spec.rb
@@ -30,23 +30,25 @@ describe "Hash thread safety" do
     }
   end
 
-  it "supports concurrent #delete" do
+  it "supports concurrent #[]= and #delete and iteration" do
     n_threads = ThreadSafetySpecs.processors
 
     n = 1_000
     operations = n * n_threads / 2
 
     h = {}
-    barrier = ThreadSafetySpecs::Barrier.new(n_threads + 1)
+    barrier1 = ThreadSafetySpecs::Barrier.new(n_threads + 2)
+    barrier2 = ThreadSafetySpecs::Barrier.new(n_threads + 1)
 
     threads = n_threads.times.map { |t|
-      base = t * n
-      n.times do |j|
-        h[base+j] = t
-      end
-
       Thread.new {
-        barrier.wait
+        barrier1.wait
+        base = t * n
+        n.times do |j|
+          h[base+j] = t
+        end
+
+        barrier2.wait
         n.times do |j|
           # delete only even keys
           h.delete(base+j).should == t if (base+j).even?
@@ -54,8 +56,23 @@ describe "Hash thread safety" do
       }
     }
 
-    barrier.wait
+    read = true
+    reader = Thread.new {
+      barrier1.wait
+      while read
+        h.each_pair { |k,v|
+          k.should.is_a?(Integer)
+          v.should.is_a?(Integer)
+          (k / n).should == v
+        }
+      end
+    }
+
+    barrier1.wait
+    barrier2.wait
     threads.each(&:join)
+    read = false
+    reader.join
 
     # odd keys are left
     h.size.should == operations
@@ -64,7 +81,6 @@ describe "Hash thread safety" do
       (key / n).should == value
     }
   end
-
 
   it "supports concurrent #[]= and #[]" do
     n_threads = ThreadSafetySpecs.processors
@@ -104,7 +120,53 @@ describe "Hash thread safety" do
     }
   end
 
-  it "supports concurrent #[]= and #delete and always returns a #size >= 0" do
+  it "supports concurrent #[]= and #[] with change to #compare_by_identity in the middle" do
+    n_threads = ThreadSafetySpecs.processors
+
+    n = 1_000
+    operations = n * n_threads / 2
+
+    h = {}
+    barrier = ThreadSafetySpecs::Barrier.new(n_threads + 1)
+
+    threads = n_threads.times.map { |t|
+      Thread.new {
+        barrier.wait
+        base = (t / 2) * n
+
+        if t.even?
+          n.times do |j|
+            k = base + j
+            h[k] = k
+          end
+        else
+          n.times do |j|
+            k = base + j
+            Thread.pass until v = h[k]
+            v.should == k
+          end
+        end
+      }
+    }
+
+    changer = Thread.new {
+      Thread.pass until h.size >= operations / 2
+      h.should_not.compare_by_identity?
+      h.compare_by_identity
+      h.should.compare_by_identity?
+    }
+
+    barrier.wait
+    threads.each(&:join)
+    changer.join
+
+    h.size.should == operations
+    h.each_pair { |key, value|
+      key.should == value
+    }
+  end
+
+  it "supports repeated concurrent #[]= and #delete and always returns a #size >= 0" do
     n_threads = ThreadSafetySpecs.processors
 
     n = 1_000
