1ca58c58f36b752dc004686248f87009429a5503
Benoit Daloze <eregontp@gmail.com>
2026-01-04 12:58:46 +0100

    Only spec standard Warning.categories

--- core/warning/categories_spec.rb
+++ core/warning/categories_spec.rb
@@ -2,8 +2,11 @@ require_relative '../../spec_helper'
 
 ruby_version_is "3.4" do
   describe "Warning.categories" do
+    # There might be more, but these are standard across Ruby implementations
     it "returns the list of possible warning categories" do
-      Warning.categories.sort.should == [:deprecated, :experimental, :performance, :strict_unused_block]
+      Warning.categories.should.include? :deprecated
+      Warning.categories.should.include? :experimental
+      Warning.categories.should.include? :performance
     end
   end
 end
