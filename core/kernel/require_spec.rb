jruby/jruby

Fix bad merge of require_spec

spec/ruby/core/kernel/require_spec.rb
@@ -22,14 +22,13 @@ describe "Kernel#require" do
     provided << "pathname"
   end
 
-  provided_requires = provided.dup.to_h {|f| [f,f]}
-  ruby_version_is "3.5" do
-    provided_requires["pathname"] = "pathname.so"
-  end
+  it "#{provided.join(', ')} are already required" do
+    out = ruby_exe("puts $LOADED_FEATURES", options: '--disable-gems --disable-did-you-mean')
+    features = out.lines.map { |line| File.basename(line.chomp, '.*') }
 
-  provided.each do |feature|
-    it "#{feature} is already required and provided in loaded features at boot" do
-      feature_require = provided_requires[feature]
+    # Ignore CRuby internals
+    features -= %w[encdb transdb windows_1252 windows_31j]
+    features.reject! { |feature| feature.end_with?('-fake') }
 
     features.sort.should == provided.sort
 

ruby/spec

core/kernel/require_spec.rb
@@ -25,14 +25,13 @@ describe "Kernel#require" do
     provided << "monitor"
   end
 
-  provided_requires = provided.dup.to_h {|f| [f,f]}
-  ruby_version_is "3.5" do
-    provided_requires["pathname"] = "pathname.so"
-  end
+  it "#{provided.join(', ')} are already required" do
+    out = ruby_exe("puts $LOADED_FEATURES", options: '--disable-gems --disable-did-you-mean')
+    features = out.lines.map { |line| File.basename(line.chomp, '.*') }
 
-  provided.each do |feature|
-    it "#{feature} is already required and provided in loaded features at boot" do
-      feature_require = provided_requires[feature]
+    # Ignore CRuby internals
+    features -= %w[encdb transdb windows_1252 windows_31]
+    features.reject! { |feature| feature.end_with?('-fake') }
 
       code = <<~RUBY
         loaded_feature_base = $\".map{|f| File.basename(f, '.*')}