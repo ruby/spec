69f7a2f7fa001e8c2e3221ffc5c78990675bc7b7
Earlopain <14981592+Earlopain@users.noreply.github.com>
2026-06-01 18:52:36 +0200

    Move Time to rely less on shared examples

--- core/time/ctime_spec.rb
+++ core/time/ctime_spec.rb
@@ -1,6 +1,8 @@
 require_relative '../../spec_helper'
-require_relative 'shared/asctime'
 
 describe "Time#ctime" do
-  it_behaves_like :time_asctime, :ctime
+  it "returns a canonical string representation of time" do
+    t = Time.now
+    t.ctime.should == t.strftime("%a %b %e %H:%M:%S %Y")
+  end
 end
--- core/time/iso8601_spec.rb
+++ core/time/iso8601_spec.rb
@@ -1,6 +1,9 @@
 require_relative '../../spec_helper'
-require_relative 'shared/xmlschema'
 
 describe "Time#iso8601" do
-  it_behaves_like :time_xmlschema, :iso8601
+  ruby_version_is "3.4" do
+    it "is an alias of Time#xmlschema" do
+      Time.instance_method(:iso8601).should == Time.instance_method(:xmlschema)
+    end
+  end
 end
