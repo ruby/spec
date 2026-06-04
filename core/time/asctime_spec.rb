9513cf75c7d99bc68b38c83ecd6cc21d8671a371
Benoit Daloze <eregontp@gmail.com>
2026-06-04 22:02:07 +0200

    {Time,Date}#asctime should have the specs and #ctime be the alias

--- core/time/asctime_spec.rb
+++ core/time/asctime_spec.rb
@@ -1,7 +1,8 @@
 require_relative '../../spec_helper'
 
 describe "Time#asctime" do
-  it "is an alias of Time#ctime" do
-    Time.instance_method(:asctime).should == Time.instance_method(:ctime)
+  it "returns a canonical string representation of time" do
+    t = Time.now
+    t.asctime.should == t.strftime("%a %b %e %H:%M:%S %Y")
   end
 end
--- core/time/ctime_spec.rb
+++ core/time/ctime_spec.rb
@@ -1,8 +1,7 @@
 require_relative '../../spec_helper'
 
 describe "Time#ctime" do
-  it "returns a canonical string representation of time" do
-    t = Time.now
-    t.ctime.should == t.strftime("%a %b %e %H:%M:%S %Y")
+  it "is an alias of Time#asctime" do
+    Time.instance_method(:ctime).should == Time.instance_method(:asctime)
   end
 end
