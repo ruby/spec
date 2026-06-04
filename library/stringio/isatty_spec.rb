7297e52f7604b432385961f5b03f1913390b4d4f
Benoit Daloze <eregontp@gmail.com>
2026-06-04 20:08:58 +0200

    Fixes for :library alias specs

--- library/stringio/isatty_spec.rb
+++ library/stringio/isatty_spec.rb
@@ -2,7 +2,7 @@ require_relative '../../spec_helper'
 require 'stringio'
 
 describe "StringIO#isatty" do
-  it "is an alias of StringIO.tty?" do
+  it "is an alias of StringIO#tty?" do
     StringIO.instance_method(:isatty).should == StringIO.instance_method(:tty?)
   end
 end
